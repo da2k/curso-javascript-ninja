f73364062131d617cf0deb9ea8445b05405915ea
Mateus Jabour <mjabourbp@gmail.com>
2015-05-01 12:45:42 -0300

    challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,18 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
-
+var array = [1, 2, 3, 4, 5];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function ShowArray(arr){
+	return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+ShowArray(array)[2];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +21,26 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function ShowArray2(arr, i){
+	return arr[i];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var array2 = [1, null, "oi", true, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+ShowArray2(array2, 0);
+ShowArray2(array2, 1);
+ShowArray2(array2, 2);
+ShowArray2(array2, 3);
+ShowArray2(array2, 4);
+//ou
+ShowArray(array2);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +56,33 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookname){
+	var table = {
+		"bookname1": {
+			quantidadePaginas: 100,
+			autor: "Fernando Daciuk",
+			editora: "Uma ai"
+		},
+		"bookname2": {
+			quantidadePaginas: 200,
+			autor: "Mateus Jabour",
+			editora: "Aquela"
+		},
+		"bookname3": {
+			quantidadePaginas: 10,
+			autor: "Fulano",
+			editora: "Outra la"
+		}
+	}
+
+		if (bookname !== undefined){
+			return table.bookname;
+		}else{
+			return table;
+		}
+
+}
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
