925d2fa30e9a130910a57ba1302f5c5fd7b016a1
Filipe Lemos <contato@filipelemos.com>
2015-06-21 19:37:40 -0300

    Challenge 07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -16,39 +16,52 @@ function addItem(item) {
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+addItem([55, 'Mamãe', {brava: true}]);
+console.log(arrey);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log('O segundo elemento do segundo array é ' + arrey[5][1] + '.');
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log('O primeiro array tem ' + arrey.length + ' itens.');
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log('O segundo array tem ' + arrey[5].length + ' itens.');
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
 console.log( 'Números pares entre 10 e 20:' );
-// ?
+
+var numero = 10;
+
+while (numero <= 20) {
+	numero % 2 === 0 ? console.log(numero) : ''
+	numero++;
+}
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log( 'Números ímpares entre 10 e 20:' );
-// ?
+
+numero = 10;
+
+while (numero <= 20) {
+	numero % 2 != 0 ? console.log(numero) : '';
+	numero++;
+}
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -57,7 +70,11 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 console.log( 'Números pares entre 100 e 120:' );
-// ?
+for (var pares = 100; pares <= 120; pares++) {
+	pares % 2 === 0 ? console.log(pares) : '';
+}
 
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+for (var impares = 111; impares <= 125; impares++) {
+	impares % 2 !== 0 ? console.log(impares) : '';
+}
