efe669503627a2a46af767729ac8d3ed6f092a81
Mateus Jabour <mjabourbp@gmail.com>
2015-06-29 19:13:44 -0300

    challenge-14

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -1,3 +1,4 @@
+(function (){
 /*
 Envolva todo o código desse desafio em uma IIFE.
 */
@@ -9,15 +10,19 @@ elemento será um objeto no formato:
 Os números devem ser de 1 a 10.
 Mostre esse array no console.
 */
-console.log( 'Number Objects Array:' );
-// ?
+console.log( 'Number Objects Array:');
+var numberObjects = [ { number: 1 }, { number: 2 }, { number: 3 }, { number: 4 }, { number: 5 }, { number: 6 }, { number: 7 }, { number: 8 }, { number: 9 },{ number: 10 }];
+console.log(numberObjects);
 
 /*
 Crie um array chamado `justNumbers`, que terá como elementos somente os
 números do array criado acima. Mostre esse novo array no console.
 */
 console.log( '\nJust Numbers:' );
-// ?
+var justNumbers = numberObjects.map(function (item) {
+	return item.number;
+});
+console.log(justNumbers);
 
 /*
 Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
@@ -25,7 +30,10 @@ somente os números que forem divisíveis por 2 ou 3. Mostre esse novo array
 no console.
 */
 console.log( '\nJust module of division by 2 or 3:' );
-// ?
+var justMod2Or3 = justNumbers.filter(function (item) {
+	return item % 2 === 0 || item % 3 === 0;
+});
+console.log(justMod2Or3);
 
 /*
 Declare uma variável chamada operation que receba, do array criado acima,
@@ -36,7 +44,10 @@ O cálculo deve começar com zero.
 Mostre o resultado no console.
 */
 console.log( '\nOperation:' );
-// ?
+var Operation = justMod2Or3.reduce(function (acumulado, item) {
+	return (1 + acumulado) * item;
+}, 0);
+console.log(Operation);
 
 /*
 Faça o mesmo cálculo passado acima, mas começando do último item para o
@@ -44,7 +55,10 @@ primeiro. O nome da variável deve ser operation2. Mostre o resultado no
 console.
 */
 console.log( '\nOperation 2:' );
-// ?
+var Operation2 = justMod2Or3.reduceRight(function (acumulado, item) {
+	return (1 + acumulado) * item;
+}, 0);
+console.log(Operation2);
 
 /*
 Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
@@ -55,20 +69,26 @@ infantil, onde você coloca a letra "P" antes de cada sílaba de uma palavra
 falada, como se você estivesse falando em código xD
 */
 console.log( '\nSeu nome na língua do "P":' );
-// ?
+var name = ['Ma', 'Te', 'Us'];
+var pName = name.reduce(function (acumulado, item) {
+	return acumulado + 'p' + item;
+}, '');
+console.log(pName);
 
 /*
 Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
 e atribuirá o seu nome invertido (usando o array criado acima).
 */
 console.log( '\nInversed Name:' );
-// ?
-
+var inversedName = name.reduceRight(function (acumulado, item) {
+	return acumulado + item;
+});
+console.log(inversedName);
 /*
 Mostre no console o array `numberObjects`.
 */
 console.log( '\nNumber objects' );
-// ?
+console.log(numberObjects);
 
 /*
 Verifique se existem em algum índice de numberObjects um objeto ìgual a
@@ -80,18 +100,29 @@ Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
 o que acontece ;)
 */
 console.log( '\nExiste um { number: 2 } em numberObjects?' );
-// ?
+if (numberObjects.indexOf( {number: 2} ) === -1) {
+	console.log('Não existe um objeto { number: 2 } em numberObjects :(');
+}else {
+	console.log('Existe um objeto { number: 2 } em numberObjects!');
+}
 
 /*
 Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
 será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
 */
 console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-// ?
+if (numberObjects.lastIndexOf( {number: 2} ) === -1) {
+	console.log('Não existe um objeto { number: 2 } em numberObjects :(');
+}else {
+	console.log('Existe um objeto { number: 2 } em numberObjects!');
+}
 
 /*
 Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
 formato de String.
 */
 console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-// ?
+if(Array.isArray(justMod2Or3)) {
+	console.log(justMod2Or3.join(', '));
+}
+})();
