a0006c9a9e33da20739a361a7d5e512e3e818573
Adams Alves <alves.adams@gmail.com>
2015-07-03 11:43:27 -0300

    Challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var elements = ['Adams', [1, 2, 3], 4, {}, false];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myArray(param) {
+  return param;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myArray([1, 2, 3])[1]); // 2
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myArray2(param, num) {
+  return param[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arr = [ true, 'JS Ninja', {}, undefined, 13 ];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(myArray2(arr, 0)); // true
+console.log(myArray2(arr, 1)); // JS Ninja
+console.log(myArray2(arr, 2)); // {}
+console.log(myArray2(arr, 3)); // undefined
+console.log(myArray2(arr, 4)); // 13
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,62 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(name) {
+  var books = {
+    neuromancer: {
+      quantidadePaginas: 271,
+      autor: 'William Gibson',
+      editora: 'Aleph'
+    },
+    mindplayers: {
+      quantidadePaginas: 276,
+      autor: 'Pat Cadigan',
+      editora: 'Bantam Spectra'
+    },
+    software: {
+      quantidadePaginas: 167,
+      autor: 'Rudy Rucker',
+      editora: 'Ace'
+    }
+  };
+
+  if ( name === 'neuromancer' ) {
+  	return books.neuromancer;
+  }
+
+  if ( name === 'mindplayers' ) {
+  	return books.mindplayers;
+  }
+
+  if ( name === 'software' ) {
+  	return books.software;
+  }
+
+  return books;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log( 'O livro Neuromancer tem '+ book('neuromancer').quantidadePaginas +' páginas!' );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log( 'O autor do livro Mindplayers é '+ book('mindplayers').autor +'.' );
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( 'O livro Software foi publicado pela editora '+ book('software').editora +'.' );
