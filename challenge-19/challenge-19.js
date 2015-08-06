fd5cc4096d8abde851ecb6d3dde84ff324180140
Mateus Jabour <mjabourbp@gmail.com>
2015-08-06 11:06:46 -0300

    challenge-19

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -25,18 +25,18 @@ linha, independente de quantos caracteres de número estiverem juntos.
 - Atribua essa regex à uma variável chamada `justNumbersRegex` e mostre-a
 no console:
 */
-console.log( 'Regex para números usando o construtor:' );
-var justNumbersRegex = new RegExp( '^\\d+', 'gm' );
-console.log(justNumbersRegex);
+	console.log( 'Regex para números usando o construtor:' );
+	var justNumbersRegex = new RegExp( '^\\d+', 'gm' );
+	console.log(justNumbersRegex);
 
 /*
 Verifique se a regex acima casa com o texto na variável `text`, mostrando o
 resultado no console. O resultado deve ser:
 "[ '10', '50' ]"
 */
-var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
-console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
-console.log(text.match(justNumbersRegex));
+	var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
+	console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
+	console.log(text.match(justNumbersRegex));
 
 /*
 - Crie uma regex que case com números no final de uma string. Atribua a
@@ -46,9 +46,9 @@ que esta tenha muitas linhas, deve sempre casar com números no fim de cada
 linha, independente de quantos caracteres de número estiverem juntos.
 Mostre a regex no console:
 */
-console.log( '\nRegex para números somente no final das linhas:' );
-var numbersAtTheEnd = new RegExp( '\\d+$', 'gm' );
-console.log(numbersAtTheEnd);
+	console.log( '\nRegex para números somente no final das linhas:' );
+	var numbersAtTheEnd = new RegExp( '\\d+$', 'gm' );
+	console.log(numbersAtTheEnd);
 
 /*
 Verifique se a regex acima casa com o texto na variável `otherText`,
@@ -56,9 +56,9 @@ mostrando o resultado no console.
 O resultado deve ser:
 "[ '12', '6' ]"
 */
-var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
-console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
-console.log(otherText.match(numbersAtTheEnd));
+	var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
+	console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
+	console.log(otherText.match(numbersAtTheEnd));
 
 /*
 Vamos criar um método que vai testar se uma classe CSS existe em uma
@@ -83,15 +83,16 @@ abaixo;
 qualquer classe que for testada. Os dados passados no exercício são somente
 para exemplificar.
 */
-var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
-console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-function hasClass (markup, cssClass) {
-	var regex = new RegExp('\\w+.+' + cssClass + '.');
-	return regex.test(markup) + ' para a classe ' + cssClass;
-}
-console.log(hasClass(markup, "container"));
-console.log(hasClass(markup, "text"));
-console.log(hasClass(markup, "date"));
-console.log(hasClass(markup, "excerpt"));
-console.log(hasClass(markup, "main"));
+	var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
+	console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
+	function hasClass (markup, cssClass) {
+		var regex = new RegExp('class=.(.+)?' + cssClass + '(.+)?.');
+		return regex.test(markup) + ' para a classe ' + cssClass;
+	}
+
+	console.log(hasClass(markup, "container"));
+	console.log(hasClass(markup, "text"));
+	console.log(hasClass(markup, "date"));
+	console.log(hasClass(markup, "excerpt"));
+	console.log(hasClass(markup, "main"));
 })();
