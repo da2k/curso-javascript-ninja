b91c0692a3a17fb1b40a79f14b5c086d335e9a17
Mauricio Junior <mjuniior92@gmail.com>
2015-08-22 16:40:17 -0300

    Fix hasClass

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -87,10 +87,10 @@
 	var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 	console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
 	function hasClass( markup, cssClass ) {
-		var regex = new RegExp( 'class=.*(' + cssClass + ').*', 'gm' );
+		var regex = new RegExp( 'class=[\'"](?:\\w+\\s+)?' + cssClass + '[\'"]?', 'g' );
 		return regex.test( markup );
 	}
-	var classes = ['container', 'text', 'date', 'excerpt', 'main'];
+	var classes = [ 'container', 'text', 'date', 'excerpt', 'main' ];
 	classes.forEach(function(cssClass) {
 		console.log( hasClass( markup, cssClass ) + ' para a classe ' + cssClass );
 	});
