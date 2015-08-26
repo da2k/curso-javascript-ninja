ff9e1ac8b9c9ad536352d95dc0e2b9b7f2eacfd4
Mateus Jabour <mjabourbp@gmail.com>
2015-08-26 19:13:12 -0300

    erros, ignorar

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,4 +1,3 @@
-(function (win, doc) {
 /*
 O desafio de hoje será um pequeno projeto: um cronômetro!
 As regras para criação do cronômetro são as seguintes:
@@ -15,29 +14,3 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-	var $startButton = doc.querySelector('[data-js="start-button"]');
-	var $stopButton = doc.querySelector('[data-js="stop-button"]');
-	var $resetButton = doc.querySelector('[data-js="reset-button"]');
-	var $chronometer = doc.querySelector('[data-js="chronometer"]');
-	var chronometerId;
-
-	function timer () {
-		($chronometer.value)++;
-		chronometerId = setTimeout(timer, 1000);
-	}
-
-	$startButton.addEventListener('click', function () {
-		timer();
-	}, false);
-
-	$stopButton.addEventListener('click', function () {
-		clearTimeout(chronometerId);
-	}, false);
-
-	$resetButton.addEventListener('click', function () {
-		clearTimeout(chronometerId);
-		$chronometer.value = 0;
-	}, false);
-
-
-})(window, document);
