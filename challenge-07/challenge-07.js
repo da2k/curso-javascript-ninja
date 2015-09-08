52a9a991e768186989a33e39ee51c9c8a8dd93d6
Fabio Oliveira <fabio20rj@gmail.com>
2015-09-07 23:48:30 -0300

    corrigido

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -17,7 +17,7 @@ Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 addItem([1,'Cola', true]);
-
+console.log(arr);
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
@@ -29,7 +29,7 @@ console.log('O segundo elemento do segundo array é ' + arr[5][1] + '.');
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-console.log('O primeira array tem ' + arr.length + ' itens.');
+console.log('O primeiro array tem ' + arr.length + ' itens.');
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
@@ -45,22 +45,22 @@ pares entre 10 e 20, inclusive esses 2.
 
 x = 10;
 while(x <= 20){
-	if (x % 2 == 0){
+	if (x % 2 === 0){
 		console.log( 'Números pares entre 10 e 20: ' + x);
 	}
 	x++;
-};
+}
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 x = 10;
 while(x <= 20){
-	if (x % 2 == 1){
+	if (x % 2 === 1){
 		console.log( 'Números impares entre 10 e 20: ' + x);
 	}
 	x++;
-};
+}
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -69,15 +69,15 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 for(x = 100; x <= 120; x++){
-	if(x % 2 == 0){
+	if(x % 2 === 0){
 		console.log( 'Números pares entre 100 e 120: ' + x );
 	}
-};
+}
 
 for(x = 111; x <= 125; x++){
-	if(x % 2 == 1){
+	if(x % 2 === 1){
 		console.log( 'Números ímpares entre 111 e 125: ' + x );	
 	}
-};
+}
 
 // ?
