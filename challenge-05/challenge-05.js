f0c64f4e902c4e2a167de849e7b8f886d540f683
Fabio Oliveira <fabio20rj@gmail.com>
2015-09-07 23:29:08 -0300

    refeito 2x

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -14,7 +14,7 @@ function myFunction(arg){
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-console.log(myFunction(arr)[1]); // 'Fabio'
+console.log(myFunction(arr)[1]); // {}
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
