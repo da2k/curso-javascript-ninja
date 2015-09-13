e06beaa770b7486a9b33051fe75aaf361df981ef
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-13 10:36:10 -0300

    Update challenge 25

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -1,5 +1,13 @@
 /*
-O desafio dessa semana vai ser estudar sobre eventos!
+Essa semana você terá dois desafios:
+1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
+tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
+ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
+o que não ficou tão claro das aulas anteriores.
+É essencial que você entenda todo o conteúdo que foi passado até aqui,
+para que possamos prosseguir para a parte mais avançada do curso :D
+
+2) Estudar eventos!
 Acesse a página do MDN:
 https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 
