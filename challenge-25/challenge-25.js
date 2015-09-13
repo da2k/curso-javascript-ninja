ffc0562823748cbbd51195f289906d2e9f7154ba
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-12 23:00:50 -0300

    Add challenge 25

---
+++ challenge-25/challenge-25.js
@@ -0,0 +1,8 @@
+/*
+O desafio dessa semana vai ser estudar sobre eventos!
+Acesse a página do MDN:
+https://developer.mozilla.org/en-US/docs/Web/Events#Categories
+
+Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
+desafio os experimentos legais que você conseguir desenvolver :D
+*/
