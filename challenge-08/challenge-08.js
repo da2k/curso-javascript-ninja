c05e6d7236ef421499e229075e7689d2665fd660
Your Name <lucasmotta888@gmail.com>
2015-09-27 18:32:28 -0300

    challenge-08

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,43 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum(a,b) {
+    return a+b;
+};
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+
+console.log('A soma de a e b é igual a ' +sum(1,2));
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+sum.name // "calculateSum"
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName() {
+    return 'lucas';
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+var varShowName = showName();
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log('A função ' +showName.name + ' retorna ' + varShowName)
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,7 +53,30 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+function calculator(operator) {
+    if(operator !== '*' || operator !=='/'  || operator !=='+' || operator !== '-' ||operator !== '%'  ){
+      return  console.log('Operação inválida.');
+    }
+    return function operation(a,b) {
+        var result ='';
+           if(operator === '*'){
+              result =  a*b;
+            }
+           if(operator === '/'){
+              result = a/b;
+            }
+            if(operator === '-'){
+               result = a-b;
+            }
+            if(operator === '+'){
+               result = a+b;
+            }
+           if(operator === '%'){
+              result = a%b;
+            }
+         console.log('Resultado da operação: ' + a + ' '+ operator+ ' ' + b + ' = ' + result);
+         };
+     }
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
---
+++ summary.md
@@ -0,0 +1,64 @@
+## Sumário
+
+Aqui vocẽ pode ver todo o conteúdo do curso disponível:
+
+- Variáveis e tipos de dados
+- Operadores aritméticos
+- Operadores de igualdade e relacionais
+- Funções
+- Operadores Lógicos
+- Operadores Unários
+- Estrutura Léxica
+- Instruções condicionais - IF
+- Truthy e Falsy
+- Condicional Ternário
+- Escopo de variáveis e funções
+- Retorno de funções com arrays e objetos
+- Parâmetros de Funções com arrays e objetos
+- Operador vírgula e estrutura condicional switch
+- Estrutura de repetição while
+- O operador módulo
+- Array - a propriedade length
+- Array - método "push()" e Loop "for"
+- A importância de nomear funções
+- Introdução à Functional Programming
+- Escopo de funções
+- Hoisting
+- IIFE - Immediately Invoked Function Expression
+- Wrapper Objects
+- typeof
+- Laços (Loops)
+- Saltos
+- Criação de objetos
+- Criação de objetos - Object.create
+- Métodos de objeto
+- Array - Métodos
+- Array - toString, concat, unshift, shift
+- slice e splice
+- forEach, every, some
+- map, filter
+- reduce() e reduceRight()
+- indexOf(), lastIndexOf() e isArray()
+- JS no browser
+- this
+- Objeto arguments
+- use strict - Criação de variáveis sem o "var"
+- use strict - Uso do width e global this === undefined
+- O operador delete, objetos e funções
+- Objeto String
+- Regex
+- Regex - Negação
+- Regex - Repetidores
+- Conhecendo outros símbolos usados na Regex
+- Métodos de string onde podemos usar regex
+- o objeto RegExp()
+- Js no browser - Selecionar elementos no DOM
+- Js no browser - Formulário e eventos
+- Sync vs async
+- setTimeout
+- setTimeout vs setInterval
+- Propriedades e métodos de funções
+- Prototype
+- Array-like e editorconfig
+- Debug
+- Modularização de código
