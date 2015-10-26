c95999483b15d93d4c8a7b97a6ee1c047405a15f
Thiago Nogueira <nogueirathiago@gmail.com>
2015-10-26 10:27:17 -0300

    Correcoes finais

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -107,15 +107,15 @@
   }
 
   function subtract(a, b) {
-    a - b;
+    return a - b;
   }
 
   function divide(a, b) {
-    a / b;
+    return a / b;
   }
 
   function multiply(a, b) {
-    a * b;
+    return a * b;
   }
 
   initButtonsEvents();
