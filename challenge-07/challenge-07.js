0e150529ca5e73e35079fb35597fdb61b1dde327
Feabbud <git@felipeabbud.com.br>
2015-11-17 13:46:17 -0200

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -17,7 +17,7 @@ function addItem( param ){
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-addItem([25. false, 'bola']);
+addItem([25, false, 'bola']);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
