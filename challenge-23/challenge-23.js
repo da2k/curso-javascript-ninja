2438b4a2f199874ed394a6311e96096815af1a07
Anderson Menezes <andersomxp@gmail.com>
2015-11-26 10:19:35 -0300

    Challenge 23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -2,7 +2,6 @@
 	/*
 	Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
 	As regras são:
-
 	- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
 	diretamente;
 	- O input deve iniciar com valor zero;
@@ -11,14 +10,13 @@
 	multiplicação(x) e divisão(÷);
 	- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
 	que irá limpar o input, deixando-o com valor 0;
-
 	- A cada número pressionado, o input deve atualizar concatenando cada valor
 	digitado, como em uma calculadora real;
 	- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
 	operação no input. Se o último caractere no input já for um símbolo de alguma
 	operação, esse caractere deve ser substituído pelo último pressionado.
 	Exemplo:
-	- Se o input teMaterial Theme, the most epic theme for Sublime Text 3 by Mattia Astorinom os valores: "1+2+", e for pressionado o botão de
+	- Se o input tem os valores: "1+2+", e for pressionado o botão de
 	multiplicação (x), então no input deve aparecer "1+2x".
 	- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
 	input;
@@ -32,22 +30,22 @@
 	var $buttonEqual = doc.querySelector('[data-js="btn-equal"]');
 
 	Array.prototype.forEach.call($buttonsNumbers, function(button){
-		button.addEventListener('click', handleClickButton, false);
+		button.addEventListener('click', handleClickNumbers, false)
 	});
 
-	Array.prototype.forEach.call($buttonOperations, function(operator){
-		operator.addEventListener('click', handleClickOperations, false);
+	Array.prototype.forEach.call($buttonOperations, function(operators){
+		operators.addEventListener('click', handleClickOperators, false)
 	});
+
 	$buttonCE.addEventListener('click', handleClickCE, false);
 	$buttonEqual.addEventListener('click', handleClickEqual, false);
 
-	function handleClickButton(){
+	function handleClickNumbers(){
 		$screen.value += this.value;
-		console.log(this);
 	}
 
-	function handleClickOperations(){
-		$screen.value = removeItemIfIsOperator($screen.value);
+	function handleClickOperators(){
+		$screen.value = removeLastItemItIsAnOperator($screen.value);
 		$screen.value += this.value;
 	}
 
@@ -55,7 +53,7 @@
 		$screen.value = 0;
 	}
 
-	function lastItemIsAnOperator(number){
+	function isLastItemAnOperator(number){
 		var operators = ['+', '-', '*', '÷'];
 		var lastItem = number.split('').pop();
 		return operators.some(function(operator){
@@ -63,31 +61,34 @@
 		});
 	}
 
-	function removeItemIfIsOperator(number){
-		if(lastItemIsAnOperator(number)){
+	function removeLastItemItIsAnOperator(number){
+		if(isLastItemAnOperator(number)) {
 			return number.slice(0,-1);
 		}
 		return number;
 	}
 
 	function handleClickEqual(){
-		$screen.value = removeItemIfIsOperator($screen.value);
-		var allValues = $screen.value.match(/\d+[+x÷-]?/g);
-		$screen.value = allValues.reduce(function(accumulated, actual){
+		$screen.value = removeLastItemItIsAnOperator($screen.value);
+		var allValues = $screen.value.match(/\d+[+*÷-]?/g);
+		
+		$screen.value =  allValues.reduce(function(accumulated, actual){
 			var firstValue = accumulated.slice(0,-1);
 			var operator = accumulated.split('').pop();
-			var lastValue = removeItemIfIsOperator(actual);
-			var lastOperator = lastItemIsAnOperator(actual) ? actual.split('').pop() : '';
+			var lastValeu = removeLastItemItIsAnOperator(actual);
+			var lastOperator = isLastItemAnOperator(actual) ? actual.split('').pop() : '';
+			
 			switch(operator) {
 				case '+':
-					return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
+					return Number(firstValue) + Number(lastValeu) + lastOperator;
 				case '-':
-					return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-				case 'x':
-					return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
+					return Number(firstValue) - Number(lastValeu) + lastOperator;
+				case '*':
+					return Number(firstValue) * Number(lastValeu) + lastOperator;
 				case '÷':
-					return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
+					return Number(firstValue) / Number(lastValeu) + lastOperator;
 			}
 		});
 	}
+
 })(window, document);
