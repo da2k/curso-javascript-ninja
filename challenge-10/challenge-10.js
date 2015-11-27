5cf473e236b2bcfebf48df5ddc51973e7a04d0a5
Filipe lemos <contato@filipelemos.com>
2015-11-27 18:40:33 -0200

    Correção do desafio 10

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -65,7 +65,7 @@
         }
 
         return function(num1, num2) {
-            if (typeof num1 !== 'number' && typeof num2 !== 'number') {
+            if (typeof num1 !== 'number' || typeof num2 !== 'number') {
                 return false;
             }
 
