46f3b1457ac2a2b26a64402e38bab33ef387870d
Fernando Daciuk <f.daciuk@gmail.com>
2015-12-25 22:44:23 -0200

    Add initial files to server

--- challenge-32/server/app.js
+++ challenge-32/server/app.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
+var routes = require('./routes');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,6 +15,10 @@ app.get('/', function(req, res) {
   res.json({ message: 'hi' });
 });
 
+app.use('/car', routes);
+
 app.listen(port, function() {
   console.log('Listening on port http://localhost:%d', port);
 });
+
+module.exports = app;
