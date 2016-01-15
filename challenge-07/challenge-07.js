85b1d479f03a85c04009e4ed324829e87569b9b5
Heverton Castro <heverton@hotmail.com.br>
2016-01-15 10:15:49 -0200

    Challenge 07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -8,9 +8,9 @@ Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 function addItem(){
-	arr.push(10);
-	arr.push('JS Ninja');
-	arr.push(['Curso', 'JS', 'Ninja']);
+	arr.push( 10 );
+	arr.push( 'JS Ninja' );
+	arr.push( [ 'Curso', 'JS', 'Ninja' ] );
 
 	return arr;
 }
@@ -19,7 +19,7 @@ function addItem(){
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-arr.push([ { color: 'navy' }, false, 1.5 ]);
+arr.push( [ { color: 'navy' }, false, 1.5 ] );
 
 console.log(arr);
 
@@ -28,19 +28,19 @@ Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-console.log('O segundo elemento do segundo array é '+ arr[8][1] +'.');
+console.log('O segundo elemento do segundo array é ' + arr[8][1] + '.');
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-console.log('O primeiro array tem '+ arr.length +' itens.');
+console.log('O primeiro array tem ' + arr.length + ' itens.');
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-console.log('O segundo array tem '+ arr[8].length +' itens.');
+console.log('O segundo array tem ' + arr[8].length + ' itens.');
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
