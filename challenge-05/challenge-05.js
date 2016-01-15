edb70497c1e3b07b5de0431d677b05a07ce4f586
William Oliveira ⌁ <w.oliveira542@gmail.com>
2016-01-14 23:19:47 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -14,7 +14,7 @@ function getArray (param) {
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-console.log(getArray(myarray[1]);
+console.log(getArray(myarray[1]));
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
