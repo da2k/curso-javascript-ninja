056456290ee0a1d8943a12dcdd4afa5f5feda8be
Daniel Castro <dancasttro@gmail.com>
2016-02-01 00:49:59 -0200

    respondendo primeira questao

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var foo = [1, 'Daniel', true, null, undefined];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
