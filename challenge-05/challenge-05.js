bac1eb598817013d3de45623c889dfc1cfb001ab
Daniel Castro <dancasttro@gmail.com>
2016-02-02 00:42:12 -0200

    adicionando respostas

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -7,12 +7,15 @@ var foo = [1, 'Daniel', true, null, undefined];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+var arr = [1, 2, 3];
+function myFunc(arg) {
+  return arg;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+myFunc(arr[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +23,21 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+var arr = [1, 2, 3];
+function myFunction(arr, arg) {
+  return arr[arg];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arr = [1, true, 'daniel', null, {}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunction(arr);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +53,29 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function Book(params) {
+  var obj = {
+    livro1: {
+      quantidadePaginas: 200,
+      autor: 'Autor1',
+      editora: 'Editora1'
+    },
+    livro2: {
+      quantidadePaginas: 250,
+      autor: 'Autor2',
+      editora: 'Editora3'
+    },
+    livro3: {
+      quantidadePaginas: 300,
+      autor: 'Autor3',
+      editora: 'Editora3'
+    }
+  };
+  return params;
+  if (params === false) {
+    return obj;
+  }
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
