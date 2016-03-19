d2840e07f348e9252dfcd96bf6936e9ef38021cb
João Pedro Nascimento <joaopedro@alternativasv.com.br>
2016-03-19 18:56:45 -0300

    challenge-09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -1,3 +1,4 @@
+(function(){
 /*
 Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
@@ -10,11 +11,11 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+    var number1 = 10;
+    var number2 = 20;
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
     return number1 + number2;
-    var number2 = 20;
 }
 myFunction();
 
@@ -22,12 +23,12 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+    var number1 = 10;
+    var number2 = 20;
     var sum = function sum() {
         return number1 + number2;
     };
-    var number1 = 10;
-    var number2 = 20;
+    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
     return sum();
 }
 myFunction2();
@@ -36,37 +37,49 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
     var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
     var number1 = 40;
-    return sum();
     function sum() {
         return number1 + number2;
     };
+    console.log( 'A soma de 40 e 50 é igual a', sum() );
+    console.log( 'Na função myFunction3, number1 é igual a', number1 );
+    return sum();
 }
 myFunction3();
 
 /*
 No desafio anterior criamos uma calculadora, usando uma estrutura funcional.
 Agora vamos criar uma outra calculadora, usando uma outra abordagem :D
+
 - Crie uma função `calculator` que recebe dois valores (números)
 por parâmetro.
 - Essa função deve retornar uma outra função, que recebe um parâmetro
 chamado `callback`.
+
 - Esse `callback` será uma função, que passaremos por parâmetro ao invocar
 o retorno de `calculator`.
+
 - O retorno dessa segunda função deve ser a função de `callback` passada
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+
+function calculator ( number1, number2 ) {
+    return function ( callback ) {
+        return callback( number1, number2 );
+    };
+}
+
+
+
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
 passando dois números por parâmetro.
 */
-// ?
+var sum = calculator( 20,20 ); // 40
+
 
 /*
 Sabemos que `sum` agora tem uma função atribuída a ela, que é o retorno de
@@ -77,15 +90,20 @@ para a chamada à `calculator` acima.
 uma função anônima que irá retornar a soma dos dois números que essa função
 anônima tem como seus argumentos.
 */
-console.log( 'O resultado da soma é:' );
-// ?
+console.log( 'O resultado da soma é: ' + sum(function( number1, number2 ){
+  return number1 + number2;
+}) );
+
 
 /*
 Agora declare outra variáveis chamadas `subtraction`, `multiplication`,
 `division` e `mod`, e atribua à elas `calculator`, passando números
 diferentes para cada chamada.
 */
-// ?
+var subtraction = calculator(30,7); // 23
+var multiplication = calculator(8,4); // 32
+var division = calculator(40,4); // 10
+var mod = calculator(8,2); // 0
 
 /*
 Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -94,14 +112,20 @@ divisão e módulo (resto de divisão), conforme a função utilizada.
 As suas respostas devem estar abaixo dos `console.log` referentes à cada
 chamada.
 */
-console.log( 'O resultado da subtração é:' );
-// ?
+console.log( 'O resultado da subtração é: ' + ' ' + subtraction( function( number1, number2 ){
+    return number1 - number2;
+}) );
 
-console.log( 'O resultado da multiplicação é:' );
-// ?
+console.log( 'O resultado da multiplicação é:' + ' ' + multiplication( function( number1, number2 ) {
+    return number1 * number2;
+} ));
 
-console.log( 'O resultado da divisão é:' );
-// ?
+console.log( 'O resultado da divisão é:' + ' ' + division( function( number1, number2 ) {
+    return number1 / number2;
+}) );
 
-console.log( 'O resto da divisão é:' );
-// ?
+console.log( 'O resto da divisão é:' + ' ' + mod( function( number1, number2 ){
+    return number1 % number2;
+}) );
+
+}());
