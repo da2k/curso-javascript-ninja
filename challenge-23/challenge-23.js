269b24ec37b4babf9766f58dc31e938f397f754b
jorginhodev <jorginhobr84@gmail.com>
2016-06-29 03:27:04 -0300

    Challenge 23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,94 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function( win, doc ){
+  'use strict';
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+  /*
+  Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+  As regras são:
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+  - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+  diretamente;
+  - O input deve iniciar com valor zero;
+  - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+  - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+  multiplicação(x) e divisão(÷);
+  - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+  que irá limpar o input, deixando-o com valor 0;
+
+  - A cada número pressionado, o input deve atualizar concatenando cada valor
+  digitado, como em uma calculadora real;
+  - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+  operação no input. Se o último caractere no input já for um símbolo de alguma
+  operação, esse caractere deve ser substituído pelo último pressionado.
+  Exemplo:
+  - Se o input tem os valores: "1+2+", e for pressionado o botão de
+  multiplicação (x), então no input deve aparecer "1+2x".
+  - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+  input;
+  - Ao pressionar o botão "CE", o input deve ficar zerado.
+  */
+  var $entry = doc.querySelector( '[data-js="entry"]' );
+  var $numbers = doc.querySelectorAll( '[data-js="numbers"]' );
+  var $buttonCE = doc.querySelector( '[data-js="operation-ce"]' );
+  var $operation = doc.querySelectorAll( '[data-js="operation"]' );
+  var $buttonEqual = doc.querySelector( '[data-js="operation-equal"]' );
+
+  Array.prototype.forEach.call( $numbers, function( button ){
+    button.addEventListener( 'click', clickedNumber, false );
+  });
+
+  Array.prototype.forEach.call( $operation, function( button ){
+    button.addEventListener( 'click', clickedOperation, false );
+  });
+
+  $buttonCE.addEventListener( 'click', clickedCE, false );
+  $buttonEqual.addEventListener( 'click', clickedEqual, false );
+
+  function clickedNumber(){
+    $entry.value += this.value;
+  }
+
+  function clickedOperation(){
+    $entry.value = removeLastOperator( $entry.value );
+    $entry.value += this.value;
+  }
+
+  function lastOperation( number ){
+    var operations = ['+', '-', '*', '/'];
+    var lastItem = number.split('').pop();
+    return operations.some( function( operator ){
+      return operator === lastItem;
+    });
+  }
+
+  function clickedCE(){
+    $entry.value = 0;
+  }
+
+  function removeLastOperator( number ){
+    if( lastOperation( number ) )
+      return number.slice( 0, -1 );
+    return number;
+  }
+
+  function clickedEqual(){
+    $entry.value = removeLastOperator( $entry.value );
+    var calculate = $entry.value.match( /\d+[+*/-]?/g);
+    $entry.value = calculate.reduce( function(accumulated, actual){
+      var firstValue = accumulated.slice(0, -1);
+      var operator = accumulated.split('').pop();
+      var lastValue = removeLastOperator ( actual );
+      var lastOperator = lastOperation ( actual ) ? actual.split('').pop() : '';
+      switch(operator){
+        case '+':
+          return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
+        case '-':
+          return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
+        case '*':
+          return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
+        case '/':
+          return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
+      }
+    })
+  }
+
+})( window, document );
