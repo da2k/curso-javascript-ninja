c1bfb041aeacc0de04b5126d86c0ba575b2f4477
Heverton Castro <heverton@hotmail.com.br>
2016-06-30 14:49:51 -0300

    Challenge 30

--- challenge-30/challenge-30.js
+++ challenge-30/challenge-30.js
@@ -29,4 +29,5 @@ nesse arquivo, dentro do `console.log`, o link para o pull request no seu
 projeto.
 */
 
-console.log('Link do pull request do seu projeto');
+console.log('https://github.com/hevertoncastro/marketcar/commit/61ee4ca2da4fb3a01804b9df85715a41b834e862');
+//OBS: Fiz na branch master mesmo.
