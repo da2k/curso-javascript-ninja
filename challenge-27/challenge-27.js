11b7bf3f8bc9ccf230282831cdbe8b6d212fce81
Adams Alves <alves.adams@gmail.com>
2016-08-02 14:21:42 -0300

    Challenge 27 original

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,89 +1,18 @@
-(function(win, doc) {
-  'use strict';
-  /*
-  Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
-  métodos semelhantes aos que existem no array, mas que sirvam para os
-  elementos do DOM selecionados.
-  Crie os seguintes métodos:
-  - forEach, map, filter, reduce, reduceRight, every e some.
-
-  Crie também métodos que verificam o tipo do objeto passado por parâmetro.
-  Esses métodos não precisam depender de criar um novo elmento do DOM, podem
-  ser métodos estáticos.
-
-  Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
-  no objeto, como nos exemplos abaixo:
-  DOM.isArray([1, 2, 3]); // true
-  DOM.isFunction(function() {}); // true
-  DOM.isNumber('numero'); // false
-
-  Crie os seguintes métodos para verificação de tipo:
-  - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
-  O método isNull deve retornar `true` se o valor for null ou undefined.
-  */
-  function DOM(string) {
-    var element = doc.querySelectorAll(string);
-    this.element = element;
-  }
-  DOM.prototype.on = function on( event, callback ) {
-    return Array.prototype.forEach.call(this.element, function(element) {
-      return element.addEventListener(event, callback, false);
-    });
-  };
-  DOM.prototype.off = function off( event, callback ) {
-    return Array.prototype.forEach.call(this.element, function(element) {
-      return element.removeEventListener(event, callback, false);
-    });
-  };
-  DOM.prototype.get = function get() {
-    return this.element;
-  };
-  DOM.prototype.forEach = function forEach() {
-    return Array.prototype.forEach.apply( this.element, arguments );
-  };
-  DOM.prototype.map = function map() {
-    return Array.prototype.map.apply( this.element, arguments );
-  };
-  DOM.prototype.filter = function filter() {
-    return Array.prototype.filter.apply( this.element, arguments );
-  };
-  DOM.prototype.reduce = function reduce() {
-    return Array.prototype.reduce.apply( this.element, arguments );
-  };
-  DOM.prototype.reduceRight = function reduceRight() {
-    return Array.prototype.reduceRight.apply( this.element, arguments );
-  };
-  DOM.prototype.every = function every() {
-    return Array.prototype.every.apply( this.element, arguments );
-  };
-  DOM.prototype.some = function some() {
-    return Array.prototype.some.apply( this.element, arguments );
-  };
-  function is(obj) {
-    return Object.prototype.toString.call(obj);
-  }
-  DOM.prototype.isArray = function isArray(obj) {
-    return is(obj) === '[object Array]';
-  }
-  DOM.prototype.isObject = function isObject(obj) {
-    return is(obj) === '[object Object]';
-  }
-  DOM.prototype.isFunction = function isFunction(obj) {
-    return is(obj) === '[object Function]';
-  }
-  DOM.prototype.isNumber = function isNumber(obj) {
-    return is(obj) === '[object Number]';
-  }
-  DOM.prototype.isString = function isString(obj) {
-    return is(obj) === '[object String]';
-  }
-  DOM.prototype.isBoolean = function isBoolean(obj) {
-    return is(obj) === '[object Boolean]';
-  }
-  DOM.prototype.isNull = function isNull(obj) {
-    return is(obj) === '[object Null]' || is(obj) === '[object Undefined]';
-  }
-
-  var dom = new DOM();
-  console.log( dom.isNull(undefined) );
-})(window, document);
+/*
+Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
+métodos semelhantes aos que existem no array, mas que sirvam para os
+elementos do DOM selecionados.
+Crie os seguintes métodos:
+- forEach, map, filter, reduce, reduceRight, every e some.
+Crie também métodos que verificam o tipo do objeto passado por parâmetro.
+Esses métodos não precisam depender de criar um novo elmento do DOM, podem
+ser métodos estáticos.
+Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
+no objeto, como nos exemplos abaixo:
+DOM.isArray([1, 2, 3]); // true
+DOM.isFunction(function() {}); // true
+DOM.isNumber('numero'); // false
+Crie os seguintes métodos para verificação de tipo:
+- isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+O método isNull deve retornar `true` se o valor for null ou undefined.
+*/
