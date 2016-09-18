d76562379fe407a2959b0b96e8b6481d64b62e37
Adams Alves <alves.adams@gmail.com>
2016-09-18 12:40:06 -0300

    adjusting console log

--- challenge-30/challenge-30.js
+++ challenge-30/challenge-30.js
@@ -29,4 +29,4 @@ nesse arquivo, dentro do `console.log`, o link para o pull request no seu
 projeto.
 */
 
-console.log('Link do pull request do seu projeto', 'https://github.com/adamsalves/my-next-car/pull/1');
+console.log('https://github.com/adamsalves/my-next-car/pull/1');
