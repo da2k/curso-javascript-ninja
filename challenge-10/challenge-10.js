a8a1bd1ca0d20bb6c69dc39c46bba36ef84c4d78
Felipe Fialho <hi@felipefialho.com>
2016-10-11 13:24:57 -0300

    Fix again

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -75,7 +75,7 @@
     }
 
     return function(x, y) {
-      if (typeof x !== 'number' && typeof y !== 'number') {
+      if (typeof x !== 'number' || typeof y !== 'number') {
         return false;
       }
 
