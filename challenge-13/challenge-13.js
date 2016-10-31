3527c4ae9fccce1960ab28d01e41123d5b07efb9
Felipe Fialho <hi@felipefialho.com>
2016-10-31 18:44:19 -0200

    Improve

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -79,7 +79,7 @@
   Adicione os estados do `nordeste` ao array `brasil`. Esses estados devem
   ficar no mesmo nível que os estados já existentes, não em um array separado.
   */
-  brasil = brasil.concat(brasil, nordeste);
+  brasil = brasil.concat(nordeste);
 
   /*
   Mostre no console os estados em `newSudeste`.
