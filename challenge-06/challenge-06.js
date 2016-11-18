dce4fa0fbc7fee3a714b8aedf0e2485e7706b3b5
luanasallles <luanasallles@gmail.com>
2016-11-18 08:51:33 -0200

    challenge 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'paranaense';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,8 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
-
+var teams = ['Atletico PR','Coritiba','Paraná Clube','Londrina','Toledo'];
 console.log( 'Times que estão participando do campeonato:', teams );
 
 /*
@@ -32,19 +32,54 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(posicao){
+    switch(posicao) {
+        case 1:
+            return('O time que está em ' + posicao + ' lugar é o ' + teams[0] + '.');
+            break;
+        case 2:
+            return('O time que está em ' + posicao + ' lugar é o ' + teams[1] + '.');
+            break;
+        case 3:
+            return('O time que está em ' + posicao + ' lugar é o ' + teams[2] + '.');
+            break;
+        case 4:
+            return('O time que está em ' + posicao + ' lugar é o ' + teams[3] + '.');
+            break;
+        case 5:
+            return('O time que está em ' + posicao + ' lugar é o ' + teams[4] + '.');
+            break;
+        case 6:
+            return('O time que está em ' + posicao + ' lugar é o ' + teams[5] + '.');
+            break;
+        default:
+            return('Não temos a informação do time que está nessa posição.');
+    }
+};
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(10));
+//"Não temos a informação do time que está nessa posição."
+console.log(showTeamPosition(1));
+//"O time que está em 1 lugar é o Atletico PR."
+console.log(showTeamPosition(2));
+//"O time que está em 2 lugar é o Coritiba."
+console.log(showTeamPosition(3));
+//"O time que está em 3 lugar é o Paraná Clube."
+console.log(showTeamPosition(5));
+//"O time que está em 5 lugar é o Toledo."
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var contador = 0
+while (contador < 31) {
+    console.log(contador++);
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +93,41 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex (cor) {
+    var red = '#000';
+    var blue = '#007fff';
+    var yellow = '#feff00';
+    var orange = '#ff7f00';
+    var black = '#000';
+    switch(cor) {
+        case 'red':
+           return('O hexadecimal para a cor red é ' + red + '.');
+           break;
+        case 'blue':
+           return('O hexadecimal para a cor blue é ' + blue + '.');
+           break;
+        case 'yellow':
+           return('O hexadecimal para a cor yellow é ' + yellow + '.');
+           break;
+        case 'orange':
+           return('O hexadecimal para a cor orange é ' + orange + '.');
+           break;
+        case 'black':
+           return('O hexadecimal para a cor black é ' + black + '.');
+           break;
+        default:
+           return('Não temos o equivalente hexadecimal para ' + cor + '.');
+           break;
+    }
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+console.log(convertToHex('white'));
+console.log(convertToHex('red'));
+console.log(convertToHex('black'));
+console.log(convertToHex('yellow'));
+console.log(convertToHex('purple'));
+console.log(convertToHex('orange'));
+console.log(convertToHex('blue'));
