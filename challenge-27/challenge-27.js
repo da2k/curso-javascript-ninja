78266e0c943fdc7fd24bdfea69ef30ed12b874fd
renangabriel27 <renanthompsom@gmail.com>
2016-11-25 18:08:47 -0200

    Challenge 27

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,3 +1,5 @@
+(function() {
+  'use strict';
 /*
 Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
 métodos semelhantes aos que existem no array, mas que sirvam para os
@@ -19,3 +21,84 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+  function DOM(elements) {
+    this.element = document.querySelectorAll(elements)
+  }
+
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.addEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.removeEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply(this.element, arguments);
+  };
+
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+  };
+
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduce = function filter() {
+    return Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments);
+  };
+
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments);
+  };
+
+  DOM.prototype.is = function is(arg, type) {
+    return Object.prototype.toString.call(arg) === '[object ' + type + ']';
+  }
+
+  DOM.prototype.isArray = function isArray(arg) {
+    return DOM.prototype.is(arg, 'Array');
+  }
+
+  DOM.prototype.isObject = function isObject(arg) {
+    return DOM.prototype.is(arg, 'Object');
+  }
+
+  DOM.prototype.isFunction = function isFunction(arg) {
+    return DOM.prototype.is(arg, 'Function');
+  }
+
+  DOM.prototype.isNumber = function isNumber(arg) {
+    return DOM.prototype.is(arg, 'Number');
+  }
+
+  DOM.prototype.isString = function isString(arg) {
+    return DOM.prototype.is(arg, 'String');
+  }
+
+  DOM.prototype.isBoolean = function isBoolean(arg) {
+    return DOM.prototype.is(arg, 'Boolean');
+  }
+
+  DOM.prototype.isNull = function isNull(arg) {
+    return DOM.prototype.is(arg, 'undefined') || DOM.prototype.is(arg, 'Null');
+  }
+
+})();
