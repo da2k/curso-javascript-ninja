42b9620a6e93ab10d59b0c5dabf0187ccb64beb0
Felipe Fialho <hi@felipefialho.com>
2016-12-03 14:06:44 -0200

    Some adjusts

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -63,7 +63,7 @@
   ["junho", "julho"]
   */
   console.log('\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":');
-  console.log('Os meses de janeiro, junho e julho começam com a letra j.'.match(/junho|julho/g));
+  console.log('Os meses de janeiro, junho e julho começam com a letra j.'.match(/ju[nl]ho/g));
 
   /*
   Crie uma expressão regular que faça o match com a abertura de uma tag
