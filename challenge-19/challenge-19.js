bdc8eada34f34fe24e028968a173322a73e0b4b3
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2017-01-08 21:15:33 -0200

    CHallenge-19

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -84,7 +84,7 @@
     var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
     console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
     function hasClass(markup, cssClass){
-      var regex = new RegExp('<[\\w\\s]+class=(["\'])(?:(?:\\w+\\s+)*' + cssClass + '[\\w\\s]*)\\1>', 'gm');
+      var regex = new RegExp('<[\\w\\s]+class=(["\'])(?:(?:\\w+\\s+)*' + cssClass + '(?:\\s+\\w+)*)\\1>', 'gm');
       return regex.test(markup);
     };
 
