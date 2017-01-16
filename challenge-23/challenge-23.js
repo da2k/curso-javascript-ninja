8aa1738b6cf5117836590cff4693864a4ab9eabc
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2017-01-16 19:26:35 -0200

    Challenge-23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -26,126 +26,70 @@
   - Ao pressionar o botão "CE", o input deve ficar zerado.
   */
 
-  var $zero = doc.querySelector('[data-js="zero"]');
-  var $um = doc.querySelector('[data-js="um"]');
-  var $dois = doc.querySelector('[data-js="dois"]');
-  var $tres = doc.querySelector('[data-js="tres"]');
-  var $quatro = doc.querySelector('[data-js="quatro"]');
-  var $cinco = doc.querySelector('[data-js="cinco"]');
-  var $seis = doc.querySelector('[data-js="seis"]');
-  var $sete = doc.querySelector('[data-js="sete"]');
-  var $oito = doc.querySelector('[data-js="oito"]');
-  var $nove = doc.querySelector('[data-js="nove"]');
-  var $soma = doc.querySelector('[data-js="soma"]');
-  var $subtracao = doc.querySelector('[data-js="subtracao"]');
-  var $multiplicacao = doc.querySelector('[data-js="multiplicacao"]');
-  var $divisao = doc.querySelector('[data-js="divisao"]');
-  var $limpar = doc.querySelector('[data-js="limpar"]');
-  var $igual = doc.querySelector('[data-js="igual"]');
+  var operacoes = {
+     '-' : function( x, y ){
+       return +x - +y;
+     },
+
+     '+' : function( x, y ){
+       return +x + +y;
+     },
+
+     '-' : function( x, y ){
+       return +x - +y;
+     },
+     '÷' : function( x, y){
+       return +x / +y;
+    },
+     '*' : function(x , y){
+       return +x * +y;
+     }
+  };
+  var $numeros = doc.querySelectorAll('[data-js="numero"]');
+  var $operadores = doc.querySelectorAll('[data-js="operador"]');
   var $resultado = doc.querySelector('[data-js="resultado"]');
 
-  $zero.addEventListener( 'click', function() {
-    setarValorInput($zero.value);
-  }, false );
-
-  $um.addEventListener( 'click', function() {
-    setarValorInput($um.value);
-  }, false );
-
-  $dois.addEventListener( 'click', function() {
-    setarValorInput($dois.value);
-  }, false );
-
-  $tres.addEventListener( 'click', function() {
-    setarValorInput($tres.value);
-  }, false );
-
-  $quatro.addEventListener( 'click', function() {
-    setarValorInput($quatro.value);
-  }, false );
-
-  $cinco.addEventListener( 'click', function() {
-    setarValorInput($cinco.value);
-  }, false );
-
-  $seis.addEventListener( 'click', function() {
-    setarValorInput($seis.value);
-  }, false );
-
-  $sete.addEventListener( 'click', function() {
-    setarValorInput($sete.value);
-  }, false );
-
-  $oito.addEventListener( 'click', function() {
-    setarValorInput($oito.value);
-  }, false );
-
-  $nove.addEventListener( 'click', function() {
-    setarValorInput($nove.value);
-  }, false );
-
-  $soma.addEventListener( 'click', function() {
-    terminaComOperadorEntaoSubstituirPor($soma.value);
-  }, false );
-
-  $subtracao.addEventListener( 'click', function() {
-    terminaComOperadorEntaoSubstituirPor($subtracao.value);
-  }, false );
-
-  $multiplicacao.addEventListener( 'click', function() {
-    terminaComOperadorEntaoSubstituirPor($multiplicacao.value);
-  }, false );
-
-  $divisao.addEventListener( 'click', function() {
-    terminaComOperadorEntaoSubstituirPor($divisao.value);
-  }, false );
-
-  $igual.addEventListener( 'click', function() {
-    $resultado.value = calcular();
+  Array.prototype.forEach.call( $numeros, function( item ) {
+   item.addEventListener( 'click', cliqueNumeros, false );
   } );
 
-  $limpar.addEventListener( 'click', function() {
-    limpar();
+  Array.prototype.forEach.call( $operadores, function ( item ) {
+    switch(item.value){
+      case 'ce':
+        item.addEventListener( 'click',  limpar, false);
+        break;
+      case '=':
+        item.addEventListener( 'click', calcular, false);
+        break;
+      default:
+        item.addEventListener( 'click', terminaComOperadorEntaoSubstituirPor, false );
+        break;
+
+    }
   });
+
+  function cliqueNumeros(){
+    setarValorInput(this.value);
+  }
 
   function setarValorInput(valor){
     $resultado.value = $resultado.value === '0' ? valor : $resultado.value + valor;
   }
 
-  function terminaComOperadorEntaoSubstituirPor(novoOperador){
+  function terminaComOperadorEntaoSubstituirPor(){
     var regex = new RegExp('[+\\-*÷]$');
 
     if (regex.test($resultado.value))
-      $resultado.value = $resultado.value.replace( regex, novoOperador );
+      $resultado.value = $resultado.value.replace( regex, this.value );
     else
-      $resultado.value += novoOperador;
+      $resultado.value += this.value;
   }
-
- var operacoes = {
-    '-' : function( x, y ){
-      return +x - +y;
-    },
-
-    '+' : function( x, y ){
-      return +x + +y;
-    },
-
-    '-' : function( x, y ){
-      return +x - +y;
-    },
-    '÷' : function( x, y){
-      return +x / +y;
-    },
-    '*' : function(x , y){
-      return +x * +y;
-    }
- };
 
   function calcular(){
     var valores = $resultado.value.match(/\d+/g);
     var operacoesDoUsuario = $resultado.value.match(/\D/g);
 
-    return valores.reduce(
+    $resultado.value = valores.reduce(
       function(acumulated, item){
         if (operacoesDoUsuario.length > 0)
           return operacoes[operacoesDoUsuario.shift()](acumulated, item);
@@ -153,8 +97,6 @@
         return 0;
       }
     );
-
-
   }
 
   function limpar(){
