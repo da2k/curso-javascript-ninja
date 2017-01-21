0b28e3beb124fbc42a9e93b1c14c027c5e826f4e
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2017-01-21 17:55:58 -0200

    Challenge-24

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -15,15 +15,38 @@ var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
 var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
 var $buttonCE = document.querySelector('[data-js="button-ce"]');
 var $buttonEqual = document.querySelector('[data-js="button-equal"]');
+var calculator = {
+  '-': function(firstValue, lastValue){ return ( Number(firstValue) - Number(lastValue) ) },
+  '+': function(firstValue, lastValue){ return ( Number(firstValue) + Number(lastValue) ) },
+  'x': function(firstValue, lastValue){ return ( Number(firstValue) * Number(lastValue) ) },
+  '÷': function(firstValue, lastValue){ return ( Number(firstValue) / Number(lastValue) ) }
+};
 
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
-  button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
-$buttonCE.addEventListener('click', handleClickCE, false);
-$buttonEqual.addEventListener('click', handleClickEqual, false);
+
+addHandleClickNumberInButtonsNumbers();
+addHandleClickOperationInButtonsOperations();
+addHandleClickInButtonCE();
+addHandleClickInButtonEqual();
+
+function addHandleClickNumberInButtonsNumbers(){
+  Array.prototype.forEach.call($buttonsNumbers, function(button) {
+    button.addEventListener('click', handleClickNumber, false);
+  });
+}
+
+function addHandleClickOperationInButtonsOperations(){
+  Array.prototype.forEach.call($buttonsOperations, function(button) {
+    button.addEventListener('click', handleClickOperation, false);
+  });
+}
+
+function addHandleClickInButtonCE(){
+  $buttonCE.addEventListener('click', handleClickCE, false);
+}
+
+function addHandleClickInButtonEqual(){
+  $buttonEqual.addEventListener('click', handleClickEqual, false);
+}
 
 function handleClickNumber() {
   $visor.value += this.value;
@@ -55,21 +78,21 @@ function removeLastItemIfItIsAnOperator(number) {
 
 function handleClickEqual() {
   $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-  $visor.value = allValues.reduce(function(accumulated, actual) {
-    var firstValue = accumulated.slice(0, -1);
-    var operator = accumulated.split('').pop();
-    var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-    switch(operator) {
-      case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-      case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-      case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-      case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-    }
-  });
+  $visor.value = calcul(getValueInDisplayFormated());
+}
+
+function getValueInDisplayFormated(){
+  return $visor.value.match(/\d+[+x÷-]?/g);
+}
+
+function calcul(values){
+  return values.reduce(doOperations);
+}
+
+function doOperations(accumulated, actual) {
+  var firstValue = accumulated.slice(0, -1);
+  var operator = accumulated.split('').pop();
+  var lastValue = removeLastItemIfItIsAnOperator(actual);
+  var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
+  return calculator[operator](firstValue, lastValue) + lastOperator;
 }
