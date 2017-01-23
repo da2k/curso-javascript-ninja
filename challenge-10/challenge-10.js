2090392edc9998c16a8ff8f8440c95b256e3b48a
Luiz Campedelli <campedelliluiz@climatempo.com.br>
2017-01-23 20:57:01 -0200

    Fix challenge-10

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -54,10 +54,7 @@
 	- O desafio é fazer o retorno sem usar "if" ou "switch".
 	*/
 	function isOperatorValid(operador){
-		if ( operador === "+" || operador === "-" || operador === "*" || operador === "/" || operador === "%" ){
-			return true;
-		}
-		return false;
+		return !!operation[operador];
 	}
 
 	/*
@@ -73,15 +70,17 @@
 	os dois parâmetros da função de retorno de "calculator".
 	*/
 	function calculator(operador){
-		if ( isOperatorValid(operador) ) {
-			return function(x, y){
-				if (typeof x === 'number' || typeof y === 'number'){
-					return operation[operador](x, y);
-				}
-				return false;
-			};
+		if ( !isOperatorValid(operador) ) {
+			return false;
 		}
-		return false;
+
+		return function(x, y){
+			if (typeof x !== 'number' || typeof y !== 'number'){
+				return false;
+			}
+			return operation[operador](x, y);
+		};
+		
 	}
 
 	/*
@@ -139,26 +138,76 @@
 	- Se "sum" for "false", mostrar no console a mensagem de erro.
 	*/
 
-	/*
-		Não consegui entender o que precisava ser feito nesta parte do exercicio
-		A Descrição está confusa. Fiz até onde entendi e irei assistir o vídeo 
-		da correção do exercicio.
-	*/
-	number1 = 10;
-	number2 = 5;
+	if ( sum ) {
+		number1 = 10;
+		number2 = 5;
 
-	console.log();
+		console.log( showOperationMessage( operationSignal, number1, number2 ), sum(number1, number2) );
+	} else {
+		console.log( showErrorMessage( operationSignal ) );
+	}
 
 	/*
 	Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
 	divisão e resto. Crie variáveis com os nomes "subtraction",
 	"multiplication", "division" e "mod".
 	*/
-	// ?
+	operationSignal = "-";
+	var subtraction = calculator(operationSignal);
+	if ( subtraction ) {
+		number1 = 10;
+		number2 = 5;
+
+		console.log( showOperationMessage( operationSignal, number1, number2 ), subtraction(number1, number2) );
+	} else {
+		console.log( showErrorMessage( operationSignal ) );
+	}
+
+	operationSignal = "*";
+	var multiplication = calculator(operationSignal);
+	if ( multiplication ) {
+		number1 = 10;
+		number2 = 5;
+
+		console.log( showOperationMessage( operationSignal, number1, number2 ), multiplication(number1, number2) );
+	} else {
+		console.log( showErrorMessage( operationSignal ) );
+	}
+
+	operationSignal = "/";
+	var division = calculator(operationSignal);
+	if ( division ) {
+		number1 = 10;
+		number2 = 5;
+
+		console.log( showOperationMessage( operationSignal, number1, number2 ), division(number1, number2) );
+	} else {
+		console.log( showErrorMessage( operationSignal ) );
+	}
+
+	operationSignal = "%";
+	var mod = calculator(operationSignal);
+	if ( mod ) {
+		number1 = 10;
+		number2 = 5;
+
+		console.log( showOperationMessage( operationSignal, number1, number2 ), mod(number1, number2) );
+	} else {
+		console.log( showErrorMessage( operationSignal ) );
+	}
 
 	/*
 	Repita o PASSO 2 novamente, mas passando um operador inválido, para ver se
 	a mensagem de erro será mostrada no console.
 	*/
-	// ?
+	operationSignal = "x";
+	var invalid = calculator(operationSignal);
+	if ( invalid ) {
+		number1 = 10;
+		number2 = 5;
+
+		console.log( showOperationMessage( operationSignal, number1, number2 ), invalid(number1, number2) );
+	} else {
+		console.log( showErrorMessage( operationSignal ) );
+	}
 })();
