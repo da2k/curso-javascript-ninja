0f621f8d2e43db9145584d2544d86d3968cb43c5
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-01-26 20:04:53 -0200

    OK

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,3 +1,4 @@
+(function(doc, win) {
 /*
 O desafio dessa semana é criar uma mini library (biblioteca) para
 reutilizarmos nossos códigos quando fizermos manipulação de DOM!
@@ -20,8 +21,28 @@ Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
 // ?
+function DOM(elements) {
+  this.element = document.querySelectorAll(elements);
+}
+
+DOM.prototype.on = function on(eventType, callback) {
+  Array.prototype.forEach.call(this.element, function(element) {
+    element.addEventListener(eventType, callback, false);
+  })
+
+};
+DOM.prototype.off = function off(eventType, callback) {
+  Array.prototype.forEach.call(this.element, function(element) {
+    element.removeEventListener(eventType,callback, false);
+  })
+};
+DOM.prototype.get = function get() {
+  return this.element;
+};
+
 
 var $a = new DOM('[data-js="link"]');
+
 $a.on('click', function(e) {
   e.preventDefault();
   console.log('clicou');
@@ -29,3 +50,5 @@ $a.on('click', function(e) {
 
 console.log('Elementos selecionados:', $a.get());
 console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+})(window, document);
