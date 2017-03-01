f3a52951640cff5b3b52553009ea188a756d6500
valdir <valdir-pereira2@hotmail.com>
2017-03-01 13:03:25 -0300

    Challenge 05 - Resolvido

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var newVar = ['Valdir', 27, 'javascript', 'ninja', true];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction(arg) {
+	return arg;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunction(newVar)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function values(arr, indice) {
+	return arr[indice];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var newArray = ['teste', true, 100, {curso: 'JS Ninja'}, null];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(values(newArray, 0));
+console.log(values(newArray, 1));
+console.log(values(newArray, 2));
+console.log(values(newArray, 3));
+console.log(values(newArray, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,49 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName) {
+	var books = {
+		'Elon Musk': {
+			quantidadePaginas: 200,
+			autor: 'Ashlle Vance',
+			editora: 'Intrinseca'	
+		},
+		'Geração de Valor': {
+			quantidadePaginas: 320,
+			autor: 'Flavio Augusto',
+			editora: 'Sextante'	
+		},
+		'Poder sem Limites': {
+			quantidadePaginas: 232,
+			autor: 'Anthony Robbins',
+			editora: 'Desconhecida'	
+		}
+	};
+	return !bookName ? books : books[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log("O livro Geração de Valor tem " + book('Geração de Valor').quantidadePaginas + " páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log( "O autor do livro Poder sem Limites é " + book('Poder sem Limites').autor + "." );
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( "O livro Geração de Valor foi publicado pela editora " + book('Geração de Valor').editora + "." );
