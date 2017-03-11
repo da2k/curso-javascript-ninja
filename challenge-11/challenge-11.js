15ac2aac2b3895d2db7fab183ae1259a8a4701c7
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2017-03-10 22:38:51 -0300

    challenge 11 working

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -9,7 +9,11 @@
     'Entrou ao menos uma vez!'
     Qual loop você deve usar para que essa mensagem seja mostrada no console?
     */
-    // ?
+    var once = false;
+    do {
+        console.log(!once);
+        console.log('Entrou ao menos uma vez!');
+    } while (once === true);
 
     /*
     Crie um objeto chamado `person`, que receba as seguintes propriedades:
