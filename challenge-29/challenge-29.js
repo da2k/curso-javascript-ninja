0f1e67ecf00fc9092d418ecb1eb461443e135513
Felipe Fialho <hi@felipefialho.com>
2017-04-05 21:32:59 -0300

    Challenge 29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,5 @@
-(function() {
+(function (d, w) {
+
   'use strict';
 
   /*
@@ -36,4 +37,110 @@
   que será nomeado de "app".
   */
 
-})();
+
+  function app() {
+
+    var $form = $('[data-app="form"]');
+    var $inputImage = $('input[name="image"]');
+    var $inputModel = $('input[name="model"]');
+    var $inputAno = $('input[name="year"]');
+    var $inputPlate = $('input[name="plate"]');
+    var $inputColor = $('input[name="color"]');
+    var $table = $('[data-app="table-data"]');
+    var $tableRow = $('[data-app="table-row"]');
+    var $tableImagem = $('[data-app="imagem"]');
+    var $tableModelo = $('[data-app="modelo"]');
+    var $tableAno = $('[data-app="ano"]');
+    var $tablePlaca = $('[data-app="placa"]');
+    var $tableCor = $('[data-app="cor"]');
+    var $companyTitle = $('[data-app="title"]');
+    var $companyPhone = $('[data-app="phone"]');
+    var ajax = new XMLHttpRequest();
+
+    function parseData() {
+      var result;
+
+      try {
+        result = JSON.parse(ajax.responseText);
+      } catch (e) {
+        result = null;
+      }
+
+      return result;
+    }
+
+    function isRequestAmazing() {
+      return ajax.readyState === 4 && ajax.status === 200;
+    }
+
+    function createAppHeader() {
+      var data = parseData();
+
+      if (!data) {
+        getMessages('error');
+      }
+
+      $companyTitle.get()[0].textContent = data.name;
+      $companyPhone.get()[0].textContent = data.phone;
+    }
+
+    function handleReadyStateChange() {
+      if (isRequestAmazing()) {
+        createAppHeader();
+      }
+    }
+
+    function initAjax() {
+      var url = 'company.json';
+      ajax.open('GET', url);
+      ajax.send();
+      ajax.addEventListener('readystatechange', handleReadyStateChange);
+    }
+
+    function createCar() {
+      var $fragment = document.createDocumentFragment();
+      var $tr = document.createElement('tr');
+      var $tdImage = document.createElement('td');
+      var $tdModel = document.createElement('td');
+      var $tdYear = document.createElement('td');
+      var $tdPlate = document.createElement('td');
+      var $tdColor = document.createElement('td');
+      var $image = document.createElement('img');
+
+      $image.setAttribute('src',  $inputImage.get()[0].value);
+      $tdImage.appendChild($image);
+
+      $tdModel.textContent = $inputModel.get()[0].value;
+      $tdYear.textContent = $inputAno.get()[0].value;
+      $tdPlate.textContent = $inputPlate.get()[0].value;
+      $tdColor.textContent = $inputColor.get()[0].value;
+
+      $tr.appendChild($tdImage);
+      $tr.appendChild($tdModel);
+      $tr.appendChild($tdYear);
+      $tr.appendChild($tdPlate);
+      $tr.appendChild($tdColor);
+
+      return $fragment.appendChild($tr);
+    }
+
+    function handleSubmitForm(ev) {
+      ev.preventDefault();
+      $table.get()[0].appendChild(createCar());
+    }
+
+    $form.on('submit', handleSubmitForm);
+
+    function init() {
+      initAjax();
+    }
+
+    return {
+      init: init
+    };
+  }
+
+  w.app = app();
+  w.app.init();
+
+})(document, window);
---
+++ challenge-29/dom.js
@@ -0,0 +1,87 @@
+(function (d, w) {
+
+  'use strict';
+
+  function DOM(elements) {
+    this.element = d.querySelectorAll(elements);
+  }
+
+  DOM.prototype.isArray = function isArray(param) {
+    return Object.prototype.toString.call(param) === '[object Array]';
+  };
+
+  DOM.prototype.isObject = function isObject(param) {
+    return Object.prototype.toString.call(param) === '[object Object]';
+  };
+
+  DOM.prototype.isFunction = function isFunction(param) {
+    return Object.prototype.toString.call(param) === '[object Function]';
+  };
+
+  DOM.prototype.isNumber = function isNumber(param) {
+    return Object.prototype.toString.call(param) === '[object Number]';
+  };
+
+  DOM.prototype.isString = function isString(param) {
+    return Object.prototype.toString.call(param) === '[object String]';
+  };
+
+  DOM.prototype.isBoolean = function isBoolean(param) {
+    return Object.prototype.toString.call(param) === '[object Boolean]';
+  };
+
+  DOM.prototype.isNull = function isNull(param) {
+    return Object.prototype.toString.call(param) === '[object Null]' || Object.prototype.toString.call(param) === '[object Undefined]';
+  };
+
+  DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply(this.element, arguments);
+  }
+
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+  };
+
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments);
+  };
+
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments);
+  };
+
+  DOM.prototype.on = function on(event, callback) {
+    this.element.forEach(function (element) {
+      element.addEventListener(event, callback);
+    });
+  }
+
+  DOM.prototype.off = function off(event, callback) {
+    this.element.forEach(function (element) {
+      element.removeEventListener(event, callback);
+    });
+  }
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  }
+
+  function $(element) {
+    return new DOM(element);
+  }
+
+  w.$ = $;
+
+})(document, window);
