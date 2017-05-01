3e5347a7e67ef2af8cde8f0ddfd733ceac84d2c6
Gustavo Martusewicz <gustavp_crj@hotmail.com>
2017-05-01 16:24:15 -0300

    revisão

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
     /*
     Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
@@ -6,38 +6,31 @@
     elementos do DOM selecionados.
     Crie os seguintes métodos:
     - forEach, map, filter, reduce, reduceRight, every e some.
-
     Crie também métodos que verificam o tipo do objeto passado por parâmetro.
     Esses métodos não precisam depender de criar um novo elmento do DOM, podem
     ser métodos estáticos.
-
     Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
     no objeto, como nos exemplos abaixo:
     DOM.isArray([1, 2, 3]); // true
     DOM.isFunction(function() {}); // true
     DOM.isNumber('numero'); // false
-
     Crie os seguintes métodos para verificação de tipo:
     - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
     O método isNull deve retornar `true` se o valor for null ou undefined.
     */
-
+    //lib DOM
     function DOM(elements) {
         this.element = document.querySelectorAll(elements);
     }
 
-    DOM.prototype.getDOMElements = function getDOMElements(elements) {
-        return document.querySelectorAll(elements);
-    };
-
     DOM.prototype.on = function on(eventType, callback) {
-        Array.prototype.forEach.call(this.element, function(element) {
+        Array.prototype.forEach.call(this.element, function (element) {
             element.addEventListener(eventType, callback, false);
         });
     };
 
     DOM.prototype.off = function off(eventType, callback) {
-        Array.prototype.forEach.call(this.element, function(element) {
+        Array.prototype.forEach.call(this.element, function (element) {
             element.removeEventListener(eventType, callback, false);
         });
     };
@@ -45,44 +38,41 @@
     DOM.prototype.get = function get() {
         return this.element;
     };
-    
     //challenge 27
-    
-    DOM.prototype.forEach =  function forEach(){
-        return Array.prototype.forEach.apply( this.element, arguments );
+    DOM.prototype.forEach = function forEach(){
+        return Array.prototype.forEach.apply(this.element, arguments );
     };
     
-    DOM.prototype.map =  function map(){
-        return Array.prototype.map.apply( this.element, arguments );
+    DOM.prototype.map = function map(){
+        return Array.prototype.map.apply(this.element, arguments );
     };
     
-    DOM.prototype.filter =  function filter(){
-        return Array.prototype.filter.apply( this.element, arguments );
+    DOM.prototype.filter = function filter(){
+        return Array.prototype.filter.apply(this.element, arguments );
     };
     
-    DOM.prototype.reduce =  function reduce(){
-        return Array.prototype.reduce.apply( this.element, arguments );
+    DOM.prototype.reduce = function reduce(){
+        return Array.prototype.reduce.apply(this.element, arguments );
     };
     
-    DOM.prototype.reduceRight =  function reduceRight(){
-        return Array.prototype.reduceRight.apply( this.element, arguments );
+    DOM.prototype.reduceRight = function reduceRight(){
+        return Array.prototype.reduceRight.apply(this.element, arguments );
     };
     
-    DOM.prototype.every =  function every(){
-        return Array.prototype.every.apply( this.element, arguments );
+    DOM.prototype.every = function every(){
+        return Array.prototype.every.apply(this.element, arguments );
     };
     
-    DOM.prototype.some =  function some(){
-        return Array.prototype.some.apply( this.element, arguments );
+    DOM.prototype.some = function some(){
+        return Array.prototype.some.apply(this.element, arguments );
     };
-    
     
     DOM.prototype.isArray = function isArray( param ){
         return Object.prototype.toString.call( param ) === '[object Array]';
     };
     
     DOM.prototype.isObject = function isObject( param ){
-        return Object.prototype.toString.call( param ) === '[object Object]';
+        return Object.prototype.toString.call( param ) === '[object object]';
     };
     
     DOM.prototype.isFunction = function isFunction( param ){
@@ -102,11 +92,31 @@
     };
     
     DOM.prototype.isNull = function isNull( param ){
-        return Object.prototype.toString.call( param ) === '[object Null]' || Object.prototype.toString.call( param ) === '[Object Undefined]';
+        return Object.prototype.toString.call( param ) === '[object Null]' || Object.prototype.toString.call( param ) === '[object Undefined]';
     };
     
-    var dom = new DOM;
+    var $a = new  DOM('[data-js="link"]');
+    console.log($a);
     
+    $a.forEach( function(item){
+        console.log(item.firstChild.nodeValue);
+    } );
+    
+    var dataJs =  $a.map( function(item){    
+        return item.getAttribute('data-js');
+    } ); 
+    console.log(dataJs);
+    
+    var dataJsreduce =  $a.reduce( function(acc, item, index){    
+        return acc + ' ' + item.getAttribute('data-js') + ' ' + index;
+    }, 0); 
+    console.log(dataJsreduce);
+    
+    console.log( DOM.prototype.isArray([1,2,3]) );
+    
+    var dom = new DOM();
     console.log( dom.isArray([1,2,3]) );
+    
+    console.log( DOM.prototype.isNull() );
     
 })();
