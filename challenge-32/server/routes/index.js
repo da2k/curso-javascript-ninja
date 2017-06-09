4b4292b08ffdc040588e3efdc23e1cee3c7067b3
Joao Dev <joaodevelop@gmail.com>
2017-06-09 16:21:50 -0300

    challenge-32

--- challenge-32/server/routes/index.js
+++ challenge-32/server/routes/index.js
@@ -2,7 +2,21 @@
 
 var express = require('express');
 var router = express.Router();
-var data = [];
+var data = [{
+  image: 'http://blog.caranddriver.com/wp-content/uploads/2015/11/BMW-2-series.jpg',
+  brandModel: 'BMW-2-series',
+  year: '2015',
+  plate: 'bmw-4625',
+  color: 'Red'
+},
+{
+  image: 'https://pictures.topspeed.com/IMG/crop/201511/1984-ferrari-testarossa-44_600x0w.jpg',
+  brandModel: 'Ferrari Testarossa',
+  year: '1993',
+  plate: 'kvs-4822',
+  color: 'Red'
+}
+];
 
 router.get('/', function(req, res) {
   res.json(data);
@@ -14,7 +28,7 @@ router.post('/', function(req, res) {
     brandModel: req.body.brandModel,
     year: req.body.year,
     plate: req.body.plate,
-    color: req.body.color 
+    color: req.body.color
   });
   res.json({ message: 'success' });
 });
