c4167b2a7a017d7fa433d7d59df6bb2ab5678c1f
Joao Dev <joaodevelop@gmail.com>
2017-06-13 14:57:42 -0300

    challenge-33

--- challenge-33/challenge-33.js
+++ challenge-33/challenge-33.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Hora de finalizar nosso projeto!
 
 Já temos o cadastro funcionando e persistindo em memória;
@@ -26,4 +26,4 @@ repositório do GitHub, e envie o pull request para lá.
 Depois, envie um pull request no repositório do curso, colocando no console.log
 abaixo a URL do pull request no seu repositório.
 */
-console.log('Link do pull request do seu projeto');
+console.log('https://github.com/JoaoPauloDev/GTCars/pull/4');
