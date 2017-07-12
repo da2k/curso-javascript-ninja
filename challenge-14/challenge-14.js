c45c3dd0086f75f1924ebafacf698a8ba96bfeb0
Virginia Cruz <virginiacruz@edsonqueiroz.com.br>
2017-07-12 11:02:00 -0300

    correcao challenge-14

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -113,27 +113,37 @@
     o que acontece ;)
     */
     console.log( '\nExiste um { number: 2 } em numberObjects?' );
-    console.log( numberObjects.indexOf({number: 2}) ? 
-                'Existe um objeto { number: 2 } em numberObjects!' : 
-                'Não existe um objeto { number: 2 } em numberObjects :(' );
+    // console.log( numberObjects.indexOf({number: 2} > -1 ) ? 
+    //             'Existe um objeto { number: 2 } em numberObjects!' : 
+    //             'Não existe um objeto { number: 2 } em numberObjects :(' );
+
+    if ( numberObjects.indexOf({number: 2}) > -1 ) {
+        console.log('Existe um objeto { number: 2 } em numberObjects!');
+    } else {
+        console.log('Não existe um objeto { number: 2 } em numberObjects :(');
+    }
    
     /*
     Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
     será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
     */
     console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-    console.log ( numberObjects.lastIndexOf({number: 2}) ? 
-                    'Existe um objeto { number: 2 } em numberObjects!' : 
-                    'Não existe um objeto { number: 2 } em numberObjects :(' );
+    // console.log ( numberObjects.lastIndexOf({number: 2}) ? 
+    //                 'Existe um objeto { number: 2 } em numberObjects!' : 
+    //                 'Não existe um objeto { number: 2 } em numberObjects :(' );
+    
+    if ( numberObjects.lastIndexOf({number: 2}, 2) > -1 ) {
+        console.log('Existe um objeto { number: 2 } em numberObjects!');
+    } 
+    console.log('Não existe um objeto { number: 2 } em numberObjects :(');
 
     /*
     Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
     formato de String.
     */
     console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-    console.log(Array.isArray(justMod2Or3));
-
-    console.log(justMod2Or3.toString());
-    console.log(justMod2Or3.join(''));
-
+    if (Array.isArray(justMod2Or3)) {
+        console.log(justMod2Or3.toString());
+    }
+    
 })();
