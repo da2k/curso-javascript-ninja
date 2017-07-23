3d769877a6162c53a1b86d9e64fe7a7e1aa7c698
Cristiano Pacheco <chris.spb25@gmail.com>
2017-07-23 10:46:47 -0300

    add quebra de linha

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -102,6 +102,6 @@
    */
   console.log('\nFazer replace dos textos das tags:');
   console.log('<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>'
-    .replace(/<(\w{1,})>([\S\s]{0,})(<\/\1>)/g, '<$1>O texto da tag "$1" é "$2"$3'));
+    .replace(/<(\w{1,})>([\S\s]{0,})(<\/\1>)/g, '<$1>O texto da tag "$1" é "$2"$3\n'));
 
 })();
