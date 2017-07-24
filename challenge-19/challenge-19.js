555f2ee9ab1c515e1ffbb29b192f483291e07c81
Adeonir Kohl <adeonir@gmail.com>
2017-07-24 08:23:03 -0300

    Challenge 19
    Resolução

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -47,7 +47,7 @@
   Mostre a regex no console:
   */
   console.log( '\nRegex para números somente no final das linhas:' );
-  var numbersAtTheEnd = new RegExp('\\d+$', 'gm');
+  var numbersAtTheEnd = /\d+$/gm;
   console.log(numbersAtTheEnd);
 
   /*
@@ -86,11 +86,12 @@
   var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
   console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
   function hasClass (markup, cssClass) {
-    var regex = new RegExp('(?:class=.*)(' + cssClass + ')(?:.)', 'g');
+    var regex = new RegExp('class=.*(' + cssClass + ').*', 'g');
+    // var regex = new RegExp('class=["\'](?:[\\w\\s]+)?' + cssClass + '(?:[\\w\\s]+)?["\']');
     return regex.test(markup);
   }
   var classes = ['container', 'text', 'date', 'excerpt', 'main'];
   classes.forEach(function (cssClass) {
-    console.log(hasClass(markup, cssClass) + ' para classe ' + cssClass);
+    console.log(hasClass(markup, cssClass) + ' para a classe ' + cssClass);
   });
 })();
