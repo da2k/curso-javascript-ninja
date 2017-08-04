5530a764e31ba5bee5ecaa7c591126f848e96a8c
Edson Pedroso <contato@edsonpedroso.com.br>
2017-08-04 09:32:02 -0300

    Challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var meuArray = [22, 'Ninja', false, {cor: 'blue'}, null];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunc(arr){
+    return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunc(meuArray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function retornaIndice(arr, indice){
+    return arr[indice];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var newArr = ['JavaScript', 37, true, {val1: 1, val2: 2}, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(retornaIndice(newArr, 0));
+console.log(retornaIndice(newArr, 1));
+console.log(retornaIndice(newArr, 2));
+console.log(retornaIndice(newArr, 3));
+console.log(retornaIndice(newArr, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,52 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(livro){
+    var library = {
+        'Foco': {
+            quantidadePaginas: 296,
+            autor: 'Daniel Goleman',
+            editora: 'Objetiva'
+        },
+        'O Poder do Hábito': {
+            quantidadePaginas: 408,
+            autor: 'Charles Duhigg',
+            editora: 'Objetiva'
+        },
+        'Rápido E Devagar': {
+            quantidadePaginas: 624,
+            autor: 'Daniel Kahneman',
+            editora: 'Objetiva'
+        }
+    }
+    if(livro){
+        return library[livro];
+    }
+    return library;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Foco tem ' + book('Foco')['quantidadePaginas'] + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro O Poder do Hábito é ' + book('O Poder do Hábito')['autor'] + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro Rápido E Devagar foi publicado pela editora ' + book('Rápido E Devagar')['editora'] + '.');
