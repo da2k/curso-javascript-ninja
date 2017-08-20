a9b1195c2c58dd1db0f0224dfb6902cd802244c7
fabioaguirre <30848609+fabioaguirre@users.noreply.github.com>
2017-08-19 22:15:48 -0300

    challenge-08 correção e melhoria

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -25,21 +25,20 @@ Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
 function showName(){
-    var name = 'Fabio';
-    return name;
+    return 'Fabio';
 }
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var varShowName = showName();
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log('A função '+showName.name+' retorna '+varShowName+'.');
+console.log('A função '+varShowName.name+' retorna '+varShowName()+'.');
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -55,21 +54,22 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
 */
 function calculator(operator){
     return function(n1,n2){
+        var result;
         switch(operator) {
         case '+':
-            var result = n1 + n2;
+            result = n1 + n2;
             break;
         case '-':
-            var result = n1 - n2;
+            result = n1 - n2;
             break;
         case '*':
-            var result = n1 * n2;
+            result = n1 * n2;
             break;
         case '/':
-            var result = n1 / n2;
+            result = n1 / n2;
             break;
         case '%':
-            var result = n1 % n2;
+            result = n1 % n2;
             break;
         default:
             return 'Operação inválida'; 
