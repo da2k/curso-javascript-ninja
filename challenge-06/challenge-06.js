c9effea33b222343231e77dc10a5f6404dadbd49
michaelsnogueira <michaelsnogueira@gmail.com>
2017-08-30 13:41:21 -0300

    Challenge 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,7 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Premier League';
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,10 +12,10 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = [ 'Tottenham', 'Chelsea', 'City', 'United', 'Liverpool' ];
 
 console.log( 'Times que estão participando do campeonato:', teams );
-
+Times que estão participando do campeonato: [ 'Tottenham', 'Chelsea', 'City', 'United', 'Liverpool' ]
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
     - A função deve receber um número por parâmetro;
@@ -32,19 +32,39 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(numPar){
+	if ( numPar > 0 && numPar < 6 ) { 
+	    return 'O time que está em ' + numPar + 'º ' + 'lugar é o ' + teams[--numPar] + '.';
+	  }else {
+	  	return 'Não temos a informação do time que está nessa posição.';
+	  }
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+> console.log(showTeamPosition(1));
+O time que está em 1º lugar é o Tottenham.
+undefined
+> console.log(showTeamPosition(2));
+O time que está em 2º lugar é o Chelsea.
+undefined
+> console.log(showTeamPosition(5));
+O time que está em 5º lugar é o Liverpool.
+undefined
+> console.log(showTeamPosition(6));
+Não temos a informação do time que está nessa posição.
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var num = 20;
+while (num < 31){
+	console.log(num);
+	num++;
+} 
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +78,59 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(cor) {
+	switch(cor){
+		case 'red': 
+		    return 'O hexadecimal para a cor ' + cor +  ' é #ff0000.';
+		    breack;
+		case 'black': 
+		    return 'O hexadecimal para a cor ' + cor +  ' é #000000.';		    
+		    breack;
+		case 'green': 
+		    return 'O hexadecimal para a cor ' + cor +  ' é #49b675.';		
+		    breack;
+		case 'yellow': 
+		    return 'O hexadecimal para a cor ' + cor +  ' é #fce903.';		
+		    breack;
+		case 'white': 
+		    return 'O hexadecimal para a cor ' + cor +  ' é #ffffff.';	
+		    breack;
+		case 'orange': 
+		    return 'O hexadecimal para a cor ' + cor +  ' é #fc9303.';	
+		    breack;
+	  default:
+	      return 'Não temos o equivalente hexadecimal para ' + cor + '.';
+	  }
+	}  
+		    		    
+		    			    	    		    
+
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+> console.log(convertToHex('red'));
+O hexadecimal para a cor red é #ff0000.
+undefined
+> console.log(convertToHex('green'));
+O hexadecimal para a cor green é #49b675.
+undefined
+> console.log(convertToHex('blue'));
+Não temos o equivalente hexadecimal para blue.
+undefined
+> console.log(convertToHex('orange'));
+O hexadecimal para a cor orange é #fc9303.
+undefined
+> console.log(convertToHex('yellow'));
+O hexadecimal para a cor yellow é #fce903.
+undefined
+> console.log(convertToHex('white'));
+O hexadecimal para a cor white é #ffffff.
+undefined
+> console.log(convertToHex('black'));
+O hexadecimal para a cor black é #000000.
+undefined
+> console.log(convertToHex('pink'));
+Não temos o equivalente hexadecimal para pink.
+undefined
+>
