a2f2f7f38a4aa2173138acadb194cabb413e3ca7
Luciano Baraúna <baraunaluciano@gmail.com>
2017-09-05 13:32:17 -0300

    update midleware gulp

--- challenge-28/gulpfile.js
+++ challenge-28/gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var baseDesafio = 'challenge-28/*';
 
 
 gulp.task('server', function(){
   browserSync.init({
     server: {
-      baseDir:'./'
+      baseDir:'./',
+      middleware: function(req, res, next){
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        next();
+      }
     }
   })
   gulp.watch('./*').on('change', browserSync.reload);
