6c5252f5abc79f2ec29798db9d2dd2906742f01b
fabioaguirre <30848609+fabioaguirre@users.noreply.github.com>
2017-09-16 15:42:05 -0300

    challenge-23 revisão

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -26,6 +26,7 @@
     - Ao pressionar o botão "CE", o input deve ficar zerado.
     */
 
+    // como eu tinha feito
     var $displayCalc = doc.querySelector('[data-js="displayCalc"]');
     var $clearCalc = doc.querySelector('[data-js="clearCalc"]');
     var $number1 = doc.querySelector('[data-js="number1"]');
@@ -100,5 +101,75 @@
     $point.addEventListener('click', inserPoint , false);
 
     $calculator.addEventListener('click', letsCalc , false);
+    // fim - como eu tinha feito
+
+    // resolução da revisão
+    /*
+    var $visor = doc.querySelector('[data-js="visor"]');
+    var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+    var $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
+    var $buttonCE = doc.querySelector('[data-js="button-ce"]');
+    var $buttonEqual = doc.querySelector('[data-js="button-equal"]');
+
+    Array.prototype.forEach.call ($buttonsNumbers, function(button){
+      button.addEventListener('click', handleClickNumber , false);
+    });
+    Array.prototype.forEach.call ($buttonsOperations, function(button){
+      button.addEventListener('click', handleClickOperation , false);
+    });
+    $buttonCE.addEventListener('click', handleClickCE , false);
+    $buttonEqual.addEventListener('click', handleClickEqual ,false);
+
+    function handleClickNumber() {
+      $visor.value += this.value;
+    }
+
+    function handleClickOperation() {
+      $visor.value = removelastItemIfItIsAnOperator($visor.value);
+      $visor.value += this.value;
+    }
+
+    function handleClickCE() {
+      $visor.value = 0;
+    }
+
+    function isLastItemAnOperation(number) {
+      var operations = ['+','-','x','÷'];
+      var lastItem = number.split('').pop();
+      return operations.some(function(operator){ // `some` varre todo array e ve se algum tem aquele valor
+        return operator === lastItem;
+      });
+    }
+
+    function removelastItemIfItIsAnOperator(number){
+      if(isLastItemAnOperation(number)) {
+          return number.slice(0,-1); // retorna tudo excluindo o último
+        }
+      return number;
+    }
+
+    function handleClickEqual(){
+      $visor.value = removelastItemIfItIsAnOperator($visor.value);
+      var allValues = $visor.value.match(/\d+[+x÷-]?/g);
+      $visor.value = allValues.reduce(function(accumulated, actual){
+        var firstValue = accumulated.slice(0,-1);
+        var operator = accumulated.split('').pop();
+        var lastValue = removelastItemIfItIsAnOperator(actual);
+        var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
+        switch(operator){
+          case '+':
+            return (Number(firstValue) + Number(lastValue)) + lastOperator;
+          case '-':
+            return (Number(firstValue) - Number(lastValue)) + lastOperator;
+          case 'x':
+            return (Number(firstValue) * Number(lastValue)) + lastOperator;
+          case '÷':
+            return (Number(firstValue) / Number(lastValue)) + lastOperator;
+          default:
+            return 'erro!';
+        }
+      });
+    }
+    Fim - resolução da revisão*/
 
 })(window,document);
