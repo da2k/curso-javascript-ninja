1163b219ca706d9228d2e959b56d09956c125fcf
jessicalewinter <jessicalewinter@gmail.com>
2017-09-18 15:08:10 -0300

    Some commit

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,20 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var array = [false, 'Brandon Grotesque', [1,2,3],{nome:'Jessica Lewinter', curso:'Engenharia da Computação'},
+             function myFun(){return 'Javascript is cool'}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function vetor(arg){
+    return arg;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(vetor([1,2,3]));
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +23,24 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunction(vet, indice){
+    return vet[indice];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myArray = [null, 'Java is an awful programming language', [1,2], 2, {b:5}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(myFunction(myArray,0));
+console.log(myFunction(myArray,1));
+console.log(myFunction(myArray,2));
+console.log(myFunction(myArray,3));
+console.log(myFunction(myArray,4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,19 +56,43 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(arg){
+    var livros = {'A Revolução dos Bichos':{
+                      quantidadePaginas: 416,
+                      autor: 'George Orwell',
+                      editora: 'Companhia das Letras'
+                         },
+               'Admiravel Mundo Novo':{
+                      quantidadePaginas: 312,
+                      autor: 'Aldous Huxley',
+                      editora: 'Biblioteca Azul'
+                         },
+               'Fahrenheit 451':{
+                      quantidadePaginas: 215,
+                      autor: 'Ray Bradbury',
+                      editora: 'Biblioteca Azul'
+                         }
+              }
+   
+    if(!arg){
+        return livros;
+    }else{
+        return livros[arg];
+    }
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var livro = "A Revolução dos Bichos";
+console.log("O livro " + livro + "tem " + book(livro).quantidadePaginas);
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
