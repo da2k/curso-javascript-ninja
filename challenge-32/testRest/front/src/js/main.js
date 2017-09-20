105c05f2e032e63697ac72dd46cc51cc4b338466
Luciano Barauna <baraunaluciano@gmail.com>
2017-09-20 08:30:20 -0300

    continuando video ajax rest api

--- challenge-32/testRest/front/src/js/main.js
+++ challenge-32/testRest/front/src/js/main.js
@@ -3,11 +3,24 @@
 
   var ajax = new XMLHttpRequest();
 
-  ajax.open('GET', 'http://localhost:4000/user/da');
+  // ajax.open('GET', 'http://localhost:4000/user/jose', false); o ultimo parâmetro significa que n é assincrono
+  ajax.open('GET', 'http://localhost:4000/user/jose');
   ajax.send();
-  ajax.addEventListener('readystatechange', function(e){
-    if (ajax.readyState === 4 && ajax.status === 200){
+  // ajax.addEventListener('readystatechange', function(e){
+  //   if (ajax.readyState === 4){
+  //     console.log(ajax.responseText, ajax.status);
+  //   }
+  // },false)
+  // O construtor ajax pode ser utilizado sem o addEventListener pois ele possui um metódo proprio para retornar a resposta
+  ajax.onreadystatechange = function (e) {
+    // if( ajax.readyState === 2){
+    //   console.log('headers OK!');
+    //   ajax.abort(); // Finaliza a requisição
+    // }
+
+    if (ajax.readyState === 4){
       console.log(ajax.responseText, ajax.status);
     }
-  },false)
+  }
+
 })()
