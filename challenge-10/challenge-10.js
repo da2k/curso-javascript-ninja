540b9d059608d880138eda825ec545275825b873
Jessica Lewinter <jessicalewinter@gmail.com>
2017-09-25 16:10:55 -0300

    Some changes in this repository

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -14,7 +14,7 @@
   var five = Number('5');
   console.log( five + ' é número?', typeof five === 'number' );
 
-  var concat = String(10 + 10);
+  var concat = String(10)+ String(10);
   console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
 
   /*
@@ -79,7 +79,7 @@
         return false;
       }else{
         return function(n1, n2){
-          if(typeof n1 != 'number' || typeof n2 != 'number'){
+          if(typeof n1 != 'number' && typeof n2 != 'number'){
             return false;
           }
           return operation[operador](n1,n2);
@@ -212,4 +212,4 @@
     console.log(showErrorMessage(operationSignal));
   }
 
-}());
+})();
