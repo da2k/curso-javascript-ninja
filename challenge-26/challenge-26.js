cdc50b1a889f7a403f845222dfd026435043d643
fabioaguirre <30848609+fabioaguirre@users.noreply.github.com>
2017-09-28 19:21:32 -0300

    challenge-26 revisão

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -54,4 +54,39 @@
   console.log('Elementos selecionados:', $a.get());
   console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
 
+  /* Revisão para comparação
+
+  function DOM(elements){
+    this.element = document.querySelectorAll(elements);
+  }
+
+  DOM.prototype.on = function on(eventType, callback){
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.addEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(eventType, callback){
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.removeEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get(){
+    return this.element;
+  };
+
+
+  var $a = new DOM('[data-js="link"]');
+  $a.on('click', function handleClick(e) {
+    e.preventDefault();
+    console.log('clicou');
+    $a.off('click', handleClick);
+  });
+
+  console.log('Elementos selecionados:', $a.get());
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+  */
+  
 })(window, document);
