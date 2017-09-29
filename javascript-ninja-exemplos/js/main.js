dc729713f42484f0af929638900d64942feb1ab2
Roger Melo <rogerw592@gmail.com>
2017-09-29 03:06:09 -0300

    challenge-20

---
+++ javascript-ninja-exemplos/js/main.js
@@ -0,0 +1,11 @@
+(function(win, doc) {
+
+    'use strict';
+
+    document.addEventListener('click', function(event) {
+
+        alert('Clicou no documento!');
+
+    }, false);
+
+}(window, document));
