4e3fe8ec86424e8ee5b4a027749bf5091b8f396e
fabioaguirre <30848609+fabioaguirre@users.noreply.github.com>
2017-10-01 12:20:41 -0300

    Update challenge-27.js

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -23,8 +23,9 @@
     this.element = document.querySelectorAll(selectedNode);
   }
 
+  /* parte 1 - como eu tinha feito
   DOM.prototype.foreachDOM = function foreachDOM(callback) {
-    Array.prototype.forEach.call(this.element, callback)
+    Array.prototype.forEach.call(this.element, callback) // poderia ser feito assim também, mas do outro jeito podem se passar mais parâmteros
   };
 
   DOM.prototype.mapDOM = function mapDOM(callback) {
@@ -50,7 +51,37 @@
   DOM.prototype.someDOM = function someDOM(callback) {
     Array.prototype.some.call(this.element, callback)
   };
+  */
 
+  DOM.prototype.forEach = function forEach(callback) {
+    return Array.prototype.forEach.apply(this.element, arguments)
+  };
+
+  DOM.prototype.map = function map(callback) {
+    return Array.prototype.map.apply(this.element, arguments)
+  };
+
+  DOM.prototype.filter = function filter(callback) {
+    return Array.prototype.filter.apply(this.element, arguments)
+  };
+
+  DOM.prototype.reduce = function reduce(callback) {
+    return Array.prototype.reduce.apply(this.element, arguments)
+  };
+
+  DOM.prototype.reduceRight = function reduceRight(callback) {
+    return Array.prototype.reduceRight.apply(this.element, arguments)
+  };
+
+  DOM.prototype.every = function every(callback) {
+    return Array.prototype.every.apply(this.element, arguments)
+  };
+
+  DOM.prototype.some = function some(callback) {
+    return Array.prototype.some.apply(this.element, arguments)
+  };
+
+  /* parte 2 - como eu tinha feito
 
   function checkObject(objToAnalise, objectType) {
     return Object.prototype.toString.call(objToAnalise) === objectType;
@@ -92,6 +123,40 @@
   };
   console.log('DOM.isNull(null);', DOM.isNull(null));
   console.log('DOM.isNull(undefined);', DOM.isNull(undefined));
+
+  */
+
+  DOM.prototype.isArray = function isArray(param) {
+    return Object.prototype.toString.call(param) === '[object Array]';
+  };
+
+  DOM.prototype.isObject = function isObject(param) {
+    return Object.prototype.toString.call(param) === '[object Object]';
+  };
+
+  DOM.prototype.isFunction = function isFunction(param) {
+    return Object.prototype.toString.call(param) === '[object Function]';
+  };
+
+  DOM.prototype.isNumber = function isNumber(param) {
+    return Object.prototype.toString.call(param) === '[object Number]';
+  };
+
+  DOM.prototype.isString = function isString(param) {
+    return Object.prototype.toString.call(param) === '[object String]';
+  };
+
+  DOM.prototype.isBoolean = function isBoolean(param) {
+    return Object.prototype.toString.call(param) === '[object Boolean]';
+  };
+
+  DOM.prototype.isNull = function isNull(param) {
+    return Object.prototype.toString.call(param) === '[object Null]'
+    || Object.prototype.toString.call(param) === '[object Undefined]';
+  };
+
+  var dom = new DOM(); // para que o prototype funcione direto
+  console.log(dom.isFunction(function(){}));
 
 
   DOM.prototype.on = function onDOM(event, callback){
