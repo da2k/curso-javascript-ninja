9159423b39a1840ebea111302ac5f8ff20c42e02
Gabriel Henrique <gabhenrique0@gmail.com>
2017-10-02 23:12:08 -0300

    Cria variável com um array de itens

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myArray = ['Gabriel', 8, null, undefined, true];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
