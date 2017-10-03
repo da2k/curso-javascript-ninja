522f55fa2c57f70c7339849330c2473763c8e82a
Natália Azevedo <naaatalia.azevedo@live.com>
2017-10-03 16:43:24 -0300

    Challenge-06 (doing...)

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Campeonato Paulista';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = [ 'Corinthians', 'São Paulo', 'Palmeiras', 'Santos', 'Ponte Preta'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +33,34 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(position) {
+    if (position > 5 || position < 1) return 'Não temos a informação do time que está nessa posição.';
+    return 'O time que está na ' + position + 'º lugar é o ' + teams[position-1] + '.';
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+showTeamPosition(1)
+'O time que está na 1º lugar é o Corinthians.'
+showTeamPosition(2)
+'O time que está na 2º lugar é o São Paulo.'
+showTeamPosition(4)
+'O time que está na 4º lugar é o Santos.'
+showTeamPosition(6)
+'Não temos a informação do time que está nessa posição.'
+
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+i = 20;
+while ( i <= 30) {
+  console.log('value is ' + i;)
+  i ++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
