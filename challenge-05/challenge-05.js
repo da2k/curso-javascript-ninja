8b9df5b40c47bbd1d206c15ac2f4dda4b8c218c2
Lucas Alves <lucas.alves_owns@hotmail.com>
2017-10-30 21:29:46 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myArray = ['lucas', 10, true, null, undefined, [2, 5 ,8]];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction( arg ) {
+    return arg;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+myFunction(myArray)[2];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,20 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunctionTwo ( arr, number ) {
+    return arr[number];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myArrayTwo = [15, 'alves', null, undefined, false];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunction(myArrayTwo);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +51,54 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book( arg ) {
+    var myVar = {
+        programacaoEmHtml5: {
+            quantidadePaginas: 200,
+            autor: 'Lucas',
+            editora: 'Novatec' 
+        },
+        asArmasDaPersuasao: {
+            quantidadePaginas: 150,
+            autor: 'Daciuk',
+            editora: 'Colombo'
+        },
+        dominandoAndroid : {
+            quantidadePaginas: 460,
+            autor: 'Nelson Glauber',
+            editora: 'Novatec'
+        }
+    }
+
+        if ( arg === undefined) {
+            return myVar;
+        } else {
+            return myVar.arg;
+        }
+    }
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Dominando Android tem ' + book().dominandoAndroid.quantidadePaginas + ' páginas!'); 
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro Dominando Android é ' + book().dominandoAndroid.autor + ".");
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro As Armas da Persuasão foi publicado pela editora ' + book().asArmasDaPersuasao.editora + ".");
