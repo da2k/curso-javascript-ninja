525ad4f3fb0c9d88251e7cf8f9f2376133c57368
Jhones Goncalves <jhones.goncalves@outlook.com>
2017-10-30 13:24:12 -0200

    challenge-21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -14,4 +14,32 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+(function (win, doc) {
+	'use strict';
+	
+	var $input = doc.querySelector('[type="text"]');
+	$input.value = 0;
+	var temporizador;
+	var $start = doc.querySelector('[data-js="start"]');
+	var $stop = doc.querySelector('[data-js="stop"]');
+	var $reset = doc.querySelector('[data-js="reset"]');
+
+	$start.addEventListener('click', function () {
+		function timer(){
+			$input.value++;
+			temporizador = setTimeout(timer, 1000);
+		}
+		timer();
+	}, false);
+
+
+	$stop.addEventListener('click', function () {
+		clearTimeout(temporizador);
+	}, false);
+
+	$reset.addEventListener('click', function () {
+		$input.value = 0;
+		clearTimeout(temporizador);
+	}, false);
+
+})(window, document);
