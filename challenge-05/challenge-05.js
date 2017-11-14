ed5a05ba5c4a631d7d843d0bb7c2a374495f4d63
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2017-11-13 23:13:42 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [ 1, true, 'bruno', -5, 'pinheiro' ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunc(myArr) {
+  return myArr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+myFunc(arr)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,20 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunc2(arr2, num) {
+  return arr2[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arr2 = [1, true, 'bruno', null, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunc(arr2);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +51,53 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nome) {
+  var obj = {
+    javascriptOGuiaDefinitivo: {
+      quantidadePaginas: 325,
+      autor: 'David Flanagan',
+      editora: 'OReilly'
+    },
+    oMelhorDoJavascript: {
+      quantidadePaginas: 456,
+      autor: 'Douglas Crockford',
+      editora: 'OReilly'
+    },
+    useACabecaJavascript: {
+      quantidadePaginas: 159,
+      autor: 'Michael Morrison',
+      editora: 'OReilly'
+    }
+  };
+  if(nome === undefined) {
+    return obj;
+  } else {
+    return obj.nome
+  }
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+"O livro " + Object.keys(book())[0] + " tem " + book('javascriptOGuiaDefinitivo').quantidadePaginas + " páginas!";
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+"O autor do livro " + Object.keys(book())[1] + " é " + book('javascriptOGuiaDefinitivo').autor + ".";
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+"O livro " + Object.keys(book())[2] + " foi publicado pela editora " + book('javascriptOGuiaDefinitivo').editora + ".";
