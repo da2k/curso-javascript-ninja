9a161ad771e1ec1f9686694cf103c38ed459b749
Jhones Goncalves <jhones.goncalves@outlook.com>
2017-11-21 12:52:37 -0200

    challenge-25

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,34 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+(function (win, doc) {
+  'use strict';
+
+  function on(element, event, callback) {
+    doc.querySelector(element)
+      .addEventListener(event, callback, false);
+  }
+
+  on('[data-js="link1"]', 'click', function (event) {
+    event.preventDefault;
+    alert('clicou');
+  })
+
+  on('[data-js="link2"]', 'mouseover', function (event) {
+    event.preventDefault;
+    alert('passou');
+  })
+
+  on('[data-js="input"]', 'input', function (event) {
+    event.preventDefault;
+    doc.querySelector('[data-js="div"]').innerHTML = this.value;
+  })
+
+  on('[data-js="select"]', 'change', function (event) {
+    event.preventDefault;
+    doc.querySelector('[data-js="number"]').value = this.value;
+  })
+
+
+
+})(window, document);
