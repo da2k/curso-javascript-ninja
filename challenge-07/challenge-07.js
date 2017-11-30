d3a29ef67140c6d462ddb5985da1881b890c95a6
lucas gabriel <lucasiguape16@gmail.com>
2017-11-29 22:38:38 -0200

    chanllenger07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -8,32 +8,42 @@ var arr = [10, 'lucas', true, {idade : 19}, [10,2,3]];
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+
+function addItem(item){
+    arr.push(item);
+    return arr;
+}
+
+
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+
+addItem([10, 'false', {bola: 'azul'}]);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+
+console.log('O segundo elemento do segundo array é ' +  arr[5][1] + '.');
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+
+console.log('O primeiro array tem ' + arr.length + ' itens');
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+
+console.log('O segundo array tem ' + arr[5].length  + ' itens');
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
