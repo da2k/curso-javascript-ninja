d188fef9ab0bb09bae8fc7be29c5c1f7b84b9337
Alexandre Flach <flachalexandret@gmail.com>
2017-12-02 15:16:37 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var valores = ['a', 1, 2.5, { posicao: 1}, [0,1]];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function funcao(lista){
+    return lista;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(funcao(valores)[1]); //1
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function funcao2(lista, indice){
+    return lista[indice];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arr = ['a', 1, 2.5, { posicao: 1}, function() { }];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(funcao2(arr, 0));
+console.log(funcao2(arr, 1));
+console.log(funcao2(arr, 2));
+console.log(funcao2(arr, 3));
+console.log(funcao2(arr, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,54 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nomeLivro){
+
+    var livros = {
+        'Livro 1': {
+            quantidadePaginas: 488,
+            autor: 'Joao',
+            editora: 'Brasil'
+        },
+        'Livro 2': {
+            quantidadePaginas: 650,
+            autor: 'Maria Helena',
+            editora: 'Abril'
+        },
+        'Livro 3': {
+            quantidadePaginas: 100,
+            autor: 'Silva Souza',
+            editora: 'EE Editora Ltda'
+        }
+    }
+
+    return !nomeLivro ? livros : livros[nomeLivro];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var nomeLivro = "Livro 1";
+console.log("O livro " + nomeLivro + " tem " + book(nomeLivro).quantidadePaginas + " páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+nomeLivro = "Livro 2";
+console.log("O autor do livro " + nomeLivro + " é " + book(nomeLivro).autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+nomeLivro = "Livro 3";
+console.log("O livro " + nomeLivro + " foi publicado pela editora " + book(nomeLivro).editora  + ".");
