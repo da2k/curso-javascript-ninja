577120500688d0baecdcfd6cee68faf256df3139
Roger Melo <rogerw592@gmail.com>
2017-12-13 13:49:32 -0200

    challenge-33

--- challenge-33/server/routes/index.js
+++ challenge-33/server/routes/index.js
@@ -9,21 +9,28 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-  data.push({
-    image: req.body.image,
-    brandModel: req.body.brandModel,
-    year: req.body.year,
-    plate: req.body.plate,
-    color: req.body.color
+  var carPlate = req.body.plate;
+  var hasCar = data.some(function(car) {
+    return car.plate === carPlate;
   });
-  res.json({ message: 'success' });
+  if(!hasCar) {
+    data.push({
+      image: req.body.image,
+      brandModel: req.body.brandModel,
+      year: req.body.year,
+      plate: req.body.plate,
+      color: req.body.color
+    });
+    return res.json({ message: 'success' });
+  }
+  res.json({message: 'carro já cadastrado'});
 });
 
 router.delete('/', function(req, res) {
   data = data.filter(function(car) {
     return car.plate !== req.body.plate;
   });
-  res.json({ message: 'success' });
+  res.json({ message: 'carro deletado' });
 });
 
 module.exports = router;
