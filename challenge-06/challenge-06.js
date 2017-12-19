d5aebcd210452eefd2c2c53bffd967b52aebbb2a
Mellina Yonashiro <pong7219@gmail.com>
2017-12-19 14:09:41 -0200

    Corre~ção Challenge 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -34,20 +34,20 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     "Não temos a informação do time que está nessa posição."
 */
 function showTeamPosition( posicao ) {
-    if (posicao > 5) {
+    if ( posicao < 1 || posicao > 5 ) {
         return "Não temos a informação do time que está nessa posição."
     }
-    return "O time que está em " + posicao + "º lugar é o " + teams[posicao-1] + ".";
+    return "O time que está em " + posicao + "º lugar é o " + teams[posicao - 1] + ".";
 }
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-console.log(showTeamPosition(1));
-console.log(showTeamPosition(4));
-console.log(showTeamPosition(5));
-console.log(showTeamPosition(6));
+console.log( showTeamPosition( 1 ) );
+console.log( showTeamPosition( 4 ) );
+console.log( showTeamPosition( 5 ) );
+console.log( showTeamPosition( 6 ) );
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
@@ -72,29 +72,37 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     "Não temos o equivalente hexadecimal para [COR]."
 */
 function converttoHex( cor ) {
+    var hexa;
     switch(cor) {
-        case "vermelho": return "O hexadecimal para a " + cor + " é #ff0000.";
-        break;
-        case "azul": return "O hexadecimal para a " + cor + " é #0000ff.";
-        break;
-        case "verde": return "O hexadecimal para a " + cor + " é #00ff00.";
-        break;
-        case "amarelo": return "O hexadecimal para a " + cor + " é #fff600.";
-        break;
-        case "laranja": return "O hexadecimal para a " + cor + " é #ff7e00.";
-        break;
-        default: return "Não temos o equivalente hexadecimal para " + cor + ".";
+        case "vermelho":
+            hexa = "#ff0000";
+            break;
+        case "azul":
+            hexa = "#0000ff";
+            break;
+        case "verde":
+            hexa = "#00ff00";
+            break;
+        case "amarelo": 
+            hexa = "#fff600";
+            break;
+        case "laranja":
+            hexa = "#ff7e00";
+            break;
+        default:
+            return "Não temos o equivalente hexadecimal para " + cor + ".";
     };
+    return 'O hexadecimal para a cor ' + cor + ' é ' + hexa + '.';
 };
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-console.log(converttoHex("vermelho"));
-console.log(converttoHex("laranja"));
-console.log(converttoHex("azul"));
-console.log(converttoHex("verde"));
-console.log(converttoHex("roxo"));
-console.log(converttoHex("prata"));
-console.log(converttoHex("amarelo"));
-console.log(converttoHex("marrom"));
+console.log(converttoHex('vermelho'));
+console.log(converttoHex('laranja'));
+console.log(converttoHex('azul'));
+console.log(converttoHex('verde'));
+console.log(converttoHex('roxo'));
+console.log(converttoHex('prata'));
+console.log(converttoHex('amarelo'));
+console.log(converttoHex('marrom'));
