1f02042b29661c28a8c37c6487d57d87c847ce28
Frankyston Lins <frankyston@gmail.com>
2017-12-23 14:02:07 -0300

    challenge 28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,5 @@
+(function(win, doc){
+  'use strict';
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +27,63 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  var $form = doc.querySelector('[data-js="send-cep"]');
+  var $inputCep = doc.querySelector('[data-js="cep"]');
+  var $status = doc.querySelector('[data-js="status"]');
+  var $session_response = doc.querySelector('[data-js="section-address"]');
+  var ajax;
+  var elements;
+  var data;
+  var cep;
+
+  function clearInput(value){
+    return value.match(/[0-9]+/g).join('');
+  }
+
+  function ajaxCep(cep){
+    ajax = new XMLHttpRequest();
+    ajax.open("GET", "https://viacep.com.br/ws/"+cep+"/json/");
+    ajax.send();
+
+    $status.appendChild(document.createTextNode("Buscando informações para o cep "+ cep));
+
+    ajax.addEventListener('readystatechange', function(){
+      if(isRequestOk(ajax)){
+
+        elements = $session_response.children;
+        data = JSON.parse(ajax.responseText);
+        clearAddress(elements);
+
+        if(!data.erro){
+          elements[0].appendChild(document.createTextNode("Endereço referente ao CEP " + cep))
+          elements[1].appendChild(document.createTextNode("Endereço: " + data.logradouro + " " + data.complemento))
+          elements[2].appendChild(document.createTextNode("Bairro: " + data.bairro))
+          elements[3].appendChild(document.createTextNode("Estado: " + data.uf))
+          elements[4].appendChild(document.createTextNode("Cidade: " + data.localidade))
+          elements[5].appendChild(document.createTextNode("CEP: " + cep))
+        }else{
+          elements[0].appendChild(document.createTextNode("Não encontramos o endereço para o CEP " + cep))
+        }
+      }
+    }, false);
+  }
+
+  function clearAddress(elements){
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].innerHTML = "";
+    }
+  }
+  function isRequestOk(ajax){
+    return ajax.readyState === 4 && ajax.status === 200
+  }
+
+  $form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if($inputCep.value === "")
+      return alert("Deve preencher com um cep");
+    cep = clearInput($inputCep.value);
+    ajaxCep(cep);
+  }, false);
+
+})(window, document);
