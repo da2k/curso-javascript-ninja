285060a36b3f1bfb196188faa26a860ddd659c9c
Lorran David <lorrantd1@gmail.com>
2018-01-02 00:13:20 +0000

    Challenge 26

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,31 +1,53 @@
-/*
-O desafio dessa semana é criar uma mini library (biblioteca) para
-reutilizarmos nossos códigos quando fizermos manipulação de DOM!
+(function(win, doc) {
+  /*
+  O desafio dessa semana é criar uma mini library (biblioteca) para
+  reutilizarmos nossos códigos quando fizermos manipulação de DOM!
 
-Requisitos:
-- O nome da lib deve ser "DOM".
-- Ela deve ser uma função construtora, que receberá uma string por parâmetro.
-Essa string será o nó do DOM a ser selecionado;
-- No construtor, você deve atribuir à `this.element` todos os elementos
-do DOM selecionados;
-- Extenda a lib para ter os métodos `on`, `off` e `get`.
-- O método `on` irá adicionar um listener de evento a todos os elementos
-selecionados.
-- O método `off` irá remover o listener de evento de todos os elementos
-selecionados.
-- O método `get` deve retornar os elementos.
-- O código abaixo deve funcionar corretamente após a lib criada.
+  Requisitos:
+  - O nome da lib deve ser "DOM".
+  - Ela deve ser uma função construtora, que receberá uma string por parâmetro.
+  Essa string será o nó do DOM a ser selecionado;
+  - No construtor, você deve atribuir à `this.element` todos os elementos
+  do DOM selecionados;
+  - Extenda a lib para ter os métodos `on`, `off` e `get`.
+  - O método `on` irá adicionar um listener de evento a todos os elementos
+  selecionados.
+  - O método `off` irá remover o listener de evento de todos os elementos
+  selecionados.
+  - O método `get` deve retornar os elementos.
+  - O código abaixo deve funcionar corretamente após a lib criada.
 
-Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
-Só passe para o próximo problema quando tiver resolvido o anterior :)
-*/
-// ?
+  Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
+  Só passe para o próximo problema quando tiver resolvido o anterior :)
+  */
+  'use strict';
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+  var DOM = function DOM(selector) {
+    this.element = doc.querySelectorAll(selector);
+  };
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+  DOM.prototype.on = function on(event, callback) {
+    Array.prototype.forEach.call(this.element, function($el) {
+      $el.addEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(event, callback) {
+    Array.prototype.forEach.call(this.element, function($el) {
+      $el.removeEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  var $a = new DOM('[data-js="link"]');
+  $a.on('click', function(e) {
+    e.preventDefault();
+    console.log('clicou');
+  });
+
+  console.log('Elementos selecionados:', $a.get());
+  console.log('$a é filho de body?', $a.get()[0].parentNode === doc.body);
+})(window, document);
