48edba55ce76d418766c362297ad11a709f3bbb9
Lorran David <lorrantd1@gmail.com>
2018-01-05 02:04:01 +0000

    Challenge-29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,39 +1,94 @@
-(function() {
+(function(doc, DOM) {
   'use strict';
 
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
+  var $companyName = new DOM('[data-js="companyName"]');
+  var $companyPhone = new DOM('[data-js="companyPhone"]');
+  var $formRegisterCar = new DOM('[data-js="formRegisterCar"]');
+  var $imgInput = new DOM('[data-js="imgInput"]');
+  var $modeloInput = new DOM('[data-js="modeloInput"]');
+  var $anoInput = new DOM('[data-js="anoInput"]');
+  var $placaInput = new DOM('[data-js="placaInput"]');
+  var $corInput = new DOM('[data-js="corInput"]');
+  var $recordsTable = new DOM('[data-js="recordsTable"]');
 
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
+  var app = function app() {
+    var init = function init() {
+      getCompanyInfo();
+    };
 
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
+    var Car = function Car(img, modelo, ano, placa, cor) {
+      this.img = img;
+      this.modelo = modelo;
+      this.ano = ano;
+      this.placa = placa;
+      this.cor = cor;
 
-  Essas informações devem ser adicionadas no HTML via Ajax.
+      return this;
+    };
 
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
+    var ajax = new XMLHttpRequest();
 
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
+    var getCompanyInfo = function getCompanyInfo() {
+      ajax.open('GET', 'company.json');
+      ajax.send();
+      ajax.addEventListener('readystatechange', ajaxHandle);
+    };
 
-})();
+    var fillCompanyInfo = function fillCompanyInfo(data) {
+      $companyName.get()[0].textContent = data.name;
+      $companyPhone.get()[0].textContent = data.phone;
+    };
+
+    var isRequestSuccessful = function isRequestSuccessful(request) {
+      return request.readyState === 4 && request.status === 200;
+    };
+
+    var ajaxHandle = function ajaxHandle() {
+      if(!isRequestSuccessful(ajax))
+        return;
+
+      try {
+        fillCompanyInfo(JSON.parse(ajax.response));
+      }catch(e) {
+        throw new Error('Aconteceu um probleminha');
+      }
+    };
+
+    var getFormData = function getFormData() {
+      return {
+        img: $imgInput.get()[0].value,
+        modelo: $modeloInput.get()[0].value,
+        ano: $anoInput.get()[0].value,
+        placa: $placaInput.get()[0].value,
+        cor: $corInput.get()[0].value
+      };
+    };
+
+    var formRegisterCarHandle = function formRegisterCarHandle(event) {
+      event.preventDefault();
+      var formData = getFormData();
+      var car = new Car(formData.img, formData.modelo, formData.ano, formData.placa, formData.cor);
+      addCarToRecords(car);
+    };
+
+    var addCarToRecords = function addCarToRecords(obj) {
+      var docFragment = doc.createDocumentFragment();
+      var newRow = doc.createElement('tr');
+
+      for(var prop in obj) {
+        var newColumn = doc.createElement('td');
+        newColumn.textContent = obj[prop];
+        newRow.append(newColumn);
+      }
+
+      docFragment.append(newRow);
+      $recordsTable.get()[0].children[1].append(docFragment);
+    };
+
+    $formRegisterCar.on('submit', formRegisterCarHandle);
+
+    init();
+  };
+
+  app();
+})(document, window.DOM);
---
+++ challenge-29/dom.js
@@ -0,0 +1,90 @@
+(function(win, doc) {
+  'use strict';
+
+  var DOM = function(selector) {
+    this.element = doc.querySelectorAll(selector);
+  };
+
+  DOM.prototype.on = function on(event, callback) {
+    Array.prototype.forEach.call(this.element, function($el) {
+      $el.addEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(event, callback) {
+    Array.prototype.forEach.call(this.element, function($el) {
+      $el.removeEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  DOM.prototype.forEach = function forEach() {
+    Array.prototype.forEach.apply(this.element, arguments);
+  };
+
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+  };
+
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments);
+  };
+
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments);
+  };
+
+  DOM.getParamType = function(param) {
+    return Object.prototype.toString.call(param);
+  };
+
+  DOM.isArray = function(param) {
+    return this.getParamType(param) === '[object Array]';
+  };
+
+  DOM.isObject = function(param) {
+    return this.getParamType(param) === '[object Object]';
+  };
+
+  DOM.isFunction = function(param) {
+    return this.getParamType(param) === '[object Function]';
+  };
+
+  DOM.isNumber = function(param) {
+    return this.getParamType(param) === '[object Number]';
+  };
+
+  DOM.isString = function(param) {
+    return this.getParamType(param) === '[object String]';
+  };
+
+  DOM.isBoolean = function(param) {
+    return this.getParamType(param) === '[object Boolean]';
+  };
+
+  DOM.isNull = function(param) {
+    return this.getParamType(param) === '[object Null]' || this.getParamType(param) === '[object Undefined]';
+  };
+
+  DOM.newElement = function(element) {
+    return doc.createElement(element);
+  };
+
+  // Pendura um método DOM no objeto Window
+  win.DOM = DOM;
+})(window, document);
