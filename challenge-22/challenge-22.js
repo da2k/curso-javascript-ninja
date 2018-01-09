0f57e17ea61178a156888e717a47b6a4bce62530
MatheusDidier <30843684+MatheusDidier@users.noreply.github.com>
2018-01-08 23:57:47 -0200

    Update challenge-22.js

--- challenge-22/challenge-22.js
+++ challenge-22/challenge-22.js
@@ -3,6 +3,8 @@
   `name` e `lastName`, preenchidos com o nome e sobrenome da pessoa.
   */
   // ?
+    var pessoa1 = {name: "Alberto", lastName: "Francisco"};
+    var pessoa2 = {name: "Penelope", lastName: "Canvas"};
 
   /*
   Agora crie uma função chamada `getFullName` que retorne as propriedades
@@ -17,6 +19,14 @@
   */
   console.log( 'O nome das pessoas é:' );
   // ?
+    function getFullName(){
+
+        return this.name + " " + this.lastName;
+
+    }
+
+    console.log(getFullName.call(pessoa1));
+    console.log(getFullName.call(pessoa2));
 
   /*
   Crie uma função chamada `sum`. Essa função pode receber uma lista de
@@ -33,6 +43,14 @@
   */
   console.log( '\nSomar alguns números:' );
   // ?
+    function sum(){
+        console.log(arguments);
+        return Array.prototype.reduce.call(arguments, function(acumulado, corrente){
+            return acumulado + corrente;
+        });
+    }
+
+    console.log(sum(3, 4, 5, 6));
 
   /*
   Declare uma variável chamada `userEntry`, que irá receber alguns valores
@@ -47,6 +65,16 @@
   console.log( '\nEntrada do usuário:' );
   // ?
 
+    var userEntry = [];
+
+    for(var i = 0; i < 5; i++){
+        userEntry.push(prompt("Entre com alguns números que serão somados: "));
+    }
+
+    userEntry.forEach(function(item){
+        console.log("Valores entrados: " + item);
+    })
+
   /*
   Crie uma função chamada `justNumbers`, que recebe por parâmetro uma string
   e remove tudo o que não for número, retornando um array somente com os números
@@ -54,6 +82,15 @@
   */
   console.log( '\nFunção que limpa entrada do usuário (somente números):' );
   // ?
+
+    function justNumbers(frase){
+        var regex = /\d+/g
+
+        return frase.match(regex);
+    }
+
+    var teste = justNumbers("123 eu sou legal 43 521 sou muito 3 21");
+    console.log(teste.toString());
 
   /*
   Usando a função acima, faça a limpeza dos valores entrados pelo usuário,
@@ -68,3 +105,17 @@
   */
   console.log( '\nSomar números entrados pelo usuário:' );
   // ?
+
+    var teste = justNumbers("123 eu sou legal 43 521 sou muito 3 21");
+    console.log(teste.toString());
+
+    
+    var $inputEntrada = document.querySelector("[data-js='entradaUsuario']");
+    var $botao = document.querySelector("[data-js='botao']");
+
+    $botao.addEventListener("click", function(){
+        var numbers = justNumbers($inputEntrada.value);
+        console.log(numbers);
+        var x = sum.apply(sum, numbers);
+        console.log(x);
+    });
