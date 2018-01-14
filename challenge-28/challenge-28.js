a724e139279152a17c7e4f7754740f94ee73211e
Vitebo <vitebo51@gmail.com>
2018-01-14 02:53:03 -0200

    Challenge 28 - Correção

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -102,60 +102,89 @@
       Object.prototype.toString( object ) === '[object Undefined]';
   }
 
-  var $cep = new DOM( '[data-js="cep"]' );
-  var $submit = new DOM( '[data-js="submit"]' );
+  var $formCEP = new DOM( '[data-js="form-cep"]' );
+  var $inputCEP = new DOM( '[data-js="input-cep"]' );
   var $logradouro = new DOM( '[data-js="logradouro"]' );
   var $bairro = new DOM( '[data-js="bairro"]' );
   var $estado = new DOM( '[data-js="estado"]' );
   var $cidade = new DOM( '[data-js="cidade"]' );
-  var $state = new DOM( '[data-js="state"]' );
+  var $cep = new DOM( '[data-js="cep"]' );
+  var $status = new DOM( '[data-js="status"]' );
+  var ajax = new XMLHttpRequest();
+  $formCEP.on( 'submit', handleSubmitFormCEP, false );
 
-  function initialize() {
-    $submit.on( 'click', handleClickSubmit, false );
-  }
-
-  function handleClickSubmit( event ) {
+  function handleSubmitFormCEP( event ) {
     event.preventDefault();
-    var cep = getCep();
-    if( cep ) createRequestCep( cep );
-  }
-
-  function createRequestCep( cep ) {
-    var ajax = new XMLHttpRequest();
-    var url = 'https://viacep.com.br/ws/' + cep + '/json/';
+    var url = getUrl();
     ajax.open( 'GET', url );
     ajax.send();
-    ajax.addEventListener( 'readystatechange', insertDataInForm, false );
+    getMessage( 'loading' );
+    ajax.addEventListener( 'readystatechange', handleReadyStateChange );
   }
 
-  function insertDataInForm() {
-    $state.get()[ 0 ].innerHTML = this.status;
-    if( !isRequestOk( this ) ) return;
-    var data = JSON.parse( this.responseText );
-    $logradouro.get()[ 0 ].value = data.logradouro;
-    $bairro.get()[ 0 ].value = data.bairro;
-    $estado.get()[ 0 ].value = data.uf;
-    $cidade.get()[ 0 ].value = data.localidade;
+  function getUrl() {
+    return replaceCep( 'https://viacep.com.br/ws/[CEP]/json' );
   }
 
-  function getCep() {
-    var cep = $cep.get()[ 0 ].value;
-    cep = numbersOnly( cep );
-    return isValidCep( cep ) ? cep : false;
+  function clearCep() {
+    return $inputCEP.get()[ 0 ].value.replace( /\D/g, '' );
   }
 
-  function numbersOnly( value ) {
-    return value.replace( /\D/g, '' );
+  function handleReadyStateChange() {
+    if( isRequestOk() ) {
+      getMessage( 'ok' );
+      fillCEPFields();
+    }
   }
 
-  function isValidCep( cep ) {
-    return cep.length === 8;
-  }
-
-  function isRequestOk( ajax ) {
+  function isRequestOk() {
     return ajax.readyState === 4 && ajax.status === 200;
   }
 
-  initialize();
+  function fillCEPFields() {
+    var data = parseData();
+    if( !data ) {
+      getMessage( 'error' );
+      data = clearData();
+    }
+    $logradouro.get()[ 0 ].textContent = data.logradouro;
+    $bairro.get()[ 0 ].textContent = data.bairro;
+    $estado.get()[ 0 ].textContent = data.uf;
+    $cidade.get()[ 0 ].textContent = data.localidade;
+    $cep.get()[ 0 ].textContent = data.cep;
+  }
+
+  function clearData() {
+    return {
+      logradouro: '-',
+      bairro: '-',
+      uf: '-',
+      localidade: '-',
+      cep: '-'
+    }
+  }
+
+  function parseData() {
+    var result;
+    try {
+      result = JSON.parse( ajax.responseText );
+    } catch( error ) {
+      result = null;
+    }
+    return result;
+  }
+
+  function getMessage( type ) {
+    var messages = {
+      loading: replaceCep( 'Buscando informações para o CEP [CEP]...' ),
+      ok: replaceCep( 'Endereço referente ao CEP [CEP] ' ),
+      error: replaceCep( 'Não encontramos o endereço para o CEP [CEP]' )
+    };
+    $status.get()[ 0 ].textContent = messages[ type ];
+  }
+
+  function replaceCep( message ) {
+    return message.replace( '[CEP]', clearCep() );
+  }
 
 }( window, document ) );
