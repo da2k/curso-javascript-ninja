81e5b502e8871a63d1615ec3f59f9af55a90502f
Gabriel Henrique <gabhenrique0@gmail.com>
2018-01-16 09:37:14 -0200

    Adiciona array com times e nome do campeonato no desafio #06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,7 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Inglês';
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +12,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Manchester United', 'Manchester City', 'Arsenal', 'Liverpool', 'Tottenham'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,7 +32,6 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
