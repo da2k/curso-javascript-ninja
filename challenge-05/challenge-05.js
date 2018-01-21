def09b832da06eba4e1f511b3a4c8e18d4f6e78f
Johnson Mauro <johnsonmauro@gmail.com>
2018-01-21 01:28:01 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myVar = [1, 2, 'eu', 'você', true];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction(arr){
+	return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+myFunction(myVar)[1]; //2
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function foo (arr, num){
+	return arr[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var omg = [1, 'eu', true, null, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+foo(omg, 0)
+foo(omg, 1)
+foo(omg, 2)
+foo(omg, 3)
+foo(omg, 4)
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,62 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(titulo){
+	var books = {
+            JavaScript: {
+                quantidadePaginas: 400,
+                autor: 'João',
+                editora: 'Casa do Código'
+            },
+			AspNet: {
+                quantidadePaginas: 500,
+                autor: 'Fernando',
+                editora: 'Casa do Código'
+            },
+			PHP: {
+                quantidadePaginas: 600,
+                autor: 'Rogério',
+                editora: 'Casa do Código'
+            }
+	};
+
+	if (titulo === 'JavaScript'){
+		return books.JavaScript;
+	}
+
+	if (titulo === 'AspNet'){
+		return books.AspNet;
+	}
+
+	if (titulo === 'PHP'){
+		return books.PHP;
+	}
+
+	return books;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log("O livro JavaScript tem " + book().JavaScript.quantidadePaginas + " páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log("O autor do livro JavaScript é " + book().JavaScript.autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log("O livro JavaScript foi publicado pela editora " + book().JavaScript.editora);
