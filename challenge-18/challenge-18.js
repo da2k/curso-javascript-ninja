cad4d73d96deda348d24556daec96162ce445a41
Isabelninja <34969457+Isabelninja@users.noreply.github.com>
2018-01-21 21:37:02 -0200

    Update challenge-18.js

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -20,7 +20,10 @@ console.log( 'Limpando CPFs:' );
 // function cleanCPF(cpf) {
 ... return cpf.replace(/\D/g, '');
 ... }
-
+ console.log( cleanCPF('049-214 3421-1') );
+ console.log( cleanCPF('210.458.522-05') );
+ console.log( cleanCPF('735 500 794 - 22') );
+  console.log( cleanCPF('101.123-131x32') );
 
 
 /*
