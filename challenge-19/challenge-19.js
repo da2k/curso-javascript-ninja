52ca7ccb13f76893d20743c93db433ec095dcf4e
Isabelninja <34969457+Isabelninja@users.noreply.github.com>
2018-01-22 17:42:52 -0200

    Update challenge-19.js

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -86,6 +86,9 @@ para exemplificar.
 */
 var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-// ?
+// function hassClass(markup, cssClass) {
+... return true;
+... }
+
    });
 
