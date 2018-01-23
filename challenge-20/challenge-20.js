03d07b9d6643761bb376a06ea999a76b965258ce
Isabelninja <34969457+Isabelninja@users.noreply.github.com>
2018-01-23 16:33:58 -0200

    Update challenge-20.js

--- challenge-20/challenge-20.js
+++ challenge-20/challenge-20.js
@@ -17,7 +17,12 @@ resultado à uma variável chamada `username`. Se o usuário não digitar um
 nome, `username` deve receber "Desconhecido".
 Com a resposta, mostre um alert com a mensagem "Bem vindo [USERNAME]!"
 */
-//  var username = prompt('Qual o seu nome'? );
+//   var username = prompt('Qual o seu nome'? );
+... if( !username )
+... username = 'Desconhecido';
+... alert( 'Bem vindo' + username );
+...
+
 
 /*
 Agora, pergunte ao usuário "Qual o seu e-mail?", atribuindo o resultado à
