95919ec0de4ede39c67e53d7a2013aef18064635
Ricardo Alves <ricardospalves@gmail.com>
2018-01-24 15:38:13 -0200

    Update challenge-18.js

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -18,13 +18,14 @@
   */
   console.log( 'Limpando CPFs:' );
   function cleanCPF(cpf) {
-    return cpf.replace(/[^\d]/g, '');
+    return cpf.replace(/\D/g, '');
   }
-  
-  console.log(cleanCPF('049-214 3421-1'));
-  console.log(cleanCPF('210.458.522-05'));
-  console.log(cleanCPF('735 500 794 - 22'));
-  console.log(cleanCPF('101.123-131x32'));
+
+  var cpfs = ['049-214 3421-1', '210.458.522-05', '735 500 794 - 22', '101.123-131x32'];
+
+  cpfs.forEach(function(item) {
+    console.log(cleanCPF(item));
+  });
 
   /*
   Usando os CPFs limpos acima, deixe-os com a formatação correta de CPF.
@@ -32,19 +33,9 @@
   Mostre o resultado no console.
   */
   console.log( '\nFormatando CPFs corretamente:' );
-  function formatCPF(cpf) {
-    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
-  }
-  
-  cpf1 = formatCPF(cpf1);
-  cpf2 = formatCPF(cpf2);
-  cpf3 = formatCPF(cpf3);
-  cpf4 = formatCPF(cpf4);
-
-  console.log(cpf1);
-  console.log(cpf2);
-  console.log(cpf3);
-  console.log(cpf4);
+  cpfs.forEach(function(item) {
+    console.log(cleanCPF(item).replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4'));
+  });
 
   /*
   Crie uma expressão regular que faça match com as palavras "junho" ou "julho",
@@ -58,9 +49,7 @@
   ["junho", "julho"]
   */
   console.log( '\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":' );
-  var text = 'Os meses de janeiro, junho e julho começam com a letra j.';
-  var regex = /ju(n|l)ho/g; 
-  console.log(text.match(regex));
+  console.log('Os meses de janeiro, junho e julho começam com a letra j.'.match(/ju[nl]ho/g));
 
   /*
   Crie uma expressão regular que faça o match com a abertura de uma tag
@@ -72,9 +61,7 @@
   ["<div>", "<section>", "<blockquote>"]
   */
   console.log( '\nMatch com a abertura de uma tag HTML:' );
-  var html = '<div><section><blockquote>Texto <img /></blockquote></section></div>';
-  var openTagHtml = /<\w+>/g;
-  console.log(html.match(openTagHtml));
+  console.log('<div><section><blockquote>Texto <img /></blockquote></section></div>'.match(/<\w+>/g));
 
   /*
   Crie uma expressão regular que faça o match com uma tag HTML vazia, casando
@@ -86,9 +73,7 @@
   ["<li></li>", "<li></li>", "<span></span>"]
   */
   console.log( '\nMatch com tags HTML vazias (abertura e fechamento da tag):' );
-  var newHtml = '<div><ul><li></li><li></li><li><span></span></li></ul></div>';
-  var tagEmpty = /<\w+><\/\w+>/g;
-  console.log(newHtml.match(tagEmpty));
+  console.log('<div><ul><li></li><li></li><li><span></span></li></ul></div>'.match(/<\w+><\/\w+>/g));
 
   /*
   Vamos complicar um pouco agora :D
@@ -113,5 +98,5 @@
   corretas, para depois aplicar no código ;)
   */
   console.log( '\nFazer replace dos textos das tags:' );
-  // ?
+  console.log('<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>'.replace(/<(\w+)>([^<]+)<\/\w+>/g,'O texto dentro da tag $1 é $2\n'));
 })();
