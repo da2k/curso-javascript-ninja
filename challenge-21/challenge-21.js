6c11dcb23838e548eb3befde6b24e109ae639360
Isabelninja <34969457+Isabelninja@users.noreply.github.com>
2018-01-24 17:31:43 -0200

    Update challenge-21.js

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -14,19 +14,33 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// (funciton(win,doc) {
-... 'use strict';
- <input type="text" value="0">
-   <input type="text" value="0">
-    <input type="text" value="0">
+// <input type="text" value="0">
     <button data-js="start"> Start</button>
     <button data-js="stop"> Stop </button>
     <butoon data-js="reset"> Reset </butoon>
-    <script src="challenge-21.js">  
-      var $timer= doc.querySelector('[data-js="timer"]');
-    var $start = doc.querySelector('[data-js="start"]');
-    var $stop = doc.querySelector('[data-js="stop"]');
-    var $reset = doc.querySelector('[data-js="reset"]');
-    var interval;
-$start.addEventListener('click',starTimer(),false)
-</script>
+    <script src="challenge-21.js"> 
+         'use strict';
+     (function(win,doc) {
+     var $timer= doc.querySelector('[data-js="timer"]');
+     var $start = doc.querySelector('[data-js="start"]');
+     var $stop = doc.querySelector('[data-js="stop"]');
+      var $reset = doc.querySelector('[data-js="reset"]');
+     var interval;    
+    
+       $start.addEventListener('click', starTimer , false);
+       $stop.addEventListener('click', stopTimer, false );
+        $reset.addEventListener('click', resetTimer, false);
+        function starTimer() {
+       $timer.value = +$timer.value +1;
+        interval = setTimeout(startTimer, 1000);
+         }
+     function stopTimer() {
+        
+         clearTimeout(interval);
+     }
+     function resetTimer() {
+         $timer.value = 0;
+         clearTimeout(interval);
+     }
+    })(window, document)
+     </script>
