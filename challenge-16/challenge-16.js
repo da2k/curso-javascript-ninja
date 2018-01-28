87b5fc100be755c2fe0f949474677eca5c8e2e4c
Mark Aurel <markaurel@protonmail.com>
2018-01-28 15:10:59 -0200

    challenge-16

--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -1,3 +1,5 @@
+(function(){
+'use strict';
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -14,7 +16,11 @@ Ex: no caso do nome ser "Fernando", deve mostrar as frases:
 E assim por diante, até a última.
 */
 console.log( 'As letras do seu nome:' );
-// ?
+var name = "Marco";
+
+for( var i = 0; i < name.length; i++ ) {
+	console.log( name.charAt(i), 'é a', (i + 1) + 'º', "letra do meu nome." );
+}
 
 /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
@@ -29,7 +35,14 @@ curso para fazer isso funcionar corretamente :)
 console.log para cada formato.
 */
 console.log( '\nNome convertido à partir de um slug:' );
-// ?
+var fullName = "marco-aurélio-andrade-santos";
+
+var newName = fullName.split( "-" ).map(function( x ) {
+	return x.charAt( 0 ).toUpperCase() + x.slice( 1 );
+});
+
+console.log(newName.join(" "));
+
 
 /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
@@ -41,22 +54,23 @@ O resultado final deve ficar mais ou menos assim:
 5 nomes foi somente uma sugestão ;)
 */
 console.log( '\nMeus amigos:' );
-// ?
+var fiveNames = [ 'Marco', 'Pedro', 'Fernanda', 'Maria', 'Gabriela' ];
+var lastName = fiveNames.pop() + " são meus amigos.";
+console.log(fiveNames.join(", ") + ' e', lastName);
 
 /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
 Mostre o resultado no console.
 */
 console.log( '\nEra "Roberto", agora é:' );
-// ?
+console.log('Roberto'.replace('to', 'ta'));
 
 /*
 Mostre no console a parte "nando" da string "Fernando". Use o método que
 faz a busca do final para o início da string.
 */
 console.log( '\nParte de uma string:' );
-// ?
-
+console.log("Fernando".substring(3));
 /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
 escrito de forma natural.
@@ -67,4 +81,13 @@ de qualquer tamanho, escrito de qualquer forma.
 Ex.: Nomes que deveriam funcionar: "Fernando", "RoBertO", "gabriEla", etc.
 */
 console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
-// ?
+var myName = "Marco";
+var newName = [];
+
+for(var i = 0, len = myName.length; i < len; i++) {
+	newName.push( i % 2 === 0 ? myName[ i ].toLowerCase() : myName[ i ].toUpperCase());
+}
+
+console.log(newName.join(""));
+
+})();
