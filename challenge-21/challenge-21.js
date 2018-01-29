182ad89dbf4c772e896f1f3f6d9542eb3e605865
Ricardo Alves <ricardospalves@gmail.com>
2018-01-29 11:53:27 -0200

    Update challenge-21.js

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,55 +1,52 @@
-/*
-O desafio de hoje será um pequeno projeto: um cronômetro!
-As regras para criação do cronômetro são as seguintes:
-1. Crie um arquivo index.html e adicione esse script a ele;
-2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
-Ele será o nosso cronômetro;
-3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
-4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
-cada segundo;
-5. Ao clicar em Stop, o cronômetro deve parar de contar;
-6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
-
-Utilize o atributo data-js para nomear o campo e os botões. Você pode
-usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
-dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
-*/
 (function(win, doc) {
   'use strict';
+
+  /*
+  O desafio de hoje será um pequeno projeto: um cronômetro!
+  As regras para criação do cronômetro são as seguintes:
+  1. Crie um arquivo index.html e adicione esse script a ele;
+  2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
+  Ele será o nosso cronômetro;
+  3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+  4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
+  cada segundo;
+  5. Ao clicar em Stop, o cronômetro deve parar de contar;
+  6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+
+  Utilize o atributo data-js para nomear o campo e os botões. Você pode
+  usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
+  dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
+  */
 
   var $inputVisor = doc.querySelector('[data-js="visor"]');
   var $buttonStart = doc.querySelector('[data-js="start"]');
   var $buttonStop = doc.querySelector('[data-js="stop"]');
   var $buttonReset = doc.querySelector('[data-js="reset"]');
-  var counter = 0;
   var temporizador;
 
-  function timer() {
+  function startTimer() {
 
-    $inputVisor.value = counter++;
+    $inputVisor.value = +$inputVisor.value + 1;
 
-    temporizador = setTimeout(timer, 1000);
+    temporizador = setTimeout(startTimer, 1000);
 
   }
 
-  $buttonStart.addEventListener('click', function() {
-
-    timer();
-
-  }, false);
-
-  $buttonStop.addEventListener('click', function() {
+  function stopTimer() {
 
     clearTimeout(temporizador);
 
-  }, false);
+  }
 
-  $buttonReset.addEventListener('click', function() {
+  function resetTimer() {
 
-    clearTimeout(temporizador);
-    counter = 0;
-    $inputVisor.value = counter;
+    stopTimer();
+    $inputVisor.value = 0;
 
-  }, false);
+  }
+
+  $buttonStart.addEventListener('click', startTimer, false);
+  $buttonStop.addEventListener('click', stopTimer, false);
+  $buttonReset.addEventListener('click', resetTimer, false);
 
 })(window, document);
