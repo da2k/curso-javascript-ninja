8feea448fd2dcab44d627daf1c3dddda23383699
Adeonir Kohl <adeonir@gmail.com>
2018-01-29 08:45:58 -0200

    Challenge 27

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -19,3 +19,92 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+
+(function () {
+  'use strict';
+
+  function DOM(elements) {
+    this.element = document.querySelectorAll(elements);
+  }
+
+  DOM.prototype.on = function (eventName, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.addEventListener(eventName, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function (eventName, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.removeEventListener(eventName, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function () {
+    return this.element;
+  };
+
+  DOM.prototype.forEach = function () {
+    Array.prototype.forEach.apply(this.element, arguments);
+  };
+
+  DOM.prototype.map = function () {
+    Array.prototype.map.apply(this.element, arguments);
+  };
+
+  DOM.prototype.filter = function () {
+    Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduce = function () {
+    Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduceRight = function () {
+    Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  DOM.prototype.every = function () {
+    Array.prototype.every.apply(this.element, arguments);
+  };
+
+  DOM.prototype.some = function () {
+    Array.prototype.some.apply(this.element, arguments);
+  };
+
+  function is(obj) {
+    return Object.prototype.toString.call(obj);
+  }
+
+  DOM.isArray = function (obj) {
+    return is(obj) === '[object Array]';
+  };
+
+  DOM.isObject = function (obj) {
+    return is(obj) === '[object Object]';
+  };
+
+  DOM.isFunction = function (obj) {
+    return is(obj) === '[object Function]';
+  };
+
+  DOM.isNumber = function (obj) {
+    return is(obj) === '[object Number]';
+  };
+
+  DOM.isString = function (obj) {
+    return is(obj) === '[object String]';
+  };
+
+  DOM.isBoolean = function (obj) {
+    return is(obj) === '[object Boolean]';
+  };
+
+  DOM.isNull = function (obj) {
+    return is(obj) === '[object Null]' || is(obj) === '[object Undefined]';
+  };
+
+  console.log(DOM.isArray([1, 2, 3]));
+  console.log(DOM.isFunction(function () { }));
+  console.log(DOM.isNumber('numero'));
+}());
