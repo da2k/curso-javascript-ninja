f5786048f3830d408583f158616ab136765232d2
Ricardo Alves <ricardospalves@gmail.com>
2018-01-30 14:20:08 -0200

    Update challenge-23.js

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -2,16 +2,28 @@
 	'use strict';
 
 	/*
-	Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
-	o código, conforme vimos na aula anterior. Quebrar as responsabilidades
-	em funções, onde cada função faça somente uma única coisa, e faça bem feito.
-
-	- Remova as duplicações de código;
-	- agrupe os códigos que estão soltos em funções (declarações de variáveis,
-	listeners de eventos, etc);
-	- faça refactories para melhorar esse código, mas de forma que o mantenha com a
-	mesma funcionalidade.
-	*/
+  Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+  As regras são:
+  - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+  diretamente;
+  - O input deve iniciar com valor zero;
+  - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+  - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+  multiplicação(x) e divisão(÷);
+  - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+  que irá limpar o input, deixando-o com valor 0;
+  - A cada número pressionado, o input deve atualizar concatenando cada valor
+  digitado, como em uma calculadora real;
+  - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+  operação no input. Se o último caractere no input já for um símbolo de alguma
+  operação, esse caractere deve ser substituído pelo último pressionado.
+  Exemplo:
+  - Se o input tem os valores: "1+2+", e for pressionado o botão de
+  multiplicação (x), então no input deve aparecer "1+2x".
+  - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+  input;
+  - Ao pressionar o botão "CE", o input deve ficar zerado.
+  */
 
 	var $visor = document.querySelector('[data-js="visor"]');
 	var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
