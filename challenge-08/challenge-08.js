7f11bb2db6124d92b0bf7fbe7b564094e14f22b3
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-02-03 20:39:16 -0200

    Corrigin erro desafio 08

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -28,7 +28,7 @@ Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
 function showName(){
-  return showName.name;
+  return 'Lucas Ferreira';
 }
 
 /*
@@ -41,7 +41,7 @@ Usando a variável criada acima, mostre no console o nome e o retorno da funçã
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log("A função showName retorna "+varShowName+".");
+console.log("A função "+showName.name+" retorna "+varShowName+".");
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
