9bb6a10d3967a76bd88a085ee5551a688e9df3ae
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-02-03 19:55:25 -0200

    Revert "Update challenge 05"
    This reverts commit b899c41d2b27c8c6bbb7eeaebeeaf1514d4c7060.

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -16,8 +16,8 @@ Imprima o segundo índice do array retornado pela função criada acima.
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
-segundo, um número. A função deve retornar o valor de um índice do array que foi passado
-no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
+segundo, um número. A função deve retornar um índice do array que foi passado
+no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
 // ?
