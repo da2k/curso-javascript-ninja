3cbb6c8da0112cc63bd96c24a3b2bfdd90f86511
Samuel Ramos <samramox@gmail.com>
2018-02-04 23:00:28 -0200

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -16,7 +16,7 @@ function addItem(item) {
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-console.log(addItem(arr, [1, 'otherString', 33]));
+console.log(addItem([1, 'otherString', 33]));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
