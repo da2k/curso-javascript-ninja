34019155ed37f55fdf1e5f108562cec01b217bff
DESKTOP-VK5930F\William Pegler <william-lpa@hotmail.com>
2018-02-05 22:57:03 -0200

    correcao while

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -52,7 +52,7 @@ Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
 var numero = 19;
-while (numero++ == 31) {
+while (numero++ !== 30) {
     console.log('numero', numero);
 }
 
