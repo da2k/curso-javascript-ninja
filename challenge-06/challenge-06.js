31b13999a972bbe9b291f40bfaa030d8d54ff79f
eduruiz <contato@eduruiz.com.br>
2018-02-14 12:38:27 -0200

    comitar altercoes pra continuar no outro pc

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'campeonato sul matogrossense';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Bahia', 'Embugacu', 'Grande Campo Grande', 'Itu', 'Jaguare'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,13 +33,20 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(posicao) {
+    if (!teams[posicao]) {
+        return 'Não temos a informação do time que está nessa posição.'
+    }
+    return 'O time que está em ' + posicao + 'º lugar é o ' + teams[++posicao] + '.';
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(4));
+console.log(showTeamPosition(5));
+console.log(showTeamPosition(10));
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
