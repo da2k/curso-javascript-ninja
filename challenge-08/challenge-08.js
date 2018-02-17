8da23f71f4a485b2aa51f92e54a3f447fdf76f00
José Luiz Coe <joseluizcoe@gmail.com>
2018-02-17 08:24:19 -0200

    Renaming var to varShowName

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -31,14 +31,14 @@ function showName(){
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var showName = showName;
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log('A função '+ showName.name +' retorna '+ showName() );
+console.log('A função '+ showName.name +' retorna '+ varShowName() );
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
