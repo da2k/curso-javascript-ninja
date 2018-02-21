e1826c4ab27ead29e758e3f03bc7b588aaab60cb
Kleber Freire <ch4r4d4@gmail.com>
2018-02-21 15:31:35 -0300

    challenge-17

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -1,3 +1,7 @@
+(function(){
+	'use strict';
+
+
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -10,15 +14,15 @@ regulares! Para isso, iremos usar o texto abaixo. Coloque-o em uma
 variável chamada `text`:
 "Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro."
 */
-// ?
-
+var text = 'Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro.';
 /*
 Vamos começar com umas brincadeiras fáceis :D
 Troque o nome "Manuel Marques de Souza" pelo seu nome, e mostre o resultado
 no console:
 */
 console.log( 'Adicionando seu nome no texto:' );
-// ?
+
+console.log(text.replace(/Manuel Marques de Sousa/g, 'Kleber Willian Dutra Freire'));
 
 /*
 Agora, substitua a palavra "brasileiro" por sua cidade natal e mostre no
@@ -26,35 +30,43 @@ console.
 Ex: Se você for da São Paulo, substitua por "paulista".
 */
 console.log( '\nTrocando naturalidade:' );
-// ?
+
+console.log( text.replace(/brasileiro/g, 'candango') );
 
 /*
 Substitua todos os números por um traço `-`. Cada caractere de número deve
 ser um traço. Mostre o resultado no console:
 */
 console.log( '\nTrocando números por -:' );
-// ?
+
+
+console.log( text.replace(/\d/g, '-') );
 
 /*
 Substitua todas as letras (somente letras) de "D" maiúsculo até "h"
 minúsculo por "0" (número zero). Mostre o resultado no console:
 */
 console.log( '\nTrocando de "D" a "h" por "0":' );
-// ?
+
+console.log( text.replace(/[D-Za-h]/g, 0) );
 
 /*
 Substitua todos os "A" (maiúsculos ou minúsculos) por "4".
 Mostre o resultado no console:
 */
 console.log( '\nTrocando "A" e "a" por "4":' );
-// ?
+
+console.log( text.replace(/[Aa]/g, 4) );
 
 /*
 Substitua a frase "O Centauro de Luvas", deixando-a em caixa alta, usando
 o método `toUpperCase()`. Mostre o resultado no console:
 */
 console.log( '\n"O Centauro de Luvas" em caixa alta:' );
-// ?
+
+console.log( text.replace(/O Centauro de Luvas/g,function(captura){
+	return captura.toUpperCase();
+}) );
 
 /*
 Agora iremos substituir as datas no formato "13 de junho de 1804" para
@@ -69,7 +81,29 @@ Use um console.log para cada mês, usando a frase:
 "O mês de [NOME DO MÊS] é representado pelo número [NÚMERO DO MÊS]."
 */
 console.log( '\nMeses representados por números:' );
-// ?
+
+function getMonthNumber (nomeMes){
+	var mes = {
+		'janeiro' : '01', 
+		'fevereiro' : '02',
+		'março' : '03',
+		'abril' : '04',
+		'maio' : '05',
+		'junho' : '06',
+		'julho': '07',
+		'agosto' : '08',
+		'setembro' : '09',
+		'outubro' : '10',
+		'novembro' : '11',
+		'dezembro' : '12'		
+	} 
+	return mes[nomeMes];
+
+}
+console.log('O mês de março é representado pelo número ' + getMonthNumber('março'));
+console.log('O mês de setembro é representado pelo número ' + getMonthNumber('setembro'));
+console.log('O mês de dezembro é representado pelo número ' + getMonthNumber('dezembro'));
+
 
 /*
 Agora, declare uma variável chamada `regexDate` que irá receber a expressão
@@ -80,7 +114,9 @@ Com o que vimos até agora, você consegue fazer :D
 Mostre a regex no console.
 */
 console.log( '\nRegex que vai fazer o match com as datas do texto:' );
-// ?
+
+var regexDate = /(\d\d) de (junho|julho) de (\d\d\d\d)/g; 
+console.log( regexDate );
 
 /*
 Agora crie a função que irá fazer o replace dos dados. A função será chamada
@@ -90,4 +126,13 @@ Após criar a função, faça o replace das datas no texto, mostrando no
 console o resultado.
 */
 console.log( '\nReplace de datas:' );
-// ?
+
+function replaceDate (regex ,dia , mes, ano){
+
+	return dia + '/' + getMonthNumber( mes ) + '/' + ano;
+}
+
+console.log( text.replace( regexDate, replaceDate ) );
+
+
+})();
