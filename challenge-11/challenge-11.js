7e8610edcf5b4628ad9692e5e4345fbf4b71518f
Christian Krzekotowski da Cunha <krzek.cunha@gmail.com>
2018-02-20 23:33:31 -0300

    Challenge-11

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,54 +1,103 @@
 // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
+(function () {
+    /*
+    Crie uma variável chamada `once`, que recebe `false` como seu valor.
+    Crie um loop que execute enquanto essa variável for verdadeira. Dentro do
+    loop, mostre no console a mensagem:
+    'Entrou ao menos uma vez!'
+    Qual loop você deve usar para que essa mensagem seja mostrada no console?
+    */
+    var once = false;
 
-/*
-Crie uma variável chamada `once`, que recebe `false` como seu valor.
-Crie um loop que execute enquanto essa variável for verdadeira. Dentro do
-loop, mostre no console a mensagem:
-'Entrou ao menos uma vez!'
-Qual loop você deve usar para que essa mensagem seja mostrada no console?
-*/
-// ?
+    do {
+        console.log('Entrou ao menos uma vez!')
+    } while (once);
 
-/*
-Crie um objeto chamado `person`, que receba as seguintes propriedades:
-- 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
-para o nome, idade, peso e data de nascimento dessa pessoa.
-*/
-// ?
+    /*
+    Crie um objeto chamado `person`, que receba as seguintes propriedades:
+    - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
+    para o nome, idade, peso e data de nascimento dessa pessoa.
+    */
+    var person = {
+        'name': 'Christian',
+        'age': 25,
+        'weight': 72,
+        'birthday': '07/12/1992'
+    }
 
-/*
-Use um loop para percorrer o objeto criado acima, mostrando no console
-a frase:
-'The [PROPERTY] of person is [VALUE]'
-Aproveite e crie uma variável `counter` que vai contar quantas propriedades
-esse objeto tem.
-Após o loop, mostre a frase:
-'The person has [COUNTER] properties'
-*/
-// ?
+    /*
+    Use um loop para percorrer o objeto criado acima, mostrando no console
+    a frase:
+    'The [PROPERTY] of person is [VALUE]'
+    Aproveite e crie uma variável `counter` que vai contar quantas propriedades
+    esse objeto tem.
+    Após o loop, mostre a frase:
+    'The person has [COUNTER] properties'
+    */
+    var counter = 0;
 
-/*
-Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
-criado acima) é mais velha que a idade passada por parâmetro.
-Se verdadeiro, retornar `true`. Senão, retornar false.
-Após a função, mostrar a mensagem no console:
-'The person has more than 25 years old? [TRUE/FALSE]'
-*/
-// ?
+    for (prop in person) {
+        counter++;
+        console.log('The ' + prop + ' of person is ' + person[prop] + '.');
+    }
+    console.log('The person has ' + counter + ' properties');
 
-/*
-Faça um loop de 0 a 20, que adicione cada número como um item de um
-array chamado `numbers`. Se o contador for maior que 10, saia do loop.
-Mostre no console os números no array.
-*/
-console.log( 'De 0 a 10:' );
-// ?
+    /*
+    Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
+    criado acima) é mais velha que a idade passada por parâmetro.
+    Se verdadeiro, retornar `true`. Senão, retornar false.
+    Após a função, mostrar a mensagem no console:
+    'The person has more than 25 years old? [TRUE/FALSE]'
+    */
+    function moreThan(age) {
+        return person.age > age ? true : false;
+    }
 
-/*
-Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
-criado acima, só precisa ser reiniciado) números de 0 a 20, inclusive
-esses. Se o número for ímpar, pular para o próximo número.
-Mostrar no console os números do array.
-*/
-console.log( 'Pares de 0 a 20:' );
-// ?
+    console.log('The person has more than 25 years old? ' + moreThan(25) + '');
+
+    /*
+    Faça um loop de 0 a 20, que adicione cada número como um item de um
+    array chamado `numbers`. Se o contador for maior que 10, saia do loop.
+    Mostre no console os números no array.
+    */
+    console.log('De 0 a 10:');
+    var numbers = [];
+    for (var i = 0; i < 20; i++) {
+        if (i > 10) {
+            break;
+        }
+
+        numbers.push(i);
+    }
+
+    console.log(numbers);
+    /*
+    Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
+    criado acima, só precisa ser reiniciado) números de 0 a 20, inclusive
+    esses. Se o número for ímpar, pular para o próximo número.
+    Mostrar no console os números do array.
+    */
+    console.log('Pares de 0 a 20:');
+    numbers = [];
+
+    for (var i = 0; i <= 20; i++) {
+        if (i % 2 !== 0) {
+            continue;
+        }
+
+        numbers.push(i);
+    }
+
+    console.log(numbers);
+
+    console.log('Pares de 0 a 20: (sem utilizar o salto de continue)');
+    numbers = [];
+
+    for (var i = 0; i <= 20; i++) {
+        if (i % 2 === 0) {
+            numbers.push(i);
+        }
+    }
+
+    console.log(numbers);
+})();
