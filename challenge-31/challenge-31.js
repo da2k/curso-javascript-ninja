2fb95612f106ed906f37ec74ff66e7247218141f
Alexandre Flach <flachalexandret@gmail.com>
2018-02-25 17:55:03 -0300

    Update challenge-31.js

--- challenge-31/challenge-31.js
+++ challenge-31/challenge-31.js
@@ -12,3 +12,4 @@ do curso, para colar o link do pull request do seu repo.
 */
 
 console.log('Link do pull request do seu projeto');
+console.log('https://github.com/alexandreflach/automaisvendafacil/pull/2');
