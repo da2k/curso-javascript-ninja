ea9d344d86dc8b511241ad24f57e579a7108f039
Lucas Ferreira <lucas.santos@fcamara.com.br>
2018-02-27 14:41:32 -0300

    Desafio 14 completo

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -46,9 +46,10 @@
     */
     console.log( '\nOperation:' );
     // ?
-    var operation = justMod2Or3.reduce(function(acumulador,valorAtual,indece){
-
-    },1);
+    var operation = justMod2Or3.reduce(function(acumulador,valorAtual){
+       return (acumulador + 1 ) *valorAtual;
+    },0);
+    console.log(operation);
     /*
     Faça o mesmo cálculo passado acima, mas começando do último item para o
     primeiro. O nome da variável deve ser operation2. Mostre o resultado no
@@ -56,8 +57,11 @@
     */
     console.log( '\nOperation 2:' );
     // ?
-
-    /*
+    var operation2 = justMod2Or3.reduceRight(function(acumulador,valorAtual){
+        return (acumulador + 1 ) * valorAtual;
+     },0);
+     console.log(operation2);
+    /* 
     Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
     do seu nome. Vamos reduzir esse array, juntando todas as sílabas, mas usando
     a "língua do P".
@@ -67,43 +71,52 @@
     */
     console.log( '\nSeu nome na língua do "P":' );
     // ?
-
+     var nome = ['Lu','cas'];
+     var reduceName = nome.reduce(function(letra,silaba,indece){
+        return letra+'p'+silaba ;
+     });
+     console.log(reduceName);
     /*
     Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
     e atribuirá o seu nome invertido (usando o array criado acima).
     */
     console.log( '\nInversed Name:' );
     // ?
-
+     var inversedName = nome.reduceRight(function(item,itemAcumulado){
+         return item + itemAcumulado;
+     });
+     console.log(inversedName);
     /*
     Mostre no console o array `numberObjects`.
     */
     console.log( '\nNumber objects' );
     // ?
-
+     console.log(numberObjects);
     /*
     Verifique se existem em algum índice de numberObjects um objeto ìgual a
     { number: 2 }. Se houver, mostre no console:
     - "Existe um objeto { number: 2 } em numberObjects!"
     Senão, mostre a frase:
-    - "Não existe um objeto { number: 2 } em numberObjects :("
+    - 
     Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
     o que acontece ;)
     */
     console.log( '\nExiste um { number: 2 } em numberObjects?' );
     // ?
-
+     console.log(numberObjects.indexOf({ number: 2 })?"Existe um objeto { number: 2 } em numberObjects!":"Não existe um objeto { number: 2 } em numberObjects :(")
     /*
     Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
     será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
     */
     console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
     // ?
-
+    console.log(numberObjects.lastIndexOf({ number: 2 })?"Existe um objeto { number: 2 } em numberObjects!":"Não existe um objeto { number: 2 } em numberObjects :(")
     /*
     Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
     formato de String.
     */
     console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
+    console.log(Array.isArray(justMod2Or3))
+    console.log(justMod2Or3.toString());
     // ?
 })();
