b0224aa9741f71bc3a418895da870221771cad56
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2018-02-27 14:44:19 -0300

    Challenge-06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,10 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+
+var championship = 'Campeonato Paulista';
+
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +15,8 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+
+var teams = ['São Paulo', 'Curitina', 'Santos', 'Parmeira', 'Ponte Preta']
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +36,70 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+
+//function showTeamPosition( number ){
+//    switch(number){
+//        case 1:
+//            return 'O time que está em ' + number + 'º ' + 'lugar é o ' + teams[0] + '.';
+//            break;
+//
+//        case 2:
+//            return 'O time que está em ' + number + 'º ' + 'lugar é o ' + teams[1] + '.';
+//            break;
+//
+//        case 3:
+//            return 'O time que está em ' + number + 'º ' + 'lugar é o ' + teams[2] + '.';
+//            break;
+//
+//        case 4:
+//            return 'O time que está em ' + number + 'º ' + 'lugar é o ' + teams[3] + '.';
+//            break;
+//
+//        case 5:
+//            return 'O time que está em ' + number + 'º ' + 'lugar é o ' + teams[4] + '.';
+//            break;
+//
+//        default:
+//            return 'Não temos a informação do time que está nessa posição.'
+//
+//    }
+//
+//}
+
+function showTeamPosition( position ){
+    if(position < 1 || position > 5){
+        return 'Não temos a informação do time que está nessa posição.'
+    }
+    return 'O time que está em ' + position + 'º ' + 'lugar é o ' + teams[position - 1] + '.';
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+
+console.log(showTeamPosition(1));
+console.log(showTeamPosition(2));
+console.log(showTeamPosition(3));
+console.log(showTeamPosition(4));
+console.log(showTeamPosition(10));
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+
+var numbers = 20;
+
+//while( numbers >= 20 && numbers <= 30 ){
+//    console.log(numbers++);
+//}
+
+while( numbers <= 30 ){
+    console.log(numbers++);
+}
+
+numbers;
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +113,73 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+
+//function convertToHex(color) {
+//    var hexaFrase = 'O hexadecimal para a cor ';
+//    
+//    switch(color){
+//        case 'red':
+//            return hexaFrase + color + ' é #ff0000';
+//
+//        case 'green':
+//            return hexaFrase + color + ' é #00ff00';
+//
+//        case 'blue':
+//            return hexaFrase + color + ' é #0000ff';
+//
+//        case 'yellow':
+//            return hexaFrase + color + ' é #ffff00';
+//
+//        case 'cyan':
+//            return hexaFrase + color + ' é #00ffff';
+//
+//        default:
+//            return 'Não temos o equivalente hexadecimal para ' + color + '.';
+//    }
+//}
+
+function convertToHex(color) {
+    var hexaPhrase = 'O hexadecimal para a cor ';
+    var hexaPhraseDont = 'Não temos o equivalente hexadecimal para ';
+    var hexadecimal;
+    
+    switch(color){
+        case 'red':
+            hexadecimal = '#ff000';
+            break;
+            
+        case 'green':
+            hexadecimal = '#00ff00';
+            break;
+            
+        case 'blue':
+            hexadecimal = '#0000ff';
+            break;
+            
+        case 'yellow':
+            hexadecimal = '#ffff00';
+            break;
+            
+        case 'cyan':
+            hexadecimal = '#00ffff';
+            break;
+
+        default:
+            return hexaPhraseDont + color + '.';
+    }
+    
+    return hexaPhrase + color + ' é ' + hexadecimal + '.';
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+
+console.log(convertToHex('red'));
+console.log(convertToHex('green'));
+console.log(convertToHex('blue'));
+console.log(convertToHex('orange'));
+console.log(convertToHex('yellow'));
+console.log(convertToHex('purple'));
+console.log(convertToHex('cyan'));
+console.log(convertToHex('brown'));
