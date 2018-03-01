e1b0dc8f5f14c8faf9a743f84806cb7bbb8c39fd
Adriano Avelino <adriano-st777@hotmail.com>
2018-02-28 21:19:14 -0300

    Challenge 29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -36,4 +36,102 @@
   que será nomeado de "app".
   */
 
+  function dealership() {
+    var $form = new DOM('[data-js=form]');
+    var $imagem = new DOM('[data-js=car-imagem]');
+    var $marcaModelo = new DOM('[data-js=car-marcamodelo]');
+    var $ano = new DOM('[data-js=car-ano]');
+    var $placa = new DOM('[data-js=car-placa]');
+    var $cor = new DOM('[data-js=car-cor]');
+    var $table = new DOM('[data-js=dealership-table]');
+    var $title = new DOM('[data-js=dealership-title]');
+    var carsList = [];
+    var ajax;
+    $form.on('submit', handleSubmitForm);
+
+    function handleSubmitForm(event) {
+      event.preventDefault();
+      var newCar = addCar();
+      carsList.push(newCar);
+      renderCarList();
+      clearForm();
+    }
+
+    function addCar() {
+      return {
+        img: $imagem.get()[0].value,
+        modelo: $marcaModelo.get()[0].value,
+        ano: $ano.get()[0].value,
+        placa: $placa.get()[0].value,
+        cor: $cor.get()[0].value
+      }
+    }
+
+    function renderCarList() {
+      $table.get()[0].innerHTML = carsList.map(makeMarkup).join('');
+    }
+
+    function makeMarkup(car) {
+      return  '<tr>' +
+                '<th>Imagem do carro:</th>' +
+                '<td>' + car.img + '</td>' +
+              '</tr>' +
+              '<tr>' +
+                '<th>Marca / Modelo:</th>' +
+                '<td>' + car.modelo + '</td>' +
+              '</tr>' +
+              '<tr>' +
+                '<th>Ano:</th>' +
+                '<td>' + car.ano + '</td>' +
+              '</tr>' +
+              '<tr>' +
+                '<th>Placa:</th>' +
+                '<td>' + car.placa + '</td>' +
+              '</tr>' +
+              '<tr>' +
+                '<th>Cor:</th>' +
+                '<td>' + car.cor + '</td>' +
+              '</tr>';
+    }
+
+    function clearForm() {
+      $imagem.get()[0].value = "";
+      $marcaModelo.get()[0].value = "";
+      $ano.get()[0].value = "";
+      $placa.get()[0].value = "";
+      $cor.get()[0].value = "";
+    }
+
+    function setTitle() {
+      ajax = new XMLHttpRequest();
+      ajax.open('GET', '/company.json');
+      ajax.send();
+      ajax.addEventListener('readystatechange', handleReadyStateChange, false);
+    }
+
+    function handleReadyStateChange() {
+      if (isRequestOK()) {
+        var data = parseData();
+        $title.get()[0].textContent = data.name + ' ' + data.phone;
+      }
+    }
+
+    function isRequestOK() {
+      return ajax.readyState === 4 && ajax.status === 200;
+    }
+
+    function parseData() {
+      var data;
+      try {
+        data = JSON.parse(ajax.responseText)
+      } catch (error) {
+        console.log('Erro:',error);
+        data = null;
+      }
+      return data;
+    }
+    setTitle();
+  }
+
+  dealership();
 })();
