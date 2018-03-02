316385ae268dfced567cafbc00831c702352dbf6
igorleme <36270062+igorleme@users.noreply.github.com>
2018-03-02 11:28:20 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -68,17 +68,17 @@ Só vamos mudar o range:
 - No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
-
-for( var num = 100; num <= 120; num++){
-  num % 2 === 0 ? console.log(num) ? '';
-}
  
 
 console.log( 'Números pares entre 100 e 120:' );
-
-  for(var num = 111; num <= 125; num++){
-  num % 2 !== 0 ? console.log(num) : ''
+for( var num = 100; num <= 120; num++){
+  num % 2 === 0 ? console.log(num) ? '';
 }
   
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+
+for(var num = 111; num <= 125; num ++){
+    if(num % 2 !== 0){
+        console.log(num)
+    }
+}
