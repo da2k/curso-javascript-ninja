6dcc8b9c09828d7d4227792b26d9da34c7a7cafd
Victor Nascimento <victon.barros@gmail.com>
2018-03-14 17:42:59 +0000

    Resolução Desafio 7

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -9,46 +9,54 @@ A função deverá retornar o array atualizado.
 */
  function addItem(arg){ 
  arr.push(arg); 
- return 'O array possui '+ arr.length + ' elementos e os seus elementos são'+arr;   
+ return 'O array possui '+ arr.length + ' elementos e os seus elementos são: '+arr;   
 }
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-'O array inserido possui 11 elementos e os seus elementos sãoteste,22,NaN,[object Object],,23,teste,30,30,Teste,teste,40,'
+addItem(['false',true,null]);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log("O segundo elemendo do segundo array é o elemento: "+ arr[5][2]);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log("O primeiro arrya tem"+ arr.length+ " Itens");
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log("O segundo array tem "+ arr[5].length + " Itens");
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
+var par = 10;
 console.log( 'Números pares entre 10 e 20:' );
-// ?
+while(par <= 20){ 
+console.log( par % 2 === 0 ? par : ''); 
+par++; 
+}  
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
+var impar = 10;
 console.log( 'Números ímpares entre 10 e 20:' );
-// ?
+while(impar <= 20){ 
+console.log( impar % 2 !== 0 ? impar : ''); 
+impar++; 
+}
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -57,7 +65,11 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 console.log( 'Números pares entre 100 e 120:' );
-// ?
+for(par=100;par<=120; par++){ 
+console.log( par%2 === 0 ? par : ''); 
+}
 
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+for(impar=111;impar<=125;impar++){ 
+console.log(impar%2 ? impar : ''); 
+}
--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,45 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum(arg1,arg2){
+    return arg1 + arg2;
+};
+
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+console.log("A soma do primeiro valor +  o segundo é igual a :"+ sum(3,4));
+
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log("O nome da função que faz a soma é: "+ sum.name);
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
-
+function showName(){
+  
+  return showName.name;
+}
+console.log(showName());
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+var varShowName = showName();
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log("A função " + varShowName + " Retorna "+ showName.name);
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,8 +55,33 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+function calculator(arg){
+    switch(arg){
+      case '+':
+        return 'Você irá realizar uma adição';
+      break;
+      case '-':
+        return 'Você irá realizar uma subtração';
+      break;
+      case '*':
+        return 'Você irá realizar uma Mutiplicação';
+      break;
+      case '/':
+        return 'Você irá realizar uma Divisão';
+      break;
+      case '%':
+        return 'Você irá realizar o resto da Divisão';
+      break;
+      default: 
+        return '"Operação inválida.';
+      break;
+    }
+    return function(x,y){
+      return arg(x,y);
+    };
+}
 
+console.log(calculator('/',10,5));
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
