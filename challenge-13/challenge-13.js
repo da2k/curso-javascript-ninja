600b3a6adafc9eb390b28fdec4ca6bf813221326
Wallace Costa <wallacedacosta@gmail.com>
2018-03-17 11:47:21 -0300

    Finalizado o desafio

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -178,24 +178,30 @@ objeto desse array, e adicione a frase abaixo na propriedade `estado`:
 - "[ESTADO] pertence ao Brasil."
 Atribua o novo array a uma variável chamada `map`.
 */
-// ?
+var map = newBrasil.map(function(item, index){
+    item.id++;
+    item.estado += ' pertence ao Brasil';
+    return item;
+});
 
 /*
 Mostre no console o array criado acima:
 */
 console.log( '\nnewBrasil agora com mais informações:' );
-// ?
+console.log(map);
 
 /*
 Filtre o array criado acima, retornando somente os estados que tiverem
 ID par. Atribua o valor à uma variável chamada `filter`.
 */
-// ?
+var filter = map.filter(function(item, index){
+    return item.id % 2 === 0;
+});
 
 /*
 Mostre o array filtrado acima no console.
 */
 console.log( '\nEstados com ID par:' );
-// ?
+console.log(filter);
 
 })();
