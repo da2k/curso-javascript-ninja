4321efe438cacdcdaa019b02932d744c7e129a3c
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2018-03-18 00:40:07 -0300

    challenge-06

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,20 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var i = [1, undefined, null, true, false];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction( arr ){
+	return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+console.log(myFunction( i[1] ));
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +23,25 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function  Funcao( arr, number){
+	return arr[number];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+var arrFive = [undefined, null, false, [5,2, 'Felipe'], 1];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log( Funcao(arrFive, 0) );
+console.log( Funcao(arrFive, 1) );
+console.log( Funcao(arrFive, 2) );
+console.log( Funcao(arrFive, 3) );
+console.log( Funcao(arrFive, 4) );
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +57,57 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName){
+	var books = {
+		'Harry Potter': {
+			quantidadePaginas: 150,
+			autor: 'J. K. Rowling',
+			editora: 'Editora Rocco'
+		},
+		'Bíblia': {
+			quantidadePaginas: 1503,
+			autor: 'Deus',
+			editora: 'Editora Fiel'
+		},
+		'The Future': {
+			quantidadePaginas: 721,
+			autor: 'Robert Van Roving',
+			editora: 'Editora Venagrunt'
+		}
+
+	};
+
+	if (!bookName) {
+		return books;
+	}
+
+	return books[ bookName ];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log( book() );
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var bookName = 'Harry Potter';
+
+console.log( 'O livro ' + bookName + ' tem ' + book( bookName ).quantidadePaginas + ' páginas!' );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log( 'O autor do livro ' + bookName + ' é '+ book(bookName).autor +'.' );
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( "O livro " + bookName + " foi publicado pela editora " + book( bookName ).editora );
--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Carioca';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Vasco', 'Botafoto', 'Fluminense', 'Madureira', 'Boa Vista'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -30,21 +31,36 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - A função só deve retornar a frase acima somente se o time estiver entre
     os 5 primeiros.
     - Se não houver time para a posição passada, deve retornar a mensagem:
-    "Não temos a informação do time que está nessa posição."
+    
 */
-// ?
+function showTeamPosition(number){
+    if(number < 1 || number > 5) {
+        return "Não temos a informação do time que está nessa posição.";
+    }
+    return  "O time que está em "+ number +"º lugar é o "+ teams[number - 1] + ".";
+    
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(1))
+console.log(showTeamPosition(3))
+console.log(showTeamPosition(2))
+console.log(showTeamPosition(5))
+console.log(showTeamPosition(6))
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var numbers = 20;
+
+while(numbers <= 30) {
+    console.log(numbers);
+    numbers++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +74,37 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(color){
+    var hexa;
+    switch( color ) {
+        case 'red':
+            hexa = '#FF0000';
+            break;
+        case 'blue':
+            hexa = '#0000FF';
+            break;
+        case 'yellow':
+            hexa = '#FFFF00';
+            break;
+        case 'white':
+            hexa = '#000000';
+            break;
+        case 'green':
+            hexa = '#00FF00';
+            break;
+        default: "Não temos o equivalente hexadecimal para"+ color +".";
+    }
+    return "O hexadecimal para a cor " + color + " é " + hexa +".";
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+console.log(convertToHex('white'));
+console.log(convertToHex('green'));
+console.log(convertToHex('blue'));
+console.log(convertToHex('yellow'));
+console.log(convertToHex('black'));
+console.log(convertToHex('purple'));
+console.log(convertToHex('orange'));
+console.log(convertToHex('gray'));
