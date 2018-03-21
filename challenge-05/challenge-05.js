95653488c7f5c3d333f56ca3f15700a154076985
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2018-03-21 08:23:55 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var array = ['ricardo',19,'masculino','maceió','brazil'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function vetor (array){
+    return array;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+vetor(array[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function twoP(array, n){
+    return array[n];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var array = ['olá',10000,'JavaScript','LifeStyle',1.0];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log( twoP(array,0));
+console.log( twoP(array,1));
+console.log( twoP(array,2));
+console.log( twoP(array,3));
+console.log( twoP(array,4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,52 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book (livro){
+    var prateleira = {
+        'Banco de dados': {
+            quantidadedePaginas: 100,
+            autor: 'Douglas Chalegre',
+            editora: 'Saraiva',
+        },
+        'Ionic and tips':{
+            quantidadedePaginas: 108,
+            autor: 'Matheus Martins',
+            editora: 'Aurora',
+        },
+        'Vida de desenvolvedor':{
+            quantidadedePaginas: 111,
+            autor: 'Mardone Alves',
+            editora: 'Unicove',
+        },
+    }
+    if(!livro){
+        return prateleira; 
+    }
+    return prateleira[livro];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Banco de dados tem '+book('Banco de dados').quantidadedePaginas+ ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro Banco de dados é '+book('Banco de dados').autor+ '!');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro Banco de dados foi publicado pela editora '+book('Banco de dados').editora+ '!');
