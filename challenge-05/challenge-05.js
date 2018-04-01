415dc872b2c91fd3e42c4bc6d5a4808cc3d3bb02
wesmelo <wsfmelo@gmail.com>
2018-04-01 15:07:35 -0300

    finalizado challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,28 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = ['valor1',
+  2,
+  function() {
+    return 'valor 3'
+  },
+  {
+    objeto: 1
+  },
+  true
+];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myfunction(arr) {
+  return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myfunction(myarray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +31,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myfunction2(arr, index) {
+  return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myvar = ['cabeca', function() {return 'ombro';}, 10, true, 'olhos'];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myfunction2(myvar, 0);
+myfunction2(myvar, 1);
+myfunction2(myvar, 2);
+myfunction2(myvar, 3);
+myfunction2(myvar, 4);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,26 +64,47 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nomeLivro) {
+    var livros = {
+        'O Vale no Fim do Mundo': {
+            quantidadePaginas: 224,
+            autor: 'Sandor Lenard',
+            editora: 'Cosac'
+        },
+        'Vá, Coloque Um Vigia': {
+            quantidadePaginas: 252,
+            autor: 'José Olympio',
+            editora: 'José Olympio'
+        },
+        'A Little Life': {
+            quantidadePaginas: 736,
+            autor: 'Hanya Yanagihara',
+            editora: 'Doubleday Books'
+        }
+    };
+    return !nomeLivro ? livros : livros[nomeLivro];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var nomeLivro = 'O Vale no Fim do Mundo';
+
+console.log('O livro '+nomeLivro+' tem '+book(nomeLivro).quantidadePaginas+' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro '+nomeLivro+' é '+ book(nomeLivro).autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
@@ -74,3 +112,4 @@ a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
 // ?
+console.log('O livro '+nomeLivro+' foi publicado pela editora '+book(nomeLivro).editora);
