7e0417bf0388ffca0ced3874f457159e7016a754
guilherme64 <guilhermemelo6@outlook.com>
2018-04-05 12:57:49 -0300

    Update challenge-08.js
    Correções no desafio 8

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -33,14 +33,14 @@ function showName() {
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var varShowName = showName();
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log('A função ' + varShowName + ' retorna ' + varShowName.name + '.');
+console.log('A função ' + varShowName.name + ' retorna ' + varShowName() + '.');
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
