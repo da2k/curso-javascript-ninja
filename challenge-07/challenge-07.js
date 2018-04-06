cfb94a81fa893bb593685f2a668773e8240dbf6a
LucasCMota <lucasmotta888@gmail.com>
2018-04-06 10:32:59 -0300

    troca de nome

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,7 +1,7 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-var myArray =[ 'lucas' , { pessoa: 'idade'} , function matriz() {} , 100 , 50 ];
+var myArray =[ 'antonio' , { pessoa: 'idade'} , function matriz() {} , 100 , 50 ];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
