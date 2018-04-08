c8f1c8b70d09228bdd69f255f46d16410ad3d411
Helivelton Barbosa <helivelton@ic.ufal.br>
2018-04-08 16:14:12 -0300

    Challenge 19

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -1,8 +1,10 @@
-/*
+(function(){
+    /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
+'use strict';
 
 /*
 Alguns detalhes importantes que faltou falar na aula:
@@ -24,6 +26,8 @@ linha, independente de quantos caracteres de número estiverem juntos.
 no console:
 */
 console.log( 'Regex para números usando o construtor:' );
+var justNumbersRegex = new RegExp('^\\d+', 'gm');
+console.log(justNumbersRegex);
 // ?
 
 /*
@@ -32,7 +36,7 @@ resultado no console. O resultado deve ser:
 "[ '10', '50' ]"
 */
 var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
-console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
+console.log( '\nNúmeros no início da linha do texto:\n' + text.match(justNumbersRegex), '\n' );
 // ?
 
 /*
@@ -44,6 +48,8 @@ linha, independente de quantos caracteres de número estiverem juntos.
 Mostre a regex no console:
 */
 console.log( '\nRegex para números somente no final das linhas:' );
+var numbersAtTheEnd = new RegExp('\\d+$', 'gm');
+console.log(numbersAtTheEnd);
 // ?
 
 /*
@@ -54,6 +60,7 @@ O resultado deve ser:
 */
 var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
 console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
+console.log(otherText.match(numbersAtTheEnd));
 // ?
 
 /*
@@ -82,3 +89,18 @@ para exemplificar.
 var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
 // ?
+function hasClass(markup, cssClass) {
+    var hasCssClass = new RegExp('class=["\']\\w*\\s*'+ cssClass +'\\s*\\w*[\'"]', 'gm');
+    console.log(hasCssClass);
+    
+    return markup.match(hasCssClass) !== null;
+}
+
+var classes = ["container", "text", "date", "excerpt", "main"];
+
+classes.forEach(function(item, index){
+    console.log(hasClass(markup, item) + " para a classe " + item);
+    return item;
+});
+
+})();
