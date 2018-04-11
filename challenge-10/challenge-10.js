7752bb1bed66775c8f00d16cba31599f9e35b978
Rafael <rafaelfariasn@gmail.com>
2018-04-11 00:34:50 -0300

    Challenge-10

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -13,7 +13,7 @@
 	var five = Number('5');
 	console.log( five + ' é número?', typeof five === 'number' );
 
-	var concat = String(10 + 10);
+	var concat = String(10) + 10;
 	console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
 
 	/*
@@ -70,7 +70,7 @@
 	os dois parâmetros da função de retorno de "calculator".
 	*/
 	function calculator(operator) {
-		if (isOperatorValid(operator) === false) {
+		if (!isOperatorValid(operator)) {
 			return false;
 		}
 		return function (num1, num2) {
