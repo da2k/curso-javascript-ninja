1f8cd2dd3e071cd2c8fb289eb6c53fdca31f7eb2
lucas2502 <lucas2502@live.com>
2018-04-20 14:51:09 -0300

    challenge-08

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -39,14 +39,14 @@ function showName(){
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var varShowName = showName();
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log('A função ' + showName.name + ' retorna ' + varShowName );
+console.log('A função ' + showName.name + ' retorna ' + varShowName() );
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -81,7 +81,7 @@ function calculator(option){
 				result = num1 % num2;
 				break;
 
-			default:
+			default :
 				return 'Operação inválida.';
 				break;
 		}
