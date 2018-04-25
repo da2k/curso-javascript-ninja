9a75f19f661e563eacfd3f70fd049f66b0e61cc8
Marcio Scavassa <marcioscavassa@hotmail.com>
2018-04-25 19:56:33 -0300

    Challange 16

--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -1,10 +1,12 @@
-/*
+(function() {
+  'use strict';
+  /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
 
-/*
+  /*
 Declare uma variável chamada `name` que receba seu primeiro nome.
 Mostre no console todas as letras do seu nome separadas, com a frase:
 - "[LETRA] é a [POSIÇÃO]ª letra do meu nome."
@@ -13,10 +15,13 @@ Ex: no caso do nome ser "Fernando", deve mostrar as frases:
 - "e é a 2ª letra do meu nome."
 E assim por diante, até a última.
 */
-console.log( 'As letras do seu nome:' );
-// ?
+  console.log('As letras do seu nome:');
+  var name = 'Joao';
+  for (var i = 0; i < name.length; i++) {
+    console.log(name[i] + ' é a ' + (i + 1) + 'ª letra do meu nome.');
+  }
 
-/*
+  /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
 escrito no formato de slug (caixa baixa e palavras separadas por um traço).
 Ex: o nome "Fernando Daciuk" ficaria "fernando-daciuk"
@@ -28,10 +33,16 @@ curso para fazer isso funcionar corretamente :)
 - Mostre no console o nome no formato slug, e o resultado final. Use um
 console.log para cada formato.
 */
-console.log( '\nNome convertido à partir de um slug:' );
-// ?
+  console.log('\nNome convertido à partir de um slug:');
+  var fullName = 'joao-da-silva';
+  var splitName = fullName.split('-').map(function(name){
+    return name[0].toUpperCase() + name.slice(1);
+  }).join(' ');
 
-/*
+  console.log(fullName);
+  console.log(splitName);
+  
+  /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
 cada nome por vírgula. Entre o penúltimo e o último nome, o separador deve
 ser um "e". No final, concatene com a frase: " são meus amigos.".
@@ -40,24 +51,38 @@ O resultado final deve ficar mais ou menos assim:
 - Detalhe: o código abaixo deve funcionar com um array de qualquer tamanho.
 5 nomes foi somente uma sugestão ;)
 */
-console.log( '\nMeus amigos:' );
-// ?
+  console.log('\nMeus amigos:');
+  var arrayNomes = ['Joao','Maria','Carlos','Andre','Fernando'];
+  var phrase = arrayNomes.reduce(function(acumulado, atual, index){
+    var separator = arrayNomes.length - 1 === index ? ' e ' : ', ';
+    return acumulado + separator + atual;
+  }).concat( ' são meus amigos.');
 
-/*
+  var ultimoItem = arrayNomes.splice(-1);
+  var nomesJoin = arrayNomes.join(', ').concat(' e ', ultimoItem, ' são meus amigos.');
+  
+  console.log(phrase);
+  console.log(nomesJoin);
+
+  /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
 Mostre o resultado no console.
 */
-console.log( '\nEra "Roberto", agora é:' );
-// ?
+  console.log('\nEra "Roberto", agora é:');
+  // ?
+  var nameRoberto = 'Roberto';
+  console.log(nameRoberto.replace('to', 'ta'));
 
-/*
+  /*
 Mostre no console a parte "nando" da string "Fernando". Use o método que
 faz a busca do final para o início da string.
 */
-console.log( '\nParte de uma string:' );
-// ?
+  console.log('\nParte de uma string:');
+  var nameFernando = 'Fernando';
+  console.log(nameFernando.substring(8,3));
+  
 
-/*
+  /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
 escrito de forma natural.
 Mostre no console esse nome, mas com as letras intercalando entre maiúsculas
@@ -66,5 +91,12 @@ e minúsculas.
 de qualquer tamanho, escrito de qualquer forma.
 Ex.: Nomes que deveriam funcionar: "Fernando", "RoBertO", "gabriEla", etc.
 */
-console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
-// ?
+  console.log('\nNome com letras intercaladas entre caixa alta e baixa:');
+  var myName = 'Marcio';
+  var myNameSplit = myName.split('');
+  for(var i=0, myNameFinal = []; i < myNameSplit.length; i++){    
+    myNameFinal.push( i % 2 === 0 ? myNameSplit[i].toUpperCase() : myNameSplit[i].toLowerCase() );
+  }
+  console.log(myNameFinal.join(''));
+  
+})();
