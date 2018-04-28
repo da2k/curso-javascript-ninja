fecb8bab4b1bb5e133372706ab1f30ebe168411e
Lucas Dias <lucrod@alunos.utfpr.edu.br>
2018-04-28 04:20:32 -0300

    Challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var qualquer = [ 'lucas', 2, true, false, -2 ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction(arr) {
+  return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log( myFunction(qualquer)[1] );
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunction2(arr, num) {
+  return arr[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myArray = [ 10, null, function() {}, 'Lucas', true ];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(myFunction2(myArray, 0));
+console.log(myFunction2(myArray, 1));
+console.log(myFunction2(myArray, 2));
+console.log(myFunction2(myArray, 3));
+console.log(myFunction2(myArray, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,51 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book( bookName ) {
+  var livros = {
+    'Aprendendo a Desenvolver Aplicações Web': {
+      quantidadePaginas: 360,
+      autor: 'Semmy Purewal',
+      editora: 'Novatec'
+    },
+    'Javascript. O Guia Definitivo': {
+      quantidadePaginas: 1080,
+      autor: 'David Flanagan',
+      editora: 'Bookman'
+    },
+    'Segurança Para Desenvolvedores Web': {
+      quantidadePaginas: 400,
+      autor: 'John Paul Mueller',
+      editora: 'Novatec'
+    }
+  };
+
+  return !bookName ? livros : livros[bookName]; /*bookname não foi passado? retorna todos os livros, caso contrário, retorne o bookname*/
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var bookName = 'Segurança Para Desenvolvedores Web';
+console.log('O livro ' + bookName + ' tem ' + book(bookName).quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro ' + bookName + ' é ' + book(bookName).autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro ' + bookName + ' foi publicado pela editora ' + book(bookName).editora + '.');
