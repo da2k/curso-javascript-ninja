b82ae62a3f11642ea55d607868aef571b7cd827b
arikier <herickgh3@gmail.com>
2018-04-29 14:14:37 -0300

    Challenge 09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -1,8 +1,10 @@
+
+
 /*
 Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
 */
-
+(function container() {
 /*
 Analise as funções abaixo (`myFunction`, `myFunction2` e `myFunction3`, e
 ajuste o posicionamento das variáveis e funções internas, para que os códigos
@@ -10,11 +12,11 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+    var number1 = 10;
+    var number2 = 20;
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
     return number1 + number2;
-    var number2 = 20;
 }
 myFunction();
 
@@ -22,12 +24,12 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
     var sum = function sum() {
         return number1 + number2;
     };
     var number1 = 10;
     var number2 = 20;
+    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
     return sum();
 }
 myFunction2();
@@ -36,10 +38,10 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
+    var number1 = 40;
     var number2 = 50;
     console.log( 'Na função myFunction3, number1 é igual a', number1 );
-    var number1 = 40;
+    console.log( 'A soma de 40 e 50 é igual a', sum() );
     return sum();
     function sum() {
         return number1 + number2;
@@ -60,7 +62,11 @@ o retorno de `calculator`.
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+calculator( x, y ) {
+    return function(callback) {
+        
+    }
+}
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
@@ -105,3 +111,8 @@ console.log( 'O resultado da divisão é:' );
 
 console.log( 'O resto da divisão é:' );
 // ?
+
+
+
+
+}());
