4e3cf794947e7d9c7e27ba9744c810642c8ed7f7
JoelSpinelli <joel.spinelli@hotmail.com>
2018-04-30 12:01:02 -0300

    challenge-09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -2,7 +2,7 @@
 Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
 */
-
+(function(){
 /*
 Analise as funções abaixo (`myFunction`, `myFunction2` e `myFunction3`, e
 ajuste o posicionamento das variáveis e funções internas, para que os códigos
@@ -10,11 +10,11 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+    var number1 = 10;
+    var number2 = 20;
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
     return number1 + number2;
-    var number2 = 20;
 }
 myFunction();
 
@@ -22,13 +22,13 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
-    var sum = function sum() {
-        return number1 + number2;
-    };
-    var number1 = 10;
-    var number2 = 20;
-    return sum();
+  var number1 = 10;
+  var number2 = 20;
+  var sum = function sum() {
+    return number1 + number2;
+  };
+  console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+  return sum();
 }
 myFunction2();
 
@@ -36,14 +36,14 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
-    var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
-    var number1 = 40;
-    return sum();
-    function sum() {
-        return number1 + number2;
-    };
+  var number2 = 50;
+  var number1 = 40;
+  function sum() {
+    return number1 + number2;
+  };
+  console.log( 'A soma de 40 e 50 é igual a', sum() );
+  console.log( 'Na função myFunction3, number1 é igual a', number1 );
+  return sum();
 }
 myFunction3();
 
@@ -60,13 +60,17 @@ o retorno de `calculator`.
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+function calculator(var1, var2){
+  return function(callback) {
+    return callback(var1, var2);
+  }
+}
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
 passando dois números por parâmetro.
 */
-// ?
+var sum = calculator(6, 7)
 
 /*
 Sabemos que `sum` agora tem uma função atribuída a ela, que é o retorno de
@@ -77,15 +81,18 @@ para a chamada à `calculator` acima.
 uma função anônima que irá retornar a soma dos dois números que essa função
 anônima tem como seus argumentos.
 */
-console.log( 'O resultado da soma é:' );
-// ?
+console.log( 'O resultado da soma é:', sum( function(a, b) { return a + b;}));
+
 
 /*
 Agora declare outra variáveis chamadas `subtraction`, `multiplication`,
 `division` e `mod`, e atribua à elas `calculator`, passando números
 diferentes para cada chamada.
 */
-// ?
+var subtraction = calculator(7, 6);
+var multiplication = calculator(5, 5);
+var division = calculator(6, 3);
+var mod = calculator(7, 6);
 
 /*
 Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -94,14 +101,15 @@ divisão e módulo (resto de divisão), conforme a função utilizada.
 As suas respostas devem estar abaixo dos `console.log` referentes à cada
 chamada.
 */
-console.log( 'O resultado da subtração é:' );
+console.log( 'O resultado da subtração é:', subtraction(function(a, b) { return  a - b; }));
 // ?
 
-console.log( 'O resultado da multiplicação é:' );
+console.log( 'O resultado da multiplicação é:', multiplication(function(a, b) { return a * b; }));
 // ?
 
-console.log( 'O resultado da divisão é:' );
+console.log( 'O resultado da divisão é:', division(function(a, b) {return a / b; }));
 // ?
 
-console.log( 'O resto da divisão é:' );
+console.log( 'O resto da divisão é:', mod(function (a,b) { return a % b;}));
 // ?
+})();
