c9f415a5bcd61a3b18bf00686ef419d0ac849e11
Victor Nascimento <victon.barros@gmail.com>
2018-05-08 12:21:36 +0000

    Desafio Challenge-21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -15,3 +15,36 @@ usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
 // ?
+
+
+(function(win, doc) {
+    'use strict';
+ 
+ 
+    var $timer = doc.querySelector('[data-js="timer"]');
+    var $start = doc.querySelector('[data-js="start"]');
+    var $stop = doc.querySelector('[data-js="stop"]');
+    var $reset = doc.querySelector('[data-js="reset"]');
+    var interval;
+
+    $start.addEventListener('click', startTimer, false);
+    $stop.addEventListener('click', stopTimer,false);
+    $reset.addEventListener('click',resetTimer,false);
+    
+    function startTimer() {
+       $timer.value = +$timer.value + 1;
+       interval  = setTimeout(startTimer,1000);
+    }
+    
+    function stopTimer(){
+        clearTimeout(interval);
+    }
+
+    function resetTimer(){
+        $timer.value = 0;
+        clearTimeout(interval);
+        stopTimer();
+    }
+
+
+})(window, document);
