1e9d1f94b068a5b3a53df3d5b519e258980c73dd
Samuel SIlva <samuelsilvawb@gmail.com>
2018-05-10 07:08:56 -0300

    improved challenge 29

--- challenge-29/ajax.js
+++ challenge-29/ajax.js
@@ -4,30 +4,28 @@
 
   function AJAX() {
 
-    function get(url, callback) {
-      var ajax = new XMLHttpRequest();
-      ajax.open('GET', url);
-      ajax.send(null);
-
-      ajax.addEventListener('readystatechange', function () {
-        if (ajax.readyState === 4 && ajax.status === 200) {
-          try {
-            var data = JSON.parse(ajax.responseText);
-            callback(data);
-          } catch (err) {
-            console.error(err);
-            callback({ err: true, msg: err, status: ajax.status });
-          }
-        }
-
-        if (ajax.readyState === 4 && ajax.status !== 200) {
-          callback({ err: true, msg: '', status: ajax.status });
-        }
-      }, false);
-    }
-
     return {
-      get: get
+      get: function get(url, callback) {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', url, true);
+        ajax.send();
+
+        ajax.addEventListener('readystatechange', function () {
+          if (ajax.readyState === 4 && ajax.status === 200) {
+            try {
+              var data = JSON.parse(ajax.responseText);
+              callback(data);
+            } catch (err) {
+              console.error(err);
+              callback({ err: true, msg: err, status: ajax.status });
+            }
+          }
+
+          if (ajax.readyState === 4 && ajax.status !== 200) {
+            callback({ err: true, msg: '', status: ajax.status });
+          }
+        }, false);
+      }
     };
   }
 
--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function () {
+(function (DOM, AJAX) {
   'use strict';
 
   /*
@@ -51,15 +51,17 @@
     var savedCars = [];
 
     function init() {
+      getCompanyData();
+      $btnCarForm.on('click', handleSubmitCarForm);
+    }
 
+    function getCompanyData() {
       AJAX.get('./company.json', function (data) {
         if (!data.err) {
           $companyName.getFirst().textContent = data.name;
           $companyTelephone.getFirst().textContent = data.phone;
         }
       });
-
-      $btnCarForm.on('click', handleSubmitCarForm);
     }
 
     function saveCar() {
@@ -74,8 +76,17 @@
 
       if (_isFormValid(newCarObj)) {
         savedCars.push(newCarObj);
-        buildCarTr(newCarObj);
+        updateTableCar(newCarObj);
+        clearForm();
       }
+    }
+
+    function clearForm() {
+      $carImage.getFirst().value = '';
+      $carBrand.getFirst().value = '';
+      $carYear.getFirst().value = '';
+      $carPlate.getFirst().value = '';
+      $carColor.getFirst().value = '';
     }
 
     function _isFormValid(objCar) {
@@ -91,36 +102,39 @@
       return formIsValid;
     }
 
+    function updateTableCar(obj) {
+
+      var $fragment = document.createDocumentFragment();
+      var $newTr = document.createElement('tr');
+
+      $newTr.appendChild(_createImg(obj.image));
+      $newTr.appendChild(_createTd(obj.brand));
+      $newTr.appendChild(_createTd(obj.year));
+      $newTr.appendChild(_createTd(obj.plate));
+      $newTr.appendChild(_createTd(obj.color));
+
+      $fragment.appendChild($newTr)
+
+      $tbodyListCars.getFirst().appendChild($fragment);
+    }
+
+    function _createTd(text) {
+      var $newTd1 = document.createElement('td');
+      $newTd1.textContent = text;
+      return $newTd1;
+    }
+
+    function _createImg(url) {
+      var $td = _createTd('');
+      var $newImg = document.createElement('img');
+      $newImg.src = url;
+      $td.appendChild($newImg)
+      return $td;
+    }
+
     function handleSubmitCarForm(e) {
       e.preventDefault();
       saveCar();
-    }
-
-    function buildCarTr(obj) {
-
-      var newTr = document.createElement('tr');
-
-      var newTd1 = document.createElement('td');
-      newTd1.textContent = obj.image;
-      newTr.appendChild(newTd1);
-
-      var newTd2 = document.createElement('td');
-      newTd2.textContent = obj.brand;
-      newTr.appendChild(newTd2);
-
-      var newTd3 = document.createElement('td');
-      newTd3.textContent = obj.year;
-      newTr.appendChild(newTd3);
-
-      var newTd4 = document.createElement('td');
-      newTd4.textContent = obj.plate;
-      newTr.appendChild(newTd4);
-
-      var newTd5 = document.createElement('td');
-      newTd5.textContent = obj.color;
-      newTr.appendChild(newTd5);
-
-      $tbodyListCars.getFirst().appendChild(newTr);
     }
 
     return {
@@ -129,4 +143,4 @@
   }
 
   appModule().init();
-})();
+})(window.DOM, window.AJAX);
