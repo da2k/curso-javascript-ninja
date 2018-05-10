7628b4d90f4ec71b4a07d996b1f07311ad142d78
lucas2502 <lucas2502@live.com>
2018-05-10 16:41:54 -0300

    challenge-14

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -1,3 +1,5 @@
+(function(){
+	
 /*
 Envolva todo o código desse desafio em uma IIFE.
 */
@@ -10,14 +12,20 @@ Os números devem ser de 1 a 10.
 Mostre esse array no console.
 */
 console.log( 'Number Objects Array:' );
-// ?
-
+var numberObjects = [];
+for(var i = 1 ; i <= 10; i++){
+	numberObjects.push({number : [i]});
+}
+console.log(numberObjects);
 /*
 Crie um array chamado `justNumbers`, que terá como elementos somente os
 números do array criado acima. Mostre esse novo array no console.
 */
 console.log( '\nJust Numbers:' );
-// ?
+var justNumbers = numberObjects.map(function(item){
+	return item.number;
+})
+console.log(justNumbers);
 
 /*
 Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
@@ -25,7 +33,10 @@ somente os números que forem divisíveis por 2 ou 3. Mostre esse novo array
 no console.
 */
 console.log( '\nJust module of division by 2 or 3:' );
-// ?
+var justMod2Or3 = justNumbers.filter(function(item){
+		return item %2 == 0 || item %3 == 0;
+});
+console.log(justMod2Or3);
 
 /*
 Declare uma variável chamada operation que receba, do array criado acima,
@@ -36,16 +47,20 @@ O cálculo deve começar com zero.
 Mostre o resultado no console.
 */
 console.log( '\nOperation:' );
-// ?
-
+var operation = justMod2Or3.reduce(function(acumulado, atual){
+	return (acumulado + 1) * atual;
+}, 0);
+console.log(operation);
 /*
 Faça o mesmo cálculo passado acima, mas começando do último item para o
 primeiro. O nome da variável deve ser operation2. Mostre o resultado no
 console.
 */
 console.log( '\nOperation 2:' );
-// ?
-
+var operation2 = justMod2Or3.reduceRight(function(acumulado, atual){
+	return (acumulado + 1) * atual;
+});
+console.log(operation2);
 /*
 Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
 do seu nome. Vamos reduzir esse array, juntando todas as sílabas, mas usando
@@ -55,20 +70,28 @@ infantil, onde você coloca a letra "P" antes de cada sílaba de uma palavra
 falada, como se você estivesse falando em código xD
 */
 console.log( '\nSeu nome na língua do "P":' );
-// ?
+var name = ['Lu', 'cas'];
+pName = name.reduce(function(acumulado, atual){
+	return acumulado + 'P' + atual  ;
+},'')
+
+console.log(pName);
+
 
 /*
 Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
 e atribuirá o seu nome invertido (usando o array criado acima).
 */
 console.log( '\nInversed Name:' );
-// ?
-
+var inverseName = name.reduceRight(function(acumulado, atual){
+	return acumulado + atual;	
+});
+console.log(inverseName);
 /*
 Mostre no console o array `numberObjects`.
 */
 console.log( '\nNumber objects' );
-// ?
+console.log(numberObjects);
 
 /*
 Verifique se existem em algum índice de numberObjects um objeto ìgual a
@@ -80,18 +103,28 @@ Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
 o que acontece ;)
 */
 console.log( '\nExiste um { number: 2 } em numberObjects?' );
-// ?
+
+	if(numberObjects.indexOf({number: 2}) > -1)
+		console.log('Existe um objeto { number: 2 } em numberObjects!');
+	else 
+		console.log('Não existe um objeto { number: 2 } em numberObjects :(');
 
 /*
 Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
 será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
 */
 console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-// ?
+
+	if(numberObjects.lastIndexOf({number: 2}, 2) > -1)
+		console.log('Existe um objeto { number: 2 } em numberObjects!');
+	else 
+		console.log('Não existe um objeto { number: 2 } em numberObjects :(');
 
 /*
 Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
 formato de String.
 */
 console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-// ?
+console.log( Array.isArray(justMod2Or3) == true ? justMod2Or3.toString() : false);
+
+})();
