8dfdaf27acc6b41249ecd1a54eb0dcf6bf145de2
Samuel SIlva <samuelsilvawb@gmail.com>
2018-05-10 15:10:12 -0300

    finished challenge 30

--- challenge-30/challenge-30.js
+++ challenge-30/challenge-30.js
@@ -29,4 +29,4 @@ nesse arquivo, dentro do `console.log`, o link para o pull request no seu
 projeto.
 */
 
-console.log('Link do pull request do seu projeto');
+console.log('https://github.com/samuelsilvadev/underground-cars/pull/1');
