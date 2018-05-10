f3a5f491db7c1af1bc722363beb56d9e987e43ff
unknown <riosney_s@hotmail.com>
2018-05-10 10:16:15 -0300

    challenge-09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -10,11 +10,11 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
-    console.log( 'Na função `myFunction`, o primeiro número é', number1 );
-    console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
-    return number1 + number2;
+    var number1 = 10;    
     var number2 = 20;
+    console.log( 'Na função `myFunction`, o primeiro número é', number1 );
+    console.log( 'Na função `myFunction`, o segundo número é', number2 );    
+    return number1 + number2;
 }
 myFunction();
 
@@ -22,13 +22,16 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
-    var sum = function sum() {
-        return number1 + number2;
-    };
     var number1 = 10;
     var number2 = 20;
-    return sum();
+
+    var sum = function sum() {
+        return number1 + number2;
+        return sum();
+    };    
+
+    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+    
 }
 myFunction2();
 
@@ -36,14 +39,17 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
     var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
     var number1 = 40;
-    return sum();
-    function sum() {
-        return number1 + number2;
+    
+    function sum() { 
+        return number1 + number2;      
+        //return sum();
     };
+
+    console.log( 'Na função myFunction3, number1 é igual a', number1 );
+    console.log( 'A soma de 40 e 50 é igual a', sum() );    
+        
 }
 myFunction3();
 
@@ -60,7 +66,9 @@ o retorno de `calculator`.
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+function calculator(){
+    
+}
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
