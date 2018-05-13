3adc82b1883c81f75239bc7d338e0842a91342b3
Tiago Satur <tiagosatur@gmail.com>
2018-05-12 21:31:58 -0300

    Challage 05 finalizado

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -46,47 +46,85 @@ seguintes características:
 - esse objeto irá receber 3 propriedades, que serão nomes de livros;
 - cada uma dessas propriedades será um novo objeto, que terá outras 3
 propriedades:
-    - `quantidadePaginas` - Number (quantidade de páginas)
-    - `autor` - String
-    - `editora` - String
+		- `quantidadePaginas` - Number (quantidade de páginas)
+		- `autor` - String
+		- `editora` - String
 - A função deve retornar o objeto referente ao livro passado por parâmetro.
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
 function book(nomeLivro) {
+
 	var biblioteca = {
-		livro1: {
-			nome: 'H. P.',
+
+		'A Guerra dos Tronos': {
+			quantidadePaginas: 888,
+			autor: 'George R. R. Martin',
+			editora: 'LeYa'
+		},
+		'O poder do Hábito': {
+			quantidadePaginas: 408,
+			autor: 'Charles Duhigg',
+			editora: 'Objetiva'
+		},
+		'Os segredos da mente milionária': {
 			quantidadePaginas: 200,
-			autor: 'J. K.Rowling',
-			editora: 'Rocco'
+			autor: 'T. Harv Eker',
+			editora: 'Sextante'
 		}
 
 	}
+
+	return !nomeLivro ? biblioteca : biblioteca[nomeLivro];
+
 }
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
+/*
+	{
+		"A Guerra dos Tronos": {
+			"quantidadePaginas": 888,
+			"autor": "George R. R. Martin",
+			"editora": "LeYa"
+		},
+		"O poder do Hábito": {
+			"quantidadePaginas": 408,
+			"autor": "Charles Duhigg",
+			"editora": "Objetiva"
+		},
+		"Os segredos da mente milionária": {
+			"quantidadePaginas": 200,
+			"autor": "T. Harv Eker",
+			"editora": "Sextante"
+		}
+	}
+*/
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log(`O livro Os segredos da mente milionária tem ${book('Os segredos da mente milionária').quantidadePaginas} páginas`);
+// O livro Os segredos da mente milionária tem 200 páginas
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log(`O autor do livro A Guerra dos Tronos é ${book('A Guerra dos Tronos').autor}.`);
+// O autor do livro A Guerra dos Tronos é George R. R. Martin.
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log(
+	`O livro O poder do Hábito foi publicado pela editora ${book('O poder do Hábito').editora}`
+);
+// O livro O poder do Hábito foi publicado pela editora Objetiva
