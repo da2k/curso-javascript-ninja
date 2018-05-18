66f8ff00b9da66d7e00f7ff281f618a84f726317
Alane Ribeiro <alane.ribeiro@gmail.com>
2018-05-18 11:55:24 -0300

    Prettier on challenge-03.js

--- challenge-03/challenge-03.js
+++ challenge-03/challenge-03.js
@@ -1,78 +1,88 @@
 var qualquer = {};
 
 var pessoa = {
-  nome: 'alane',
-  sobrenome: 'ribeiro',
-  sexo: 'feminino',
+  nome: "alane",
+  sobrenome: "ribeiro",
+  sexo: "feminino",
   idade: 35,
   altura: 1.54,
   peso: 62,
   andando: false,
   caminhouQuantosMetros: 0
-}
+};
 
 pessoa.fazerAniversario = function() {
-  return pessoa.idade++
-}
+  return pessoa.idade++;
+};
 
 pessoa.andar = function(qtd) {
   pessoa.andando = true;
-  return pessoa.caminhouQuantosMetros += qtd
-}
+  return (pessoa.caminhouQuantosMetros += qtd);
+};
 
 pessoa.parar = function() {
   pessoa.andando = false;
-}
+};
 
 pessoa.nomeCompleto = function() {
-  return 'Meu nome completo é ' +pessoa.nome + ' ' +pessoa.sobrenome;
-}
+  return "Meu nome completo é " + pessoa.nome + " " + pessoa.sobrenome;
+};
 
 pessoa.mostrarIdade = function() {
- return 'Olar, eu tenho ' +pessoa.idade + ' anos';
-}
+  return "Olar, eu tenho " + pessoa.idade + " anos";
+};
 
 pessoa.mostrarPeso = function() {
- return 'Eu peso ' +pessoa.peso + ' quilos';
-}
+  return "Eu peso " + pessoa.peso + " quilos";
+};
 
 pessoa.mostrarAltura = function() {
-  return 'Minha altura é ' +pessoa.altura;
-}
+  return "Minha altura é " + pessoa.altura;
+};
 
 pessoa.apresentacao = function() {
   var artigo;
-  if (pessoa.sexo === 'feminino') {
-    artigo = 'a'
+  if (pessoa.sexo === "feminino") {
+    artigo = "a";
   } else {
-    artigo = 'o'
+    artigo = "o";
   }
-  console.log(artigo)
+  console.log(artigo);
 
   var ano;
-  if (pessoa.idade > 1){
-    ano = 'anos'
+  if (pessoa.idade > 1) {
+    ano = "anos";
   } else {
-    ano = 'ano'
+    ano = "ano";
   }
-  console.log(ano)
+  console.log(ano);
 
   var metro;
-  if (pessoa.caminhouQuantosMetros === 1){
-    metro = 'metro'
+  if (pessoa.caminhouQuantosMetros === 1) {
+    metro = "metro";
   } else {
-    metro = 'metros'
+    metro = "metros";
   }
 
-  var eu = "Olar, eu sou " +artigo + " "
-  +pessoa.nome+ " " +pessoa.sobrenome+
-  ", tenho "
-  +pessoa.idade+ " "
-  +ano+  ", tenho "
-  +pessoa.altura+ " de altura, meu peso é "
-  +pessoa.peso+ " quilos e, só hoje, eu já caminhei "
-  +pessoa.caminhouQuantosMetros+ " "
-  +metro
+  var eu =
+    "Olar, eu sou " +
+    artigo +
+    " " +
+    pessoa.nome +
+    " " +
+    pessoa.sobrenome +
+    ", tenho " +
+    pessoa.idade +
+    " " +
+    ano +
+    ", tenho " +
+    pessoa.altura +
+    " de altura, meu peso é " +
+    pessoa.peso +
+    " quilos e, só hoje, eu já caminhei " +
+    pessoa.caminhouQuantosMetros +
+    " " +
+    metro;
 
   return eu;
-}
+};
