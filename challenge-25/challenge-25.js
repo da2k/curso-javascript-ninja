bf1104bc540a49dfbe83199cd714e080eddb0a55
Gabriel Dias Bahniuk <gabrieldiasbnu@hotmail.com>
2018-05-21 17:08:06 -0300

    Challenge 25 v1.0

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,60 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+
+(function(win, doc) {
+
+  'use strict';
+
+  var $inputs = doc.querySelectorAll('[data-class="input-text"]');
+
+  var $inputCPF = doc.querySelector('[data-js="cpf"]');
+  var $inputNome = doc.querySelector('[data-js="nome"]');
+  var $inputIdade = doc.querySelector('[data-js="idade"]');
+  var $inputRg = doc.querySelector('[data-js="rg"]');
+  var $firstInput = $inputs[$inputs.length-$inputs.length].focus();
+  var $createdObjects = doc.querySelector('[data-js="created-objects"]');
+  var $spanError = doc.querySelector('[data-js="span-error"]');
+  var $button = doc.querySelector('[data-js="btnEnviar"]');
+
+  Array.prototype.forEach.call($inputs, function(input) {
+    input.addEventListener('blur', validateInput, false);
+    input.addEventListener('change', function () {
+      this.readOnly = true;
+      $createdObjects.innerHTML += '{"'+this.name+'"' + ' : ' +'"'+this.value+'"}, <br/>';
+    }, false);
+  });
+
+  function addObjectToHtml(elem, obj) {
+    for(var props in obj) {
+      obj[props] += obj[props];
+      elem.innerHTML = '<p>'+ obj[props] + '</p>';
+    }
+  }
+
+  function isInputValid(input) {
+    return getRegexOperations(input.name).test(input.value);
+  }
+
+  function validateInput() {
+    if (isInputValid(this))
+      return this.classList.remove('invalid');
+
+    return this.classList.add('invalid');
+  }
+
+  function getRegexOperations(oper) {
+    switch (oper) {
+      case 'cpf':
+      return new RegExp('^\\d{11}$', 'g');
+      case 'rg':
+      return new RegExp('^\\d{7}$', 'g');
+      case 'idade':
+      return new RegExp('^\\d{1,3}$', 'g');
+      case 'nome':
+      return new RegExp('^\\D+$', 'g');
+    }
+  }
+
+})(window, document);
