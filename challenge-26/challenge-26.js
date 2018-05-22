c4da5faedb05900b93ea03ffa82d3724a1a9e20d
Gabriel Dias Bahniuk <gabrieldiasbnu@hotmail.com>
2018-05-22 00:01:16 -0300

    Challenge 26

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -20,12 +20,60 @@ Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
 // ?
+(function() {
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+  'use strict';
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+  function DOM(elements) {
+    this.element = this.getElements(elements);
+  }
+
+  DOM.prototype.on = function on(event, callback){
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.addEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(event, callback){
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.removeEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.getElements = function getElements(element) {
+    return document.querySelectorAll(element);
+  };
+
+  DOM.prototype.substituteElements = function substituteElements(newElement, oldElement) {
+    return oldElement.replaceWith(newElement);
+  };
+
+  DOM.prototype.get = function get(){
+    return this.element;
+  };
+
+  var $dom = new DOM('[data-js="link"]');
+  $dom.on('click', function parou(e) {
+    e.preventDefault();
+    console.log('clicou');
+    $dom.off('click', parou);
+  });
+
+  //Testes
+  var $h1 = document.createElement('h1');
+  var $textNode = document.createTextNode('Titulo !!! ');
+  $h1.append($textNode);
+
+  var $h4 = document.createElement('h4');
+  var $textNode2 = document.createTextNode('Titulo menor !!! ');
+  $h4.append($textNode2);
+
+
+  document.body.append($h1);
+  document.body.append($h4);
+
+  $dom.substituteElements($h4, $h1);
+
+  console.log('Elementos selecionados:', $dom.get());
+  console.log('$a é filho de body?', $dom.get()[0].parentNode === document.body);
+})();
