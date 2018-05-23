836355b2cbc4c1ea272095159eddf457de0fa8ed
Alane Ribeiro <alane.ribeiro@gmail.com>
2018-05-23 10:56:18 -0300

    prettier and update code

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,7 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-var championship = 'Campeonato Brasileiro';
+var championship = "Campeonato Brasileiro";
 console.log(championship);
 
 /*
@@ -13,8 +13,8 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-var teams = ['Atlético', 'Flamengo', 'Corinthians', 'Palmeiras', 'Fluminense']
-console.log( 'Times que estão participando do campeonato:', teams );
+var teams = ["Atlético", "Flamengo", "Corinthians", "Palmeiras", "Fluminense"];
+console.log("Times que estão participando do campeonato:", teams);
 
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
@@ -33,40 +33,86 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     "Não temos a informação do time que está nessa posição."
 */
 function showTeamPosition(posicao) {
-  switch (posicao){
-    case 1 : return 'O time que está em '+ (posicao) +'º lugar é o '+teams[--posicao]+'.';
-    break;
-    case 2 : return 'O time que está em '+ (posicao) +'º lugar é o '+teams[--posicao]+'.';
-    break;
-    case 3 : return 'O time que está em '+ (posicao) +'º lugar é o '+teams[--posicao]+'.';
-    break;
-    case 4 : return 'O time que está em '+ (posicao) +'º lugar é o '+teams[--posicao]+'.';
-    break;
-    case 5 : return 'O time que está em '+ (posicao) +'º lugar é o '+teams[--posicao]+'.';
-    break;
+  switch (posicao) {
+    case 1:
+      return (
+        "O time que está em " +
+        posicao +
+        "º lugar é o " +
+        teams[--posicao] +
+        "."
+      );
+      break;
+    case 2:
+      return (
+        "O time que está em " +
+        posicao +
+        "º lugar é o " +
+        teams[--posicao] +
+        "."
+      );
+      break;
+    case 3:
+      return (
+        "O time que está em " +
+        posicao +
+        "º lugar é o " +
+        teams[--posicao] +
+        "."
+      );
+      break;
+    case 4:
+      return (
+        "O time que está em " +
+        posicao +
+        "º lugar é o " +
+        teams[--posicao] +
+        "."
+      );
+      break;
+    case 5:
+      return (
+        "O time que está em " +
+        posicao +
+        "º lugar é o " +
+        teams[--posicao] +
+        "."
+      );
+      break;
 
-    default : return 'Não temos a informação do time que está nessa posição.'
+    default:
+      return "Não temos a informação do time que está nessa posição.";
   }
-};
+}
+
+//OR
+
+function otherShowTeamPosition(position) {
+  if (position <= 0 || position > 5) {
+    return "Não temos a informação do time que está nessa posição.";
+  }
+
+  return "O time que está em " + position + "º lugar é o " + teams[--position];
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-showTeamPosition(3)//'O time que está em 3º lugar é o Corinthians.'
-showTeamPosition(1)//'O time que está em 1º lugar é o Atlético.'
-showTeamPosition(5)//'O time que está em 5º lugar é o Fluminense.'
-showTeamPosition(2)//'O time que está em 2º lugar é o Flamengo.'
-showTeamPosition(8)//'Não temos a informação do time que está nessa posição.'
+showTeamPosition(3); //'O time que está em 3º lugar é o Corinthians.'
+showTeamPosition(1); //'O time que está em 1º lugar é o Atlético.'
+showTeamPosition(5); //'O time que está em 5º lugar é o Fluminense.'
+showTeamPosition(2); //'O time que está em 2º lugar é o Flamengo.'
+showTeamPosition(8); //'Não temos a informação do time que está nessa posição.'
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
 var i = 20;
-while (i<31){
-  console.log(i++)
-};
+while (i < 31) {
+  console.log(i++);
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -81,30 +127,40 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     "Não temos o equivalente hexadecimal para [COR]."
 */
 function convertToHex(cor) {
-  switch(cor) {
-    case 'black' : console.log('O hexadecimal para a cor ' + cor.toUpperCase() + ' é #000000');
-    break;
-    case 'red' : console.log('O hexadecimal para a cor ' + cor.toUpperCase() + ' é #FF0000');
-    break;
-    case 'pink' : console.log('O hexadecimal para a cor ' + cor.toUpperCase() + ' é #FFC0CB');
-    break;
-    case 'yellow' : console.log('O hexadecimal para a cor ' + cor.toUpperCase() + ' é #FFFF00');
-    break;
-    case 'white' : console.log('O hexadecimal para a cor ' + cor.toUpperCase() + ' é #FFFFFF');
-    break;
+  var hexa;
+  switch (cor) {
+    case "black":
+      hexa = "#000000";
+      break;
+    case "red":
+      hexa = "#FF0000";
+      break;
+    case "pink":
+      hexa = "#FFC0CB";
+      break;
+    case "yellow":
+      hexa = "#FFFF00";
 
-    default: console.log('Não temos o equivalente hexadecimal para ' + cor + '.');
+      break;
+    case "white":
+      hexa = "#FFFFFF";
+      break;
+
+    default:
+      return 'Não temos o equivalente hexadecimal para ' + cor + '.';
   }
-};
+
+  return 'O hexadecimal para a cor ' + cor.toUpperCase() + ' é ' + hexa;
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-convertToHex('red');//O hexadecimal para a cor RED é #FF0000
-convertToHex('white');//O hexadecimal para a cor WHITE é #FFFFFF
-convertToHex('brown');//Não temos o equivalente hexadecimal para brown.
-convertToHex('blue');//Não temos o equivalente hexadecimal para blue.
-convertToHex('pink');//O hexadecimal para a cor PINK é #FFC0CB
-convertToHex('gold');//Não temos o equivalente hexadecimal para gold.
-convertToHex('black');//O hexadecimal para a cor BLACK é #000000
-convertToHex('shade');//Não temos o equivalente hexadecimal para shade.
+convertToHex('red'); //O hexadecimal para a cor RED é #FF0000
+convertToHex('white'); //O hexadecimal para a cor WHITE é #FFFFFF
+convertToHex('brown'); //Não temos o equivalente hexadecimal para brown.
+convertToHex('blue'); //Não temos o equivalente hexadecimal para blue.
+convertToHex('pink'); //O hexadecimal para a cor PINK é #FFC0CB
+convertToHex('gold'); //Não temos o equivalente hexadecimal para gold.
+convertToHex('black'); //O hexadecimal para a cor BLACK é #000000
+convertToHex('shade'); //Não temos o equivalente hexadecimal para shade.
