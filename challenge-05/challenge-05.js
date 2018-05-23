166ec85aeaaaea355c04a9fe227c44dea5f0cf64
Alane Ribeiro <alane.ribeiro@gmail.com>
2018-05-23 09:10:56 -0300

    prettier on challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,12 +2,12 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-var valores = [ "propriedade", 5, true, function() {}, { d : 3}, false ];
+var valores = ["propriedade", 5, true, function() {}, { d: 3 }, false];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-function retorna( arg ) {
+function retorna(arg) {
   return arg;
 }
 
@@ -18,7 +18,7 @@ function retorna( arg ) {
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-console.log( retorna(valores)[1] ); //5
+console.log(retorna(valores)[1]); //5
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -26,14 +26,14 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-function recebe( arg, num ) {
-  return arg[ num ];
+function recebe(arg, num) {
+  return arg[num];
 }
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-var vetor = [ "rua", true, null, {alane : 'alane'}, 45 ];
+var vetor = ["rua", true, null, { alane: "alane" }, 45];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
@@ -60,26 +60,25 @@ propriedades:
 os livros.
 */
 function book(nomeLivro) {
-
   var obj = {
-    'Irmandade da Adaga Negra 1' : {
-      quantidadePaginas : 2561,
-      autor : 'JR Ward1',
-      editora : 'Moderna1'
+    "Irmandade da Adaga Negra 1": {
+      quantidadePaginas: 2561,
+      autor: "JR Ward1",
+      editora: "Moderna1"
     },
-    'Irmandade da Adaga Negra 2' : {
-      quantidadePaginas : 2562,
-      autor : 'JR Ward2',
-      editora : 'Moderna2'
+    "Irmandade da Adaga Negra 2": {
+      quantidadePaginas: 2562,
+      autor: "JR Ward2",
+      editora: "Moderna2"
     },
-    'Irmandade da Adaga Negra 3' : {
-      quantidadePaginas : 2563,
-      autor : 'JR Ward3',
-      editora : 'Moderna3'
+    "Irmandade da Adaga Negra 3": {
+      quantidadePaginas: 2563,
+      autor: "JR Ward3",
+      editora: "Moderna3"
     }
-  }
+  };
 
-  return !nomeLivro ? obj : obj [nomeLivro]
+  return !nomeLivro ? obj : obj[nomeLivro];
 }
 
 /*
@@ -92,18 +91,28 @@ Ainda com a função acima, imprima a quantidade de páginas de um livro qualque
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-console.log( 'O livro Irmandade da Adaga Negra 3 tem ' + book('Irmandade da Adaga Negra 3').quantidadePaginas + ' páginas!');
+console.log(
+  "O livro Irmandade da Adaga Negra 3 tem " +
+    book("Irmandade da Adaga Negra 3").quantidadePaginas +
+    " páginas!"
+);
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-console.log( 'O autor do livro Irmandade da Adaga Negra 2 é ' + book('Irmandade da Adaga Negra 2').autor )
+console.log(
+  "O autor do livro Irmandade da Adaga Negra 2 é " +
+    book("Irmandade da Adaga Negra 2").autor
+);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-console.log( 'O livro Irmandade da Adaga Negra 1 foi publicado pela editora ' + book('Irmandade da Adaga Negra 1').editora )
+console.log(
+  "O livro Irmandade da Adaga Negra 1 foi publicado pela editora " +
+    book("Irmandade da Adaga Negra 1").editora
+);
