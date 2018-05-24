a5eb7c2b90a1878962dd98da32d03da632cfa846
Gabriel Dias Bahniuk <gabrieldiasbnu@hotmail.com>
2018-05-24 19:06:29 -0300

    Challenge 28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -25,3 +25,152 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+(function(win, doc) {
+
+  'use strict';
+
+  // BEGIN - DOM API
+
+  function DOM(elements) {
+    this.element = this.getElements(elements);
+  }
+
+  DOM.prototype.on = function on(event, callback){
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.addEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(event, callback){
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.removeEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.getElements = function getElements(element) {
+    return document.querySelectorAll(element);
+  };
+
+  DOM.prototype.substituteElements = function substituteElements(newElement, oldElement) {
+    return oldElement.replaceWith(newElement);
+  };
+
+  DOM.prototype.get = function get(){
+    return this.element;
+  };
+
+  DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply(this.element, arguments);
+  };
+
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+  };
+
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments);
+  };
+
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments);
+  };
+
+  DOM.prototype.getObjType = function getObjType(obj) {
+    return Object.prototype.toString.call(obj);
+  };
+
+  DOM.prototype.isObjectOfGivenType = function isObjectOfGivenType(obj, typeExpected) {
+    return this.getObjType(obj) === '[object '+typeExpected+']';
+  }
+
+  // END - DOM API
+
+  var $form = new DOM('form');
+  var dom = new DOM();
+  var ajax = new XMLHttpRequest();
+  var $statusArea = new DOM('[data-js="load-area"]');
+  var $inputCEP = new DOM('[data-js="input-cep"]');
+
+  $form.on('submit', handleFormOnSubmit);
+
+  function handleFormOnSubmit(evt) {
+    evt.preventDefault();
+    ajax.open('GET', createRequestURL($inputCEP.get()[0].value));
+    ajax.send();
+    ajax.addEventListener('readystatechange', handleChangeState, false);
+  }
+
+  function handleChangeState() {
+    if (isRequestOk(this.readyState, this.status))
+      setFields();
+  }
+
+  function isRequestOk(readyState, status) {
+    return readyState === 4 && status === 200;
+  }
+
+  function parseJSON() {
+     try {
+      return JSON.parse(ajax.responseText);
+     } catch(e) {
+      return ajax.responseText;
+     }
+  }
+
+  function validateCEP(cep) {
+    return cep.replace(/\D/g, '');
+  }
+
+  function createRequestURL(cep) {
+    return 'https://apps.widenet.com.br/busca-cep/api/cep.json?code=[CEP]'.replace('[CEP]', validateCEP(cep));
+  }
+
+  function setFields() {
+    var $inputLogradouro = new DOM('[data-js="input-logradouro"]');
+    var $inputBairro = new DOM('[data-js="input-bairro"]');
+    var $inputCidade = new DOM('[data-js="input-cidade"]');
+    var $inputUf = new DOM('[data-js="input-estado"]');
+    var data = parseJSON(ajax);
+
+    setFieldValue($statusArea, 'Carregando...');
+    if (hasResponse(data)) {
+      setFieldValue($inputLogradouro, data.address);
+      setFieldValue($inputBairro, data.district);
+      setFieldValue($inputCidade, data.city);
+      setFieldValue($inputUf, data.state);
+      setFieldValue($statusArea, 'Dados referentes ao CEP ' + $inputCEP.get()[0].value);
+    } else
+        setFieldValue($statusArea, 'Nenhum dado para o CEP ' + $inputCEP.get()[0].value);
+  }
+
+  function setFieldValue(field, data) {
+    if (dom.isObjectOfGivenType(field.get()[0], 'HTMLInputElement')) {
+      field.get()[0].value = data;
+    } else if (dom.isObjectOfGivenType(field.get()[0], 'HTMLParagraphElement')) {
+      field.get()[0].innerHTML = data;
+    }
+  }
+
+  function hasResponse(dataObj) {
+    switch (dataObj.status) {
+      case 0:
+        return false;
+      case 1:
+        return true;
+    }
+  }
+
+})(window, document);
