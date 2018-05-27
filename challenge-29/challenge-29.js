e137e105244c9e82d27530281709094e7c309274
Gabriel Dias Bahniuk <gabrieldiasbnu@hotmail.com>
2018-05-26 22:34:22 -0300

    Challenge 29 without validation

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function($) {
   'use strict';
 
   /*
@@ -35,5 +35,76 @@
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
+  var app = (function() {
+    return {
+      init : function() {
+        console.log('App init...');
+        this.loadInfo();
+        this.initEvents();
+      },
+      loadInfo : function loadInfo() {
+        console.log('Loading company info...');
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', '/company.json', true);
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
+      },
+      getCompanyInfo : function getCompanyInfo() {
+        if (!app.isReady.call(this))
+          return;
+        let data = JSON.parse(this.responseText);
+        const $infoCompany = $('[data-js="company-name"]').get();
+        const $infoPhone = $('[data-js="company-phone"]').get();
+        $infoCompany.textContent = data.name;
+        $infoPhone.textContent = data.phone;
+      },
+      isReady : function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      },
+      initEvents : function initEvents() {
+        const $formulario = $('form');
+        $formulario.on('submit', this.handleFormSubmit);
+      },
+      handleFormSubmit : function handleFormSubmit(e) {
+        e.preventDefault();
+        let $table = $('[data-js="tabela-carros"]').get();
+        $table.appendChild(app.insertNewCar());
+      },
+      insertNewCar : function insertNewCar() {
+        const $fragment = document.createDocumentFragment();
+        const $linha = document.createElement('tr');
+        const $tdImagem = document.createElement('td');
+        const $tdMarca = document.createElement('td');
+        const $tdModelo = document.createElement('td');
+        const $tdPlaca = document.createElement('td');
+        const $tdAno = document.createElement('td');
+        const $tdCor = document.createElement('td');
 
-})();
+        const $imagem = document.createElement('img');
+        $imagem.setAttribute('src', $('[data-js="input-imagem"]').get().value);
+        $imagem.setAttribute('height', '100px');
+        $imagem.setAttribute('width', '150px');
+        $tdImagem.appendChild($imagem);
+
+        $tdMarca.textContent = $('[data-js="input-marca"]').get().value;
+        $tdModelo.textContent = $('[data-js="input-modelo"]').get().value;
+        $tdPlaca.textContent = $('[data-js="input-placa"]').get().value;
+        $tdAno.textContent = $('[data-js="input-ano"]').get().value;
+        $tdCor.textContent = $('[data-js="input-cor"]').get().value;
+
+        $linha.appendChild($tdImagem);
+        $linha.appendChild($tdMarca);
+        $linha.appendChild($tdModelo);
+        $linha.appendChild($tdPlaca);
+        $linha.appendChild($tdAno);
+        $linha.appendChild($tdCor);
+
+        return $fragment.appendChild($linha);
+      }
+    };
+  })();
+
+  app.init();
+
+
+})(window.DOM);
