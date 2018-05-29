e822a3f4c7b1fd0e963e026d8c1d7765dc646cda
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2018-05-29 00:11:39 -0300

    Update challenge-20.js

--- challenge-20/challenge-20.js
+++ challenge-20/challenge-20.js
@@ -1,3 +1,5 @@
+
+(function (window, document){
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -6,86 +8,113 @@
 a marcação criada nele para fazer nosso desafio ;)
 
 O HTML NÃO PODE ser alterado!
-*/
 
-/*
-Ao carregar a página, pergunte ao usuário "Qual o seu nome?". Atribua o
+Ao carregar a página, pergunte ao usuário Qual o seu nome. Atribua o
 resultado à uma variável chamada `username`. Se o usuário não digitar um
-nome, `username` deve receber "Desconhecido".
-Com a resposta, mostre um alert com a mensagem "Bem vindo [USERNAME]!"
+nome, `username` deve receber Desconhecido.
+Com a resposta, mostre um alert com a mensagem Bem vindo [USERNAME]!
 */
-// ?
+
+(function(){
+    'use strict'
+
+    var username = (prompt('Qual o seu nome'));
+    if(username)
+    console.log(alert('Bem vindo '+ username));
+    else
+    console.log(username = 'Desconhecido');
+
+    console.log(username);
+})();
 
 /*
-Agora, pergunte ao usuário "Qual o seu e-mail?", atribuindo o resultado à
+Agora, pergunte ao usuário Qual o seu e-mail, atribuindo o resultado à
 uma variável chamada `email`.
 */
-// ?
+
+    var email = (prompt('Qual o seu email'));
+   
 
 /*
-- Selecione o input de "Nome", atribuindo-o à uma variável chamada
+- Selecione o input de Nome, atribuindo-o à uma variável chamada
 `$inputUsername`.
 */
-// ?
+ var $inputUsername = doc.querySelectorAll('input[type=text]');
 
 /*
-- Selecione o input de "Email", atribuindo-o à uma variável chamada
+- Selecione o input de Email, atribuindo-o à uma variável chamada
 `$inputEmail`.
 */
-// ?
+  var $inputemail = doc.querySelectorAll('input[type=email]');
 
-/*
-- Selecione o campo de "Mensagem", atribuindo-o à uma variável chamada
+ /*
+- Selecione o campo de Mensagem, atribuindo-o à uma variável chamada
 `$message`.
 */
-// ?
+var $messagem = doc.querySelectorAll('textarea');
 
-/*
+ /*
 - Selecione o botão de envio do formulário, atribuindo-o à uma variável
 chamada `$button`.
 */
-// ?
+var $button = doc.querySelectorAll('button');
 
-/*
-Preencha os campos de "Nome" e "Email" que estão no documento com os valores
+ /*
+Preencha os campos de Nome e Email que estão no documento com os valores
 entrados pelo usuário.
 */
-// ?
+    $inputUsername.value = username;
+    $inputemail.value = email;
 
-/*
-Adicione um listener de evento de click ao botão que faça o seguinte:
-1. Verificar se todos os campos estão preenchidos:
-- Mostrar um alert para cada campo não preenchido, como abaixo:
-- Se o campo de "Nome" não estiver preenchido, mostrar:
-    - "Preencha o nome do usuário!"
-- Se o campo de "Email" não estiver preenchido, mostrar:
-    - "Preencha o e-mail!"
-- Se o campo de "Mensagem" não estiver preenchido, mostrar:
-    - "Preencha a mensagem!"
-- Se o campo de "Email" for inválido, mostrar:
-    - "Entre com um e-mail válido!"
+ /*
+Adicione um listener de evento de click ao botão que faça o seguinte
+1. Verificar se todos os campos estão preenchidos
+- Mostrar um alert para cada campo não preenchido, como abaixo
+- Se o campo de Nome não estiver preenchido, mostrar
+    - Preencha o nome do usuário!
+- Se o campo de Email não estiver preenchido, mostrar
+    - Preencha o e-mail!
+- Se o campo de Mensagem não estiver preenchido, mostrar
+    - Preencha a mensagem!
+- Se o campo de Email for inválido, mostrar
+    - Entre com um e-mail válido!
 
 2. Para verificar se o e-mail é válido use a função `isValidEmail`, passando
-o e-mail que foi entrado no campo de "Email" por parâmetro. (A função
+o e-mail que foi entrado no campo de Email por parâmetro. (A função
 `isValidEmail` será criada logo abaixo).
 
-3. Se tudo estiver OK, pergunte ao usuário:
-    - "Tem certeza que deseja enviar o formulário?"
-Se for confirmado, mostre um alerta com a mensagem:
-    - "Enviado com sucesso!"
-Caso contrário, mostre um alerta com a mensagem:
-    - "Não enviado."
+3. Se tudo estiver OK, pergunte ao usuário
+    - Tem certeza que deseja enviar o formulário
+Se for confirmado, mostre um alerta com a mensagem
+    - Enviado com sucesso!
+Caso contrário, mostre um alerta com a mensagem
+    - Não enviado.
 */
-// ?
+$button.addEventListener('click', function(event){
+        event.preventDefault();
+        if( !$inputUsername.value)
+            return alert( 'Preencha o nome do usuário!' );
+        if( !$inputEmail.value )
+            return alert( 'Preencha o e-mail!' );
+        if ( !isValidEmail ($inputEmail.value) )
+            return alert( ' Entre com um e-mail válido! ');
+        if( !$messagem.value )
+            return alert( 'Preencha a mensagem!' );
+        if( !confirm ('Tem certeza que deseja enviar o formulário? '))
+        return alert('Não Enviado');  
+        return alert('Enviado com sucesso!');    
+    }, false);
+
+
 
 /*
 Crie uma função chamada `isValidEmail`, que será usada na validação do
 envio do formulário.
 Essa função deve receber o e-mail por parâmetro e verificar se é um e-mail
 válido.
-As regras para validação são:
+As regras para validação são
     - O nome do usuário (antes do arroba), pode ser qualquer caractere
-    alfanumérico, incluindo o underscore, sinal de "+" e o ponto;
+    alfanumérico, incluindo o underscore, sinal de + e o ponto;
     - Após o arroba, o domínio pode conter somente caracteres alfanuméricos
     e o underscore;
     - Para a extensão, o domínio deve vir seguido de um ponto, e no mínimo
@@ -93,15 +122,21 @@ As regras para validação são:
     - O final do domínio é opcional, mas se existir, deve começar com um
     ponto, seguido de no máximo 2 caracteres alfanuméricos.
 
-Alguns e-mails válidos que podem ser usados para testar:
-    - "meu.email+categoria@gmail.com"
-    - "juca_malandro@bol.com.br"
-    - "pedrobala@hotmail.uy"
-    - "sandro@culinaria.dahora"
+Alguns e-mails válidos que podem ser usados para testar
+    - meu.email+categoria@gmail.com
+    - juca_malandro@bol.com.br
+    - pedrobala@hotmail.uy
+    - sandro@culinaria.dahora
 
-Alguns e-mails inválidos:
-    - "walter-da-silva@maraca.br"
-    - "rita-marica@titica.a.b"
-    - "agua_@evida.br.com"
+Alguns e-mails inválidos
+    - walter-da-silva@maraca.br
+    - rita-marica@titica.a.b
+    - agua_@evida.br.com
+
 */
-// ?
+function isValidEmail(email){
+        return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email); 
+    }
+
+
+})(window, document);
