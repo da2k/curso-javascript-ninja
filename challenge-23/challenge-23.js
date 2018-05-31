ebf51b28036fce40383d2ecbf3d44590b288d661
fernando.ramon <fernando.diogenes.ramon@gmail.com>
2018-05-31 15:52:19 -0300

    challenge-23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,70 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function (window, document) {
+  'use strict';
+  /*
+  Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+  As regras são:
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+  - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+  diretamente;
+  - O input deve iniciar com valor zero;
+  - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+  - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+  multiplicação(x) e divisão(÷);
+  - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+  que irá limpar o input, deixando-o com valor 0;
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+  - A cada número pressionado, o input deve atualizar concatenando cada valor
+  digitado, como em uma calculadora real;
+  - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+  operação no input. Se o último caractere no input já for um símbolo de alguma
+  operação, esse caractere deve ser substituído pelo último pressionado.
+  Exemplo:
+  - Se o input tem os valores: "1+2+", e for pressionado o botão de
+  multiplicação (x), então no input deve aparecer "1+2x".
+  - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+  input;
+  - Ao pressionar o botão "CE", o input deve ficar zerado.
+  */
+  var $valor = document.querySelector('[data-js="valor"]');
+  var $numeros = document.querySelectorAll('[data-js="numero"]');
+  var $operacoes = document.querySelectorAll('[data-js="operacao"]');
+  var $limpar = document.querySelector('[data-js="limpar"]');
+  var $resultado = document.querySelector('[data-js="resultado"]');
+  var regex = /[+\-x÷]$/g;
+
+  // Event Listeners
+  Array.prototype.forEach.call($numeros, function(numero) {
+    numero.addEventListener('click', handleClickNumero, false);
+  });
+
+  Array.prototype.forEach.call($operacoes, function(operacao) {
+    operacao.addEventListener('click', handleClickOperation, false);
+  });
+
+  $limpar.addEventListener('click', handleClickCE, false);
+  $resultado.addEventListener('click', handleClickIgual, false);
+
+  // Handler Functions
+  function handleClickNumero() {
+    var valorBotao = this.value;
+    $valor.value === '0' ? $valor.value = valorBotao : $valor.value += valorBotao;
+  }
+
+  function handleClickOperation() {
+    $valor.value = $valor.value.replace(regex, '');
+    var simboloBotao = this.value;
+    $valor.value += simboloBotao;
+  }
+
+  function handleClickIgual() {
+    $valor.value = $valor.value.replace(regex, '').replace(/[x÷]/g, function(match) {
+      return match === '÷' ? '/' : '*';
+    });
+    $valor.value = String(eval($valor.value));
+  }
+
+  function handleClickCE() {
+    $valor.value = '0';
+  }
+
+})(window, document);
