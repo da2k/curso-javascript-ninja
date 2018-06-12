45a3ade5aec983fe12805e1093ecc042c261b5eb
Clauter Carlos <clauter.carlos@gmail.com>
2018-06-12 04:24:43 -0300

    Challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = ['scott', 'carlos', 'santos', 34, 89];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function recebeArray(myarray) {
+    return myarray;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(recebeArray(myarray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,19 +22,25 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function recebeDois(array1, number) {
+    return array1[number];
+
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var newArray = ['Samba', 127, undefined, true, null];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
-
+console.log(recebeDois(newArray, 0));
+console.log(recebeDois(newArray, 1));
+console.log(recebeDois(newArray, 2));
+console.log(recebeDois(newArray, 3));
+console.log(recebeDois(newArray, 4));
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
@@ -47,30 +55,52 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book (bookName){
+
+  var allBooks = {
+    "Python Fluence" : {
+      quantidadePaginas : 634,
+      autor: "Luciano Ramalho",
+      editora: "Pearson"
+    },
+    "Eloquent Javascript" :{
+      quantidadePaginas : 234,
+      autor: "Fulanho Douos",
+      editora: "Pearson"
+    },
+    "Estrutura de Dados" :{
+      quantidadePaginas : 234,
+      autor: "Ciclano James",
+      editora: "Alta Books"
+    }
+  };
+
+  return !bookName ? allBooks : allBooks[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var bookName = 'Estrutura de Dados';
+console.log("O livro " + bookName + " tem " + book(bookName).quantidadePaginas + " páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log("O autor do livro " + bookName + " é " + book(bookName).autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log("O livro " + bookName + " foi publicado pela editora " + book(bookName).editora);
