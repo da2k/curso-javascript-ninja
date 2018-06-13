61e4b6324688f2bd57ba0a8608cdf1a84b1dffad
yogmel <pong7219@gmail.com>
2018-06-12 22:31:14 -0300

    correcao challenge 21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -18,31 +18,58 @@ dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 (function(win, doc) {
     'use strict';
 
-    var counter = doc.querySelector('input[type="text"]').value;
+    // var counter = doc.querySelector('input[type="text"]').value;
 
-    var $startbtn = doc.querySelector('[data-js="startBtn"]');
-    var $stopbtn = doc.querySelector('[data-js="stopBtn"]');
-    var $resetbtn = doc.querySelector('[data-js="resetBtn"]');
+    // var $startbtn = doc.querySelector('[data-js="startBtn"]');
+    // var $stopbtn = doc.querySelector('[data-js="stopBtn"]');
+    // var $resetbtn = doc.querySelector('[data-js="resetBtn"]');
 
-    var Interval;
+    // var Interval;
 
-    $startbtn.addEventListener( 'click', function (){
-        clearInterval( Interval );
-        Interval = setInterval( startTimer, 900 );
-    }, false);
+    // $startbtn.addEventListener( 'click', function (){
+    //     clearInterval( Interval );
+    //     Interval = setInterval( startTimer, 900 );
+    // }, false);
 
-    $stopbtn.addEventListener( 'click', function() {
-        clearInterval( Interval );
-    }, false );
+    // $stopbtn.addEventListener( 'click', function() {
+    //     clearInterval( Interval );
+    // }, false );
 
-    $resetbtn.addEventListener( 'click', function() {
-        clearInterval( Interval );
-        doc.querySelector('input[type="text"]').value = 0;
-        counter = 0;
-    }, false);
+    // $resetbtn.addEventListener( 'click', function() {
+    //     clearInterval( Interval );
+    //     doc.querySelector('input[type="text"]').value = 0;
+    //     counter = 0;
+    // }, false);
 
-    function startTimer(){
-        doc.querySelector('input[type="text"]').value = counter++;
+    // function startTimer(){
+    //     doc.querySelector('input[type="text"]').value = counter++;
+    // };
+
+
+    var $timer = doc.querySelector('[type="text"]');
+    var $start = doc.querySelector('[data-js="startBtn"]');
+    var $stop = doc.querySelector('[data-js="stopBtn"]');
+    var $reset = doc.querySelector('[data-js="resetBtn"]');
+    var interval;
+
+    $start.addEventListener( 'click', startTimer, false );
+    $stop.addEventListener( 'click', stopTimer, false );
+    $reset.addEventListener( 'click', resetTimer, false );
+
+    function startTimer() {
+        $timer.value = +$timer.value + 1;
+        interval = setTimeout( startTimer, 1000 );
+    };
+
+    function stopTimer() {
+        clearTimeout( interval );
     }
+
+    function resetTimer() {
+        $timer.value = 0;
+        stopTimer();
+    }
+
+
 
 })(window, document);
