d20ec66353636479b85c65d7442425ad65f90ae0
silvaaraujo <silvaaraujo.dev@gmail.com>
2018-06-18 19:06:49 -0400

    challenge-26.js

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,31 +1,62 @@
-/*
-O desafio dessa semana é criar uma mini library (biblioteca) para
-reutilizarmos nossos códigos quando fizermos manipulação de DOM!
+(function() {
 
-Requisitos:
-- O nome da lib deve ser "DOM".
-- Ela deve ser uma função construtora, que receberá uma string por parâmetro.
-Essa string será o nó do DOM a ser selecionado;
-- No construtor, você deve atribuir à `this.element` todos os elementos
-do DOM selecionados;
-- Extenda a lib para ter os métodos `on`, `off` e `get`.
-- O método `on` irá adicionar um listener de evento a todos os elementos
-selecionados.
-- O método `off` irá remover o listener de evento de todos os elementos
-selecionados.
-- O método `get` deve retornar os elementos.
-- O código abaixo deve funcionar corretamente após a lib criada.
+	'use strict';
 
-Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
-Só passe para o próximo problema quando tiver resolvido o anterior :)
-*/
-// ?
+	/*
+	O desafio dessa semana é criar uma mini library (biblioteca) para
+	reutilizarmos nossos códigos quando fizermos manipulação de DOM!
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+	Requisitos:
+	- O nome da lib deve ser "DOM".
+	- Ela deve ser uma função construtora, que receberá uma string por parâmetro.
+	Essa string será o nó do DOM a ser selecionado;
+	- No construtor, você deve atribuir à `this.element` todos os elementos
+	do DOM selecionados;
+	- Extenda a lib para ter os métodos `on`, `off` e `get`.
+	- O método `on` irá adicionar um listener de evento a todos os elementos
+	selecionados.
+	- O método `off` irá remover o listener de evento de todos os elementos
+	selecionados.
+	- O método `get` deve retornar os elementos.
+	- O código abaixo deve funcionar corretamente após a lib criada.
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+	Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
+	Só passe para o próximo problema quando tiver resolvido o anterior :)
+	*/
+
+	function DOM(elementos) {
+		this.elements = this.getDOMElements(elementos);
+	}
+
+	DOM.prototype.getDOMElements = function getDOMElements(elementos) {
+		return document.querySelectorAll(elementos);
+	}
+
+	DOM.prototype.on = function on(evento, callback) {
+		Array.prototype.forEach.call(this.elements, function(item) {
+			item.addEventListener(evento, callback, false);
+		});
+	}
+
+	DOM.prototype.off = function off(evento, callback) {
+		Array.prototype.forEach.call(this.elements, function(item) {
+			item.removeEventListener(evento, callback, false);
+		});
+	}
+
+	DOM.prototype.get = function get() {
+		return this.elements;
+	}
+
+	var $a = new DOM('[data-js="link"]');
+
+	$a.on('click', function dispararEvento(e) {
+	  e.preventDefault();
+	  console.log('clicou');
+	  $a.off('click', dispararEvento);
+	});
+
+	console.log('Elementos selecionados:', $a.get());
+	console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+})();
