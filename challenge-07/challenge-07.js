c48064015178466c325645a7d9441b3d8463e11f
Renan <renan.serverti@gmail.com>
2018-06-28 17:15:37 -0300

    challenge-07 - corrigido

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -7,22 +7,23 @@ var arr = [1, 2, 3, 'Ninja', null, { carro: 'Fiat' }];
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-function addItem() {
-    arr.push();
+function addItem(item) {
+    arr.push(item);
+    return arr;
 }
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-arr.push([true, false, { pessoa: 'nome' }]);
+console.log(addItem([true, false, { pessoa: 'nome' }]));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-console.log('O segundo elemento do segundo array é ' + arr[6][2].pessoa + '.');
+console.log('O segundo elemento do segundo array é ' + arr[6][2] + '.');
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
@@ -52,8 +53,9 @@ Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log('Números ímpares entre 10 e 20:');
 var num = 10;
-while (num <= 20) {
-    num % 2 === 1 ? console.log(num) : '';
+while (num < 20) {
+    num % 2 !== 0 ? console.log(num) : '';
+    num++;
 }
 
 /*
@@ -69,5 +71,5 @@ for (var cont = 100; cont <= 120; cont++) {
 
 console.log('Números ímpares entre 111 e 125:');
 for (var cont = 111; cont <= 125; cont++) {
-    cont % 2 === 1 ? console.log(cont) : '';
+    cont % 2 !== 0 ? console.log(cont) : '';
 }
