eb94d441e85697e9632a7b338852fd708ed458e0
Vinicius Assis <believe.182@hotmail.com>
2018-06-30 12:22:25 -0300

    ab

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -1,3 +1,5 @@
+(function(window, document) {
+  'use strict';
 /*
 Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
 o código, conforme vimos na aula anterior. Quebrar as responsabilidades
@@ -16,17 +18,28 @@ var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]
 var $buttonCE = document.querySelector('[data-js="button-ce"]');
 var $buttonEqual = document.querySelector('[data-js="button-equal"]');
 
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
-  button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
-$buttonCE.addEventListener('click', handleClickCE, false);
-$buttonEqual.addEventListener('click', handleClickEqual, false);
+function initialize() {
+  initEvents();
+}
+
+function initEvents() {
+
+  Array.prototype.forEach.call($buttonsNumbers, function(button) {
+    button.addEventListener('click', handleClickNumber, false);
+  });
+  Array.prototype.forEach.call($buttonsOperations, function(button) {
+    button.addEventListener('click', handleClickOperation, false);
+  });
+  $buttonCE.addEventListener('click', handleClickCE, false);
+  $buttonEqual.addEventListener('click', handleClickEqual, false);
+
+}
 
 function handleClickNumber() {
-  $visor.value += this.value;
+  if($visor.value == 0)
+    return $visor.value = this.value;
+  return $visor.value += this.value;
+
 }
 
 function handleClickOperation() {
@@ -39,37 +52,59 @@ function handleClickCE() {
 }
 
 function isLastItemAnOperation(number) {
-  var operations = ['+', '-', 'x', '÷'];
+  var operations = getOperations();
   var lastItem = number.split('').pop();
   return operations.some(function(operator) {
     return operator === lastItem;
   });
 }
 
-function removeLastItemIfItIsAnOperator(number) {
-  if(isLastItemAnOperation(number)) {
-    return number.slice(0, -1);
-  }
-  return number;
+function getOperations() {
+  return Array.prototype.map.call($buttonsOperations, function(button){
+    return button.value;
+  });
+}
+
+function removeLastItemIfItIsAnOperator(string) {
+  if(isLastItemAnOperation(string))
+    return string.slice(0, -1);
+  return string;
 }
 
 function handleClickEqual() {
   $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-  $visor.value = allValues.reduce(function(accumulated, actual) {
-    var firstValue = accumulated.slice(0, -1);
-    var operator = accumulated.split('').pop();
-    var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-    switch(operator) {
-      case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-      case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-      case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-      case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-    }
-  });
+  var allValues = $visor.value.match(getRegexOperations());
+  $visor.value = allValues.reduce(calculateAllValues);
 }
+
+function getRegexOperations() {
+  return new RegExp('\\d+[' + getOperations().join('') + ']?', 'g');
+};
+
+function calculateAllValues(accumulated, actual) {
+  var firstValue = accumulated.slice(0, -1);
+  var operator = accumulated.split('').pop();
+  var lastValue = removeLastItemIfItIsAnOperator(actual);
+  var lastOperator = getLastOperator(actual);
+  return doOperation(operator, firstValue, lastValue)  + lastOperator;
+};
+
+function getLastOperator(value) {
+  return isLastItemAnOperation(value) ? value.split('').pop() : '';
+};
+
+function doOperation(operator, firstValue, lastValue) {
+  switch(operator) {
+    case '+':
+      return Number(firstValue) + Number(lastValue);
+    case '-':
+      return Number(firstValue) - Number(lastValue);
+    case 'x':
+      return Number(firstValue) * Number(lastValue);
+    case '÷':
+      return Number(firstValue) / Number(lastValue);
+  }
+};
+
+  initialize();
+})(window, document);
--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,31 +1,55 @@
-/*
-O desafio dessa semana é criar uma mini library (biblioteca) para
-reutilizarmos nossos códigos quando fizermos manipulação de DOM!
+(function(window, document) {
+  'use strict';
+  /*
+  O desafio dessa semana é criar uma mini library (biblioteca) para
+  reutilizarmos nossos códigos quando fizermos manipulação de DOM!
 
-Requisitos:
-- O nome da lib deve ser "DOM".
-- Ela deve ser uma função construtora, que receberá uma string por parâmetro.
-Essa string será o nó do DOM a ser selecionado;
-- No construtor, você deve atribuir à `this.element` todos os elementos
-do DOM selecionados;
-- Extenda a lib para ter os métodos `on`, `off` e `get`.
-- O método `on` irá adicionar um listener de evento a todos os elementos
-selecionados.
-- O método `off` irá remover o listener de evento de todos os elementos
-selecionados.
-- O método `get` deve retornar os elementos.
-- O código abaixo deve funcionar corretamente após a lib criada.
+  Requisitos:
+  - O nome da lib deve ser "DOM".
+  - Ela deve ser uma função construtora, que receberá uma string por parâmetro.
+  Essa string será o nó do DOM a ser selecionado;
+  - No construtor, você deve atribuir à `this.element` todos os elementos
+  do DOM selecionados;
+  - Extenda a lib para ter os métodos `on`, `off` e `get`.
+  - O método `on` irá adicionar um listener de evento a todos os elementos
+  selecionados.
+  - O método `off` irá remover o listener de evento de todos os elementos
+  selecionados.
+  - O método `get` deve retornar os elementos.
+  - O código abaixo deve funcionar corretamente após a lib criada.
 
-Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
-Só passe para o próximo problema quando tiver resolvido o anterior :)
-*/
-// ?
+  Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
+  Só passe para o próximo problema quando tiver resolvido o anterior :)
+  */
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+  function DOM(element) {
+    this.element = document.querySelectorAll(element);
+  }
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element){
+      element.addEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element){
+      element.removeEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get(element) {
+    return this.element;
+  };
+
+  var $a = new DOM('[data-js="link"]');
+  $a.on('click', function handleClick(e) {
+    e.preventDefault();
+    console.log('clicou');
+    $a.off('click', handleClick);
+  });
+
+  console.log('Elementos selecionados:', $a.get());
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+}) (window, document);
