95c0a3c2a3c6296ed81842332d39ca2e776e8e40
Drohilux <dr.oliveira88@gmail.com>
2018-07-07 22:33:18 -0300

    challenge-09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -3,6 +3,8 @@ Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
 */
 
+( function() { return 'Ola mundo'; } )
+
 /*
 Analise as funções abaixo (`myFunction`, `myFunction2` e `myFunction3`, e
 ajuste o posicionamento das variáveis e funções internas, para que os códigos
@@ -10,24 +12,24 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+    var number1 = 10;
+    var number2 = 20;
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
     return number1 + number2;
-    var number2 = 20;
 }
 myFunction();
 
 /*
     myFunction2();
 */
-function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+function myFunction2() {    
     var sum = function sum() {
         return number1 + number2;
     };
     var number1 = 10;
     var number2 = 20;
+    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
     return sum();
 }
 myFunction2();
@@ -36,10 +38,10 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
-    var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
     var number1 = 40;
+    var number2 = 50;
+    console.log( 'A soma de 40 e 50 é igual a', sum() );    
+    console.log( 'Na função myFunction3, number1 é igual a', number1 );   
     return sum();
     function sum() {
         return number1 + number2;
@@ -60,13 +62,18 @@ o retorno de `calculator`.
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+function calculator ( x, y ) {
+  return function ( callback ) {
+    return callback( x, y );
+  };
+}
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
 passando dois números por parâmetro.
 */
-// ?
+
+var sum = calculator( 2, 5 );
 
 /*
 Sabemos que `sum` agora tem uma função atribuída a ela, que é o retorno de
@@ -77,15 +84,20 @@ para a chamada à `calculator` acima.
 uma função anônima que irá retornar a soma dos dois números que essa função
 anônima tem como seus argumentos.
 */
+
 console.log( 'O resultado da soma é:' );
-// ?
+console.log( sum(function( num1, num2 ){ return num1 + num2; }));
 
 /*
 Agora declare outra variáveis chamadas `subtraction`, `multiplication`,
 `division` e `mod`, e atribua à elas `calculator`, passando números
 diferentes para cada chamada.
 */
-// ?
+
+var subtraction = calculator( 72, 29 );
+var multiplication = calculator( 324, 987 );
+var division = calculator( 875, 12 );
+var mod = calculator( 875, 12 );
 
 /*
 Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -94,14 +106,27 @@ divisão e módulo (resto de divisão), conforme a função utilizada.
 As suas respostas devem estar abaixo dos `console.log` referentes à cada
 chamada.
 */
+
+
 console.log( 'O resultado da subtração é:' );
-// ?
+console.log( subtraction(function( num1, num2 ) { return num1 - num2; }));
+console.log( 'O resultado da multiplicação é:' );
+console.log( multiplication(function( num1, num2 ) { return num1 * num2; }));
+console.log( 'O resultado da divisão é:' );
+console.log( division(function( num1, num2 ) { return num1 / num2; }));
+console.log( 'O resultado do resto da divisão é:' );
+console.log( mod(function( num1, num2 ) { return num1 % num2; }));
+
 
 console.log( 'O resultado da multiplicação é:' );
-// ?
+//319788
 
 console.log( 'O resultado da divisão é:' );
-// ?
+//72.91666666666667
 
 console.log( 'O resto da divisão é:' );
-// ?
+//11
+
+//var string = "Davi";
+var string = new String('Davi');
+var idade = new Number('30');
