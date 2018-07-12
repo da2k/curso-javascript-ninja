90b42c84102586a900796b16c0e968f2db755931
yogmel <pong7219@gmail.com>
2018-07-12 14:57:32 -0300

    Tentativa challenge 27

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -19,3 +19,75 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+(function(){
+  'use strict';
+  function DOM(str){
+    this.element = doc.querySelectorAll(str);
+  }
+
+  DOM.forEach = function forEach( arr, func ) {
+    return Array.prototype.forEach.call( arr, func );
+  }
+
+  DOM.map = function map( arr, func ) {
+    return Array.prototype.map.call( arr, func );
+  }
+
+  DOM.filter = function filter( arr, func ) {
+    return Array.prototype.filter.call( arr, func );
+  }
+
+  DOM.reduce = function reduce( arr, func ) {
+    return Array.prototype.reduce.call( arr, func );
+  }
+
+  DOM.reduceRight = function reduceRight( arr, func ) {
+    return Array.prototype.reduceRight.call( arr, func );
+  }
+
+  DOM.every = function every( arr, func ) {
+    return Array.prototype.every.call( arr, func );
+  }
+
+  DOM.some = function some( arr, func ) {
+    return Array.prototype.some.call( arr, func );
+  }
+
+  function transformType(a){
+    return Object.prototype.toString.call(a);
+  }
+
+  DOM.isArray = function isArray(obj){
+    return transformType(obj) === '[object Array]';
+  }
+
+  DOM.isObject = function isObject(obj){
+    return transformType(obj) === '[object Object]';
+  }
+
+  DOM.isFunction = function isFunction(obj){
+    return transformType(obj) === '[object Function]';
+  }
+
+  DOM.isNumber = function isNumber(obj){
+    return transformType(obj) === '[object Number]';
+  }
+
+  DOM.isString = function isString(obj){
+    return transformType(obj) === '[object String]';
+  }
+
+  DOM.isBoolean = function isBoolean(obj){
+    return transformType(obj) === '[object Boolean]';
+  }
+
+  DOM.isNull = function isNull(obj){
+    return transformType(obj) === '[object Null]' || '[object Undefined]';
+  }
+
+  console.log (DOM.isArray([1, 2, 3]));
+  console.log (DOM.isFunction(function() {}));
+  console.log (DOM.isNumber('numero'));
+
+})();
