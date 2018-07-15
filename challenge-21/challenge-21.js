e60e6d7ee7ef2b8d87f577c70eafe608ba0f0752
Vinicius Meneses <vini23hot@gmail.com>
2018-07-15 15:49:39 -0300

    Challenge 21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,17 +1,46 @@
-/*
-O desafio de hoje será um pequeno projeto: um cronômetro!
-As regras para criação do cronômetro são as seguintes:
-1. Crie um arquivo index.html e adicione esse script a ele;
-2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
-Ele será o nosso cronômetro;
-3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
-4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
-cada segundo;
-5. Ao clicar em Stop, o cronômetro deve parar de contar;
-6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+(function (win, doc) {
+    'use strict';
+    /*
+    O desafio de hoje será um pequeno projeto: um cronômetro!
+    As regras para criação do cronômetro são as seguintes:
+    1. Crie um arquivo index.html e adicione esse script a ele;
+    2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
+    Ele será o nosso cronômetro;
+    3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+    4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
+    cada segundo;
+    5. Ao clicar em Stop, o cronômetro deve parar de contar;
+    6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+    
+    Utilize o atributo data-js para nomear o campo e os botões. Você pode
+    usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
+    dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
+    */
+    var $inputCronometro = doc.querySelector('[data-js="txtCronometro"]');
+    var $btnStart = doc.querySelector('[data-js="btnStart"]');
+    var $btnStop = doc.querySelector('[data-js="btnStop"]');
+    var $btnReset = doc.querySelector('[data-js="btnReset"]');
 
-Utilize o atributo data-js para nomear o campo e os botões. Você pode
-usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
-dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
-*/
-// ?
+    var temporizador;
+
+    function startTimer() {
+        $inputCronometro.value++;
+        temporizador = setTimeout(startTimer, 1000);
+    }
+
+    function stopTimer() {
+        clearTimeout(temporizador);
+    }
+
+    function resetTimer() {
+        stopTimer();
+        $inputCronometro.value = 0;
+    }
+
+    $btnStart.addEventListener('click', startTimer, false);
+
+    $btnStop.addEventListener('click', stopTimer, false);
+
+    $btnReset.addEventListener('click', resetTimer, false);
+
+})(window, document);
