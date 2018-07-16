1801c73686d3b685a0e8afb3a3c91974cd09fc21
Vinicius Meneses <vini23hot@gmail.com>
2018-07-16 12:26:35 -0300

    Challenge 24

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -1,75 +1,79 @@
-/*
-Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
-o código, conforme vimos na aula anterior. Quebrar as responsabilidades
-em funções, onde cada função faça somente uma única coisa, e faça bem feito.
+(function (win, doc) {
+  'use strict';
+  /*
+  Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
+  o código, conforme vimos na aula anterior. Quebrar as responsabilidades
+  em funções, onde cada função faça somente uma única coisa, e faça bem feito.
+  - Remova as duplicações de código;
+  - agrupe os códigos que estão soltos em funções (declarações de variáveis,
+  listeners de eventos, etc);
+  - faça refactories para melhorar esse código, mas de forma que o mantenha com a
+  mesma funcionalidade.
+  */
 
-- Remova as duplicações de código;
-- agrupe os códigos que estão soltos em funções (declarações de variáveis,
-listeners de eventos, etc);
-- faça refactories para melhorar esse código, mas de forma que o mantenha com a
-mesma funcionalidade.
-*/
 
-var $visor = document.querySelector('[data-js="visor"]');
-var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
-var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
-var $buttonCE = document.querySelector('[data-js="button-ce"]');
-var $buttonEqual = document.querySelector('[data-js="button-equal"]');
+  //SELECIONANDO OS ELEMENTOS
+  var $inputResultado = doc.querySelector('[data-js="inputResultado"]');
 
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
-  button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
-$buttonCE.addEventListener('click', handleClickCE, false);
-$buttonEqual.addEventListener('click', handleClickEqual, false);
+  var $btnIgual = doc.querySelector('[data-js="btnIgual"]');
+  var $btnCE = doc.querySelector('[data-js="btnCE"]');
 
-function handleClickNumber() {
-  $visor.value += this.value;
-}
+  var $btnNumeros = doc.querySelectorAll('[data-js="btnNumero"]');
+  var $btnOperacoes = doc.querySelectorAll('[data-js="btnOperacao"]');
 
-function handleClickOperation() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  $visor.value += this.value;
-}
 
-function handleClickCE() {
-  $visor.value = 0;
-}
-
-function isLastItemAnOperation(number) {
-  var operations = ['+', '-', 'x', '÷'];
-  var lastItem = number.split('').pop();
-  return operations.some(function(operator) {
-    return operator === lastItem;
-  });
-}
-
-function removeLastItemIfItIsAnOperator(number) {
-  if(isLastItemAnOperation(number)) {
-    return number.slice(0, -1);
+  //CRIANDO AS FUNÇÕES
+  function handleClickCE() {
+    $inputResultado.value = 0;
   }
-  return number;
-}
 
-function handleClickEqual() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-  $visor.value = allValues.reduce(function(accumulated, actual) {
-    var firstValue = accumulated.slice(0, -1);
-    var operator = accumulated.split('').pop();
-    var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-    switch(operator) {
-      case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-      case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-      case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-      case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
+  function handleClickNumero() {
+    if ($inputResultado.value.charAt(0) === '0') {
+      $inputResultado.value = $inputResultado.value.substring(1);
     }
-  });
-}
+    $inputResultado.value += this.value;
+  }
+
+  function handleClickOperacao() {
+    removeLastItemOperator();
+    $inputResultado.value += this.value;
+  }
+
+  function isLastItemAnOperation() {
+    var operations = ['+', '-', '*', '/'];
+    var lastItem = $inputResultado.value.split('').pop();
+
+    return operations.some(function(op) {
+      return op === lastItem;
+    })
+  }
+
+  function removeLastItemOperator() {
+    if (isLastItemAnOperation()) {
+      $inputResultado.value = $inputResultado.value.slice(0, -1);
+    }
+  }
+
+  function handleClickIgual() {
+    removeLastItemOperator();
+    $inputResultado.value = eval($inputResultado.value);
+  }
+
+  function addEventListenerAll() {
+    $btnIgual.addEventListener('click', handleClickIgual, false);
+    $btnCE.addEventListener('click', handleClickCE, false);
+
+    Array.prototype.forEach.call($btnNumeros, function(button) {
+      button.addEventListener('click', handleClickNumero, false);
+    })
+
+    Array.prototype.forEach.call($btnOperacoes, function(button) {
+      button.addEventListener('click', handleClickOperacao, false);
+    })
+  }
+
+
+  //ADICIONANDO EVENTO CLICK AOS BOTÕES
+  addEventListenerAll();
+
+})(window, document);
