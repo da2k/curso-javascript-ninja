c647fceebf46036561770a6f1caaabb6652b9cc3
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2018-07-20 14:47:15 -0300

    Update challenge-26.js

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -20,12 +20,42 @@ Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
 // ?
+(function(doc){
+	'use strict';
+
+function DOM(elements){
+	this.elements = doc.querySelectAll(elements);
+}
+
+DOM.prototype.on = function on(eventType, callback){
+	Array.prototype.forEach.call(this.element, function(element){
+		element.addEventListener(eventType, callback, false);
+	});
+}};
+
+DOM.prototype.off = function off(eventType, callback){
+	Array.prototype.forEach.call(this.element, function(element){
+		element.removeEventListener(eventType, callback, false);
+	});
+};
+
+DOM.prototype.get = function get(){
+	return this.elements;
+};
 
 var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
+
+$a.on('click', function handleClick(e) {
   e.preventDefault();
   console.log('clicou');
-});
+  $a.off('click, handleClick');
+}, false);
+
+$a.get('click', function(e) {
+  e.preventDefault();
+  console.log('clicou');
+}, false);
 
 console.log('Elementos selecionados:', $a.get());
 console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+})(document);
