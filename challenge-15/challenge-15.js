cbebae85dc8454fe726d7b6599f1e831e931f22d
machap <paulinhodj10@gmail.com>
2018-08-15 00:18:49 -0300

    Resolução do desafio #16

--- challenge-15/challenge-15.js
+++ challenge-15/challenge-15.js
@@ -1,54 +1,83 @@
-/*
-Envolva todo o código desse desafio em uma IIFE.
-Crie um arquivo chamado index.html e adicione esse script ao HTML.
-*/
+(function () {
+  /*
+  Envolva todo o código desse desafio em uma IIFE.
+  Crie um arquivo chamado index.html e adicione esse script ao HTML.
+  */
 
-/*
-Crie uma função construtora chamada "Person". Esse construtor deve ter
-as seguintes características:
-- Deve receber 3 parâmetros: `name`, `lastName` e `age`;
-- Deverá ter 3 propriedades:
-  - `name` - que receberá o valor do parâmetro `name`;
-  - `lastName` - que receberá o valor do parâmetro `lastName`;
-  - `age` - que receberá o valor do parâmetro `age`;
-- Deverá ter 3 métodos:
-  - `getFullName` - que deve retornar o nome completo do objeto criado,
-  no formato:
-    - "[NAME] [LASTNAME]"
-  - `getAge` - que deverá retornar a idade (age);
-  - `addAge` - esse método não deverá ter nenhum parâmetro, mas ao invocá-lo
-  iremos passar um único argumento, que é a quantidade de anos que devem ser
-  adicionados à idade original (age). Esse método deverá retornar o objeto
-  que será instanciado.
-*/
-// ?
+  /*
+  Crie uma função construtora chamada "Person". Esse construtor deve ter
+  as seguintes características:
+  - Deve receber 3 parâmetros: `name`, `lastName` e `age`;
+  - Deverá ter 3 propriedades:
+    - `name` - que receberá o valor do parâmetro `name`;
+    - `lastName` - que receberá o valor do parâmetro `lastName`;
+    - `age` - que receberá o valor do parâmetro `age`;
+  - Deverá ter 3 métodos:
+    - `getFullName` - que deve retornar o nome completo do objeto criado,
+    no formato:
+      - "[NAME] [LASTNAME]"
+    - `getAge` - que deverá retornar a idade (age);
+    - `addAge` - esse método não deverá ter nenhum parâmetro, mas ao invocá-lo
+    iremos passar um único argumento, que é a quantidade de anos que devem ser
+    adicionados à idade original (age). Esse método deverá retornar o objeto
+    que será instanciado.
+  */
+  
+  var Person = function(name, lastName, age){
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.getFullname = function getFullname(){
+      return this.name + ' ' + this.lastName;
+    };
+    this.getAge = function getAge(){
+      return this.age;
+    };
+    this.addAge = function addAge(){
+      this.age += arguments[0];
+      return this;
+    };
+  }
 
-/*
-Crie 3 novos objetos usando o construtor acima. Os objetos serão novas
-pessoas. As variáveis deverão ser o primeiro nome da pessoa. Passe os
-parâmetros corretamente para o construtor para criar as novas pessoas.
-Mostre as 3 novas pessoas criadas no console (Um console.log por pessoa).
-*/
-console.log( 'Novas pessoas criadas à partir de Person:' );
-// ?
+  /*
+  Crie 3 novos objetos usando o construtor acima. Os objetos serão novas
+  pessoas. As variáveis deverão ser o primeiro nome da pessoa. Passe os
+  parâmetros corretamente para o construtor para criar as novas pessoas.
+  Mostre as 3 novas pessoas criadas no console (Um console.log por pessoa).
+  */
+  console.log('Novas pessoas criadas à partir de Person:');
+  var personPaulo = new Person('Paulo', 'Victor', 23);
+  var personMaria = new Person('Maria', 'Joaquina', 50);
+  var personAlex = new Person('Alex', 'Santos', 18);
+  console.log(personPaulo);
+  console.log(personMaria);
+  console.log(personAlex);
 
-/*
-Mostre no console o nome completo de cada pessoa.
-*/
-console.log( '\nNomes das pessoas:' );
-// ?
+  /*
+  Mostre no console o nome completo de cada pessoa.
+  */
+  console.log('\nNomes das pessoas:');
+  console.log(personPaulo.getFullname());
+  console.log(personMaria.getFullname());
+  console.log(personAlex.getFullname());
 
-/*
-Mostre no console as idades de cada pessoa, com a frase:
-- "[NOME COMPLETO] tem [IDADE] anos."
-*/
-console.log( '\nIdade das pessoas:' );
-// ?
+  /*
+  Mostre no console as idades de cada pessoa, com a frase:
+  - "[NOME COMPLETO] tem [IDADE] anos."
+  */
+  console.log('\nIdade das pessoas:');
+  console.log(personPaulo.getFullname() + ' tem ' + personPaulo.getAge() + ' anos');
+  console.log(personMaria.getFullname() + ' tem ' + personMaria.getAge() + ' anos');
+  console.log(personAlex.getFullname() + ' tem ' + personAlex.getAge() + ' anos');
 
-/*
-Adicione alguns anos à cada pessoa, e mostre no console a nova idade de
-cada um. A frase deverá ser no formato:
-- "[NOME COMPLETO] agora tem [NOVA IDADE] anos."
-*/
-console.log( '\nNova idade das pessoas:' );
-// ?
+  /*
+  Adicione alguns anos à cada pessoa, e mostre no console a nova idade de
+  cada um. A frase deverá ser no formato:
+  - "[NOME COMPLETO] agora tem [NOVA IDADE] anos."
+  */
+  console.log('\nNova idade das pessoas:');
+  console.log(personPaulo.getFullname() + ' agora tem ' + personPaulo.addAge(10).getAge() + ' anos');
+  console.log(personMaria.getFullname() + ' agora tem ' + personMaria.addAge(2).getAge() + ' anos');
+  console.log(personAlex.getFullname() + ' agora tem ' + personAlex.addAge(7).getAge() + ' anos');
+
+})();
--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -1,3 +1,6 @@
+(function(){
+    'use strict'
+
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -14,7 +17,11 @@ Ex: no caso do nome ser "Fernando", deve mostrar as frases:
 E assim por diante, até a última.
 */
 console.log( 'As letras do seu nome:' );
-// ?
+
+var name = 'Paulo';
+for(var i = 0, len = name.length; i < len; i++){
+    console.log(name.charAt(i) + ' é a ' + (i + 1) + 'ª letra do meu nome.');
+}
 
 /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
@@ -29,7 +36,15 @@ curso para fazer isso funcionar corretamente :)
 console.log para cada formato.
 */
 console.log( '\nNome convertido à partir de um slug:' );
-// ?
+
+var fullName = 'paulo-victor-da-silva-machado';
+var newFullName = fullName.split('-').map(function(name){
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}).join(' ');
+
+console.log(fullName);
+console.log(newFullName);
+ 
 
 /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
@@ -41,21 +56,27 @@ O resultado final deve ficar mais ou menos assim:
 5 nomes foi somente uma sugestão ;)
 */
 console.log( '\nMeus amigos:' );
-// ?
+
+var friendes = ['Paulo', 'Marcos', 'Ana', 'Pedro', 'Maria'];
+var phrase = friendes.reduce(function(acumulado, atual, index){
+    var separator = friendes.length -1 === index ? ' e ' : ', ';
+    return acumulado + separator + atual;
+}).concat(' são meus amigos.');
+console.log(phrase);
 
 /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
 Mostre o resultado no console.
 */
 console.log( '\nEra "Roberto", agora é:' );
-// ?
+console.log('Roberto'.replace('to', 'ta'));
 
 /*
 Mostre no console a parte "nando" da string "Fernando". Use o método que
 faz a busca do final para o início da string.
 */
 console.log( '\nParte de uma string:' );
-// ?
+console.log('Fernado'.substring(8,3));
 
 /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
@@ -67,4 +88,11 @@ de qualquer tamanho, escrito de qualquer forma.
 Ex.: Nomes que deveriam funcionar: "Fernando", "RoBertO", "gabriEla", etc.
 */
 console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
-// ?
+var myName = 'Paulo';
+var myNewName = [];
+for(var i = 0, len = myName.length; i < len; i++){
+    myNewName.push(i % 2 === 0 ? myName[i].toLowerCase() : myName[i].toUpperCase());
+}
+console.log(myNewName.join(''));
+
+})();
