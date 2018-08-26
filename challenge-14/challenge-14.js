67e4f16639ba58c9fbc4c2528f2742bd0d236885
Davi Sousa <davi.sousa10@gmail.com>
2018-08-26 14:26:09 -0300

    desafio finalizado

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -104,7 +104,13 @@
   o que acontece ;)
   */
   console.log('\nExiste um { number: 2 } em numberObjects?');
-
+  if (numberObjects.indexOf({
+      number: 2
+    }) > -1) {
+    console.log("Existe um objeto { number: 2 } em numberObjects!");
+  } else {
+    console.log("Não existe um bjeto { number: 2 } em numberObjects :(");
+  }
 
   /*
   Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
@@ -112,13 +118,20 @@
   */
   console.log(
     '\nE buscando a partir do último índice, o { number: 2 } existe?');
-  // ?
-
+  if (numberObjects.lastIndexOf({
+      number: 2
+    }, 2) > -1) {
+    console.log("Existe um objeto { number: 2 } em numberObjects!");
+  } else {
+    console.log("Não existe um bjeto { number: 2 } em numberObjects :(");
+  }
   /*
   Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
   formato de String.
   */
   console.log(
     '\njustMod2Or3 é um array? Se for, a representação dele em String é:');
-  // ?
+  if (Array.isArray(justMod2Or3)) {
+    console.log(justMod2Or3.toString());
+  }
 })()
