7074879734a89a9bcd106a012572847fb9f94d37
Yasminn Vaz <yasminnvaz@gmail.com>
2018-08-28 23:47:59 -0300

    Challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -98,18 +98,21 @@ Ainda com a função acima, imprima a quantidade de páginas de um livro qualque
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-console.log("O livro ABC tem "+ book('ABC').quantidadePaginas + " páginas!");
+var name = 'ABC'
+console.log("O livro " + name + " tem "+ book(name).quantidadePaginas + " páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-console.log("O autor do livro 123 é " + book('123').autor + "."); 
+var name = '123'
+console.log("O autor do livro " + name + " é " + book(name).autor + "."); 
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-console.log("O livro ABC123 foi publicado pela editora " + book('ABC123').editora +".");
+var name = 'ABC123'
+console.log("O livro " + name + " foi publicado pela editora " + book(name).editora +".");
