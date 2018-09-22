35a27afa812bed137402341f9f9535cc76473d49
Rafaela Diniz <rafaela.diniz@hotmail.com>
2018-09-22 19:36:15 -0300

    challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,20 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = ['supernatural', true, 1967, {creatures: 'wendigo'}, function() {} ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+var myFunction = function myFunction(arr) {
+	return arr;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunction(myarray)[1]);
+// true
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +23,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function func2(arr, index) {
+	return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myarray2 = ['parks and rec', true, 100, 'friends', 'mr. robot']
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(myfunc2(myarray2, 0));
+console.log(myfunc2(myarray2, 1));
+console.log(myfunc2(myarray2, 2));
+console.log(myfunc2(myarray2, 3));
+console.log(myfunc2(myarray2, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +56,55 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName) {
+	var allBooks = {
+		'O Rei do Inverno': {
+			qtdPaginas: '546',
+			autor: 'Bernard Cornwell',
+			editora: 'Record'
+		},
+		'O Inimigo de Deus': {
+			qtdPaginas: '518',
+			autor: 'Bernard Cornwell',
+			editora: 'Record'
+		},
+		'Excalibur': {
+			qtdPaginas: '532',
+			autor: 'Bernard Cornwell',
+			editora: 'Record'
+		}
+	};
+	
+	return !bookName ? allBooks : allBooks[bookName];
+}
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var bookName = 'Excalibur';
+console.log('O livro ' + bookName + ' tem ' + book ( bookName).qtdPaginas + ' páginas! ' );
+O livro Excalibur tem 532 páginas!
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro ' + bookName + ' é ' + book(bookName).autor + '.' );
+O autor do livro Excalibur é Bernard Cornwell.
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro ' + bookName + ' foi publicado pela editora ' + book(bookName).editora + '.' );
+// O livro Excalibur foi publicado pela editora Record.
