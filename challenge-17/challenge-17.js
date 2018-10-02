e0fdaf277edb33a7a45a51806a587a945e9ade45
Diana Dias <dianadiasds@gmail.com>
2018-10-02 16:13:00 -0300

    Update challenge-17.js

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -10,7 +10,8 @@ regulares! Para isso, iremos usar o texto abaixo. Coloque-o em uma
 variável chamada `text`:
 "Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro."
 */
-// ?
+var text = 'Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro.';
+
 
 /*
 Vamos começar com umas brincadeiras fáceis :D
@@ -18,43 +19,47 @@ Troque o nome "Manuel Marques de Sousa" pelo seu nome, e mostre o resultado
 no console:
 */
 console.log( 'Adicionando seu nome no texto:' );
-// ?
-
+console.log(text.replace(/Manuel Marques de Sousa/g , 'Diana Dias'));
 /*
 Agora, substitua a palavra "brasileiro" por sua cidade natal e mostre no
 console.
 Ex: Se você for da São Paulo, substitua por "paulista".
 */
 console.log( '\nTrocando naturalidade:' );
-// ?
+
+console.log(text.replace(/brasileiro/g , 'paulista'));
 
 /*
 Substitua todos os números por um traço `-`. Cada caractere de número deve
 ser um traço. Mostre o resultado no console:
 */
 console.log( '\nTrocando números por -:' );
-// ?
+
+console.log(text.replace(/\d/g , '-'));
 
 /*
 Substitua todas as letras (somente letras) de "D" maiúsculo até "h"
 minúsculo por "0" (número zero). Mostre o resultado no console:
 */
 console.log( '\nTrocando de "D" a "h" por "0":' );
-// ?
+console.log(text.replace(/[D-Hd-h]/g , '0'));
 
 /*
 Substitua todos os "A" (maiúsculos ou minúsculos) por "4".
 Mostre o resultado no console:
 */
 console.log( '\nTrocando "A" e "a" por "4":' );
-// ?
+console.log(text.replace(/a/gi , '4'));
+
 
 /*
 Substitua a frase "O Centauro de Luvas", deixando-a em caixa alta, usando
 o método `toUpperCase()`. Mostre o resultado no console:
 */
 console.log( '\n"O Centauro de Luvas" em caixa alta:' );
-// ?
+console.log(text.replace(/O Centauro de Luvas/g, function (item){
+  return item.toUpperCase();
+}));
 
 /*
 Agora iremos substituir as datas no formato "13 de junho de 1804" para
@@ -69,8 +74,26 @@ Use um console.log para cada mês, usando a frase:
 "O mês de [NOME DO MÊS] é representado pelo número [NÚMERO DO MÊS]."
 */
 console.log( '\nMeses representados por números:' );
-// ?
-
+function getMonthNumber (nameMonth) {
+    var month = {
+    janeiro: '01',
+    fevereiro: '02',
+    'março': '03',
+    abril: '04',
+    maio: '05',
+    junho: '06',
+    julho: '07',
+    agosto: '08',
+    setembro: '09',
+    outubro: '10',
+    novembro: '11',
+    dezembro: '12'
+    };
+  return month[nameMonth];
+}
+console.log('O mês de março é representado pelo número ' +  getMonthNumber('março'));
+console.log('O mês de setembro é representado pelo número ' +  getMonthNumber('setembro'));
+console.log('O mês de dezembro é representado pelo número ' +  getMonthNumber('dezembro'));
 /*
 Agora, declare uma variável chamada `regexDate` que irá receber a expressão
 regular que irá fazer o match com as datas. Crie grupos de captura para o
@@ -80,8 +103,9 @@ Com o que vimos até agora, você consegue fazer :D
 Mostre a regex no console.
 */
 console.log( '\nRegex que vai fazer o match com as datas do texto:' );
-// ?
 
+var regexDate = /(\d\d) de (junho|julho) de (\d\d\d\d)/g;
+console.log(regexDate);
 /*
 Agora crie a função que irá fazer o replace dos dados. A função será chamada
 de `replaceDate`. Ela deve retornar a data no formato:
@@ -90,4 +114,7 @@ Após criar a função, faça o replace das datas no texto, mostrando no
 console o resultado.
 */
 console.log( '\nReplace de datas:' );
-// ?
+function replaceDate (regex, day, month, year) {
+  return day + '/' + getMonthNumber(month) + '/' + year
+}
+console.log(text.replace(regexDate , replaceDate));
