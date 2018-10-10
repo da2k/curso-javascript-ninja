e371d87781e9f83e8189edc897d8101e2e010f41
Diana Dias <dianadiasds@gmail.com>
2018-10-09 23:00:13 -0300

    Update challenge-28.js

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,4 +1,6 @@
-  /*
+(function (win, doc){
+'use strict';
+/*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
   de submit;
@@ -7,7 +9,6 @@
   preenchidas com os dados da requisição feita no JS.
   - Crie uma área que receberá mensagens com o status da requisição:
   "Carregando, sucesso ou erro."
-
   No JS:
   - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
   deve ser limpo e enviado somente os números para a requisição abaixo;
@@ -25,3 +26,125 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+
+
+
+  function DOM (stg) { 
+    this.element = doc.querySelectorAll(stg);
+  }
+
+  DOM.prototype.on = function on (event, callback) {
+    Array.prototype.forEach.call(this.element, function(element){
+      element.addEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function on (event, callback) {
+    Array.prototype.forEach.call(this.element, function(element){
+      element.removeEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element
+  };
+
+  DOM.prototype.forEach = function forEach () {
+    return Array.prototype.forEach.apply(this.element, arguments)
+  };
+
+  DOM.prototype.map = function map () {
+    return Array.prototype.map.apply(this.element, arguments)
+  };
+  
+  DOM.prototype.filter = function filter () {
+    return Array.prototype.filter.apply(this.element, arguments)
+  };
+  DOM.prototype.reduce = function reduce () {
+    return Array.prototype.reduce.apply(this.element, arguments)
+  };
+
+  DOM.prototype.reduceRight = function reduceRight () {
+    return Array.prototype.reduceRight.apply(this.element, arguments)
+  };
+
+  DOM.prototype.every = function every () {
+    return Array.prototype.every.apply(this.element, arguments)
+  };
+
+  DOM.prototype.some = function some () {
+    return Array.prototype.some.apply(this.element, arguments)
+  };
+
+  DOM.prototype.isArray = function isArray ( obj ) {
+    return Object.prototype.toString.call( obj ) === '[object Array]';
+  }
+
+  DOM.prototype.isObject = function isObject ( obj ) {
+    return Object.prototype.toString.call( obj ) === '[object Object]';
+  }
+
+  DOM.prototype.isFunction = function isFunction ( obj ) {
+    return Object.prototype.toString.call( obj ) === '[object Function]';
+  }
+
+  DOM.prototype.isNumber = function isNumber ( obj ) {
+    return Object.prototype.toString.call( obj ) === '[object Number]';
+  }
+
+  DOM.prototype.isString = function isString (obj) {
+    return Object.prototype.toString.call( obj ) === '[object String]';
+  }
+
+  DOM.prototype.isBoolean = function isBoolean (obj) {
+    return Object.prototype.toString.call( obj ) === '[object Boolean]';
+  }
+
+  DOM.prototype.isNull = function isNull (obj) {
+    return Object.prototype.toString.call( obj ) === '[object Null]' ||
+    Object.prototype.toString.call( obj ) === '[object Undefined]' ;
+  }
+
+  var $form = new DOM('[data-js="form"]');
+  var $input = new DOM('[data-js="input"]');
+  var $buscar = new DOM('[data-js="buscar"]');
+  var $logradouro = new DOM('[data-js="logradouro"]');
+  var $bairro = new DOM('[data-js="bairro"]');
+  var $estado = new DOM('[data-js="estado"]');
+  var $cidade = new DOM('[data-js="cidade"]');
+  var $cep = new DOM('[data-js="cep"]');
+  var ajax = new XMLHttpRequest();
+  var data;
+
+  $form.on('submit', handleForm);
+
+  function handleForm (event){
+    event.preventDefault();
+    var url = 'http://apps.widenet.com.br/busca-cep/api/cep/[CEP].json'.replace(
+      '[CEP]', 
+      $input.get()[0].value
+    );
+    ajax.open('GET' , url);
+    ajax.send();
+    ajax.addEventListener('readystatechange', handleState);
+
+  }
+
+  function handleState (){
+    if(ajax.readyState === 4 && ajax.status === 200) {
+      var data = JSON.parse(ajax.responseText);
+      $logradouro.get()[0].textContent = data.address;
+      $bairro.get()[0].textContent = data.district;
+      $estado.get()[0].textContent = data.state;
+      $cidade.get()[0].textContent = data.city;
+      $cep.get()[0].textContent = data.code;
+      }
+      console.log(ajax.responseText)
+
+  }
+
+
+})(window, document); 
+
+  
