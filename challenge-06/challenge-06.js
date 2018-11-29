6f78599ab412dd4e4d9d79cee4677c33fb89b6ca
Filippo <barcellos.filippo@gmail.com>
2018-11-29 13:29:41 +0100

    challenge-06 done

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,9 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+
+var championship = 'Campeonato Carioca';
+console.log(`O nome do campeonato é ${championship}`);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,8 +14,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
-
+var teams = ['Botafogo', 'Vasco', 'Fluminense', 'Flamengo', 'Niteroi'];
 console.log( 'Times que estão participando do campeonato:', teams );
 
 /*
@@ -32,7 +33,12 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition( p ) {
+  if(p > 4) {
+    console.log(`Não temos a informação do time que está nessa posição.`);
+  }
+console.log(`O time que esta em ${p} lugar é o ${teams[(p+1)]}`);
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
@@ -44,7 +50,12 @@ função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+
+var num = 20;
+while(num <= 30) {
+  console.log(num);
+  num++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
