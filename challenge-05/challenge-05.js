21fea6ffc9fe3a18d20f4e6de74aeae2a56f3bf2
Daniel Bastos <danielbastos.17@hotmail.com>
2018-12-03 13:47:41 -0200

    Challenge 05 done.

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [1, 1, 2, 3, 5]; 
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function foo(arr) {
+    return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+foo(arr)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function fun(arr, num) {
+    return arr[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arr2 = ["string", 1, null, undefined, NaN];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+fun(arr2, 0);
+fun(arr2, 1);
+fun(arr2, 2);
+fun(arr2, 3);
+fun(arr2, 4);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,49 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName) {
+    var books = {
+        'Use a Cabeça! Java': {
+            quantidadePaginas: 562,
+            autor: "Paul Barry",
+            editora: "Alan"
+        }, 
+        'O Poder dos Quietos': {
+            quantidadePaginas: 382,
+            autor: "Susan Cain",
+            editora: "Saraiva"    
+        }, 
+        'Linux a Bíblia': {
+            quantidadePaginas: 483,
+            autor: "Christopher Negus",
+            editora: "Inova"        
+        }
+    };
+    return bookName !== undefined ? books[bookName] : books;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+"O livro Linux a Bíblia tem " + book('Linux a Bíblia').quantidadePaginas + " páginas!";
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+"O livro Use a Cabeça! Java é " + book('Use a Cabeça! Java').autor + ".";
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+"O livro O Poder dos Quietos foi publicado pela editora " + book('Poder dos Quietos').editora + ".";
