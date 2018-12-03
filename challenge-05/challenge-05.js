035c81337d59c1a704bcf86af310d24e99cde878
Antonio Marcos <antonio.malves.lima@gmail.com>
2018-12-03 15:50:50 -0200

    resolucao do desafio 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [true, [1, 2, 3], "teste", 10, function(){}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction(arg){
+    return arg;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunction(arr)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function functionPositionArray(arr, pos){
+    return arr[pos-1];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myArray = [{a:1}, [2, 4], true, "teste", 1.5 ];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(functionPositionArray(myArray, 1))
+console.log(functionPositionArray(myArray, 2))
+console.log(functionPositionArray(myArray, 3))
+console.log(functionPositionArray(myArray, 4))
+console.log(functionPositionArray(myArray, 5))
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,49 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nomeLivro){
+    var livros = {
+        'Clean Code':{
+            quantidadePaginas: 423,
+            autor: 'Robert C. Martin',
+            editora: 'Alta Books 1'
+        },
+        'Domain Drive Design':{
+            quantidadePaginas: 497,
+            autor: 'Eric Evans',
+            editora: 'Alta Books 2'
+        },
+        'Spring em ação':{
+            quantidadePaginas: 561,
+            autor: 'Craig Walls',
+            editora: 'Alta books 3'
+        }
+    }
+    return nomeLivro ? livros[nomeLivro] : livros;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Clean Code tem ' + book('Clean Code').quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro Domain Drive Design é ' + book('Domain Drive Design').autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro Spring foi publicado pela editora ' + book('Spring em ação').editora + '.');
