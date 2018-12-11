1faea24d42e42b82e1779991e5142254bd89828a
Oliveira <210063966@LOGON.DS.GE.COM>
2018-12-11 02:11:29 -0200

    teste

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// ??
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
@@ -12,7 +12,7 @@ Crie uma função que receba um array como parâmetro, e retorne esse array.
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+// ? 
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
