7277a72427166f3eff7c40e4ea83c4d102d56af0
thales <koben_rock@hotmail.com>
2018-12-20 10:14:41 -0200

    challenge-29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,6 @@
 (function() {
+  (function(win, doc, $) {
+
   'use strict';
 
   /*
@@ -35,5 +37,70 @@
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
+
+    var app = (function() {
+      return {
+        init: function() {
+          this.loadCompany();
+          this.initEvents();
+        },
+        initEvents: function() {
+          $('[data-js="formSubmit"]').on('submit', this.handleSubmit);
+        },
+        handleSubmit: function(e) {
+          e.preventDefault();
+          var $tableCarros = $('[data-js="tableCarros"]').get();
+          $tableCarros.appendChild(app.createNewCar());
+        },
+        createNewCar: function() {
+          var $fragment = doc.createDocumentFragment();
+          var $tr = doc.createElement('tr');
+          var $tdImg = document.createElement('td');
+          var $image = document.createElement('img');
+          var $tdMarcaModelo = document.createElement('td');
+          var $tdAno = document.createElement('td');
+          var $tdPlaca = document.createElement('td');
+          var $tdCor = document.createElement('td');
+
+          $image.src = $('[data-js="formUrl"]').get().value;
+          $tdImg.appendChild($image);
+
+          $tdMarcaModelo.textContent = $('[data-js="formMarcaModelo"]').get().value;
+          $tdAno.textContent = $('[data-js="formAno"]').get().value;
+          $tdPlaca.textContent = $('[data-js="formPlaca"]').get().value;
+          $tdCor.textContent = $('[data-js="formCor"]').get().value;
+
+          $tr.appendChild($tdImg);
+          $tr.appendChild($tdMarcaModelo);
+          $tr.appendChild($tdAno);
+          $tr.appendChild($tdPlaca);
+          $tr.appendChild($tdCor);
+
+          return $fragment.appendChild($tr);
+        },
+        loadCompany: function() {
+          var ajax = new XMLHttpRequest();
+          ajax.open('GET', './company.json', true);
+          ajax.send();
+          ajax.addEventListener('readystatechange', this.getCompanyInfo, false)
+        },
+        getCompanyInfo: function() {
+          if (app.isReady.call(this)) {
+            var companyData = JSON.parse(this.responseText);
+            var $companyNome = $('[data-js="companyNome"]').get();
+            var $companyTelefone = $('[data-js="companyTelefone"]').get();
+            $companyNome.textContent = companyData.name;
+            $companyTelefone.textContent = companyData.phone;
+          }
+        },
+        isReady: function() {
+          return this.status === 200 && this.readyState === 4;
+        }
+      }
+    })();
+
+    app.init();
+    })(window, document, window.DOM);
+
 
 })();
---
+++ challenge-29/dom.js
@@ -0,0 +1,50 @@
+
+  (function(win, doc) {
+    'use strict';
+
+    function DOM(nodes) {
+      if (!(this instanceof DOM)) {
+        return new DOM(nodes);
+      }
+
+      this.element = doc.querySelectorAll(nodes);
+      this.on = function (event, callback) {
+        Array.prototype.forEach.call(this.element, function (el) {
+          el.addEventListener(event, callback, false);
+        });
+      };
+      this.off = function (event, callback) {
+        Array.prototype.forEach.call(this.element, function (el) {
+          el.removeEventListener(event, callback, false);
+        });
+      };
+      this.get = function (index) {
+        if (!index)
+          return this.element[0];
+        return this.element[index];
+      };
+      this.forEach = function () {
+        return Array.prototype.forEach.apply(this.element, arguments);
+      };
+      this.map = function () {
+        return Array.prototype.map.apply(this.element, arguments);
+      };
+      this.filter = function () {
+        return Array.prototype.filter.apply(this.element, arguments);
+      };
+      this.reduce = function () {
+        return Array.prototype.reduce.apply(this.element, arguments);
+      };
+      this.reduceRight = function () {
+        return Array.prototype.reduceRight.apply(this.element, arguments);
+      };
+      this.every = function () {
+        return Array.prototype.every.apply(this.element, arguments);
+      };
+      this.some = function () {
+        return Array.prototype.some.apply(this.element, arguments);
+      };
+    }
+
+    win.DOM = DOM;
+  })(window, document);
