5fad3394257807ca26bf80145a61cb4792358eb7
unknown <worldrulesdev@gmail.com>
2019-01-17 13:54:14 -0200

    teste 16

--- index/main.js
+++ index/main.js
@@ -1,7 +1,6 @@
-(function () {  
-    'use strict';    
-    var name = 'Leonardo Carvalho'
-    console.log(name);   
-
-})();
-    
+console.log( 'As letras do seu nome:' );
+// ?
+var name = 'Leonardo';
+for (var i = 0, len = name.length; i < len; i++) {
+    console.log(name.charAt(0) + ' é a ' + (i+1) + 'ª letra do meu nome.');
+}
