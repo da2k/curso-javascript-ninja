bf1bff6400ba1057d890ecdc5ffd5dffe049c218
Helio Alves <heleralvess@gmail.com>
2019-01-21 06:48:01 -0200

    explicacoes da aula 02

---
+++ challenge-02/aula002.js
@@ -0,0 +1,50 @@
+// Operadores Logicos
+// && (and)
+
+var x = 3;
+var y = 7;
+
+x === 3; //true
+y === 5; //false
+
+x === 3 && y === 7; //true (ambos são verdadeiros)
+x === 3 && y === 5; //false (um deles é falso)
+
+// || (or)
+
+x === 2 || y === 7; //true (pelo menos um é verdadeiro)
+
+// ! (inverte o valor)
+
+! x === 3; //false (ele testa se o x não é 3, retorna false porque é verdadeiro)
+// mas utilizamos teste através do !==
+
+// Operadores Unários
+// + converte string em numero 
+// concatenar strings
+
+var nome = 'fernando';
+var sobrenome = 'daciuk';
+
+var nomeCompleto = nome + ' ' + sobrenome;
+var nomeCompleto1 = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
+console.log(nomeCompleto1);
+
+// estrutura lexica: conjunto de regras que definem a escrita da linguagem
+// case sensitive: diferencia letras maiusculas de minusculas
+var animal = 'cachorro';
+var Animal = 'coelho'; // animal !== de Animal
+
+console.log(animal);
+console.log(Animal);
+
+// identificadores: nomear variavel e funcao
+// podem inciar com _ ou $, letras de a a z, letras de A a Z
+// podem conter numeros, mas nao inciar
+// podem conter qualquer caractere Unicode, mas nao se deve utilizar, para facilitar manutencao
+
+// palavras reservadas: nao se pode utilizar de jeito nenhum
+
+
