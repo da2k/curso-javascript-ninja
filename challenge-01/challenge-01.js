15e7da2cd608d01e12fa46d12ba169961f55ae21
Thaylla <thayllachristineo@gmail.com>
2019-02-01 11:38:02 -0200

    Challenge 01 : done

---
+++ challenge-01/challenge-01.js
@@ -0,0 +1,31 @@
+let myVar
+
+myVar = 10
+
+let soma = 15 + 8
+
+soma += 1
+
+soma *= 3
+
+//soma = 72
+
+let souNinja = true 
+
+let comida = ['arroz','feijao','ovo']
+
+comida[1]
+
+if (myVar === soma) {
+    true
+}
+
+if (myvar <= soma) {
+    true
+}
+
+function divisao (n1, n2) {
+    return n1/n2
+}
+
+divisao(10,2) //5
