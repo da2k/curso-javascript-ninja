8b646b1920009ea0e7f09241963f3af067f4c4a2
Gugaah <srt.gugah@gmail.com>
2019-02-14 21:38:24 -0200

    Challenge 23

---
+++ challenge-23/js/challenge-23.js
@@ -0,0 +1,96 @@
+(function(window, document) {
+   'use strict';
+
+   /*
+   Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+   As regras são:
+
+   - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+   diretamente;
+   - O input deve iniciar com valor zero;
+   - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+   - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+   multiplicação(x) e divisão(÷);
+   - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+   que irá limpar o input, deixando-o com valor 0;
+
+   - A cada número pressionado, o input deve atualizar concatenando cada valor
+   digitado, como em uma calculadora real;
+   - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+   operação no input. Se o último caractere no input já for um símbolo de alguma
+   operação, esse caractere deve ser substituído pelo último pressionado.
+   Exemplo:
+   - Se o input tem os valores: "1+2+", e for pressionado o botão de
+   multiplicação (x), então no input deve aparecer "1+2x".
+   - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+   input;
+   - Ao pressionar o botão "CE", o input deve ficar zerado.
+   */
+
+   var $inputVisor = document.querySelector('[data-js="visor"]');
+   var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
+   var $buttonOperations = document.querySelectorAll('[data-js="button-operation"]');
+   var $buttonCE = document.querySelector('[data-js="button-ce"]');
+   var $buttonEqual = document.querySelector('[data-js="button-equal"]');
+
+   Array.prototype.forEach.call( $buttonsNumbers, function(button) {
+      button.addEventListener( 'click', clickButtonNumbers, false );
+   });
+   Array.prototype.forEach.call( $buttonOperations, function(button) {
+      button.addEventListener( 'click', clickButtonOperations, false );
+   });
+
+   $buttonCE.addEventListener('click', clickButtonCE, false);
+   $buttonEqual.addEventListener( 'click', clickButtonEqual, false );
+
+   function clickButtonNumbers() {
+      $inputVisor.value += this.value;
+   }
+
+   function clickButtonOperations() {
+      $inputVisor.value = removeLastItemIfItIsAnOperator( $inputVisor.value );
+      $inputVisor.value += this.value;
+   }
+
+   function clickButtonCE() {
+      $inputVisor.value = 0;
+   }
+
+   function clickButtonEqual() {
+      $inputVisor.value = removeLastItemIfItIsAnOperator( $inputVisor.value );
+      var allValues = $inputVisor.value.match( /\d+[x÷+-]?/g );
+      var result = allValues.reduce(function(lastItem, actualItem) {
+         var firstValue = lastItem.slice( 0, -1 );
+         var operator = lastItem.split( '' ).pop();
+         var lastValue = removeLastItemIfItIsAnOperator( actualItem );
+         var lastOperator = isLastItemAnOperator(actualItem) ? actualItem.split( '' ).pop() : '';
+         switch( operator ) {
+            case '÷':
+               return ( Number (firstValue) / Number (lastValue) ) + lastOperator;
+            case 'x':
+               return ( Number (firstValue) * Number (lastValue) ) + lastOperator;
+            case '-':
+               return ( Number (firstValue) - Number (lastValue) ) + lastOperator;
+            case '+':
+               return ( Number (firstValue) + Number (lastValue) ) + lastOperator;
+         }
+      });
+      $inputVisor.value = result;
+   }
+
+   function removeLastItemIfItIsAnOperator(number) {
+      if (isLastItemAnOperator( number )) {
+         return number.slice( 0, -1 );
+      }
+      return number;
+   }
+
+   function isLastItemAnOperator(number) {
+      var operations = [ '+', '-', 'x', '÷' ];
+      var lastItem = number.split( '' ).pop();
+      return operations.some(function(operator) {
+         return operator === lastItem;
+      })
+   }
+
+})(window, document);
