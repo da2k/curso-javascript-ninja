8419ad235196fdfc448ee560fcbf8d2fcd9fa2e9
Getúlio Magela Silva <getuliomagela@outlook.com>
2019-03-02 03:07:23 -0300

    Refactored Challenge-23 so that it is similar to version in the exercise correction.

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -26,146 +26,81 @@
   - Ao pressionar o botão "CE", o input deve ficar zerado.
   */
   var $display = document.querySelector('[data-js=display]');
+  var $numericButtons = document.querySelectorAll('[data-js=button-number]');
+  var $operationsButtons = document.querySelectorAll('[data-js=button-operation]');
+  var $buttonEquals = document.querySelector('[data-js=button-equals');
+  var $buttonCE = document.querySelector('[data-js=button-ce]')
 
-  console.log($buttons);
+  Array.prototype.forEach.call($numericButtons, function (btn) {
+    btn.addEventListener('click', handleClickNumber, false);
+  });
+  Array.prototype.forEach.call($operationsButtons, function (btn) {
+    btn.addEventListener('click', handleClickOperation, false);
+  });
 
-  var $buttons = {
-    zero: document.querySelector('[data-js=zeroBtn]'),
-    one: document.querySelector('[data-js=oneBtn]'),
-    two: document.querySelector('[data-js=twoBtn]'),
-    three: document.querySelector('[data-js=threeBtn]'),
-    four: document.querySelector('[data-js=fourBtn]'),
-    five: document.querySelector('[data-js=fiveBtn]'),
-    six: document.querySelector('[data-js=sixBtn]'),
-    seven: document.querySelector('[data-js=sevenBtn]'),
-    eight: document.querySelector('[data-js=eightBtn]'),
-    nine: document.querySelector('[data-js=nineBtn]'),
-    plus: document.querySelector('[data-js=plusBtn]'),
-    minus: document.querySelector('[data-js=minusBtn]'),
-    times: document.querySelector('[data-js=timesBtn]'),
-    div: document.querySelector('[data-js=divBtn]'),
-    equals: document.querySelector('[data-js=equalsBtn'),
-    clear: document.querySelector('[data-js=clearBtn]')
-  };
+  $buttonEquals.addEventListener('click', handleClickEquals, false);
 
-  addButtonsEventListeners();
+  $buttonCE.addEventListener('click', handleClickCE, false);
 
-  function isFirstOperation() {
-    return $display.value.length === 1 && $display.value === '0';
+
+
+
+  function handleClickNumber() {
+    removeLeadingZero();
+    $display.value += this.value;
   }
 
-  function eraseDisplay() {
-    $display.value = '';
+  function handleClickOperation() {
+    $display.value = removeLastItemIfItIsAnOperator($display.value);
+    $display.value += this.value;
   }
 
-  function addButtonsEventListeners() {
-    Object.keys($buttons).forEach(function (btn) {
-      $buttons[btn].addEventListener('click', function () {
-        var signalsInEndRegex = /[+\-x\/]$/;
-        if (isFirstOperation())
-          eraseDisplay();
-
-        switch (btn) {
-          case 'zero':
-            $display.value += '0';
-            break;
-          case 'one':
-            $display.value += '1';
-            break;
-          case 'two':
-            $display.value += '2';
-            break;
-          case 'three':
-            $display.value += '3';
-            break;
-          case 'four':
-            $display.value += '4';
-            break;
-          case 'five':
-            $display.value += '5';
-            break;
-          case 'six':
-            $display.value += '6';
-            break;
-          case 'seven':
-            $display.value += '7';
-            break;
-          case 'eight':
-            $display.value += '8';
-            break;
-          case 'nine':
-            $display.value += '9';
-            break;
-
-          case 'plus':
-            if (signalsInEndRegex.test($display.value))
-              $display.value = $display.value.replace(signalsInEndRegex, '')
-            $display.value += '+';
-            break;
-
-          case 'minus':
-            if (signalsInEndRegex.test($display.value))
-              $display.value = $display.value.replace(signalsInEndRegex, '')
-            $display.value += '-';
-            break;
-
-          case 'times':
-            if (signalsInEndRegex.test($display.value))
-              $display.value = $display.value.replace(signalsInEndRegex, '')
-            $display.value += 'x';
-            break;
-
-          case 'div':
-            if (signalsInEndRegex.test($display.value))
-              $display.value = $display.value.replace(signalsInEndRegex, '')
-            $display.value += '/';
-            break;
-
-          case 'equals':
-            $display.value = calculator($display.value.replace(signalsInEndRegex, ''));
-            break;
-
-          case 'clear':
-            $display.value = '0';
-            break;
-        }
-      }, false);
+  function handleClickEquals() {
+    $display.value = removeLastItemIfItIsAnOperator($display.value);
+    var expression = $display.value.match(/\d+[+\-x÷]?/g);
+    $display.value = expression.reduce(function (acc, current) {
+      var firstValue = acc.slice(0, -1);
+      var operator = acc.split('').pop();
+      var secondValue = removeLastItemIfItIsAnOperator(current);
+      var lastOperator = isLastItemAnOperation(current) ? current.split('').pop() : '';
+      switch(operator) {
+        case '+':
+        return (Number(firstValue) + Number(secondValue)) + lastOperator;
+        case '-':
+        return (Number(firstValue) - Number(secondValue)) + lastOperator;
+        case 'x':
+        return (Number(firstValue) * Number(secondValue)) + lastOperator;
+        case '÷':
+        return (Number(firstValue) / Number(secondValue)) + lastOperator;
+      }
     });
   }
 
-  function calculator(expression) {
-    // var expressionRegex = /[+\-x\/]/;
-    var expressionRegex = /\d+[+\-x\/]\d+/;
-    var timesDivRegex = /(\d+)([x\/])(\d+)/;
-    var plusMinusRegex = /(\d+)([+\-])(\d+)/;
+  function handleClickCE() {
+    $display.value = 0;
+  }
 
-    var operations = {
-      '+': function (x, y) {
-        return x + y;
-      },
-      '-': function (x, y) {
-        return x - y;
-      },
-      'x': function (x, y) {
-        return x * y;
-      },
-      '/': function (x, y) {
-        return y === 0 ? 'Divisão Por Zero' : x / y;
-      }
-    };
+  function removeLastItemIfItIsAnOperator(number) {
+    if (isLastItemAnOperation(number))
+      return number.slice(0, -1);
+    return number;
+  }
 
-    while (expressionRegex.test(expression)) {
-      if (timesDivRegex.test(expression))
-        expression = expression.replace(timesDivRegex, calculate);
-      else
-        expression = expression.replace(plusMinusRegex, calculate);
-    }
+  function isLastItemAnOperation(number) {
+    var operations = ['+', '-', 'x', '÷'];
+    var lastItem = number.split('').pop();
+    return operations.some(function (operator) {
+      return operator === lastItem;
+    });
+  }
 
-    function calculate(full, x, op, y) {
-      return operations[op](+x, +y);
-    }
+  function removeLeadingZero() {
+    if (isLeadingZero())
+      $display.value = '';
+  }
 
-    return expression;
+  function isLeadingZero() {
+    return $display.value === '0';
   }
 
 })(window, document);
