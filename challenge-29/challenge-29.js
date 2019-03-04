176623d4f4ba44bc06fe49bbfcf491de64ccd316
Hallan Christian <hallanchristian@MacBook-Pro-de-Hallan.local>
2019-03-04 13:48:39 -0300

    challenge-29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function(win, doc, $) {
   'use strict';
 
   /*
@@ -35,5 +35,72 @@
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
+  var app = (function controllerApp() {
+    return {
+      init: function init() {
+        this.companyInfo();
+        this.initEvents();
+      },
 
-})();
+      initEvents: function initEvents() {
+        $('[data-js="form-register"]').on('submit', this.handleSubmit);
+      },
+
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
+        console.log('submit');
+        var $tableCar = $('[data-js="table-car"]').get();
+        $tableCar.appendChild(app.createNewCar());
+      },
+
+      createNewCar: function createNewCar() {
+        var $fragment = document.createDocumentFragment();
+        var $tr = document.createElement('tr');
+        var $tdImage = document.createElement('td');
+        var $image = document.createElement('img');
+        var $tdBrand = document.createElement('td');
+        var $tdYear = document.createElement('td');
+        var $tdPlate = document.createElement('td');
+        var $tdColor = document.createElement('td');
+
+        $image.src = $('[data-js="image"]').get().value;
+        $tdImage.appendChild($image);
+
+        //$tdImage.textContent = $('[data-js="image"]').get().value;
+        //$tdImage.textContent = $('[data-js="image"]').get().value;
+        $tdBrand.textContent = $('[data-js="brand-model"]').get().value;
+        $tdYear.textContent = $('[data-js="year"]').get().value;
+        $tdPlate.textContent = $('[data-js="plate"]').get().value;
+        $tdColor.textContent = $('[data-js="color"]').get().value;
+
+        $tr.appendChild($tdImage);
+        $tr.appendChild($tdBrand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdPlate);
+        $tr.appendChild($tdColor);
+
+        return $fragment.appendChild($tr);
+      },
+
+      companyInfo: function companyInfo() {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', './company.json');
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getCampanyInfo, false);
+      },
+      getCampanyInfo: function getCampanyInfo() {
+        if (!app.isReady.call(this)) return;
+        var data = JSON.parse(this.responseText);
+        var $companyName = $('[data-js="company-name"]').get();
+        var $companyPhone = $('[data-js="company-phone"]').get();
+        $companyName.textContent = data.name;
+        $companyPhone.textContent = ' ' + data.phone;
+      },
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      }
+    };
+  })();
+
+  app.init();
+})(window, document, window.DOM);
