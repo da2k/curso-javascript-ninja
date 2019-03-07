c192d3215d6abb4bf64cce022758d38db97ae306
Getúlio Magela Silva <getuliomagela@outlook.com>
2019-03-06 23:31:27 -0300

    Realizado Challenge 29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   /*
@@ -36,4 +36,89 @@
   que será nomeado de "app".
   */
 
+  var app = function () {
+    var xhr = new XMLHttpRequest();
+
+    var $companyName = new DOM('[data-js="company-name"]');
+    var $companyPhone = new DOM('[data-js="company-phone"]');
+
+    var $form = new DOM('[data-js="form-register"]');
+    var $imageUrl = new DOM('[data-js="form-img-url"]');
+    var $brandModel = new DOM('[data-js="form-brand-model"]');
+    var $year = new DOM('[data-js="form-year"]');
+    var $plate = new DOM('[data-js="form-plate"]');
+    var $color = new DOM('[data-js="form-color"]');
+
+    function init() {
+      xhr.addEventListener('readystatechange', setCompanyData, false);
+      $form.get()[0].addEventListener('submit', addDataToTableOnScreen, false);
+      getCompanyData();
+    }
+
+    function showTable() {
+      var $tbody = getTableBody();
+      console.log($tbody)
+      if ($tbody.childElementCount === 0) {
+        var $tableDisplay = new DOM('[data-js="table"]').get()[0];
+        $tableDisplay.setAttribute('style', 'display:initial');
+      }
+    }
+
+    function getTableBody() {
+      return new DOM('[data-js="table"]').get()[0].children[1];
+    }
+
+    function addDataToTableOnScreen(event) {
+      event.preventDefault();
+
+      var $table = new DOM('[data-js="table"]').get()[0].children[1];
+      var row = document.createElement('tr');
+      var thumbnail = document.createElement('td');
+      var brandModel = document.createElement('td');
+      var year = document.createElement('td');
+      var plate = document.createElement('td');
+      var color = document.createElement('td');
+      var img = document.createElement('img');
+
+      $table.appendChild(row);
+      row.appendChild(thumbnail);
+
+      img.setAttribute('src', $imageUrl.get()[0].value);
+      img.setAttribute('width', '180');
+      img.setAttribute('height', '75');
+      thumbnail.appendChild(img);
+
+      brandModel.appendChild(document.createTextNode($brandModel.get()[0].value));
+      year.appendChild(document.createTextNode($year.get()[0].value));
+      plate.appendChild(document.createTextNode($plate.get()[0].value));
+      color.appendChild(document.createTextNode($color.get()[0].value));
+
+      row.appendChild(brandModel);
+      row.appendChild(year);
+      row.appendChild(plate);
+      row.appendChild(color);
+    }
+
+    function getCompanyData() {
+      xhr.open('GET', 'company.json');
+      xhr.send();
+    }
+
+    function setCompanyData() {
+      if (isXHRDataReady()) {
+        var data = JSON.parse(xhr.responseText);
+        $companyName.get()[0].textContent = data.name;
+        $companyPhone.get()[0].textContent = data.phone;
+      }
+    }
+
+    function isXHRDataReady() {
+      return xhr.status === 200 && xhr.readyState === XMLHttpRequest.DONE;
+    }
+
+    return {
+      init: init
+    }
+  }
+  app().init();
 })();
