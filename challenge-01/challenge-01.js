0d9c0e36a7350284a12f3dd2fc2dbc214b786bad
KlausSantos <klausantos82@gmail.com>
2019-06-17 13:56:27 -0300

    challenge-01 Correção

--- challenge-01/challenge-01.js
+++ challenge-01/challenge-01.js
@@ -32,9 +32,9 @@ console.log(soma === myvar);
 console.log(myvar <= soma);
 
 // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-var divisao = function(a, b) {
+function divisao(a, b) {
   return a / b;
-};
+}
 
 // Invoque a função criada acima, passando os parâmetros 10 e 2.
 console.log(divisao(10, 2));
