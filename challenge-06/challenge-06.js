9b02c7140c6dcf7168ff844b5bbccb551b6bbd18
Kaio Silveira <35278957+kaiosilveira89@users.noreply.github.com>
2019-06-21 19:47:15 -0300

    Desafio 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,10 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+
+var championship = "Campeonato Cearense"
+
+console.log(championship)
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,8 +15,8 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
 
+var teams = ["Ceara", "Ferroviario","Fortaleza","Horizonte","Icasa"]
 console.log( 'Times que estão participando do campeonato:', teams );
 
 /*
@@ -32,19 +35,41 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+
+function showTeamPosition(x){
+    teams.includes(teams[x]) ? 
+    console.log( `O time que está em ${x+1}º lugar é o ${teams[x]}.`) :  
+    console.log( `Não temos a informação do time que está nessa posição.`)   
+}
+
+
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
 // ?
+showTeamPosition(0)
+showTeamPosition(1)
+showTeamPosition(2)
+showTeamPosition(3)
+showTeamPosition(5)
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+
+function contador(){
+let x=0
+
+    while (x < 30){
+        x++
+        console.log(x)
+    }
+}
+
+contador()
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -59,6 +84,8 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     "Não temos o equivalente hexadecimal para [COR]."
 */
 // ?
+
+
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
