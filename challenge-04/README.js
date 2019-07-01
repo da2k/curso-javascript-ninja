76739d3fb79aca142ae19c228a23f9afbfd13d13
Igor Fae <igor.sfae@gmail.com>
2019-07-01 00:04:00 -0300

    challenge-06 js finalizado

--- challenge-04/README.js
+++ challenge-04/README.js
@@ -7,15 +7,33 @@ um único parâmetro como argumento. Essa função deve retornar `true` se o
 equivalente booleano para o valor passado no argumento for `true`, ou `false`
 para o contrário.
 */
-?
+var isTruthy = function (argumento) {
+	//return argumento ? true : false;
+	return !!argumento;
+};
 
 // Invoque a função criada acima, passando todos os tipos de valores `falsy`.
-?
-
+console.log(isTruthy(false));
+console.log(isTruthy(undefined));
+console.log(isTruthy(null));
+console.log(isTruthy(NaN));
+console.log(isTruthy(0));
+console.log(isTruthy(-0));
+console.log(isTruthy(''));
+console.log(isTruthy(""));
 /*
 Invoque a função criada acima passando como parâmetro 10 valores `truthy`.
 */
-?
+console.log(isTruthy(1));
+console.log(isTruthy('0'));
+console.log(isTruthy('igor'));
+console.log(isTruthy(true));
+console.log(isTruthy(-1));
+console.log(isTruthy(function () {}));
+console.log(isTruthy({}));
+console.log(isTruthy(1 === 1));
+console.log(isTruthy([]));
+console.log(isTruthy(10 + 30));
 
 /*
 Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
@@ -29,35 +47,56 @@ seguintes propriedades (os valores devem ser do tipo mostrado abaixo):
 - `assentos` - Number - cinco por padrão
 - `quantidadePessoas` - Number - zero por padrão
 */
-?
+var carro = {
+	marca: 'Ford',
+	modelo: 'Fiesta',
+	placa: 'ABC-1234',
+	ano: 2017,
+	cor: 'Preto',
+	quantasPortas: 4,
+	assentos: 5,
+	quantidadePessoas: 0
+};
+console.log(carro);
 
 /*
 Crie um método chamado `mudarCor` que mude a cor do carro conforme a cor
 passado por parâmetro.
 */
-?
+carro.mudarCor = function(novaCor){
+	carro.cor = novaCor
+}
 
 /*
 Crie um método chamado `obterCor`, que retorne a cor do carro.
 */
-?
+carro.obterCor = function(){
+	return carro.cor;
+}
+console.log(carro.obterCor());
 
 /*
 Crie um método chamado `obterModelo` que retorne o modelo do carro.
 */
-?
+carro.obterModelo = function(){
+	return carro.modelo;
+}
 
 /*
 Crie um método chamado `obterMarca` que retorne a marca do carro.
 */
-?
+carro.obterMarca = function(){
+	return carro.marca;
+}
 
 /*
 Crie um método chamado `obterMarcaModelo`, que retorne:
 "Esse carro é um [MARCA] [MODELO]"
 Para retornar os valores de marca e modelo, utilize os métodos criados.
 */
-?
+carro.obterMarcaModelo = function(){
+	return "Esse carro é um " + carro.obterMarca() + ' ' + carro.obterModelo(); 
+}
 
 /*
 Crie um método que irá adicionar pessoas no carro. Esse método terá as
@@ -75,7 +114,41 @@ mostrar quantos assentos ainda podem ser ocupados, com a frase:
 - Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
 citado acima, no lugar de "pessoas".
 */
-?
+carro.adicionarPessoasNoCarro = function(numeroPessoas){
+	
+	var verificaExcessoPessoas = numeroPessoas + carro.quantidadePessoas - carro.assentos
+	var pessoa = 'pessoa';
+	var qtdePessoasAtual = carro.quantidadePessoas;
+
+	if (carro.quantidadePessoas === carro.assentos) {
+		 return "O carro já está lotado!";
+	} 	else if (numeroPessoas + carro.quantidadePessoas > carro.assentos) {
+			if (verificaExcessoPessoas > 1) {
+				pessoa = 'pessoas';
+			}
+		return "Só cabem mais "+ verificaExcessoPessoas - carro.assentos + " " + pessoa + "!";
+	}	else {
+		carro.quantidadePessoas = qtdePessoasAtual + numeroPessoas;
+		return "Já temos " + carro.quantidadePessoas + " pessoas no carro!";
+	}	
+}
+
+carro.adicionarPessoasNoCarroCorrecao = function(numeroPessoas){
+	var totalPessoas = carro.quantidadePessoas + numeroPessoas;
+
+	if (carro.quantidadePessoas === carro.assentos && totalPessoas >= carro.assentos) {
+		 return "O carro já está lotado!";
+	}
+
+	if (totalPessoas > carro.assentos) {
+		var quantasPessoasCabem = carro.assentos - carro.quantidadePessoas;
+		var pluralOuSingular = quantasPessoasCabem === 1 ? ' pessoa' : ' pessoas';
+		return "Só cabem mais " + quantasPessoasCabem + pluralOuSingular;
+	}
+
+	carro.quantidadePessoas += numeroPessoas;
+	return "Já temos " + carro.quantidadePessoas + " pessoas no carro!";
+}
 
 /*
 Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
@@ -85,37 +158,38 @@ retornar algum valor.
 
 Qual a cor atual do carro?
 */
-?
+console.log(carro.obterCor());
 
 // Mude a cor do carro para vermelho.
-?
+carro.mudarCor('Vermelho');
 
 // E agora, qual a cor do carro?
-?
+console.log(carro.obterCor());
 
 // Mude a cor do carro para verde musgo.
-?
+carro.mudarCor('Verde Musgo');
 
 // E agora, qual a cor do carro?
-?
+console.log(carro.obterCor());
 
 // Qual a marca e modelo do carro?
-?
+console.log(carro.obterMarcaModelo());
 
 // Adicione 2 pessoas no carro.
-?
+console.log(carro.adicionarPessoasNoCarroCorrecao(2));
 
 // Adicione mais 4 pessoas no carro.
-?
+console.log(carro.adicionarPessoasNoCarroCorrecao(4));
 
 // Faça o carro encher.
-?
+console.log(carro.adicionarPessoasNoCarroCorrecao(3));
+console.log(carro.adicionarPessoasNoCarroCorrecao(1));
 
 // Tire 4 pessoas do carro.
-?
+console.log(carro.adicionarPessoasNoCarroCorrecao(-4));
 
 // Adicione 10 pessoas no carro.
-?
+console.log(carro.adicionarPessoasNoCarroCorrecao(10));
 
 // Quantas pessoas temos no carro?
-?
+console.log(carro.quantidadePessoas);
--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [1, 'Igor', true, 'Foto.jpg', null, { a: 1}, function() {}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function recebeArray(arr) {
+	return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(recebeArray(arr)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function recebeDoisParams(arr, numero) {
+	return	arr[numero];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arr2 = [1, 'Igor', 'Analista Senior', true, '2016-02-20'];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(recebeDoisParams(arr2, 0));
+console.log(recebeDoisParams(arr2, 1));
+console.log(recebeDoisParams(arr2, 2));
+console.log(recebeDoisParams(arr2, 3));
+console.log(recebeDoisParams(arr2, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,56 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nomeLivro) {
+	var objeto = {
+		'livro 1': {
+			quantidadePaginas: 1,
+			autor: 'Igor',
+			editora: 'Nova' 
+		},
+		'livro 2': {
+			quantidadePaginas: 2,
+			autor: 'Daniela',
+			editora: 'Costuras' 
+		},
+		'livro 3': {
+			quantidadePaginas: 3,
+			autor: 'Melissa',
+			editora: 'Abaco' 
+		},
+	};
+
+	if (!nomeLivro) {
+		return objeto;
+	}
+	return objeto[nomeLivro];
+
+	//return !nomeLivro ? objeto : objeto[nomeLivro];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro livro 2 tem ' + book('livro 2').quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
-
+console.log('O autor do livro livro 2 é ' + book('livro 2').autor + ' páginas!');
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+var nomeLivro = 'livro 2'
+console.log('O livro ' + nomeLivro + ' foi publicado pela editora ' + book(nomeLivro).editora + '.');
+
