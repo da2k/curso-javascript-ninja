ce33160d15b2cf06aeccd6c33e8239a65ee6e014
Raphael2790 <rssshev@gmail.com>
2019-07-09 17:22:02 -0300

    Feitos os exercicios 08

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,52 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+
+var sum = function calculateSum( x ) {
+    return function( y ) {
+        return x + y
+    }
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+
+
+console.log(`A soma de 2 e 4 é igual a ${sum ( 2 ) ( 4 )}`)
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+
+console.log(`O nome da função que faz a soma é ${sum.name}`)
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+
+function showName(func) {
+    return func.name
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
 
+var varShowName = function showName(func) {
+    return func.name
+}
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+
+console.log(`A função ${varShowName.name} retorna `)
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,8 +62,17 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
 
+function calculator(operação) {
+    function calculo( x , y ) {
+        switch(operação) {
+            case '+':
+                console.log(`Resultado da operação: ${x} ${operação} ${y} = ${x + y}`)
+        }
+    }
+}
+
+calculator('+')(2, 4)
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
