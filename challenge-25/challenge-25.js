59888b44c9ad1f7f37f6e0264bf9fd7e8edf9347
jungpatrick <38076346+jungpatrick@users.noreply.github.com>
2019-07-10 14:00:57 -0300

    Update challenge-25.js

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,30 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+(function(win, doc){
+	'use strict';
+
+	var $password = document.querySelector('input[data-js="pass"]');
+	var $user = document.querySelector('input[data-js="user"]');
+	var $buttonGetScreenResolution = document.querySelector('button[data-js="getScreenResolution"]');
+
+	$password.addEventListener('focus', handeClickInput, false);
+	$password.addEventListener('blur', handleLeaveInput, false);
+	$user.addEventListener('focus', handeClickInput, false);
+	$user.addEventListener('blur', handleLeaveInput, false);
+	$buttonGetScreenResolution.addEventListener('click', getScreenResolution, false);
+
+	function handeClickInput(){
+		this.style.background = 'yellow';
+	}
+
+	function handleLeaveInput(){
+		this.style.background = ''; 
+	}
+
+	function getScreenResolution(){
+		alert(window.innerWidth +'px de largura por ' +window.innerHeight +'px de altura');
+	}
+
+}(window, document));
