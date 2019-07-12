f2571bbc0025b326b13b56da7fd0eaa7ab270ed5
peperoschach <luiz.felipe_23@yahoo.com.br>
2019-07-12 15:49:14 -0300

    Challenge-02

---
+++ challenge-02/index.js
@@ -0,0 +1,95 @@
+/* Desafio da semana #2
+
+Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções!
+ */
+
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+function soma(x, y) {
+  return x + y;
+}
+
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+var resultado = soma(3, 8) + 5;
+
+// Qual o valor atualizado dessa variável?
+16;
+
+// Declare uma nova variável, sem valor.
+var variavelNova;
+
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é VALOR.
+Onde VALOR é o novo valor da variável.
+*/
+function addValue() {
+  variavelNova = 10;
+
+  return "O valor da variável agora é " + variavelNova;
+}
+
+// Invoque a função criada acima.
+addValue();
+
+// Qual o retorno da função? (Use comentários de bloco).
+/*
+  O valor da variável agora é 10
+
+*/
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+
+function multiArguments(x, y, z) {
+  if (x === undefined || y === undefined || z === undefined) {
+    return "Preencha todos os valores corretamente!";
+  } else {
+    return x * y * z + 2;
+  }
+}
+
+// Invoque a função criada acima, passando só dois números como argumento.
+multiArguments(2, 3);
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+("Preencha todos os valores corretamente!");
+
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+multiArguments(2, 3, 3);
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+20;
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos.
+2. Se somente um argumento for passado, retorne o valor do argumento.
+3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+*/
+function conditionalArguments(x, y, z) {
+  if (x !== undefined && y === undefined && z === undefined) {
+    return x;
+  } else if (x !== undefined && y !== undefined && z === undefined) {
+    return x + y;
+  } else if (x !== undefined && y !== undefined && z !== undefined) {
+    return (x + y) / z;
+  } else if (x === undefined && y === undefined && z === undefined) {
+    return false;
+  } else {
+    return null;
+  }
+}
+
+// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+conditionalArguments() //false
+conditionalArguments(2) // 2
+conditionalArguments(2, 3) // 5
+conditionalArguments(2, 3, 4) // 1.25
+
