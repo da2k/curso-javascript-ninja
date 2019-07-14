ffabd6e8f98f275cfec375004476eaf38b0a3add
Pedro Carvalho <pedrosophbc@gmail.com>
2019-07-13 21:53:28 -0300

    challenge-19

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -1,7 +1,12 @@
+(function(){
+
+    'use strict'
+
+
 /*
-1. Envolva todo o conteúdo desse desafio em uma IIFE.
-2. Adicione a diretiva 'use strict';
-3. Crie um arquivo index.html e adicione esse script à ele.
+    1. Envolva todo o conteúdo desse desafio em uma IIFE.
+    2. Adicione a diretiva 'use strict';
+    3. Crie um arquivo index.html e adicione esse script à ele.
 */
 
 /*
@@ -23,8 +28,11 @@ linha, independente de quantos caracteres de número estiverem juntos.
 - Atribua essa regex à uma variável chamada `justNumbersRegex` e mostre-a
 no console:
 */
+
+var justNumbersRegex = new RegExp('^\\d+','gm')
+
 console.log( 'Regex para números usando o construtor:' );
-// ?
+console.log(justNumbersRegex)
 
 /*
 Verifique se a regex acima casa com o texto na variável `text`, mostrando o
@@ -33,7 +41,8 @@ resultado no console. O resultado deve ser:
 */
 var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
 console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
-// ?
+
+console.log(text.match(justNumbersRegex))
 
 /*
 - Crie uma regex que case com números no final de uma string. Atribua a
@@ -43,8 +52,10 @@ que esta tenha muitas linhas, deve sempre casar com números no fim de cada
 linha, independente de quantos caracteres de número estiverem juntos.
 Mostre a regex no console:
 */
+var numbersAtTheEnd = new RegExp('\\d+$','gm')
+
 console.log( '\nRegex para números somente no final das linhas:' );
-// ?
+console.log(numbersAtTheEnd)
 
 /*
 Verifique se a regex acima casa com o texto na variável `otherText`,
@@ -54,7 +65,8 @@ O resultado deve ser:
 */
 var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
 console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
-// ?
+
+console.log(otherText.match(numbersAtTheEnd))
 
 /*
 Vamos criar um método que vai testar se uma classe CSS existe em uma
@@ -79,6 +91,19 @@ abaixo;
 qualquer classe que for testada. Os dados passados no exercício são somente
 para exemplificar.
 */
+
+let hasClass = (markup, cssClass) => {
+    let regex = new RegExp('\\"')
+    //    /("|\\')cssClass(\1)/
+   let match = markup.match(regex)
+   return match
+}
+
 var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-// ?
+
+console.log(hasClass(markup,'container'))
+
+
+
+})()
