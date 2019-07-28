c2bf339eeb513a7ab409c1832e7c3c17e02b7541
alegalliard <ale.sz.design@gmail.com>
2019-07-27 22:09:23 -0300

    Challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [1, 50, 88, 7, 98, 120, 14];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
 
+var printList = function(list) {
+    return list;
+}
+
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+printList(arr)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,22 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+var values = function(list, num) {
+    return list[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arr2 = ['hahaha', 15, true, [1,2], 5.106];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+for(var i=0; i < arr2.length; i++) {
+    console.log(values(arr2, i));
+}
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,64 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+var book = function(bookName)
+{ //achei a descrição do exercício confusa
+
+    var list = {
+        'Silvio Santos Vem Aí': {
+                quantidadePaginas: 500,
+                autor: "Plutônio Xenônio Argônio",
+                editora: "ErreideTV"
+        },
+        'Oh my God they killed Kenny': {
+            quantidadePaginas: 68,
+            autor: "Cartman Smith Jr",
+            editora: "MTVbooks"
+        },
+        'Mafi Osa': {
+            quantidadePaginas: 150,
+            autor: "Carmen San Diego",
+            editora: "Gambino"
+        },
+        'Receitas da tia Nena': {
+            quantidadePaginas: 135,
+            autor: "Magali",
+            editora: "Panini"
+        }        
+    };
+
+    if(bookName) {
+        return list[bookName];
+    }
+    
+    return list;
+    
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var silvio ='Silvio Santos Vem Aí';
+var livroDoSilvio = book(silvio);
+console.log("O livro "+ silvio +" tem "+ livroDoSilvio.quantidadePaginas +" páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log("O autor do livro "+ silvio +" é "+ livroDoSilvio.autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log("O livro "+ silvio +" é "+ livroDoSilvio.editora);
