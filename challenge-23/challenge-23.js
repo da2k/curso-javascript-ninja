3d51f4102dfefffbf14aa47b11a363fb5b54f2ea
Amanda Almeida Matos <allonsmandy@gmail.com>
2019-08-15 20:15:16 -0300

    Resolução challenge-23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,3 +1,5 @@
+(function() {
+ 'use strict';
 /*
 Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
 As regras são:
@@ -23,3 +25,98 @@ multiplicação (x), então no input deve aparecer "1+2x".
 input;
 - Ao pressionar o botão "CE", o input deve ficar zerado.
 */
+  
+ //index.html
+ <input type="text" value="0" readonly data-js="visor"/>
+<button data-js="number" value="1">1</button>
+<button data-js="number" value="2">2</button>
+<button data-js="number" value="3">3</button>
+<button data-js="number" value="4">4</button>
+<button data-js="number" value="5">5</button>
+<button data-js="number" value="6">6</button>
+<button data-js="number" value="7">7</button>
+<button data-js="number" value="8">8</button>
+<button data-js="number" value="9">9</button>
+
+<button data-js="operation" value="+">+</button>
+<button data-js="operation" value="-">-</button>
+<button data-js="operation" value="÷">÷</button>
+<button data-js="operation" value="*">*</button>
+
+<button data-js="igual" value="=">=</button>
+<button data-js="ce" value="ce">CE</button>
+
+<script src="script.js"></script>
+   
+ //script.js
+ (function() {
+
+    var $visor = document.querySelector('[data-js="visor"]');
+    var $buttonNumbers = document.querySelectorAll('[data-js="number');
+    var $buttonCE = document.querySelector('[data-js="ce"]');
+    var $operations = document.querySelectorAll('[data-js="operation"]');
+    var $igual = document.querySelector('[data-js="igual"]');
+    
+    Array.prototype.forEach.call($buttonNumbers, function(button) {
+      button.addEventListener('click', clickNumber, false);
+    });
+    
+    Array.prototype.forEach.call($operations, function(operation) {
+      operation.addEventListener('click', clickOperation, false);
+    });
+    
+    $buttonCE.addEventListener('click', clickCE, false);
+    $igual.addEventListener('click', clickIgual, false);
+    
+    function clickNumber() {
+      $visor.value += this.value; //cada numero pressionado será adicionado no visor
+    }
+    
+    function clickOperation() {
+      $visor.value = removeUltimoItemSeForOperador($visor.value);
+      $visor.value += this.value;
+    }
+    
+    function clickCE() {
+      $visor.value = 0;
+    }
+    
+    function isOperation(number) {
+       var operations = ['+', '-', '*', '÷'];
+       var lastItem = number.split('').pop(); //quebra esse cara aqui em um array e pega o ultimo item 
+      return operations.some(function(operator) { //passa pelo array e verifica se um deles existe no retorno 
+          return operator === lastItem;
+      })
+    }
+    
+     function removeUltimoItemSeForOperador(number) {
+        if(isOperation(number)) {
+          return number.slice(0, -1); //pega todos os itens e remove o ultimo
+        }
+        return number;
+      }
+      
+     function clickIgual() {
+        $visor.value = removeUltimoItemSeForOperador($visor.value);
+        var todosValores = $visor.value.match(/\d+[+*÷-]?/g);
+        $visor.value = todosValores.reduce(function(acumulado, atual) {
+          var primeiroValor = acumulado.slice(0, -1);
+          var operador = acumulado.split('').pop();
+          var ultimoValor = removeUltimoItemSeForOperador(atual);
+          var ultimoOperador = isOperation(atual) ? atual.split('').pop() : '';
+          switch(operador) {
+            case '+':
+              return (Number(primeiroValor) + Number(ultimoValor)) + ultimoOperador;
+            case '-':
+              return (Number(primeiroValor) - Number(ultimoValor)) + ultimoOperador;
+            case '*':
+              return (Number(primeiroValor) * Number(ultimoValor)) + ultimoOperador;
+            case '÷':
+              return (Number(primeiroValor) / Number(ultimoValor)) + ultimoOperador;
+          }
+        });
+      }
+     
+     })()
+  
+}()
