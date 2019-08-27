c7e5b0de31cbc3b91fa16911db7320b1a162e00c
Paulo Teixeira <paulo.teixeira541@gmail.com>
2019-08-27 20:10:02 -0300

    challenge-29

---
+++ challenge-29/dom.js
@@ -0,0 +1,98 @@
+(function(win, doc){
+  'use strict';
+
+  function DOM(elements){
+    if(!(this instanceof DOM)){
+      return new DOM(elements);
+    }
+    this.element = doc.querySelectorAll(elements);
+  }
+
+
+
+  DOM.isArray = function isArray (param) {
+    return Object.prototype.toString.call(param) === '[object Array]';
+  };
+
+  DOM.isObject = function isObject (param) {
+      return Object.prototype.toString.call(param) === '[object Object]';
+  };
+
+  DOM.isFunction = function isFunction (param) {
+      return Object.prototype.toString.call(param) === '[object Function]';
+  };
+
+  DOM.isNumber = function isNumber (param) {
+      return Object.prototype.toString.call(param) === '[object Number]';
+  };
+
+  DOM.isString = function isString (param) {
+    return Object.prototype.toString.call(param) === '[object String]';
+  };
+
+  DOM.isBoolean = function isBoolean (param) {
+    return Object.prototype.toString.call(param) === '[object Boolean]';
+  };
+
+  DOM.isNull = function isNull (param) {
+    return Object.prototype.toString.call(param) === '[object Null]'
+    || Object.prototype.toString.call(param) === '[object Undefined]';
+  };
+
+
+
+
+  DOM.prototype.on = function on(eventType, callback){
+    Array.prototype.forEach.call(this.element, function(element){
+    element.addEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(eventType, callback){
+    Array.prototype.forEach.call(this.element, function(element){
+      element.removeEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get(index){
+    //if(!index){
+    //  return this.element[0];
+    //}
+    return this.element;
+  };
+
+  DOM.prototype.forEach = function forEach(){
+    return Array.prototype.forEach.apply(this.element, arguments);
+  };
+
+  DOM.prototype.map = function map(){
+    return Array.prototype.map.apply(this.element, arguments);
+  };
+
+  DOM.prototype.filter = function filter(){
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduce = function reduce(){
+    return Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduceRight = function reduceRight(){
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  DOM.prototype.every = function every(){
+    return Array.prototype.every.apply(this.element, arguments);
+  };
+
+  DOM.prototype.some = function some(){
+    return Array.prototype.some.apply(this.element, arguments);
+  };
+
+
+
+
+
+
+  win.DOM = DOM;
+})(window, document);
