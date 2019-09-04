0cddb509f3e8c800437bc05d9263d1c8a2100437
Luan Vinícius <40446521+nalucode@users.noreply.github.com>
2019-09-04 14:46:31 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -78,11 +78,7 @@ var book = function(arg){
       editora: 'Creativa'
     }
   }
-  if (arg == undefined){
-    return x} 
-  else{
-    return x[arg]
-  };
+  return !arg ? x : x[ arg ]
 }
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
@@ -95,18 +91,20 @@ Ainda com a função acima, imprima a quantidade de páginas de um livro qualque
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// 
+console.log('O livro1 possui' +' '+book('livro1).quantidadePaginas+' páginas') 
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+//
+console.log(`O autor do livro livro2 é ${book('livro2').autor}`)
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+//
+console.log(`O livro livro3 foi publicado pela editora ${book('livro3').editora}`)
