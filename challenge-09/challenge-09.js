63ed38535e526ee9baa7d032a9034084434e2c83
wtorresx <47874244+wtorresx@users.noreply.github.com>
2019-09-06 19:01:57 -0300

    Update challenge-09.js

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -1,4 +1,4 @@
-/*
+(function(){/*
 Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
 */
@@ -10,11 +10,13 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+    var number1 = 10;
+    var number2 = 20;
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
+
     return number1 + number2;
-    var number2 = 20;
+    
 }
 myFunction();
 
@@ -22,12 +24,15 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+    var number1 = 10;
+    var number2 = 20;
+    
     var sum = function sum() {
         return number1 + number2;
     };
-    var number1 = 10;
-    var number2 = 20;
+  
+    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+
     return sum();
 }
 myFunction2();
@@ -36,11 +41,14 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
-    var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
     var number1 = 40;
+    var number2 = 50;
+  
+    console.log( 'A soma de 40 e 50 é igual a', sum() );
+    console.log( 'Na função myFunction3, number1 é igual a', number1 );
+
     return sum();
+  
     function sum() {
         return number1 + number2;
     };
@@ -105,3 +113,4 @@ console.log( 'O resultado da divisão é:' );
 
 console.log( 'O resto da divisão é:' );
 // ?
+}());
