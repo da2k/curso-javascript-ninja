e61b6ac006ffa1b5fd144e9889ea0b24f82342ad
Marcelo Henrique <marcel-h-silva@hotmail.com>
2019-10-31 14:02:48 -0300

    Update Challenge-07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -8,23 +8,20 @@ Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 function addItem (array) {
-    var array = arr.push(array);
-    return array;
+    arr.push(array);
+    return arr;
 }
-addItem({time: 'palmeiras'});
-console.log(arr);
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-addItem(['Campinas', function(){}, false]);
-console.log(arr);
+console.log(addItem(['Campinas', function(){}, false]));
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-console.log('O segundo elemento do segundo array é ' + arr[7][1]);
+console.log('O segundo elemento do segundo array é ' + arr[6][1]);
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
@@ -34,7 +31,7 @@ console.log('O primeiro array tem ' + arr.length + ' items.');
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-console.log('O segundo array tem ' + arr[7].length + ' items.');
+console.log('O segundo array tem ' + arr[6].length + ' items.');
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
