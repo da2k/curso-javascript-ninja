a97ab0735c5b5b5de41d334ab8ed65e0e16386e5
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2019-11-11 15:18:03 -0300

    Challenge 17

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -117,18 +117,18 @@
 
   function getMonthNumber(monthName) {
     const months = {
-      'janeiro': '01',
-      'fevereiro': '02',
-      'março': '03',
-      'abril': '04',
-      'maio': '05',
-      'junho': '06',
-      'julho': '07',
-      'agosto': '08',
-      'setembro': '09',
-      'outubro': '10',
-      'novembro': '11',
-      'dezembro': '12'
+      janeiro: '01',
+      fevereiro: '02',
+      março: '03',
+      abril: '04',
+      maio: '05',
+      junho: '06',
+      julho: '07',
+      agosto: '08',
+      setembro: '09',
+      outubro: '10',
+      novembro: '11',
+      dezembro: '12'
     };
 
     return months[monthName];
