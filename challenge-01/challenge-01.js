95d806d4636de2409e161b86c049f5a37ab4a6f9
Daniel Brandao <dannnbrandao@gmail.com>
2019-12-13 20:34:32 -0300

    Atualizado challenge-01

--- challenge-01/challenge-01.js
+++ challenge-01/challenge-01.js
@@ -1,42 +1,42 @@
-# Desafio Semana #1
+/* # Desafio Semana #1 */
 
-```js
-// Declarar uma variável chamada `myvar`, sem valor.
-?
+// Declarar uma variável chamada `myVar`, sem valor.
+var myVar;
 
-// Após declarada, atribua o valor 10 à variável `myvar`.
-?
+// Após declarada, atribua o valor 10 à variável `myVar`.
+myVar = 10;
 
 // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
-?
+var soma =  15 + 8;
 
 // Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
-?
+soma += 1; // Ou soma++;
 
 // Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
-?
+soma *= 3;
 
 // Qual é o valor da variável `soma` até aqui?
-?
+console.log(soma); // 72
 
 // Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
-?
+var souNinja = true;
 
 // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
-?
+var comida = [ 'Arroz', 'Feijão', 'Ovo' ];
 
 // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
-?
+console.log( comida[1] ); // Passando o índice 1 = Feijão
 
-// Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
-?
+// Digite o código que verifica se a variável `soma' é igual a variável `myVar` (testando também o tipo).
+console.log( soma === myVar ); // false
 
-// Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
-?
+// Digite o código que verifica se a variável `myVar` é menor ou igual à variável `soma`.
+console.log( myVar <= soma); // true
 
 // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-?
+function divisao( x, y ){
+    return x / y;
+}
 
 // Invoque a função criada acima, passando os parâmetros 10 e 2.
-?
-```
+console.log( divisao(10, 2)); // 5
