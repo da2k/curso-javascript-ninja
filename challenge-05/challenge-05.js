130270a342f9afe58c90e87810e233ff4d779933
Daniel Brandao - Notebook <dannnbrandao@gmail.com>
2019-12-25 18:25:26 -0300

    Atualizando challenge-05, 1/2.

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -1,18 +1,25 @@
+/* # Desafio da semana #4 */
+
 /*
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var meuArray = [    3, 'informação', [1,2,3], {name: 'Daniel'}, true];  
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function arrayFunction(arrayReturn){
+    return arrayReturn;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+console.log(arrayFunction(meuArray[1])); // Ok, this function return array 'Informação'
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +27,27 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function newArrayFunction(value, index){
+    return value[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+var newArray = meuArray;
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+console.log(newArrayFunction(newArray,0));
+console.log(newArrayFunction(newArray,1));
+console.log(newArrayFunction(newArray,2));
+console.log(newArrayFunction(newArray,3));
+console.log(newArrayFunction(newArray,4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,19 +63,45 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book(bookName){
+
+    var allBooks = {
+
+        'Javascript Básico': { 
+            qtd_paginas: 100,
+            autor: 'Fulano',
+            editora: 'Aprender 1'
+        },
+        'Javascript Intermediario': {
+            qtd_paginas: 200,
+            autor: 'Beltrano',
+            editora: 'Aprender 2'
+        },
+        'Javascript Avançado': {
+            qtd_paginas: 300,
+            autor: 'Sicrano',
+            editora: 'Aprender 3'
+        }
+    };
+    
+    return !bookName ? allBooks: allBooks[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+
+var bookName = 'Javascript basico';
+
+console.log(`O livro ${bookName} tem ${book(bookName).qtdPaginas} páginas!`);
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
