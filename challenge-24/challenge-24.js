cee23406191b1d4a1148fcf182abb7879cc7975e
Marcelo Henrique <marcel-h-silva@hotmail.com>
2020-01-18 20:56:06 -0300

    update challenge-24.js

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -16,12 +16,12 @@ var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]
 var $buttonCE = document.querySelector('[data-js="button-ce"]');
 var $buttonEqual = document.querySelector('[data-js="button-equal"]');
 
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
-  button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
+// Array.prototype.forEach.call($buttonsNumbers, function(button) {
+//   button.addEventListener('click', handleClickNumber, false);
+// });
+// Array.prototype.forEach.call($buttonsOperations, function(button) {
+//   button.addEventListener('click', handleClickOperation, false);
+// });
 $buttonCE.addEventListener('click', handleClickCE, false);
 $buttonEqual.addEventListener('click', handleClickEqual, false);
 
