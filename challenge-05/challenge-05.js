1a47dfff5aeea025af5ba62c5e9d2a3a3d4b0b44
Felipe Fortunato <fefortunato@live.com>
2020-01-21 17:28:15 -0300

    Challenge-07

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var values = ["Felipe", 26, true, NaN, undefined];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myArray(arr) {
+  return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myArray(values[1]));
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,20 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function showArrayValue(arr, index) {
+  return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myValues = ["Felipe", 26, true, NaN, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(myValues);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +51,59 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName) {
+  var books = {
+    bibliaSagrada: {
+      quantidadePaginas: 1557,
+      autor: "God",
+      editora: "Kingdown"
+    },
+    javascriptNinja: {
+      quantidadePaginas: 5467,
+      autor: "Fernandoi Daciuk",
+      editora: "Da2K"
+    },
+    javascriptForNoobs: {
+      quantidadePaginas: 7656,
+      autor: "Felipe Fortunato",
+      editora: "FAF Books"
+    }
+  };
+  return !bookName ? books : books[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log(
+  `O livro Biblia Sabrada tem ${
+    book("bibliaSagrada").quantidadePaginas
+  } páginas!`
+);
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log(
+  `O autor do livro Biblia Sagrada é ${book("bibliaSagrada").autor}.`
+);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log(
+  `O livro Biblia Sagrada foi publicado pela editora ${
+    book("bibliaSagrada").editora
+  }.`
+);
