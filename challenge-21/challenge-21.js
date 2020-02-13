b5eb679105b8e1160f025f20a9f57f25f08c65b4
Paulo Lima <47723610+prmlimajr@users.noreply.github.com>
2020-02-12 23:57:06 -0300

    Update challenge-21.js

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,17 +1,44 @@
-/*
-O desafio de hoje será um pequeno projeto: um cronômetro!
-As regras para criação do cronômetro são as seguintes:
-1. Crie um arquivo index.html e adicione esse script a ele;
-2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
-Ele será o nosso cronômetro;
-3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
-4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
-cada segundo;
-5. Ao clicar em Stop, o cronômetro deve parar de contar;
-6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+(function(){
+    'use strict'
+    /*
+    O desafio de hoje será um pequeno projeto: um cronômetro!
+    As regras para criação do cronômetro são as seguintes:
+    1. Crie um arquivo index.html e adicione esse script a ele;
+    2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
+    Ele será o nosso cronômetro;
+    3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+    4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
+    cada segundo;
+    5. Ao clicar em Stop, o cronômetro deve parar de contar;
+    6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+  
+    Utilize o atributo data-js para nomear o campo e os botões. Você pode
+    usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
+    dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
+    */
+    var timer = document.querySelector('[data-js="timer"]')
+    var btnStart = document.querySelector('[data-js="btnStart"]')
+    var btnStop = document.querySelector('[data-js="btnStop"]')
+    var btnReset = document.querySelector('[data-js="btnReset"]')
+    var counter = 0
+    var temporizador
 
-Utilize o atributo data-js para nomear o campo e os botões. Você pode
-usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
-dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
-*/
-// ?
+    function cronometro(){
+        timer.value = counter++
+        temporizador = setTimeout(cronometro, 100);
+    }
+    
+    btnStart.addEventListener('click', function(){
+        cronometro()
+    })
+    
+    btnStop.addEventListener('click', function(){
+        clearTimeout(temporizador)
+    })
+
+    btnReset.addEventListener('click', function(){
+        timer.value = 0
+        counter = 0
+        clearTimeout(temporizador)
+    })
+  })()
