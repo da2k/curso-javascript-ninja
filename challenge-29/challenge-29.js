bd02f399f2d04d2bb90302ba3c6e638de4778651
Paulo Lima <47723610+prmlimajr@users.noreply.github.com>
2020-02-22 18:34:33 -0300

    Update challenge-29.js

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function(DOM) {
   'use strict';
 
   /*
@@ -16,24 +16,91 @@
     - Placa
     - Cor
     - e um botão "Cadastrar"
-
   Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
   carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
   aparecer no final da tabela.
-
   Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
   empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
   Dê um nome para a empresa e um telefone fictício, preechendo essas informações
   no arquivo company.json que já está criado.
-
   Essas informações devem ser adicionadas no HTML via Ajax.
-
   Parte técnica:
   Separe o nosso módulo de DOM criado nas últimas aulas em
   um arquivo DOM.js.
-
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
 
-})();
+  var app = (function appController(){
+    return {
+        init: function init(){
+            console.log('app init')
+            this.companyInfo()
+            this.initEvents()
+        },
+
+        initEvents: function initEvents(){
+            DOM('[data-js="form-register"]').on('submit', this.handleSubmit)
+        },
+
+        handleSubmit: function handleSubmit(e){
+            e.preventDefault()
+            console.log('submit')
+            var tableCar = DOM('[data-js="tableCar"]').get()
+            tableCar.appendChild(app.createNewCar())
+        },
+
+        createNewCar: function createNewCar(){
+            var fragment = document.createDocumentFragment()
+            var tr = document.createElement('tr')
+            var tdImage = document.createElement('td')
+            var image = document.createElement('img')
+            var tdBrand = document.createElement('td')
+            var tdYear = document.createElement('td')
+            var tdPlate = document.createElement('td')
+            var tdColor = document.createElement('td')
+
+            image.src = DOM('[data-js="img"]').get().value
+            tdImage.appendChild(image)
+            tdBrand.textContent = DOM('[data-js="marca"]').get().value
+            tdYear.textContent = DOM('[data-js="ano"]').get().value
+            tdPlate.textContent = DOM('[data-js="placa"]').get().value
+            tdColor.textContent = DOM('[data-js="cor"]').get().value
+
+            tr.appendChild(tdImage)
+            tr.appendChild(tdBrand)
+            tr.appendChild(tdYear)
+            tr.appendChild(tdPlate)
+            tr.appendChild(tdColor)
+
+            return fragment.appendChild(tr)
+        },
+
+        companyInfo: function companyInfo(){
+            var ajax = new XMLHttpRequest()
+            ajax.open('GET', 'company.json', true)
+            ajax.send()
+            ajax.addEventListener('readystatechange', this.getCompanyInfo, false)
+        },
+
+        getCompanyInfo: function getCompanyInfo(){
+            if(!app.isReady.call(this))
+                return
+
+            var data = JSON.parse(this.responseText)
+            var companyName = DOM('[data-js="nomeDaLoja"]')
+            var companyPhone = DOM('[data-js="telefoneDaLoja"]')
+            companyName.textContent = data.name
+            companyPhone.textContent = data.phone
+        },
+        
+        isReady: function isReady(){
+            return this.readyState === 4 && this.status === 200
+        }
+    }
+
+  })()
+
+  app.init()
+
+})(window.DOM);
