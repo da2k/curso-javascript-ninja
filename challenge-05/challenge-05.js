79e4ebcdd517e008b8477b31f36ef675d2a74bf5
André Rodrigues <andre_santos_t@hotmail.com>
2020-03-12 17:03:13 -0400

    challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,8 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
-
+var myArray = [ 'da2k', 5, true, null, undefined ];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
