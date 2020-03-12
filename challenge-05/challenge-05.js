d277144a22028e4e29fa401ff1ad0cf43e4b94f5
Vagner Esdras <vagneresdras@DESKTOP-EL4VC9B.localdomain>
2020-03-12 00:23:18 -0300

    Challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arrayRandom = ["ok", true, 1.5, undefined, null];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function retornarArray(array) {
+    return array;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+retornarArray(arrayRandom)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function retornarDoisValores(arrayValores, numero) {
+    return arrayValores[numero];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var array = [{nome: 'Vagner'}, function() {}, 7, true, NaN];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+retornarDoisValores(array, 0);
+retornarDoisValores(array, 1);
+retornarDoisValores(array, 2);
+retornarDoisValores(array, 3);
+retornarDoisValores(array, 4);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,52 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName) {
+    var allBooks = {
+        'Harry Potter': {
+            quantidadePaginas: 255,
+            autor: 'J. K. Rowling',
+            editora: 'Bloomsbury Publishing'
+        },
+        'O Senhor Dos Aneis': {
+            quantidadePaginas: 1232 ,
+            autor: 'J. R. R. Tolkien',
+            editora: 'Martins Fontes' 
+        },
+        'O Amuleto de Samarkanda': {
+            quantidadePaginas: 462 ,
+            autor: 'Jonathan Stroud',
+            editora: 'Doubleday' 
+        },
+    }
+    if (!bookName) {
+        return allBooks;
+    }
+    return allBooks[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log("O Livro O Amuleto de Samarkanda tem " + book("O Amuleto de Samarkanda").quantidadePaginas + "páginas");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log("O autor do livro Harry Potter é " + book("Harry Potter").autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log("O livro Harry Potter foi publicado pela editora " + book("Harry Potter").editora);
