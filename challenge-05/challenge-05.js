06fde79ee87dea6be26984e658e89764fc8e4b9b
Iago Velasco <iagovelasco@hotmail.com>
2020-03-20 09:10:46 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,17 +3,21 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
+ var array = ['1', 2, 'tres', 4, 'cinco']
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
+var myFunction = function(arr){
+	return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
-
+myFunction(array);
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
@@ -21,17 +25,27 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
+var arrayIndice = function(arr, number){
+	return arr[number]
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
+ var array = ['1', 2, 'tres', 4, 'cinco']
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
+ arrayIndice(array, 0)
+ arrayIndice(array, 1)
+ arrayIndice(array, 2)
+ arrayIndice(array, 3)
+ arrayIndice(array, 4)
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -49,24 +63,49 @@ os livros.
 */
 // ?
 
+function book(bookName) {
+    var allBooks = {
+        'Iago': {
+            quantidadePaginas: 255,
+            autor: 'J. K. Rowling',
+            editora: 'Bloomsbury Publishing'
+        },
+        'Teste': {
+            quantidadePaginas: 1232 ,
+            autor: 'J. R. R. Tolkien',
+            editora: 'Martins Fontes' 
+        },
+        'Iago2': {
+            quantidadePaginas: 462 ,
+            autor: 'Jonathan Stroud',
+            editora: 'Doubleday' 
+        },
+    }
+    if (!bookName) {
+        return allBooks;
+    }
+       return allBooks[bookName];
+}
+
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
 // ?
-
+book()
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
 // ?
-
+"O livro Iago tem " + book("Iago").quantidadePaginas  + " Paginas"
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
 // ?
+"O autor do livro Iago é " + book("Iago").autor
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
@@ -74,3 +113,4 @@ a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
 // ?
+"O livro Iago foi publicado pela editora " + book("Iago").editora
