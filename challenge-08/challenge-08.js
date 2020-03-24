4685447be73779a9f4376f2cc8a165e45ede5313
André Rodrigues <andre_santos_t@hotmail.com>
2020-03-24 16:17:31 -0400

    challenge 08 - Ajuste de retorno do nome

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -27,20 +27,20 @@ Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
 function showName() {
-  return showName.name;
+  return 'André Rodrigues';
 }
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-var varShowName = showName();
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log( 'A função ' + varShowName + ' retorna ' + showName() + '.' );
+console.log( 'A função ' + varShowName.name + ' retorna ' + varShowName() + '.' );
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
