908651066c8e85f5c55ede2ad6b668a249b30443
Your Name <Lucas Rafael Ricci>
2020-03-25 11:48:26 -0300

    Challenge 25

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -1,16 +1,38 @@
-/*
-Essa semana você terá dois desafios:
-1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
-tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
-ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
-o que não ficou tão claro das aulas anteriores.
-É essencial que você entenda todo o conteúdo que foi passado até aqui,
-para que possamos prosseguir para a parte mais avançada do curso :D
+(function() {
+    /*
+    Essa semana você terá dois desafios:
+    1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
+    tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
+    ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
+    o que não ficou tão claro das aulas anteriores.
+    É essencial que você entenda todo o conteúdo que foi passado até aqui,
+    para que possamos prosseguir para a parte mais avançada do curso :D
 
-2) Estudar eventos!
-Acesse a página do MDN:
-https://developer.mozilla.org/en-US/docs/Web/Events#Categories
+    2) Estudar eventos!
+    Acesse a página do MDN:
+    https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 
-Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
-desafio os experimentos legais que você conseguir desenvolver :D
-*/
+    Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
+    desafio os experimentos legais que você conseguir desenvolver :D
+    */
+
+    var copy = 0;
+    var send = 0;
+
+    function on(element, event, callback) {
+        document.querySelector(element)
+            .addEventListener(event, callback, false); 
+    }
+
+    function handleClickInput() {
+        alert('O usuário copiou ' + ++copy + ' vez(es)!');
+    }
+
+    function handleCLickEnviar() {
+        alert('O usuário clicou no enviar ' + ++send + ' vez(es)!');
+    }
+
+    on('[data-js="inputEntrada"]', 'copy', handleClickInput);
+    on('[data-js="buttonEnviar"]', 'click', handleCLickEnviar);
+
+})();
