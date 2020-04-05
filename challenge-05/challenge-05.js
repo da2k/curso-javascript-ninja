f30dd7edbdde15582d63660029ba2f52a09afc20
Rodrigo Nogueira <rdg6design@gmail.com>
2020-04-04 23:00:26 -0300

    challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,8 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = ['41rdg6br', 5, true, null, undefined]
+//
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
