2cba883baa7728a6610b67a4ec09d6ec502dbff3
Natalia Luiza <natalialuizas@live.com>
2020-04-10 11:48:33 -0300

    challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+const myArray = ['arroz', function() {}, true, 1.6, { hello: 'oi'}]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+const myFunction = (arr) => {
+    return arr;
+} 
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+myFunction(myArray)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,20 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+const indiceArray = (arr, index) => {
+    return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+const myArr = ['string', 0, true, false, {}]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunction(myArr);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +51,50 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+const book = (nameBook) => {
+    const obj = {
+        eloquentJavaScript: {
+            pages: 250,
+            autor: 'Marijn Haverbeke',
+            publishing: 'MIT',
+        },
+        jsNinja: {
+            pages: 350,
+            autor: 'John Resig',
+            publishing: 'Novatec'
+        },
+        jsTheGoodParts:{
+            pages: 650,
+            autor: 'Douglas Crockford',
+            publishing: 'O\'Reilly'
+        }
+    }
+
+    return !!nameBook ?  obj[nameBook]: obj;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log(`O livro Eloquent JavaScript tem ${book('eloquentJavaScript').pages} páginas!`)
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log(`O autor do livro Javascript The Good Parts é ${book('jsTheGoodParts').autor}`)
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log(`O livro Segredo do JavaScript Ninja foi publicado pela editora ${book('jsNinja').publishing}.`)
