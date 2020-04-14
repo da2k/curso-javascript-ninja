d7abd16728b13d579da40a1b7ca051109e3c35a2
Rodrigo <rhrc09@gmail.com>
2020-04-14 17:47:34 -0300

    Update challenge-18.js

---
+++ testes/regexp/regexp.js
@@ -0,0 +1,5 @@
+(function () {
+  var text = 'Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas",[nota 1] foi um militar, político, abolicionista e monarquista brasileiro. Ele nasceu em uma família rica e de tradição militar, entrando no exército em 1817 quando ainda era criança. Sua iniciação militar ocorreu na Guerra contra Artigas, que teve seu território anexado e se tornou em 1821 a província brasileira da Cisplatina. Ele ficou envolvido durante boa parte da década de 1820 no esforço brasileiro para manter a Cisplatina como parte de seu território, primeiro durante a independência do Brasil e depois na Guerra da Cisplatina. No final a província conseguiu se separar e se tornou a nação independente do Uruguai.';
+  var myLetter = text.match(/\w/g);
+  console.log('My letter: ', myLetter);
+})();
