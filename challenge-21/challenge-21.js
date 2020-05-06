6da52fdf06b1288a6f90b48d1f79f26ceb6cca75
André Gonçalves Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-05-05 22:04:16 -0300

    Doing challenge 21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -9,9 +9,41 @@ Ele será o nosso cronômetro;
 cada segundo;
 5. Ao clicar em Stop, o cronômetro deve parar de contar;
 6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
-
 Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
 // ?
+
+(function(win, doc){
+  'use strict';
+
+  var $cronometer = doc.querySelector('[data-js="cronometer"]');
+  var $startButton = doc.querySelector('[data-js="start-button"]');
+  var $stopButton = doc.querySelector('[data-js="stop-button"]');
+  var $resetButton = doc.querySelector('[data-js="reset-button"]');
+
+  var value = 0;
+  var cronometerId;
+
+  $startButton.addEventListener('click', function() {
+    cronometerId = setInterval(function() {
+      $cronometer.value = value++;
+    }, 1000)
+  }, false);
+
+  $stopButton.addEventListener('click', function() {
+    stopCronometer();
+  }, false);
+
+  $resetButton.addEventListener('click', function() {
+    stopCronometer();
+    value = 0;
+    $cronometer.value = value;
+  }, false);
+
+  function stopCronometer() {
+    clearInterval(cronometerId);
+  }
+
+})(window, document);
