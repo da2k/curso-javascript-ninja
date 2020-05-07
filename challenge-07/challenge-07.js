650d76247be3f3c4e1e92385d319ac54125e6764
Evelyn Cristini Oliveira <30937883+EvyOliveira@users.noreply.github.com>
2020-05-07 15:58:15 -0300

    Lista 7 corrigida

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -3,20 +3,18 @@ Crie um array com 5 items (tipos variados).
 */
 
 var arr = [1, 'Evelyn', null, true, { papel: 'branca' } ];
+var arr = [ true, 15, 'Daciuk', function() {}, { curso: 'Javascript Ninja' } ];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 
-var qtd = arr.length;
-
 arr.push( { cor: 'cinza', caderno: 'preto' } );
 
 function addItem ( arr ) {
-   while ( qtd > 5 ) {
-     console.log( arr );
-       }
+   arr.push( item );
+  return arr;
   }
 
 /*
@@ -24,21 +22,7 @@ Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 
-arr.push('Oliveira', false, { cor: 'vermelho' } );
-
-arr
-/* [
-  1,
-  'Evelyn',
-  null,
-  true,
-  { papel: 'branca' },
-  { cor: 'cinza', caderno: 'preto' },
-  'Oliveira',
-  false,
-  { cor: 'vermelho' }
-]
-*/
+console.log( addItem ( [ 'Oliveira', false, { cor: 'vermelho' } ] };
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
@@ -46,8 +30,8 @@ frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
 
-console.log('O segundo elemento do segundo array é ' + arr[5].caderno );
-// O segundo elemento do segundo array é preto
+console.log( 'O segundo elemento do segundo array é ' + arr[5][1] + '.' );
+// O segundo elemento do segundo array é preto.
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
@@ -55,14 +39,14 @@ Mostre no console quantos itens tem o primeiro array criado, com a frase:
 */
 
 console.log('O primeiro array tem ' + arr.length + ' itens.');
-// O primeiro array tem 9 itens.
+// O primeiro array tem 6 itens.
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
 
-console.log('O segundo array tem ' + arr[4].papel.length + ' itens.');
+console.log('O segundo array tem ' + arr[5].length + ' itens.');
 // O segundo array tem 6 itens.
 
 /*
@@ -70,10 +54,11 @@ Utilizando a estrutura de repetição `while`, mostre no console todos os númer
 pares entre 10 e 20, inclusive esses 2.
 */
 
-var num = 10;
+var counter = 10;
 
-while(num <= 20) {
-... ( num++ ) % 2 === 0 ? console.log( 'Números pares entre 10 e 20:' + num ) : '';
+while( counter <= 20 ) {
+... counter % 2 === 0 ? console.log( 'Números pares entre 10 e 20:' + counter ) : '';
+... counter++;
 ... }
 
 console.log( 'Números pares entre 10 e 20:');
@@ -82,10 +67,10 @@ console.log( 'Números pares entre 10 e 20:');
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
   
-var num = 10;
+var counter = 10;
 
-while(num <= 20) {
-... ( num++ ) % 2 === 1 ? console.log( 'Números ímpares entre 10 e 20:' + num ) : '';
+while( counter < 20 ) {
+... counter % 2 !== 0 ? console.log( 'Números ímpares entre 10 e 20:' + counter ) : '';
 ... }
 
 console.log( 'Números ímpares entre 10 e 20:' );
@@ -97,15 +82,15 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 
-for( var n = 100; n <= 120; n++ ) {
-... n % 2 === 0 ? console.log( 'Números pares entre 100 e 200:' + n ) : '';
+for( var counter = 100; counter <= 120; counter++ ) {
+... counter % 2 === 0 ? console.log( 'Números pares entre 100 e 200:' + counter ) : '';
 ... }
   
-console.log( 'Números pares entre 100 e 120: ' + n );
+console.log( 'Números pares entre 100 e 120: ' + counter );
 
-for( var n = 111; n <= 125; n++ ) {
-... n % 2 === 1 ? console.log( 'Números ímpares entre 111 e 125:' + n ) : '';
+for( var counter = 111; counter <= 125; counter++ ) {
+... counter % 2 !== 0 ? console.log( 'Números ímpares entre 111 e 125:' + counter ) : '';
 ... }
 
-console.log( 'Números ímpares entre 111 e 125:' );
+console.log( 'Números ímpares entre 111 e 125:' + counter );
 
