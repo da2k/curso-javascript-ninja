cedbe31cf137fabb730fa2e22896f1f9226363e7
Caroline Araujo <carolaraujo@users.noreply.github.com>
2020-06-02 22:45:23 -0300

    challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [1,2,3,4,5];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function funcaoMy (arr) {
+    return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log( funcaoMy(arr) [1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,57 +22,82 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
-
+function blackLivesMatter (arr, index) {
+    return arr [index];
+}
+    console.log(blackLivesMatter);
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var meuArray = ['carol', 26, true, false, null];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(blackLivesMatter(meuArray, 0));
+console.log(blackLivesMatter(meuArray, 1));
+console.log(blackLivesMatter(meuArray, 2));
+console.log(blackLivesMatter(meuArray, 3));
+console.log(blackLivesMatter(meuArray, 4));
 
-/*
-Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
-livro. Dentro dessa função, declare uma variável que recebe um objeto com as
-seguintes características:
-- esse objeto irá receber 3 propriedades, que serão nomes de livros;
-- cada uma dessas propriedades será um novo objeto, que terá outras 3
-propriedades:
-    - `quantidadePaginas` - Number (quantidade de páginas)
-    - `autor` - String
-    - `editora` - String
-- A função deve retornar o objeto referente ao livro passado por parâmetro.
-- Se o parâmetro não for passado, a função deve retornar o objeto com todos
-os livros.
-*/
-// ?
+// Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
+// livro. Dentro dessa função, declare uma variável que recebe um objeto com as
+// seguintes características:
+// - esse objeto irá receber 3 propriedades, que serão nomes de livros;
+// - cada uma dessas propriedades será um novo objeto, que terá outras 3
+// propriedades:
+//     - `quantidadePaginas` - Number (quantidade de páginas)
+//     - `autor` - String
+//     - `editora` - String
+// - A função deve retornar o objeto referente ao livro passado por parâmetro.
+// - Se o parâmetro não for passado, a função deve retornar o objeto com todos
+// os livros.
+
+ function book(nomeDoLivro) {
+     var todosOsLivros = {
+         'livro1': {
+             quantidadePaginas: 266,
+             autor: 'Caroline Araujo',
+             editora: 'Araujo Edições'
+         },
+         'livro2': {
+             quantidadePaginas: 688,
+             autor: 'Christopher Bank',
+             editora: 'Investments'
+         },
+         'livro3': {
+             quantidadePaginas: 1080,
+             autor: 'Le Pictor',
+             editora:'Picture Falls'
+         }
+        };
+
+        return !nomeDoLivro ? todosOsLivros : todosOsLivros [nomeDoLivro];
+    }
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro 1 tem ' + book('livro1').quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro 2 é ' + book('livro2').autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( 'O livro ' + nomeDoLivro + ' foi publicado pela editora ' + book(nomeDoLivro).editora + '.');
