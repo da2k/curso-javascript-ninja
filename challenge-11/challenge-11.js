622ff65b2244c7d79dba2929688c60bcf7cf8de0
Emerson <emerson_c.morais@hotmail.com>
2020-06-11 23:05:52 -0300

    challenge-11

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,54 +1,97 @@
-// Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
+(function () {
+  // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
 
-/*
-Crie uma variável chamada `once`, que recebe `false` como seu valor.
+  /*
+Crie uma varável chamada once, que recebe false como seu valor.
 Crie um loop que execute enquanto essa variável for verdadeira. Dentro do
 loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
-// ?
 
-/*
-Crie um objeto chamado `person`, que receba as seguintes propriedades:
+  const once = false;
+
+  do {
+    console.log('Entrou ao menos uma vez');
+  } while (once);
+
+  /*
+Crie um objeto chamado person, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
-// ?
+  const person = {
+    name: 'Emerson ',
+    age: 31,
+    wight: 78,
+    birthday: '20/10/1989',
+  };
 
-/*
+  /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
 a frase:
 'The [PROPERTY] of person is [VALUE]'
-Aproveite e crie uma variável `counter` que vai contar quantas propriedades
+Aproveite e crie uma variável counter que vai contar quantas propriedades
 esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
-// ?
+  let counter = 0;
+  for (prop in person) {
+    let objProps = person[prop];
+    console.log(`The ${prop} of person is ${objProps}`);
+    counter++;
+  }
 
-/*
-Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
+  console.log(`The person has ${counter} properties`);
+
+  /*
+Crie uma função chamada moreThan, que vai verificar se a pessoa (objeto
 criado acima) é mais velha que a idade passada por parâmetro.
-Se verdadeiro, retornar `true`. Senão, retornar false.
+Se verdadeiro, retornar true. Senão, retornar false.
 Após a função, mostrar a mensagem no console:
 'The person has more than 25 years old? [TRUE/FALSE]'
 */
-// ?
 
-/*
+  const moreThan = (age) => {
+    const moreThanAge = age < person.age ? true : false;
+    return `The person has more than 25 years old ? => ${moreThanAge}` ;
+  };
+
+  console.log(moreThan(25));
+
+  /*
 Faça um loop de 0 a 20, que adicione cada número como um item de um
-array chamado `numbers`. Se o contador for maior que 10, saia do loop.
+array chamado numbers. Se o contador for maior que 10, saia do loop.
 Mostre no console os números no array.
 */
-console.log( 'De 0 a 10:' );
-// ?
 
-/*
-Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
+  let arrayNumbers = [];
+
+  for (counter = 0; counter <= 20; counter++) {
+    arrayNumbers.push(counter);
+    if (counter >= 10) {
+      break;
+    }
+  }
+
+  console.log(`De 0 a 10: ${arrayNumbers}`);
+
+  /*
+Faça outro loop de 0 a 20, que adicione a um array chamado numbers (já
 criado acima, só precisa ser reiniciado) números de 0 a 20, inclusive
 esses. Se o número for ímpar, pular para o próximo número.
 Mostrar no console os números do array.
 */
-console.log( 'Pares de 0 a 20:' );
-// ?
+
+  let arrayNumbers2 = [];
+
+  for (counter = 0; counter <= 20; counter++) {
+    if (counter % 2 === 1) {
+      continue;
+    }
+    arrayNumbers2.push(counter);
+  }
+
+  console.log(`Pares de 0 a 20: ${arrayNumbers2}`);
+})();
