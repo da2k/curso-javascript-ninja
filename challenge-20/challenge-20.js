16c36dd3aeaa270e1b423f249721fa6396f050f1
Maicon <maicon_souza09@hotmail.com>
2020-07-26 11:51:34 -0300

    Challenge 20

--- challenge-20/challenge-20.js
+++ challenge-20/challenge-20.js
@@ -1,3 +1,6 @@
+(function(doc, win){
+'use strict'
+
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -14,43 +17,52 @@ resultado à uma variável chamada `username`. Se o usuário não digitar um
 nome, `username` deve receber "Desconhecido".
 Com a resposta, mostre um alert com a mensagem "Bem vindo [USERNAME]!"
 */
-// ?
+
+ var username = prompt('Qual seu nome?')
+
+username === '' ? alert(`Desconhecido`) : alert(`Bem-vindo ${username}`)
+    
+
+
 
 /*
 Agora, pergunte ao usuário "Qual o seu e-mail?", atribuindo o resultado à
 uma variável chamada `email`.
 */
-// ?
+var email = prompt('Qual seu e-mail ?')
 
 /*
 - Selecione o input de "Nome", atribuindo-o à uma variável chamada
 `$inputUsername`.
 */
-// ?
+var $inputUsername = doc.querySelector('input[type="text"]')
 
 /*
 - Selecione o input de "Email", atribuindo-o à uma variável chamada
 `$inputEmail`.
 */
-// ?
+var $inputEmail = doc.querySelector('input[type="email"]')
+
 
 /*
 - Selecione o campo de "Mensagem", atribuindo-o à uma variável chamada
 `$message`.
 */
-// ?
+var $message= doc.querySelector('textarea')
+
 
 /*
 - Selecione o botão de envio do formulário, atribuindo-o à uma variável
 chamada `$button`.
 */
-// ?
+var $button = doc.querySelector('button[type="submit"]')
 
 /*
 Preencha os campos de "Nome" e "Email" que estão no documento com os valores
 entrados pelo usuário.
 */
-// ?
+$inputUsername.value = username
+$inputEmail.value = email
 
 /*
 Adicione um listener de evento de click ao botão que faça o seguinte:
@@ -104,4 +116,29 @@ Alguns e-mails inválidos:
     - "rita-marica@titica.a.b"
     - "agua_@evida.br.com"
 */
-// ?
+$button.addEventListener('click', function(event){
+    event.preventDefault()
+    if (!$inputUsername.value){
+       return alert('Preencha o nome do usuário!')
+    }
+
+    if (!$inputEmail.value){
+        return alert('Preencha seu email!')
+    }
+
+    if (!isValidEmail($inputEmail.value)){
+        return alert('Entre com um email válido')
+    }
+
+    if(!confirm('Tem certeza que deseja enviar o formulário?')){
+        return alert('Não enviado!!!') 
+    }
+
+    alert ('Enviado com sucesso!!!')
+}, false)
+
+function isValidEmail (email){
+    return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/gm.test(email)
+}
+
+}(document, window))
