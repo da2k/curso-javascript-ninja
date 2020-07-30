34429ed4586851843b42f3fee4fdfa82191e9bdb
Felipe Medeiros <fliphc@gmail.com>
2020-07-30 16:39:35 -0300

    Update challenge-12.js

--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -1,3 +1,4 @@
+(function () {
 /*
 Envolva todo o conteúdo desse arquivo em uma IIFE.
 */
@@ -11,18 +12,19 @@ Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
 de valor para cada propriedade.
 */
 // ?
-console.log( 'Propriedades de "person":' );
+let person = {name: 'Felipe', lastname: 'Medeiros', age: 31}
+
 
 /*
 Mostre no console, em um array, todas as propriedades do objeto acima.
 Não use nenhuma estrutura de repetição, nem crie o array manualmente.
 */
-// ?
+console.log('Propriedades de "person":', Object.keys(person));
 
 /*
 Crie um array vazio chamado `books`.
 */
-// ?
+let books = []
 
 /*
 Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
@@ -30,49 +32,57 @@ seguintes propriedades:
 `name`: String
 `pages`: Number
 */
-// ?
-console.log( '\nLista de livros:' );
+let book1 = {name: 'book1 title', pages: 1};
+let book2 = {name: 'book2 title', pages: 2}
+let book3 = {name: 'book3 title', pages: 3}
+books.push(book1, book2, book3);
+
 
 /*
 Mostre no console todos os livros.
 */
-// ?
+console.log('\nLista de livros:', books);
 
-console.log( '\nLivro que está sendo removido:' );
+
 /*
 Remova o último livro, e mostre-o no console.
 */
+console.log('\nLivro que está sendo removido:', books.pop());
 // ?
 
-console.log( '\nAgora sobraram somente os livros:' );
 /*
 Mostre no console os livros restantes.
 */
-// ?
+console.log( '\nAgora sobraram somente os livros:', books);
 
 /*
 Converta os objetos que ficaram em `books` para strings.
 */
-// ?
-console.log( '\nLivros em formato string:' );
+var booksStr = JSON.stringify(books)
 
 /*
 Mostre os livros nesse formato no console:
 */
-// ?
+console.log('\nLivros em formato string:', booksStr);
 
 /*
 Converta os livros novamente para objeto.
 */
-// ?
-console.log( '\nAgora os livros são objetos novamente:' );
+booksStr = JSON.parse(booksStr)
+console.log('\nAgora os livros são objetos novamente:', booksStr);
 
 /*
 Mostre no console todas as propriedades e valores de todos os livros,
 no formato abaixo:
     "[PROPRIEDADE]: [VALOR]"
 */
-// ?
+var x = 0;
+for (prop in books) {
+  console.log(x)
+  console.log(books[x].name)
+  x++;
+}
+
 
 /*
 Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
@@ -98,3 +108,4 @@ console.log( '\nAgora em ordem alfabética:' );
 Mostre todos os itens do array acima, odenados alfabéticamente.
 */
 // ?
+})();
