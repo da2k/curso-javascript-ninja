0a66043d912f28f41f3d513c4218c9b96fc0c1ee
Wellington Fernando da Silva <prico182@gmail.com>
2020-09-16 21:18:43 -0300

    Challenge 14

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -1,3 +1,6 @@
+( function() {
+
+
 /*
 Envolva todo o código desse desafio em uma IIFE.
 */
@@ -10,14 +13,25 @@ Os números devem ser de 1 a 10.
 Mostre esse array no console.
 */
 console.log( 'Number Objects Array:' );
-// ?
+
+var numberObjects = [];
+
+for ( var i = 1; i <= 10; i++ ) {
+    numberObjects.push( { number: i } );
+}
+
+console.log( numberObjects );
 
 /*
 Crie um array chamado `justNumbers`, que terá como elementos somente os
 números do array criado acima. Mostre esse novo array no console.
 */
 console.log( '\nJust Numbers:' );
-// ?
+var justNumbers = numberObjects.map( function( elem ) {
+    return elem.number;
+} );
+
+console.log( justNumbers );
 
 /*
 Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
@@ -25,7 +39,11 @@ somente os números que forem divisíveis por 2 ou 3. Mostre esse novo array
 no console.
 */
 console.log( '\nJust module of division by 2 or 3:' );
-// ?
+var justMod2Or3 = justNumbers.filter( function( elem ) {
+    return elem % 2 === 0 || elem % 3 === 0;
+});
+
+console.log( justMod2Or3 );
 
 /*
 Declare uma variável chamada operation que receba, do array criado acima,
@@ -36,7 +54,11 @@ O cálculo deve começar com zero.
 Mostre o resultado no console.
 */
 console.log( '\nOperation:' );
-// ?
+var operation = justMod2Or3.reduce( function( prev, next ) {
+    return ( prev + 1 ) * next;
+}, 0);
+
+console.log( operation );
 
 /*
 Faça o mesmo cálculo passado acima, mas começando do último item para o
@@ -44,7 +66,11 @@ primeiro. O nome da variável deve ser operation2. Mostre o resultado no
 console.
 */
 console.log( '\nOperation 2:' );
-// ?
+var operation2 = justMod2Or3.reduceRight( function( prev, next ) {
+    return ( prev + 1 ) * next;
+}, 0);
+
+console.log( operation2 );
 
 /*
 Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
@@ -55,20 +81,29 @@ infantil, onde você coloca a letra "P" antes de cada sílaba de uma palavra
 falada, como se você estivesse falando em código xD
 */
 console.log( '\nSeu nome na língua do "P":' );
-// ?
+var nome = [ 'Wel', 'ling', 'ton' ];
+
+var linguaP = nome.reduce( function( prev, next ) {
+    return  prev + 'P' + next;
+}, '' );
+
+console.log( linguaP );
 
 /*
 Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
 e atribuirá o seu nome invertido (usando o array criado acima).
 */
 console.log( '\nInversed Name:' );
-// ?
+var inversedName = nome.reduceRight( function( prev, next ) {
+    return prev + next;
+} );
 
+console.log( inversedName );
 /*
 Mostre no console o array `numberObjects`.
 */
 console.log( '\nNumber objects' );
-// ?
+console.log( numberObjects );
 
 /*
 Verifique se existem em algum índice de numberObjects um objeto ìgual a
@@ -80,18 +115,32 @@ Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
 o que acontece ;)
 */
 console.log( '\nExiste um { number: 2 } em numberObjects?' );
-// ?
+if ( numberObjects.indexOf( { number: 2 } ) > -1 ) {
+    console.log( 'Existe um objeto { number: 2 } em numberObjects!' );
+ }
+ else {
+    console.log( 'Não existe um objeto { number: 2 } em numberObjects :(' );
+ }
 
 /*
 Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
 será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
 */
 console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-// ?
+if ( numberObjects.lastIndexOf( { number: 2 }, 2 ) > -1 ) {
+    console.log( 'Existe um objeto { number: 2 } em numberObjects!' );
+ }
+ else {
+    console.log( 'Não existe um objeto { number: 2 } em numberObjects :(' );
+ }
 
 /*
 Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
 formato de String.
 */
 console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-// ?
+if ( Array.isArray(justMod2Or3) ) {
+    justMod2Or3.toString();    
+} 
+
+} )();
