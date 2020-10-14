d64021eb24e837cc474affe5361c76db208cab02
Lucas Ferreira <lucasferreira9b@gmail.com>
2020-10-14 18:29:43 -0300

    update

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,8 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [1, 'lk', true, null, undefined];
+
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
@@ -12,7 +13,7 @@ Crie uma função que receba um array como parâmetro, e retorne esse array.
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(arr);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
