e7a8c5c60e2e9af41198e61519863218dc260049
AndreBarroso <andre_barroso17@hotmail.com>
2020-10-28 19:27:48 -0300

    Challenge 005

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr=[1,'xx','b', 'lala']
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function retArr(a){
+	return a;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(retArr(arr)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,23 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function newf(array,num){
+	return array[num];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var newArray=['x',1,null,false,undefined];
+
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+for(var i=0;i<5;i++){
+	console.log(newf(newArray,[i]));
+};
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +54,52 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName){
+	var allBooks= {
+				'Segredos do Ninha Javascript': {
+					quantidadePaginas: 488,
+					autor: 'John Resig & Bear Beault',
+					editora: 'Novatec',
+				},
+				'Introdução ao HTML5':{
+					quantidadePaginas: 220 ,
+					autor: 'Bruce Lawson & Remy Sharp',
+					editora: 'Alta Books',
+				},
+				'Smasshig CSS': {
+					quantidadePaginas: 283,
+					autor: 'Erick A. Meyer',
+					editora: 'Bookman',
+				}
 
+	};
+	if (bookName===undefined){
+		return allBooks;
+	}
+	return allBooks[bookName];
+}
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Segredos do Ninha Javascript tem '+ book('Segredos do Ninha Javascript').quantidadePaginas +  ' páginas!')
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro Segredos do Ninha Javascript é ' + book('Segredos do Ninha Javascript').autor + ' .');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro Segredos do Ninha Javascript foi publicado pela editora ' + book('Segredos do Ninha Javascript').editora + ' .');
