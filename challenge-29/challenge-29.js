b395b54fcc49349a01cb5bfacd288891d378bf73
Felipe dos Anjos <felipe.b.abencoado@gmail.com>
2020-10-31 10:25:35 -0300

    challenge-29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,6 +1,5 @@
-(function() {
+(function( $ ) {
   'use strict';
-
   /*
   Vamos estruturar um pequeno app utilizando módulos.
   Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
@@ -36,4 +35,92 @@
   que será nomeado de "app".
   */
 
-})();
+  function app () {
+    var ajax = new XMLHttpRequest();
+    var $title = $( '[data-js="title"]' );
+    var $phone = $( '[data-js="phone"]' );
+    var $carFeatures = $( '[data-js="input"]' );
+    var $button = $( '[data-js="button"]' );
+    var $carSalon = $( '[data-js="car-salon"]' );
+
+    function initAddEvents () {
+      ajax.addEventListener( 'readystatechange' , handleAjax );
+      $button.on( 'click' , handleNewCar );
+    }
+
+    function chargeCompanyTitleAndPhone () {
+      var response = JSON.parse( ajax.responseText );
+
+      $title.get().textContent = response.name;
+      $phone.get().textContent = response.phone;
+    }
+
+    function handleAjax () {
+      if ( isRequestOk() ) {
+        try {
+          chargeCompanyTitleAndPhone();
+        } catch ( e ) {
+          console.log(e);
+          alert('Ops... Algo deu errado, tente mais tarde!')
+        }
+      }
+    }
+
+    function openAjaxRequest () {
+      ajax.open( 'GET' , 'company.json' );
+      ajax.send( null );
+    }
+
+    function isRequestOk () {
+      return ajax.status === 200 && ajax.readyState === 4;
+    }
+
+    function handleNewCar () {
+      var $fragment = document.createDocumentFragment();
+      var $divMain = document.createElement( 'div' );
+      $divMain.className = 'table';
+      $carFeatures.forEach( function ( item , index ) {
+      item.hasAttribute('data-id') ?
+        handleImage( item , index , $divMain , $fragment ) :
+        handleElements( item , index , $divMain );
+      } )
+      $carSalon.get().appendChild( $fragment );
+    }
+
+    function handleImage ( item , index , $divMain , $fragment ) {
+      var $img = document.createElement( 'img' )
+      $img.setAttribute( 'src' , item.value )
+      $img.style = handleStyleTable( index );
+      $divMain.appendChild( $img );
+      $fragment.appendChild( $divMain );
+    }
+
+    function handleElements ( item , index , $divMain ) {
+      var $div = document.createElement( 'div' );
+      $div.style = handleStyleTable( index )
+      $div.textContent = item.value;
+      $divMain.appendChild( $div )
+    }
+
+    function handleStyleTable ( index ) {
+      var obj = [
+        function model () { return 'grid-area: 1 / 2'; },
+        function year () { return 'grid-area: 1 / 5'; },
+        function plate () { return 'grid-area: 1 / 4'; },
+        function color () { return 'grid-area: 1 / 3'; },
+        function image () { return 'grid-area: 1 / 1'; }
+      ];
+
+      return obj[ index ]();
+    }
+
+    return {
+      init: function () {
+        openAjaxRequest();
+        initAddEvents();
+      }
+    }
+  }
+
+  app().init();
+})( window.DOM );
