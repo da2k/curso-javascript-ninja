da5d10cb5e6c6e47ac5c76fdc9eaf0dc8f7d00db
Wellington Fernando da Silva <prico182@gmail.com>
2020-12-08 21:25:52 -0300

    challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,6 @@
+(function( win, doc ) {
+  'use strict';
+  
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +28,137 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  function DOM( selector ) {
+    this.element = doc.querySelectorAll( selector );
+  }
+  
+  DOM.prototype.on = function( eventType, callback ) {
+    this.forEach( function( elem ) {
+        elem.addEventListener( eventType, callback, false );
+    } );
+  };
+  
+  DOM.prototype.off = function( eventType, callback ) {
+    this.forEach( function( elem ) {
+        elem.removeEventListener( eventType, callback, false );
+    } );
+  };
+  
+  DOM.prototype.get = function() {
+    return this.element;
+  };
+  
+  [ 'forEach', 'map', 'filter', 'reduce', 'reduceRight', 'every', 'some' ].forEach( function( fn ) {
+    DOM.prototype[fn] = function( callback ) {
+        return Array.prototype[fn].call( this.element, callback );
+    }
+  } );
+  
+  [ 'isArray', 'isObject', 'isFunction', 'isNumber', 'isString', 'isBoolean' ].forEach( function( fn  ) {
+    DOM[fn] = function( arg ) {
+        return Object.prototype.toString.call( arg ) === '[object ' + fn.replace('is', '') + ']';
+    };
+  } );
+  
+  DOM.isNull = function( arg ) {
+    return Object.prototype.toString.call( arg ) === '[object Null]' ||   Object.prototype.toString.call( arg ) === '[object Undefined]';
+  }
+  
+
+    var $form = new DOM( '[data-js="form"]' );
+    $form.on( 'submit', handleSubmit );
+
+    var $cep = new DOM( '[data-js="cep"]' );
+    var cep;
+
+    var $status = new DOM( '[data-js="status"]' ).get()[0];
+    var $logradouro = new DOM( '[data-js="logradouro"]' ).get()[0];
+    var $bairro = new DOM( '[data-js="bairro"]' ).get()[0];
+    var $cidade = new DOM( '[data-js="cidade"]' ).get()[0];
+    var $estado = new DOM( '[data-js="estado"]' ).get()[0];
+    var $cep2 = new DOM( '[data-js="cep2"]' ).get()[0];
+
+    function handleSubmit( e ) {
+      e.preventDefault();
+
+      if ( !clearCEP() ) {
+        $status.textContent = 'Caracteres inválidos';
+  
+        return;
+      } 
+      
+      request();
+    }
+    
+    function clearCEP() {
+      cep = $cep.get()[0].value.replace( /\D/g, '' ).replace( /(\d{3})$/, '-$1' );
+
+      return cep.length > 0 ? true : false;
+    }
+
+    function request() {            
+        var xhr = new XMLHttpRequest();
+        if ( xhr ) {
+            getMessage( 'loading' );
+
+            xhr.addEventListener( 'readystatechange', function() {
+              handleReadyStateChange( xhr );
+            }, false );
+        }
+
+        xhr.open( 'GET', 'https://ws.apicep.com/cep/'+ cep + '.json', false );
+        xhr.send(null);
+    }
+
+    function handleReadyStateChange( xhr ) {
+      if ( xhr.readyState == 4 ) {
+        handleResponse( xhr );
+      }
+    }
+
+    function handleResponse( xhr ) {
+        if ( xhr.status == 200 || xhr.status == 304 ) {
+          showResponse( xhr.responseText );
+        }
+        else {
+          alert("Página não encontrada");
+          $status.textContent = 'URL inválida';
+        }
+    }
+
+    function showResponse( response ) {
+      response = JSON.parse( response );
+
+      if ( response.status == 200 ) {
+        getMessage( 'ok' );
+
+        $logradouro.value = response.address;
+        $bairro.value = response.district;
+        $cidade.value = response.city;
+        $estado.value = response.state;
+        $cep2.value = response.code;
+      }
+      else {
+        getMessage( 'error' );
+
+        $logradouro.value = '';
+        $bairro.value = '';
+        $cidade.value = '';
+        $estado.value = '';
+        $cep2.value = '';
+      }
+    }
+
+    function getMessage( type ) {
+        var messages = {
+          loading: 'Buscando informações para o CEP ' + cep + '...',
+          ok: 'Endereço referente ao CEP ' + cep,
+          error: 'Não encontramos o endereço para o CEP ' + cep
+        }
+
+        $status.textContent = messages[type];
+    }
+
+
+  } )( window, document);
