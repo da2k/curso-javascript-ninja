897d13480f5e73f14f7af81e864db2702e065ce0
Julia Ramos <julia.souza@ahgora.com.br>
2021-02-18 01:15:39 -0300

    resolucao challenge-13

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -75,25 +75,25 @@
   Remova de `brasil` os estados do `sudeste`, colocando-os em uma variável
   chamada `newSudeste`.
   */
-
+  const newSudeste = brasil.splice(5,4);
 
   /*
   Adicione os estados do `nordeste` ao array `brasil`. Esses estados devem
   ficar no mesmo nível que os estados já existentes, não em um array separado.
   */
-  // ?
+  brasil = brasil.concat(nordeste);
 
   /*
   Mostre no console os estados em `newSudeste`.
   */
   console.log( '\nEstados em newSudeste:' );
-  // ?
+  console.log(newSudeste);
 
   /*
   Mostre no console os estados do `brasil`.
   */
   console.log( '\nAlguns estados do Brasil:' );
-  // ?
+  console.log(brasil);
 
   /*
   usando forEach, percorra o array `brasil` e gere um novo array chamado
@@ -102,13 +102,19 @@
   - `id`: que será o índice do array `brasil`,
   - `estado`: que será o estado do array `brasil`.
   */
-  // ?
+  const newBrasil = [];
+  brasil.forEach(function(item, index){
+    newBrasil.push({
+      id: index,
+      estado: item
+    });
+  });
 
   /*
   Mostre o array `newBrasil` no console
   */
   console.log( '\nnewBrasil:' );
-  // ?
+  console.log(newBrasil);
 
   /*
   Percorra o array `brasil` e verifique se os estados tem mais de 7 letras cada,
@@ -118,7 +124,15 @@
   - "Nem todos os estados tem mais de 7 letras!"
   */
   console.log( '\nTodos os estados de `brasil` tem mais de 7 letras?' );
-  // ?
+  const every = brasil.every(function(item) {
+    return item.length > 7;
+  });
+
+  console.log(
+    every
+    ? 'Sim, todos os estados tem mais de 7 letras!'
+    : 'Nem todos os estados tem mais de 7 letras!'
+  );
 
   /*
   Percorra o array `brasil` e verifique se o Ceará está incluído, atribuindo o
@@ -129,7 +143,15 @@
   - "Ceará não foi incluído :("
   */
   console.log( '\nCeará está incluído em `brasil`?' );
-  // ?
+  const some = brasil.some(function(item) {
+    return item === 'Ceará';
+  });
+  console.log(
+    some ? 'Ceará está incluído!' : 'Ceará não foi incluído :('
+  );
+  // se precisa verificar TODOS OS ITENS array batem com uma expressão, uso o every
+  // se precisa verificar que pelo menos UM ITEM bate com uma expressão, uso o some
+  // se precisa mapear TODOS OS ITENS de um array e modificar todos eles, usa o map
 
   /*
   Percorra o array `newBrasil` e crie um novo array que some 1 no ID de cada
@@ -137,23 +159,47 @@
   - "[ESTADO] pertence ao Brasil."
   Atribua o novo array a uma variável chamada `map`.
   */
-  // ?
+  const map = newBrasil.map(function(item, index){
+    // item.id++;
+    // item.estado += ' pertence ao Brasil';
+    // return item;
+    return {
+      id: item.id + 1,
+      estado: item.estado + ' pertence ao Brasil'
+    };
+  });
+
 
   /*
   Mostre no console o array criado acima:
   */
   console.log( '\nnewBrasil agora com mais informações:' );
-  // ?
+  console.log(map)
 
   /*
   Filtre o array criado acima, retornando somente os estados que tiverem
   ID par. Atribua o valor à uma variável chamada `filter`.
   */
-  // ?
+  const filter = map.filter(function(item, index) {
+    return item.id % 2 === 0;
+  });
 
   /*
   Mostre o array filtrado acima no console.
   */
   console.log( '\nEstados com ID par:' );
-  // ?
+  console.log(filter);
 })();
+
+// filter retorna o item se a expressão for verdadeira
+// map retorna exatamente o que passar para a função
+// some retorna se algum item da expressão for verdadeira
+// every só retorna true se a expressão for verdadeira pra todos os item
+// forEach parecido com o for itera em cada item e nao retorna nada
+// concat concatena itens no array
+// splice adiciona ou remove itens do array, baseado em 1 parametro o indice da onde vai começar
+// a adicionar ou remover, o segundo paramentro a quantidade de itens, e os 3,4,5 parametro itens que serão adicionados no array
+// slice cria um novo array com os itens, primeiro parametro indice da onde vai pegar os itens, e o segundo o indice onde vai parar de mostrar os itens(nao mostra o ultimo item)
+// shift remove do inicio do array
+// unshift adiciona no inicio do array
+// toString converte array para string
