89878e721fcb56e79d3b1f79986dfcca2004a66c
Tél Noronhes <73230985+matheus1994santos@users.noreply.github.com>
2021-03-09 11:46:10 -0300

    Exercicios 6

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,10 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+
+var championship = 'baiano';
+
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +15,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = [ 'Bahia de Feira', 'Atlético', 'Juazeirense', 'Unirb', 'Vitoria' ]
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,35 +35,71 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
 
-/*
-Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
-função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
-*/
-// ?
+function showTeamPosition(x,y) {
+    switch(x){
+        case 1:
+          console.log('O time que está em 1º lugar é o ' + teams[0] + '.');
+        break;
+        case 2:
+          console.log('O time que está em 2º lugar é o ' + teams[1] + '.');
+        break;
+        case 3:
+          console.log('O time que está em 3º lugar é o ' + teams[2] + '.');
+        break;
+        case 4:
+          console.log('O time que está em 4º lugar é o ' + teams[3] + '.');
+        break;
+        case 5:
+          console.log('O time que está em 5º lugar é o ' + teams[4] + '.');
+        break;  
+        default:
+            console.log('Não temos a informação do time que está nessa posição.')
+    }
 
-/*
-Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
-repetição "while".
-*/
-// ?
-
-/*
-Crie uma função chamada `convertToHex`, com as seguintes características:
-    - A função recebe uma cor por parâmetro, do tipo string. Exemplo: "red";
-    - Escolha 5 cores que serão convertidas do nome da cor para o seu
-    equivalente hexadecimal (pode ser qualquer tom);
-    - Usando a estrutura switch, verifique se a cor passada por parâmetro é
-    algum hexa escolhido. Se for, retorne a frase:
-    "O hexadecimal para a cor [COR] é [HEXADECIMAL].";
-    - Se a cor passada por parâmetro não estiver entre as selecionadas, mostre
-    a frase:
-    "Não temos o equivalente hexadecimal para [COR]."
-*/
-// ?
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+function corHexadecimal(y){
+    var cores = { 
+        'pink': '#ffc0cb', 
+        'black': '#000000', 
+        'blue': '#0000ff', 
+        'green': '#008000', 
+        'yellow': '#ffff00', 
+        'purple': '#800080',
+        'red': '#ff0000',
+        'gray': '#808080', 
+    }
+
+    switch(y){
+        case 'pink':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;
+        case 'black':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;
+        case 'blue':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;
+        case 'green':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;
+        case 'yellow':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;  
+        case 'purple':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;
+        case 'red':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;
+        case 'gray':
+            console.log('A cor ' + y + ' tem como codigo hexadecimal ' + cores[y] + '.');
+        break;  
+        default:
+            console.log('Não temos a informação do Hexadeximal para essa cor.')
+    }
+}
