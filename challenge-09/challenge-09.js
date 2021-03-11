a27d6d6277127eb80ba84ee5b4d7ed871c63d720
BrunoSilva-Correia <65832022+BrunoSilva-Correia@users.noreply.github.com>
2021-03-10 21:43:12 -0300

    Update challenge-09.js

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -11,11 +11,11 @@
   correto da variável ou função chamada.
   */
   function myFunction() {
-      console.log( 'Na função `myFunction`, o primeiro número é', number1 );
-      console.log( 'Na função `myFunction`, o segundo número é', number2 );
-      var number1 = 10;
-      return number1 + number2;
-      var number2 = 20;
+    var number1 = 10;
+    var number2 = 20;
+    console.log( 'Na função `myFunction`, o primeiro número é', number1 );
+    console.log( 'Na função `myFunction`, o segundo número é', number2 );
+    return number1 + number2;
   }
   myFunction();
 
@@ -23,13 +23,13 @@
       myFunction2();
   */
   function myFunction2() {
-      console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
-      var sum = function sum() {
-          return number1 + number2;
-      };
-      var number1 = 10;
-      var number2 = 20;
-      return sum();
+    var number1 = 10;
+    var number2 = 20;
+    var sum = function sum() {
+        return number1 + number2;
+    };
+    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+    return sum();
   }
   myFunction2();
 
@@ -37,14 +37,15 @@
       myFunction3();
   */
   function myFunction3() {
-      console.log( 'A soma de 40 e 50 é igual a', sum() );
-      var number2 = 50;
-      console.log( 'Na função myFunction3, number1 é igual a', number1 );
-      var number1 = 40;
-      return sum();
-      function sum() {
-          return number1 + number2;
-      };
+    function sum() {
+        return number1 + number2;
+    }
+
+    var number1 = 40;
+    var number2 = 50;
+    console.log( 'A soma de 40 e 50 é igual a', sum() );
+    console.log( 'Na função myFunction3, number1 é igual a', number1 );
+    return sum();
   }
   myFunction3();
 
