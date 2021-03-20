9274a9b0b68e219750aa83f6262da2a24aea5dae
Kayque Almeida <38868120+kayquealmeida@users.noreply.github.com>
2021-03-20 14:07:27 -0300

    Update challenge-23.js

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,128 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function (DOM, doc) {
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+  'use strict';
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+  /*
+  No HTML:
+  - Crie um formulário com um input de texto que receberá um CEP e um botão
+  de submit; --- OK
+  - Crie uma estrutura HTML para receber informações de endereço:
+  "Logradouro, Bairro, Estado, Cidade e CEP." Essas informações serão
+  preenchidas com os dados da requisição feita no JS. --- OK
+  - Crie uma área que receberá mensagens com o status da requisição:
+  "Carregando, sucesso ou erro." --- OK
+
+  No JS:
+  - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
+  deve ser limpo e enviado somente os números para a requisição abaixo; --- Ok
+  - Ao submeter esse formulário, deve ser feito um request Ajax para a URL:
+  "https://ws.apicep.com/cep/CepDeExemplo.json", onde [CEP] será o CEP passado
+  no input criado no HTML; -- OK
+  - Essa requisição trará dados de um CEP em JSON. Preencha campos na tela
+  com os dados recebidos. --- OK
+  - Enquanto os dados são buscados, na área de mensagens de status, deve mostrar
+  a mensagem: "Buscando informações para o CEP [CEP]..." --- OK
+  - Se não houver dados para o CEP entrado, mostrar a mensagem:
+  "Não encontramos o endereço para o CEP [CEP]." --- OK
+  - Se houver endereço para o CEP digitado, mostre a mensagem:
+  "Endereço referente ao CEP [CEP]:" --- OK
+  - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
+  adicionar as informações em tela. --- OK
+  */
+
+  var app = function () {
+
+    var $formCep = new DOM('[data-js="form-cep"]');
+    var $inputReceberCep = new DOM('[data-js="inputReceberCep"]');
+    var $mensagemStatus = new DOM('[data-js="mensagem-status"]');
+    var $inserirLogradouro = new DOM('[data-js="logradouro"]');
+    var $inserirBairro = new DOM('[data-js="bairro"]');
+    var $inserirEstado = new DOM('[data-js="estado"]');
+    var $inserirCidade = new DOM('[data-js="cidade"]');
+    var $inserirCep = new DOM('[data-js="cep"]');
+    var ajax = new XMLHttpRequest();
+
+    $formCep.on('submit', handleSubmit);
+
+    function handleSubmit(event) {
+      event.preventDefault();
+      var url = getUrl();
+      ajax.open('GET', url);
+      ajax.send(null);
+      statusMessage('loading');
+      ajax.addEventListener('readystatechange', handleInformation);
+    }
+
+    function handleInformation() {
+      if (isRequestOk()) {
+        statusMessage('ok')
+        fillFields();
+      }
+    }
+
+    function isRequestOk() {
+      return ajax.status === 200 && ajax.readyState === 4;
+    }
+
+    function statusMessage(type) {
+      var messages = {
+        loading: replaceCep('Loading information about this [CEP]!'),
+        ok: replaceCep('Information about this [CEP]!'),
+        error: replaceCep('[CEP] not found!!')
+      };
+
+      return $mensagemStatus.get()[0].textContent = messages[type];
+    }
+
+    function clearFields() {
+      $inserirLogradouro.get()[0].textContent = '404 - Not Found!!';
+      $inserirBairro.get()[0].textContent = '404 - Not Found!!';
+      $inserirEstado.get()[0].textContent = '404 - Not Found!!';
+      $inserirCidade.get()[0].textContent = '404 - Not Found!!';
+      $inserirCep.get()[0].textContent = '404 - Not Found!!';
+    }
+
+    function fillFields() {
+      var data = parsedData();
+      if(data.status === 400){
+        return clearFields()
+      }
+      $inserirLogradouro.get()[0].textContent = data.address;
+      $inserirBairro.get()[0].textContent = data.district;
+      $inserirEstado.get()[0].textContent = data.state;
+      $inserirCidade.get()[0].textContent = data.city;
+      $inserirCep.get()[0].textContent = data.code;
+    }
+
+    function parsedData() {
+      var result;
+
+      try {
+        result = JSON.parse(ajax.responseText);
+      } catch {
+        result = null;
+      }
+
+      return result;
+    }
+
+    function clearCep() {
+      return $inputReceberCep.get()[0].value.replace(/\D/g, '');
+    }
+
+    function getUrl() {
+      return replaceCep('https://ws.apicep.com/cep/[CEP].json');
+    }
+
+    function replaceCep(urlToTreat) {
+      return urlToTreat.replace('[CEP]', clearCep());
+    }
+
+
+  }
+
+  window.app = app;
+  app();
+
+
+})(window.DOM, document)
