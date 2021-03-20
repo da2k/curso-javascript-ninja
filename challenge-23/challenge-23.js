fa7eec7d46cdd8253f6c8cad2802897c237fb36c
Kayque Almeida <38868120+kayquealmeida@users.noreply.github.com>
2021-03-20 14:05:30 -0300

    Update challenge-23.js

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,106 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function (win, doc) {
+  'use strict';
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+  /*
+  Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
+  o código, conforme vimos na aula anterior. Quebrar as responsabilidades
+  em funções, onde cada função faça somente uma única coisa, e faça bem feito.
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+  - Remova as duplicações de código;
+  - agrupe os códigos que estão soltos em funções (declarações de variáveis,
+  listeners de eventos, etc);
+  - faça refactories para melhorar esse código, mas de forma que o mantenha com a
+  mesma funcionalidade.
+  */
+
+  var $visor = doc.querySelector('[data-js="visor"]');
+  var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+  var $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
+  var $buttonCE = doc.querySelector('[data-js="button-ce"]');
+  var $buttonEqual = doc.querySelector('[data-js="button-equal"]');
+
+  function initialize() {
+    initEvents();
+  }
+
+  function initEvents() {
+    Array.prototype.forEach.call($buttonsNumbers, function (button) {
+      button.addEventListener('click', handleClickNumber, false);
+    });
+    Array.prototype.forEach.call($buttonsOperations, function (button) {
+      button.addEventListener('click', handleClickOperation, false);
+    });
+    $buttonCE.addEventListener('click', handleClickCE, false);
+    $buttonEqual.addEventListener('click', handleClickEqual, false);
+  }
+
+  function handleClickNumber() {
+    $visor.value += this.value;
+  }
+
+  function handleClickOperation() {
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
+    $visor.value += this.value;
+  }
+
+  function handleClickCE() {
+    $visor.value = 0;
+  }
+
+  function isLastItemAnOperation(number) {
+    var operations = getOperations();
+    var lastItem = number.split('').pop();
+    return operations.some(function (operator) {
+      return operator === lastItem;
+    });
+  }
+
+  function getOperations() {
+    return Array.prototype.map.call($buttonsOperations, function(button){
+      return button.value;
+    });
+  }
+
+  function removeLastItemIfItIsAnOperator(string) {
+    if (isLastItemAnOperation(string))
+      return string.slice(0, -1);
+    return string;
+  }
+
+  function calculateAllValues(accumulated, actual) {
+    var firstValue = accumulated.slice(0, -1);
+    var lastValue = removeLastItemIfItIsAnOperator(actual);
+    var operator = accumulated.split('').pop();
+    var lastOperator = getLastOperator(actual);
+    return doOperation(operator, firstValue, lastValue) + lastOperator;
+  }
+
+  function getLastOperator(value) {
+    return isLastItemAnOperation(value) ? value.split('').pop() : '';
+  }
+
+  function doOperation(operator, firstValue, lastValue) {
+    switch (operator) {
+      case '+':
+        return Number(firstValue) + Number(lastValue);
+      case '-':
+        return Number(firstValue) - Number(lastValue);
+      case 'x':
+        return Number(firstValue) * Number(lastValue);
+      case '÷':
+        return Number(firstValue) / Number(lastValue);
+    }
+  }
+
+  function handleClickEqual() {
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
+    var allValues = $visor.value.match(getRegexOperations());
+    $visor.value = allValues.reduce(calculateAllValues);
+  }
+
+  function getRegexOperations() {
+    return new RegExp('\\d+[' + getOperations().join('') + ']?', 'g');
+  }
+
+  initialize();
+})(window, document);
