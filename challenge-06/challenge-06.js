208589b5828f517a143e4954d1bafb3c54f07173
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-03-22 15:24:31 -0300

    I finished challenge-07

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,8 +4,7 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-var championship = 'cearense';
-console.log(championship);
+// ?
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -13,7 +12,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-var teams = ['Fortaleza', 'Ceará', 'Botafogo', 'Corintians', 'Palmeiras'];
+// ?
 
 
 /*
@@ -32,29 +31,18 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-function showTeamPosition(n){
-    return n <= 4 ? 'O time que está em '+(n + 1)+'º lugar é o '+teams[n] : 'Não temos a informação do time que está nessa posição.';
-}
+// ?
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-console.log(showTeamPosition(0));
-console.log(showTeamPosition(1));
-console.log(showTeamPosition(2));
-console.log(showTeamPosition(3));
-console.log(showTeamPosition(4));
-console.log(showTeamPosition(5));
+// ?
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-var numero = 20;
-while(numero <= 30){
-    console.log(numero);
-    numero++;
-}
+// ?
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -68,33 +56,9 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-function convertToHex(cor){
-    var cores = ['rosa', 'azul', 'vermelho', 'branco', 'preto'];
-    var hexa = ['#001100', '#00qwer', '#asdfgh', '#0qwert', '#zxcvb'];
-    switch(cor){
-        case cores[0]:
-            console.log('O hexadecimal para a cor '+cor+' é '+hexa[0]);
-            break;
-        case cores[1]:
-            console.log('O hexadecimal para a cor '+cor+' é '+hexa[1]);
-            break;
-        case cores[2]:
-            console.log('O hexadecimal para a cor '+cor+' é '+hexa[2]);
-            break;
-        case cores[3]:
-            console.log('O hexadecimal para a cor '+cor+' é '+hexa[3]);
-            break;
-        case cores[4]:
-            console.log('O hexadecimal para a cor '+cor+' é '+hexa[4]);
-            break;
-        default:
-            console.log('Não temos o equivalente hexadecimal para '+cor);
-            break;                     
-
-    }
-}
+// ?
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-console.log(convertToHex('preto'));
+// ?
--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,16 +1,18 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-var array = ['string', null, true, 33, [1, 2, 3]];
+let array = ['string', null, true, 33, [1, 2, 3]];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 
-function addItem(a){
+function addItem(a) {
+
 	array.push(a);
 	return array;
+
 }
 
 /*
@@ -24,40 +26,44 @@ Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-console.log('O segundo elemento do segundo array é: '+array[5][1].bola);
+console.log(`O segundo elemento do segundo array é: ${array[5][1].bola}`);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-console.log('O primeiro array tem '+array.length+' itens.');
+console.log(`O primeiro array tem ${array.length} itens.`);
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-console.log('O segundo array tem '+array[5].length+' itens.');
+console.log(`O segundo array tem ${array[5].length} itens.`);
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
 console.log( 'Números pares entre 10 e 20:' );
-var x = 10;
+let x = 10;
 
-while(x <= 20){
+while(x <= 20) {
+
 	console.log(x % 2 == 0 ? x : '');
 	x++;
+
 }
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log( 'Números ímpares entre 10 e 20:' );
-var x = 10;  
-while(x <= 20){
+let x = 10;  
+while(x <= 20) {
+
 	console.log(x % 2 != 0 ? x : '');
 	x++;
+
 }
 
 /*
@@ -67,11 +73,15 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 console.log( 'Números pares entre 100 e 120:' );
-for(var n = 100; n <= 120; n++){
+for(var n = 100; n <= 120; n++) {
+
 	console.log(n % 2 == 0 ? n : ''); 
+
 }
 
 console.log( 'Números ímpares entre 111 e 125:' );
-for(var n = 101; n <= 125; n++){
+for(var n = 101; n <= 125; n++) {
+
 	console.log(n % 2 != 0 ? n : ''); 
+	
 }
