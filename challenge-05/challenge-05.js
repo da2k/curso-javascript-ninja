ae60bbfa95ba912ab45ae3106b16bf75cfea9d9a
José Airton <52473544+airtonsm@users.noreply.github.com>
2021-04-27 19:23:34 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,12 +2,12 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [1,4,8,5,9];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
