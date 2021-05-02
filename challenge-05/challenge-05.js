d8c54edc932fefb943a55f58dedfe47b351922a2
Danilo Silva <58179830+daniloessejota@users.noreply.github.com>
2021-05-01 22:46:32 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,23 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var myArray = ["Danilo", 10, 20, 30, 40]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function recebeArray (parametro) {
+    return parametro;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+recebeArray (myArray)[2]
+//20
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +26,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function agenteDuplo (umArray, umNumero) {
+    return umArray[umNumero];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+var variavelAGrande = ["Danilo", true, false, 26, null]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+ recebeArray (variavelAGrande)
+//[ 'Danilo', true, false, 26, null ]
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
