95d9f27b5b4962a8517dfb2e420fda7b89ddeeff
andersonmirandasousa <51092774+andersonmirandasousa@users.noreply.github.com>
2021-06-09 15:45:46 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,18 +2,23 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// 
+var varray = [ 12 , 34 , 56, 7 , 90 ]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+// 
+function MyFunc(arg) {
+... return arg;
+... }
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
-
+// 
+MyFunc(varray)[1];
+34
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
@@ -21,19 +26,26 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
-
+function myFunc2(arg,ind) {
+... return arg[ind];
+... }
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
-
+var arrayInd = [ 'Anderson' , 35 , 'masculino' , 'são paulo' , true ]
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
+myFunc2(arrayInd,0); //'Anderson'
+myFunc2(arrayInd,1); // 35
+myFunc2(arrayInd,2); // 'masculino'  
+myFunc2(arrayInd,3); // 'são paulo'
+myFunc2(arrayInd,4); // true
 
-/*
+ /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
 seguintes características:
