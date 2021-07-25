2365ed0bef2e205b0c791b43e86c5389d689bcae
jnorberto23 <joaovictor.norberto@outlook.com>
2021-07-25 15:31:02 -0300

    challenge09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -10,11 +10,11 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
-    console.log( 'Na função `myFunction`, o primeiro número é', number1 );
-    console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
-    return number1 + number2;
-    var number2 = 20;
+  var number1 = 10;
+  var number2 = 20;
+  console.log('Na função `myFunction`, o primeiro número é', number1);
+  console.log('Na função `myFunction`, o segundo número é', number2);
+  return number1 + number2;
 }
 myFunction();
 
@@ -22,13 +22,13 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
-    var sum = function sum() {
-        return number1 + number2;
-    };
-    var number1 = 10;
-    var number2 = 20;
-    return sum();
+  var number1 = 10;
+  var number2 = 20;
+  var sum = function sum() {
+    return number1 + number2;
+  };
+  console.log('A soma de 10 e 20 é igual a', sum ? sum() : undefined);
+  return sum();
 }
 myFunction2();
 
@@ -36,14 +36,15 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
-    var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
-    var number1 = 40;
-    return sum();
-    function sum() {
-        return number1 + number2;
-    };
+  var number1 = 40;
+  var number2 = 50;
+  console.log('A soma de 40 e 50 é igual a', sum());
+  console.log('Na função myFunction3, number1 é igual a', number1);
+  return sum();
+
+  function sum() {
+    return number1 + number2;
+  };
 }
 myFunction3();
 
@@ -60,14 +61,18 @@ o retorno de `calculator`.
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+
+function calculator(num1, num2) {
+  return function (callback) {
+    return callback(num1, num2);
+  };
+}
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
 passando dois números por parâmetro.
 */
-// ?
-
+var sum = calculator(2,5);
 /*
 Sabemos que `sum` agora tem uma função atribuída a ela, que é o retorno de
 `calculator`. E essa função espera um parâmetro `callback`. O `callback`
@@ -78,14 +83,21 @@ uma função anônima que irá retornar a soma dos dois números que essa funç�
 anônima tem como seus argumentos.
 */
 console.log( 'O resultado da soma é:' );
-// ?
+console.log(sum(function (number1, number2) {
+  return number1 + number2;
+}));
+
 
 /*
 Agora declare outra variáveis chamadas `subtraction`, `multiplication`,
 `division` e `mod`, e atribua à elas `calculator`, passando números
 diferentes para cada chamada.
 */
-// ?
+var subtraction = calculator(2,5);
+var division = calculator(2,5);
+var multiplication = calculator(2,5);
+var mod = calculator(2,5);
+
 
 /*
 Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -94,14 +106,26 @@ divisão e módulo (resto de divisão), conforme a função utilizada.
 As suas respostas devem estar abaixo dos `console.log` referentes à cada
 chamada.
 */
-console.log( 'O resultado da subtração é:' );
-// ?
+console.log('O resultado da subtração é:');
+console.log(subtraction(function (number1, number2) {
+  return number1 - number2;
+}));
 
-console.log( 'O resultado da multiplicação é:' );
-// ?
 
-console.log( 'O resultado da divisão é:' );
-// ?
+console.log('O resultado da multiplicação é:');
+console.log(multiplication(function (number1, number2) {
+  return number1 * number2;
+}));
 
-console.log( 'O resto da divisão é:' );
-// ?
+
+console.log('O resultado da divisão é:');
+console.log(division(function (number1, number2) {
+  return number1 / number2;
+}));
+
+
+console.log('O resto da divisão é:');
+console.log(mod(function (number1, number2) {
+  return number1 % number2;
+}));
+
