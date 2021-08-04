8611e60c620138e9dabf8b96efe736e67a795160
marcelzanatta <35149853+marcelzanatta@users.noreply.github.com>
2021-08-03 21:02:56 -0300

    Update challenge-11.js

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,3 +1,4 @@
+( function (){
 // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
 
 /*
@@ -7,14 +8,23 @@ loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
-// ?
+// 
+  var once = false;
+  do {
+   console.log( 'Entrou ao menos uma vez!' );  
+  } while ( once === 'true' );
 
 /*
 Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
-// ?
+var person = { 
+  name: 'Marcel' ,
+  age: 34 ,
+  weight: 69 ,
+  birthy: '28/08'
+}
 
 /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
@@ -25,7 +35,15 @@ esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
-// ?
+
+var counter = 0;
+
+for ( var prop in person ){
+   console.log ( 'The'+ prop + 'of person is ' + person[prop] + '.' );
+   counter++;
+}
+ console.log( 'The person has ' + counter + ' properties' );
+
 
 /*
 Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
@@ -34,7 +52,13 @@ Se verdadeiro, retornar `true`. Senão, retornar false.
 Após a função, mostrar a mensagem no console:
 'The person has more than 25 years old? [TRUE/FALSE]'
 */
-// ?
+function moreThan(x){
+  
+  if( person.age > 25 ){
+   return console.log( 'The person has more than 25 years old? TRUE' );
+  }
+    console.log( 'The person has more than 25 years old? FALSE' );
+}
 
 /*
 Faça um loop de 0 a 20, que adicione cada número como um item de um
@@ -52,3 +76,4 @@ Mostrar no console os números do array.
 */
 console.log( 'Pares de 0 a 20:' );
 // ?
+})();
