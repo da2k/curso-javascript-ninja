27ec058fe66df06db388dcf0d2fcd7abf21181af
Hiago Ramalho <77689171+hiago-ramalho@users.noreply.github.com>
2021-08-30 06:57:04 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,38 +1,46 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+var arr = ['Hiago', 26, {peso: 79, altura: 1.70}, ['swell', 'sereia'], null];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+function addItem(item) {
+  arr.push(item)
+  return arr
+}
+addItem('Ramalho') // ["Hiago", 26, {…}, Array(2), null, "Ramalho"]
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+addItem([true, 'Front-End', 2021]) 
+// ["Hiago", 26, {…}, Array(2), null, "Ramalho", Array(3)] 
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log(`O segundo elemento do segundo array é ${arr[6][1]}.`)
+// O segundo elemento do segundo array é Front-End.
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log(`O primeiro array tem ${arr.length} itens.`)
+// O primeiro array tem 7 itens.
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log(`O segundo array tem ${arr[6].length} itens.`)
+// O segundo array tem 3 itens.
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
