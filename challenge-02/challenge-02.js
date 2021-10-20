3a73203880ffd1beffbb39bb3b3ade2da9ea1aa4
Henrique Araujo <henriquemooreira@gmail.com>
2021-10-20 17:17:09 -0300

    challenge-02

--- challenge-02/challenge-02.js
+++ challenge-02/challenge-02.js
@@ -1,32 +1,37 @@
-# Desafio da semana #2
+// # Desafio da semana #2
 
-Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! :smile:
+// Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! :smile:
 
-```js
+// ```js
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-?
+function soma(x, y) {
+  return x + y;
+}
 
 // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
-?
+let somando = soma(5, 8) + 5;
 
 // Qual o valor atualizado dessa variável?
-?
+console.log(somando); // 18
 
 // Declare uma nova variável, sem valor.
-?
+let myVar;
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
     O valor da variável agora é VALOR.
 Onde VALOR é o novo valor da variável.
 */
-?
+function addValue() {
+  myVar = 15;
+  return `O valor da variável myVar é ${myVar}`;
+}
 
 // Invoque a função criada acima.
-?
+console.log(addValue());
 
 // Qual o retorno da função? (Use comentários de bloco).
-?
+// O valor da variável myVar é 15
 
 /*
 Crie uma função com as seguintes características:
@@ -35,19 +40,24 @@ Crie uma função com as seguintes características:
     Preencha todos os valores corretamente!
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
-?
+function validation(x, y, z) {
+  if (x === undefined || y === undefined || z === undefined) {
+    return 'Preencha todos os valores corretamente!';
+  }
+  return x * y * z + 2;
+}
 
 // Invoque a função criada acima, passando só dois números como argumento.
-?
+console.log(validation(1));
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+// Preencha todos os valores corretamente!
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
-?
+console.log(validation(5, 9, 10));
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+// 452
 
 /*
 Crie uma função com as seguintes características:
@@ -58,8 +68,25 @@ Crie uma função com as seguintes características:
 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
-?
+function validation2(x, y, z) {
+  if (x !== undefined && y === undefined && z === undefined) {
+    return x;
+  }
+  if (x !== undefined && y !== undefined && z === undefined) {
+    return x + y;
+  }
+  if (x !== undefined && y !== undefined && z !== undefined) {
+    return (x + y) / z;
+  }
+  if (x === undefined && y === undefined && z === undefined) {
+    return false;
+  }
+
+  return null;
+}
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
-?
-```
+console.log(validation2()); // false
+console.log(validation2(2)); // 2
+console.log(validation2(2, 5)); // 7
+console.log(validation2(2, 8, 9)); // 1.1111....
