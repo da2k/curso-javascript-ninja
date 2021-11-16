8da69475dcc0e20ec48daf4632838a3837a98778
LucasImposimato <lucas.imposimato@gmail.com>
2021-11-16 14:27:41 -0400

    Final

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -84,25 +84,27 @@ function book(bookName) {
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book( ))
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Book 01 tem ' + book('Book 01').pagesAmmount + ' páginas!');
+
+
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro Book 02 é ' + book('Book 02').author + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro ' + bookName + ' foi publicado pela editora ' + book(bookName).editor + '.');
