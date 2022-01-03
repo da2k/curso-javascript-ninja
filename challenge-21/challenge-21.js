23d223db76d948554a57204cf8c4fc162fc4b0bd
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-01-03 10:22:22 -0300

    funcionalidade de um cronometro

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,17 +1,24 @@
-/*
-O desafio de hoje será um pequeno projeto: um cronômetro!
-As regras para criação do cronômetro são as seguintes:
-1. Crie um arquivo index.html e adicione esse script a ele;
-2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
-Ele será o nosso cronômetro;
-3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
-4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
-cada segundo;
-5. Ao clicar em Stop, o cronômetro deve parar de contar;
-6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
 
-Utilize o atributo data-js para nomear o campo e os botões. Você pode
-usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
-dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
-*/
-// ?
+    var inicio = document.getElementById('cronometro')
+    var start = document.getElementById('inicio')
+    var parar = document.getElementById('parar')
+    var resetar = document.getElementById('reset')
+    var interval;
+
+  start.addEventListener('click', startTimer, false);
+ parar.addEventListener('click', pararTimer, false);
+resetar.addEventListener('click', resetarTimer,false )
+
+function startTimer() {
+   inicio.value = +inicio.value  + 1;
+   interval = setTimeout(startTimer, 1000)
+}
+
+function pararTimer() {
+    clearTimeout(interval)
+}
+
+function resetarTimer(){
+    inicio.value = 0;
+    pararTimer()
+}
