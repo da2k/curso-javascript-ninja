cd87f1568877c34e6290e586b35661cce900e6e5
anderoidy <anderoidy@gmail.com>
2022-03-05 22:17:49 -0300

    criado novo branch pra faze os desafico

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -2,18 +2,25 @@
 Crie um array com 5 items (tipos variados).
 */
 // ?
-
+primeiroArr = ['Anderson', 41, segundoArr = [20, 21, 22], objetoDetro = { linguagen: 'java Script', framework: 'Angular'}, function (){ console.log(' Ola mundo')}]
+console.log(primeiroArr)
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 // ?
+function additem(item) {
+    primeiroArr.push(item)
+    console.log(primeiroArr)
+}
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 // ?
+additem(terceiroArr = ['novo arrai', 3, quartoArr = [1, 'esse arrai é o quarto ele esta dentro do terceiro', 
+function (){ return 'arrai dentro de dentro'}], objetoZ = {curso: 'JsNinja', professor: 'Fernando', sobrenome: 'daciuk'}])
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
@@ -21,40 +28,95 @@ frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
 // ?
-
+//Obs fiz varios pois eu criei mais arrays um dentro do aoutro
+console.log(`O segundo elemento do segundo array é: ${primeiroArr[2][1]}`)  //  21 
+console.log(`O segundo elemento do terceiro array é: ${primeiroArr[5][1]}`)   //  3
+console.log(`O segundo elemento do quarto array é: ${primeiroArr[5][2][1]}`)
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
 // ?
-
+console.log('O primeiro array tem: ' + primeiroArr.length + ' itens')
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
 // ?
+console.log('O segundo array tem: ' + segundoArr.length + ' itens')
+console.log('O terceiro array tem: ' + terceiroArr.length + ' itens ')
+console.log('O quarto array tem: ' + quartoArr.length + ' itens ')
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
-console.log( 'Números pares entre 10 e 20:' );
+
+var numerosPares = 10
+console.log( '\nNúmeros pares entre 10 e 20:');
+while(numerosPares <= 20){
+    
+    if(numerosPares%2==0){
+        console.log(numerosPares++)
+    }   
+    ++numerosPares
+}
+
 // ?
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
-console.log( 'Números ímpares entre 10 e 20:' );
+var numerosImpares = 10
+console.log( '\nNúmeros Impares entre 10 e 20:');
+while(numerosImpares <= 20){
+    
+    if(numerosImpares%2!=0){
+        console.log(numerosImpares++)
+    }   
+    ++numerosImpares
+}
 // ?
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
 Só vamos mudar o range:
-- No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
+- No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;  
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
-console.log( 'Números pares entre 100 e 120:' );
+console.log( 'Números ímpares entre 10 e 20:' );
+for(i=10;i<=20;i++){
+    if(i%2===0){
+    console.log(i)
+    }
+}
+
+//console.log( 'Números pares entre 100 e 120:' );
+function mostraPar(inicio, fim) {
+  console.log("Números pares entre :", + inicio );
+  for (i = inicio; i <= fim; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+}
+
+//invocando a funcao
+mostraPar(100, 150)
+//invocando a funcao
+mostraPar(111, 125)
+
 // ?
 
-console.log( 'Números ímpares entre 111 e 125:' );
+//console.log( 'Números ímpares entre 111 e 125:' );
 // ?
+function mostraImpar(inicio, fim) {
+    console.log("Números ímpares entre: "+ inicio + ' e ' + fim);
+    for (i = inicio; i <= fim; i++) {
+      if (i % 2 !== 0) {
+        console.log(i);
+      }
+    }
+  }
+
+  mostraImpar(111, 125)
