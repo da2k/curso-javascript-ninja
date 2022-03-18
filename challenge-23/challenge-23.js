0feffb803b3ff256d017028ac4c21befe1fd41e0
MaxwellOliveira01 <71512334+MaxwellOliveira01@users.noreply.github.com>
2022-03-17 21:02:54 -0300

    Update challenge-23.js

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,51 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function(doc) {
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+    'use strict';
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+    var $display = doc.querySelector('[data-js="tela"]');
+    var $btnNumbers = doc.querySelectorAll('[data-js="btnNumbers"]');
+
+    var $btnPlus = doc.querySelector('[data-js="btnPlus"]');
+    var $btnMinus = doc.querySelector('[data-js="btnMinus"]');
+    var $btnMult = doc.querySelector('[data-js="btnMult"]');
+    var $btnDiv = doc.querySelector('[data-js="btnDiv"]');
+
+    var $btnEqual = doc.querySelector('[data-js="btnEqual"]');
+    var $btnClear = doc.querySelector('[data-js="btnClear"]');
+
+    function addNumber(){
+        if($display.value === '0') 
+             $display.value = this.value;        
+        else
+            $display.value += this.value;        
+    };
+    
+    $btnNumbers.forEach(function(button) {
+        button.addEventListener('click', addNumber, false);
+    });
+
+    $btnPlus.addEventListener('click', function(){
+        $display.value += '+';
+    }, false);
+
+    $btnMinus.addEventListener('click', function(){
+        $display.value += '-';
+    }, false);
+
+    $btnMult.addEventListener('click', function(){
+        $display.value += '*';
+    }, false);
+
+    $btnDiv.addEventListener('click', function(){
+        $display.value += '/';
+    }, false);
+
+    $btnClear.addEventListener('click', function(){
+        $display.value = '0';
+    }, false);
+
+    $btnEqual.addEventListener('click', function(){
+        $display.value = eval($display.value);
+    }, false);
+
+})(document);
