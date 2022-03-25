1771b95a6dc002cfa6948bae4aefe08d96791888
anderoidy <anderoidy@gmail.com>
2022-03-25 04:12:03 -0300

    finished challenge-16

--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -1,9 +1,9 @@
-/*
+(function (){/*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
-
+'use strict'
 /*
 Declare uma variável chamada `name` que receba seu primeiro nome.
 Mostre no console todas as letras do seu nome separadas, com a frase:
@@ -13,9 +13,13 @@ Ex: no caso do nome ser "Fernando", deve mostrar as frases:
 - "e é a 2ª letra do meu nome."
 E assim por diante, até a última.
 */
+
 console.log( 'As letras do seu nome:' );
 // ?
-
+var name = 'anderson'
+for(var i=0; i!==name.length; i++) {
+  console.log('a letra ->', name.charAt(i), 'esta na posicao ', i+1)
+}
 /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
 escrito no formato de slug (caixa baixa e palavras separadas por um traço).
@@ -30,7 +34,16 @@ console.log para cada formato.
 */
 console.log( '\nNome convertido à partir de um slug:' );
 // ?
+var fullName = 'anderson-pereira-de-oliveira-santos'
 
+var newFullname = fullName.split('-')
+
+newFullname = fullName.split('-').map(function(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1)
+})
+
+console.log('Nome no formato slug -> ', fullName)
+console.log('Nome no formato com caixa alta -> ', newFullname)
 /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
 cada nome por vírgula. Entre o penúltimo e o último nome, o separador deve
@@ -40,31 +53,59 @@ O resultado final deve ficar mais ou menos assim:
 - Detalhe: o código abaixo deve funcionar com um array de qualquer tamanho.
 5 nomes foi somente uma sugestão ;)
 */
-console.log( '\nMeus amigos:' );
-// ?
+
+var arrDeNomes = ['Anderson', 'Andrew', 'fabiana', 'Sueli', 'Nina']
+//primeira maneira de mostrar como uma string
+//console.log( '\nMeus amigos:', arrDeNomes.join(', ') );
+//console.log( '\nMeus amigos:', arrDeNomes.join(', ') );
+
+// agora usando o reduce
+var arrReduzido = arrDeNomes.reduce(function (acumulado, atual, index ) {
+  var separador = arrDeNomes.length - 1 === index ? ' e ' : ', '
+  return acumulado + separador + atual
+}).concat(' são meus amigos')  //como  o reduce reduziu o array para uma string podemos usar o concat
+
+console.log( '\nMeus amigos:', arrReduzido) //podia colcoar aqui tbm o (... arrReduzido + ' sao meus amigos')
+//Usando o typeof para ver os tipos vimos que a variavel arrDeNomes apos ser passado pelo reduce viroou string atribuido a variavel arrReduzido
+console.log(typeof arrDeNomes)
+console.log(typeof arrReduzido)
 
 /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
 Mostre o resultado no console.
 */
-console.log( '\nEra "Roberto", agora é:' );
+var namex = 'Roberto'
+var nomeNovo = namex.replace('to', 'ta')
+console.log( '\nEra "Roberto", agora é:', nomeNovo );
 // ?
 
 /*
 Mostre no console a parte "nando" da string "Fernando". Use o método que
 faz a busca do final para o início da string.
 */
-console.log( '\nParte de uma string:' );
+
+console.log( '\nParte de uma string:', 'Fernando'.substring(3) );
 // ?
 
 /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
 escrito de forma natural.
+
 Mostre no console esse nome, mas com as letras intercalando entre maiúsculas
 e minúsculas.
+
 - Detalhe: o código abaixo deve funcionar da mesma forma para qualquer nome,
 de qualquer tamanho, escrito de qualquer forma.
 Ex.: Nomes que deveriam funcionar: "Fernando", "RoBertO", "gabriEla", etc.
 */
 console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
+var myName = 'valdenito pereira\n'
+var myNewName = []
+for (var i = 0; i<myName.length; i++) {
+    myNewName.push( i % 2 === 0 ? myName.charAt(i).toUpperCase(): myName.charAt(i).toLowerCase())
+}
+console.log('\n',myName)
+console.log(myNewName)
+console.log(myNewName.join(''))
 // ?
+})()
