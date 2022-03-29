583566a35d28db999501ddfcd56aa3aa9e8c75ef
Carlos Filipe <thecarlosfilipe@gmail.com>
2022-03-29 15:36:22 -0300

    challenge-05 solution

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,22 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var array = [ 'JavaScript', 2077, {a: {b: 'Xablau', c: function(x, y) {return x * y; }}}, function(args) { return args; }, false, ['Red', 'Yellow', 'Orange'] ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function returnArray(arr) {
+  return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+console.log(returnArray(array[1]));
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +25,27 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function returnArrayByNumber(arr, index) {
+  return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+var values = ['Carl Sagan', 1996, {nameSerie: 'Cosmos'}, ['Princeton', 'Harvard'], function(){ return 'Retorno de função!';}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+console.log(returnArrayByNumber(values, 0))
+console.log(returnArrayByNumber(values, 1))
+console.log(returnArrayByNumber(values, 2))
+console.log(returnArrayByNumber(values, 3))
+console.log(returnArrayByNumber(values, 4))
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +61,56 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book(bookName) {
+  var allBooks = {
+    'Clean Code': {
+      quantidadePaginas: 431,
+      autor: 'Robert C. Martin',
+      editora: 'Pearson'
+    },
+    'Refactoring': {
+      quantidadePaginas: 448,
+      autor: 'Martin Fowler',
+      editora: 'Addison-Wesley'
+    },
+    'Domain Driven Design': {
+      quantidadePaginas: 529,
+      autor: 'Eric Evans',
+      editora: 'Addison-Wesley Professional'
+    }
+  }
+  return !bookName ? allBooks : allBooks[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+
+var booksNames = ['Clean Code', 'Refactoring', 'Domain Driven Design'];
+
+console.log('O livro ' + booksNames[0] + ' tem ' + book('Clean Code').quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+
+console.log('O autor do livro ' + booksNames[1] + ' é ' + book('Refactoring').autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+
+console.log('O livro ' + booksNames[2] + ' foi publicado pela editora ' + book('Domain Driven Design').editora + '.');
