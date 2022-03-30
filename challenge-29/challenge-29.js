ec6ff84fd82f738aa3fa3c768f61343b4d9b00d8
MaxwellOliveira01 <71512334+MaxwellOliveira01@users.noreply.github.com>
2022-03-30 14:51:28 -0300

    Update challenge-29.js

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,6 +1,5 @@
-(function() {
+(function(win, doc) {
   'use strict';
-
   /*
   Vamos estruturar um pequeno app utilizando módulos.
   Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
@@ -35,5 +34,66 @@
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
+  
+  function app(){
 
-})();
+    var $nomeEmp = new DOM('[data-js="nomeEmpresa"]');
+    var $telEmp = new DOM('[data-js="telefoneEmpresa"]');
+
+    var $marca = new DOM('[data-js="marca"]');
+    var $ano = new DOM('[data-js="ano"]');
+    var $placa = new DOM('[data-js="placa"]');
+    var $cor = new DOM('[data-js="cor"]');
+    var $img = new DOM('[data-js="img"]');
+    var $submit = new DOM('[data-js="submit"]');
+
+    var $table = new DOM('[data-js="table"]');
+    
+    var addCompanyInfo = function() {
+      var request = new httpClient();
+      request.get('/company.json', function(data){
+        data = JSON.parse(data);
+        $nomeEmp.get()[0].innerText = "Empresa: " + data.name;
+        $telEmp.get()[0].innerText = "Contato: " + data.phone;
+      });
+    }
+
+    $submit.on('click', function() {
+      var $documentFrag = doc.createDocumentFragment();
+      var $tr = doc.createElement('tr');
+
+      var $tagMarca = doc.createElement('td');
+      var $tagAno = doc.createElement('td');
+      var $tagPlaca = doc.createElement('td');
+      var $tagCor = doc.createElement('td');
+      var $tdImg = doc.createElement('td');
+      
+      var $tagImg = doc.createElement('img');
+
+      $tagMarca.textContent = $marca.get()[0].value;
+      $tagAno.textContent = $ano.get()[0].value;
+      $tagPlaca.textContent = $placa.get()[0].value;
+      $tagCor.textContent = $cor.get()[0].value;
+      
+      $tagImg.setAttribute('src', $img.get()[0].value);
+      $tagImg.setAttribute('style', 'max-width:150px; max-height:150px');
+
+      $tdImg.appendChild($tagImg);
+
+      $tr.appendChild($tagMarca);
+      $tr.appendChild($tagAno);
+      $tr.appendChild($tagPlaca);
+      $tr.appendChild($tagCor);
+      $tr.appendChild($tagImg);
+
+      $documentFrag.appendChild($tr);
+      $table.get()[0].appendChild($documentFrag);
+    })
+
+    addCompanyInfo();
+  }
+
+  win.app = app;
+  app();
+
+})(window, document);
