ab809b5e61138eedd6083c18dfec77556d320f5a
anderoidy <anderoidy@gmail.com>
2022-04-05 01:13:10 -0300

    finished challenge-21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,10 +1,12 @@
-/*
+(function (win, doc){
+  'use strict'/*
 O desafio de hoje será um pequeno projeto: um cronômetro!
 As regras para criação do cronômetro são as seguintes:
 1. Crie um arquivo index.html e adicione esse script a ele;
 2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
 Ele será o nosso cronômetro;
 3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+
 4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
 cada segundo;
 5. Ao clicar em Stop, o cronômetro deve parar de contar;
@@ -14,4 +16,32 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
+var contador = 0
+var $inputContador = document.querySelector('[data-js="timer"]')
+var $botaoStart = document.querySelector('[data-js="start"]')
+var $botaoStop = document.querySelector('[data-js="stop"]')
+var $botaoReset = document.querySelector('[data-js="reset"]')
+
+$inputContador.value = contador
+
+function cronom() {
+  contador += 1
+  $inputContador.value = contador
+
+  contador = setTimeout( cronom, 1000)
+}
+
+$botaoStart.addEventListener('click', function() {
+  cronom()
+})
+
+$botaoStop.addEventListener('click', function() {
+    clearTimeout(contador)
+}, false)
+
+$botaoReset.addEventListener('click', function(){
+    contador = 0
+    $inputContador.value = contador
+ })
 // ?
+})(window, document)
