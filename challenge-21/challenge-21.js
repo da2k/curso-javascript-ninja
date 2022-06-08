ecfff308a1d1cc1297f12e056aeed2fa20850607
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-06-08 18:03:32 -0400

    Update challenge-21.js

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -15,3 +15,42 @@ usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
 // ?
+
+
+(function(window , document){
+         'use strict'
+
+var $start = document.querySelector('[data-button="start"]');  
+console.log($start, 'butao de inicio !!!'); 
+       
+var $stop = document.querySelector('[data-button="Stop"]'); 
+console.log($stop, 'button de stop'); 
+       
+var $reset = document.querySelector('[data-button="Reset"]');  
+console.log($reset , 'buttao de reset');    
+
+var $timer = document.querySelector('[data-button="timer"]'); 
+console.log($timer); 
+
+var interval ;
+
+$start.addEventListener( 'click' , startTimer , false);  //  START 
+$stop.addEventListener( 'click' , stopTimer , false);   // PARAR 
+$reset.addEventListener( 'click', resetTime ,  false);  // RESETAR 
+
+
+function startTimer(){ 
+    $timer.value = +$timer.value + 1 ; 
+   interval =  setTimeout(startTimer , 1000); 
+} 
+
+function stopTimer () {
+   clearTimeout(interval); 
+}
+
+function resetTime() {
+  $timer.value = 0; 
+  stopTimer();  
+}
+
+})(window , document);
