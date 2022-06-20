31952ed27e634cd1ad87c15c715149e2f1e395d7
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-06-20 16:41:00 -0400

    Update challenge-25.js

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,7 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+
+
+'ja exercictei e estudei tambem...'
