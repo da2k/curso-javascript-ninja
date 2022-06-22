617de1d6dec6f2fd69a31e8d3c3fcdef3b02734d
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-06-22 17:40:58 -0400

    Update challenge-26.js

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,3 +1,5 @@
+  
+  
 /*
 O desafio dessa semana é criar uma mini library (biblioteca) para
 reutilizarmos nossos códigos quando fizermos manipulação de DOM!
@@ -21,11 +23,36 @@ Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
 // ?
 
+// o elements ai no function DOM é a propria referencia do HTML... 
+
+  class DOM {
+    constructor(elements) {
+      this.element = document.querySelectorAll(elements); 
+    }
+  }
+
+
+DOM.prototype.on = function on(eventType , callback) {
+  Array.prototype.forEach(this.element , function(element) {
+    element.addEventListener( eventType , callback , false );
+  }); 
+} ; 
+
+DOM.prototype.off = function off( eventType , callback){ 
+  Array.prototype.forEach(this.element , function(element) {
+    element.removeEventListener( eventType , callback , false );
+  }; 
+
+DOM.prototype.get = function get () {
+  return this.element; 
+}; 
+
 var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
+$a.on('click', function handleClick(e) {
   e.preventDefault();
   console.log('clicou');
+  $a.off('click', handleClick); 
 });
 
 console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+console.log('$a é filho de body?', $a.get().parentNode === document.body); 
