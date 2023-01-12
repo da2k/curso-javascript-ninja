86977474feb634d58c3d685648205c0cbd9bb832
GiselleVMPovoa <121452014+GiselleVMPovoa@users.noreply.github.com>
2023-01-12 16:07:32 -0300

    Update challenge-09.js

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -2,7 +2,7 @@
 Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
 */
-
+(function(){
 /*
 Analise as funções abaixo (`myFunction`, `myFunction2` e `myFunction3`, e
 ajuste o posicionamento das variáveis e funções internas, para que os códigos
@@ -10,42 +10,52 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+  var number2 = 20;
+  var number1 = 10;
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
-    return number1 + number2;
-    var number2 = 20;
+    return number1 + number2;  
 }
 myFunction();
+Na função `myFunction`, o primeiro número é 10
+Na função `myFunction`, o segundo número é 20
+30
 
 /*
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
-    var sum = function sum() {
+   var number1 = 10;
+    var number2 = 20;
+   var sum = function sum() {
         return number1 + number2;
     };
-    var number1 = 10;
-    var number2 = 20;
+       console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+   
     return sum();
 }
 myFunction2();
+A soma de 10 e 20 é igual a 30
+30
 
 /*
     myFunction3();
 */
 function myFunction3() {
+  var number1 = 40;
+  var number2 = 50;
     console.log( 'A soma de 40 e 50 é igual a', sum() );
-    var number2 = 50;
     console.log( 'Na função myFunction3, number1 é igual a', number1 );
-    var number1 = 40;
+    
     return sum();
     function sum() {
         return number1 + number2;
     };
 }
 myFunction3();
+A soma de 40 e 50 é igual a 90
+Na função myFunction3, number1 é igual a 40
+90
 
 /*
 No desafio anterior criamos uma calculadora, usando uma estrutura funcional.
@@ -60,13 +70,16 @@ o retorno de `calculator`.
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+function calculator(num1,num2){
+return function( callback ){
+return callback(num1,num2)
+};};
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
 passando dois números por parâmetro.
 */
-// ?
+var sum = calculator(5,6);
 
 /*
 Sabemos que `sum` agora tem uma função atribuída a ela, que é o retorno de
@@ -78,14 +91,18 @@ uma função anônima que irá retornar a soma dos dois números que essa funç�
 anônima tem como seus argumentos.
 */
 console.log( 'O resultado da soma é:' );
-// ?
-
+console.log(sum(function(number1,number2){return number1 + number2;}));
+O resultado da soma é:
+11
 /*
 Agora declare outra variáveis chamadas `subtraction`, `multiplication`,
 `division` e `mod`, e atribua à elas `calculator`, passando números
 diferentes para cada chamada.
 */
-// ?
+var substraction = calculator(3,9);
+var multiplication = calculator(5,3);
+var division = calculator(8,2);
+var mod = calculator(134,48);
 
 /*
 Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -95,13 +112,23 @@ As suas respostas devem estar abaixo dos `console.log` referentes à cada
 chamada.
 */
 console.log( 'O resultado da subtração é:' );
-// ?
+console.log(substraction(function(number1,number2){return number1 - number2;}));
+O resultado da subtração é:
+-6
 
 console.log( 'O resultado da multiplicação é:' );
-// ?
+console.log(multiplication(function(number1,number2){return number1 * number2;}));
+O resultado da multiplicação é:
+15
 
 console.log( 'O resultado da divisão é:' );
-// ?
+console.log(division(function(number1,number2){return number1 / number2;}));
+O resultado da divisão é:
+4
 
 console.log( 'O resto da divisão é:' );
-// ?
+console.log(mod(function(number1,number2){return number1 % number2;}));
+O resto da divisão é:
+38
+  
+})();
