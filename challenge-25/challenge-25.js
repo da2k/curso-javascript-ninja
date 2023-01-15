a2d637ced516fe12100c3041a7556731a948d07e
Cris Pereira <78980507+CrislandePereira@users.noreply.github.com>
2023-01-15 15:38:23 -0300

    Challenge 25 in Progress

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -1,3 +1,5 @@
+(function(win,doc){
+  'use strict';
 /*
 Essa semana você terá dois desafios:
 1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
@@ -14,3 +16,39 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+  var $nome = doc.querySelector('[data-js="nome"]');
+  var $email = doc.querySelector('[data-js="email"]');
+  var $data = doc.querySelector('[data-js="data"]');
+  var $image = doc.querySelector('[data-js="image"]');
+  var $button = doc.querySelector('[data-js="button"]');
+
+  var $nomeTable = doc.querySelector('[data-js="nome-user"]');
+  var $emailTable = doc.querySelector('[data-js="email-user"]');
+  var $dataTable = doc.querySelector('[data-js="data-user"]');
+  var $imageTable = doc.querySelector('[data-js="image-user"]');
+
+  // var $edit = doc.querySelector('[data-js="edit"]');
+  // var $excluir = doc.querySelector('[data-js="excluir"]');
+
+  function initialize(){
+  initEvents();
+  }
+
+  function handleClick() {
+  $nomeTable.value += $nome.value;
+  $emailTable.value += $email.value;
+  $dataTable.value += $data.value;
+  $imageTable.value += $image.value;
+  }
+
+  function initEvents(){
+  function clickButton() {
+    $button.addEventListener('click', handleClick, false);
+  }
+  clickButton();
+  }
+
+  initialize();
+
+})(window, document);
