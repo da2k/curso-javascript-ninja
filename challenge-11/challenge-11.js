275e529c6d897395c8c60a37643d2583a3fe07ad
Caio Camizão <109806265+CaioCamizao@users.noreply.github.com>
2023-09-26 16:58:42 -0300

    Update challenge-11.js

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -46,7 +46,15 @@ Se verdadeiro, retornar `true`. Senão, retornar false.
 Após a função, mostrar a mensagem no console:
 'The person has more than 25 years old? [TRUE/FALSE]'
 */
-// ?
+
+function moreThan(age) {
+  if(person.age > 25) {
+    return true;
+  } else {
+    return false;
+  };
+}
+console.log("The person has more than 25 years old? ", moreThan(25));
 
 /*
 Faça um loop de 0 a 20, que adicione cada número como um item de um
