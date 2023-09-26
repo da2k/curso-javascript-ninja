ee1e23f6edbb789a43b75a8ac5062ab17bf836c4
Caio Camizão <109806265+CaioCamizao@users.noreply.github.com>
2023-09-26 16:46:55 -0300

    Update challenge-11.js

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,3 +1,4 @@
+(function(){
 // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
 
 /*
@@ -7,14 +8,19 @@ loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
-// ?
+
+var once = false;
+do {
+  console.log("Entrou ao menos uma vez!")
+} while(once);
 
 /*
 Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
-// ?
+
+var person = {name: 'Caio',age: 30, weight: 75, birthday: '31/08' };
 
 /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
@@ -25,7 +31,13 @@ esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
-// ?
+
+var counter = 0;
+for( var prop in person) {
+  console.log('The ' + prop + ' of person is ' + person[prop]);
+  counter++;
+}
+console.log('The person has ' + counter + ' properties');
 
 /*
 Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
@@ -52,3 +64,4 @@ Mostrar no console os números do array.
 */
 console.log( 'Pares de 0 a 20:' );
 // ?
+  })();
