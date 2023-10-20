c2f0b3d1c855fb503e9fc8f640e3e28e421614d8
Caio Camizão <109806265+CaioCamizao@users.noreply.github.com>
2023-10-20 16:40:36 -0300

    Update challenge-25.js

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,4 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+OK!
