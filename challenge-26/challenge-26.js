36baa8c679ac5e49d9d9d96053008d1599ce4bbf
Caio Camizão <109806265+CaioCamizao@users.noreply.github.com>
2023-10-23 11:44:38 -0300

    Update challenge-26.js

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,3 +1,4 @@
+(function(){
 /*
 O desafio dessa semana é criar uma mini library (biblioteca) para
 reutilizarmos nossos códigos quando fizermos manipulação de DOM!
@@ -19,13 +20,32 @@ selecionados.
 Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
-// ?
+function DOM(elements) {
+  this.element = document.querySelectorAll(elements);
+}
+
+DOM.prototype.on = function on(eventType, callback){
+  Array.prototype.forEach.call(this.element, function(element) {
+    element.addEventListener(eventType, callback, false);
+  });
+};
+
+DOM.prototype.off = function off(){
+  
+}
+
+DOM.prototype.get = function get(){
+  return this.element;
+}
 
 var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
+$a.on('click', function handleClick(e) {
   e.preventDefault();
   console.log('clicou');
+  $a.off('click', handleClick);
 });
 
 console.log('Elementos selecionados:', $a.get());
 console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+})();
