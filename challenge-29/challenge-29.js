82ad33c79aece87394d9b4853ee60c3ad87d484a
Caio Camizão <109806265+CaioCamizao@users.noreply.github.com>
2023-10-26 16:46:53 -0300

    Update challenge-29.js

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,39 +1,77 @@
-(function() {
+(function ($, doc) {
   'use strict';
+  
+  var app = (function () {
+    return {
+      init: function init () {
+        console.log('app init');
+        this.companyInfo();
+        this.initEvents();
+      },
 
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
+      initEvents () {
+        $('[data-js="form-register"]').on('submit', this.handleSubmit);
+      },
 
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
+      handleSubmit: function handleSubmit (e) {
+        e.preventDefault();
+        console.log('submit');
+        var $tableCar = $('[data-js="table-car"]').get();
+        $tableCar.appendChild(app.createNewCar());
+      },
 
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
+      createNewCar: function createNewCar () {
+        const $fragment = doc.createDocumentFragment();
+        var $tr = doc.createElement('tr');
+        var $tdImage = doc.createElement('td');
+        var $image = document.createElement('img')
+        var $tdBrand= doc.createElement('td');
+        var $tdYear = doc.createElement('td');
+        var $tdPlate = doc.createElement('td');
+        var $tdColor = doc.createElement('td');
 
-  Essas informações devem ser adicionadas no HTML via Ajax.
+        $image.setAttribute('src', $('[data-js="image"]').get().value);
+        $tdImage.appendChild($image);
 
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
+        $tdBrand.textContent = $('[data-js="brand-model"]').get().value;
+        $tdYear.textContent = $('[data-js="year"]').get().value;
+        $tdPlate.textContent = $('[data-js="plate"]').get().value;
+        $tdColor.textContent = $('[data-js="color"]').get().value;
 
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
+        $tr.appendChild($tdImage);
+        $tr.appendChild($tdBrand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdPlate);
+        $tr.appendChild($tdColor);
 
-})();
+        return $fragment.appendChild($tr);
+      },
+
+      companyInfo: function companyInfo () {
+        console.log('company info');
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', '/company.json', true);
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
+      },
+
+      getCompanyInfo: function getCompanyInfo () {
+        if (!app.isReady.call(this)) {
+          return;
+        }
+
+        var data = JSON.parse(this.responseText);
+        var $companyName = $('[data-js="company-name"]').get();
+        var $companyPhone = $('[data-js="company-phone"]').get();
+        $companyName.textContent = data.name;
+        $companyPhone.textContent = data.phone;
+      },
+
+      isReady: function isReady () {
+        return this.readyState === 4 && this.status === 200;
+      },
+    };
+  })();
+
+  app.init();
+})(window.DOM, document);
