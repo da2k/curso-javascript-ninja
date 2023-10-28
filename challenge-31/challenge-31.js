ac7529327ca05d26820690b2db95d0a601899e3c
Caio Camizão <109806265+CaioCamizao@users.noreply.github.com>
2023-10-28 09:48:21 -0300

    Update challenge-31.js

--- challenge-31/challenge-31.js
+++ challenge-31/challenge-31.js
@@ -12,3 +12,95 @@ do curso, para colar o link do pull request do seu repo.
 */
 
 console.log('Link do pull request do seu projeto');
+
+(function ($, doc) {
+  'use strict';
+
+  var app = (function () {
+    return {
+      init: function init() {
+        console.log('app init');
+        this.companyInfo();
+        this.initEvents();
+      },
+
+      initEvents: function () {
+        $('[data-js="form-register"]').on('submit', this.handleSubmit);
+        // Anexe um manipulador de eventos de clique à tabela para remover as linhas
+        $('[data-js="table-car"]').on('click', '[data-js="btn-remove"]', this.handleRemove);
+      },
+
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
+        console.log('submit');
+        var $tableCar = $('[data-js="table-car"]').get();
+        $tableCar.appendChild(app.createNewCar());
+      },
+
+      createNewCar: function createNewCar() {
+        const $fragment = doc.createDocumentFragment();
+        var $tr = doc.createElement('tr');
+        var $tdImage = doc.createElement('td');
+        var $image = document.createElement('img');
+        var $tdBrand = doc.createElement('td');
+        var $tdYear = doc.createElement('td');
+        var $tdPlate = doc.createElement('td');
+        var $tdColor = doc.createElement('td');
+        var $tdRemove = doc.createElement('td'); // Adicione uma nova coluna para o botão Remover
+        var $btnRemove = doc.createElement('button');
+        
+        $image.setAttribute('src', $('[data-js="image"]').get().value);
+        $tdImage.appendChild($image);
+
+        $tdBrand.textContent = $('[data-js="brand-model"]').get().value;
+        $tdYear.textContent = $('[data-js="year"]').get().value;
+        $tdPlate.textContent = $('[data-js="plate"]').get().value;
+        $tdColor.textContent = $('[data-js="color"]').get().value;
+
+        $btnRemove.textContent = 'Remover';
+        $btnRemove.setAttribute('data-js', 'btn-remove'); // Adicione um atributo data-js ao botão Remover
+
+        $tr.appendChild($tdImage);
+        $tr.appendChild($tdBrand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdPlate);
+        $tr.appendChild($tdColor);
+        $tr.appendChild($tdRemove);
+        $tdRemove.appendChild($btnRemove); // Adicione o botão Remover à nova coluna
+
+        return $fragment.appendChild($tr);
+      },
+
+      handleRemove: function handleRemove() {
+        // Remova a linha quando o botão Remover for clicado
+        this.closest('tr').remove();
+      },
+
+      companyInfo: function companyInfo() {
+        console.log('company info');
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', '/company.json', true);
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
+      },
+
+      getCompanyInfo: function getCompanyInfo() {
+        if (!app.isReady.call(this)) {
+          return;
+        }
+
+        var data = JSON.parse(this.responseText);
+        var $companyName = $('[data-js="company-name"]').get();
+        var $companyPhone = $('[data-js="company-phone"]').get();
+        $companyName.textContent = data.name;
+        $companyPhone.textContent = data.phone;
+      },
+
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      },
+    };
+  })();
+
+  app.init();
+})(window.DOM, document);
